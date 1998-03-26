183d8c1cfac408f3d45a3b7773afa837e3727cba
peter <peter@freepascal.org>
1998-03-26 12:25:22 +0000

    * integrated both mouse units

--- rtl/dos/mouse.pp
+++ rtl/dos/mouse.pp
@@ -1,8 +1,7 @@
 {
     $Id$
     This file is part of the Free Pascal run time library.
-    Copyright (c) 1993,97 by Florian Klamepfl,
-    member of the Free Pascal development team
+    Copyright (c) 1993,97 by the Free Pascal development team
 
     See the file COPYING.FPC, included in this distribution,
     for details about the copyright.
@@ -11,179 +10,399 @@
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
- **********************************************************************}
+ **********************************************************************
+}
+Unit Mouse;
+Interface
 
 {
-  History:
-  30.3.1994: Version 0.5
-             Unit ist elementar implementiert
-             (INT $33 Funktionen 0-3)
-  2.4.1995:  Version 0.55
-             - mousereset in eine function umgewandelt
-             - mousesetpos implementiert
-             - mouserelx, mouserely implementiert
-             - mousex und mousey erweitern nun ihre Resultate
-               immer auf 32 Bit
-  14.4.1995: Version 0.56
-             - mouserelx und mouserely mÅssen ihre
-               Resultate natÅrlich Vorzeichen erweitert auf
-               32 Bit kopieren
-            - mouserelx und mouserely zu einer Funktion zusammen-
-              gefa·t, da sonst viele Bewegungen verloren gehen
+  Mouse support functions and procedures,with error checking if mouse
+  isn't present then the routine ends,if you want to remove error checking
+  remove the next define.
 }
 
-{$E-}
+{$DEFINE MOUSECHECK}
 
-unit mouse;
+{check if mouse is present and sets the mouse variable}
+  Function Check_Mouse:Boolean;
+{shows mouse pointer,text+graphics screen support}
+  Procedure Show_Mouse;
+{hides mouse pointer}
+  Procedure Hide_Mouse;
+{reads mouse position in pixels,divide by 8 to get text position,and reads
+  buttons state(1-left button,2=right button,7=middle button)}
+  Procedure read_mouse (var x,y:Longint;var buttons:Longint);
+{sets mouse pointer in text mode}
+  Procedure Mouse_Cur(X,Y:Longint);
+{sets the mouse shape in text mode}
+  Procedure Mouse_Shape(BackColor,ForColor,Ascii:LongInt);
+{sets the mouse ascii in text mode}
+  Procedure Mouse_Ascii(Ascii:LongInt);
+{returns which button was pressed after last call to function}
+  Function mouse_press(var x,y:Longint;button:Longint):Longint;
+{returns which button was realeased after last call to function}
+  Function mouse_release (var row,col:Longint;button : Longint):integer;
+{set's mouse y range}
+  Procedure mouse_yrange (min,max:Longint);
+{set's mouse y range}
+  Procedure mouse_xrange (min,max:Longint);
+{set mouse speed}
+  Procedure Micky(Horizontal ,Vertical:Longint);
+{set rectangle on screen that mouse will disappear if will point on it}
+  Procedure Unseen_Mouse(x1,y1,x2,y2:Longint);
+{return if right button pressed}
+  Function IsRPressed:Boolean;
+{return if left button pressed}
+  Function IsLPressed:Boolean;
+{return mouse X coordinate in textmode}
+  Function MouseX:longint;
+{return mouse Y coordinate in textmode}
+  Function MouseY:longint;
+{return which mouse buttons are pressed, only in bit 0-2}
+  Function MouseButtons:longint;
+{set window for mouse}
+  Procedure MWindow(x1,y1,x2,y2:Longint);
 
-{$I os.inc}
+Var
+  MouseFound:Boolean;
 
-  interface
-  
-    function mousereset : word;
-    procedure mouseon;
-    procedure mouseoff;
-    function mousex : longint;
-    function mousey : longint;
-    procedure mouserel(var x,y : longint);
-    function mousebuttons : longint;
-    procedure mousesetpos(x,y : longint);
-  
-  implementation
-  
-    function mousereset : word;
+Implementation
 
-      begin
-         asm
-            movw $0,%ax
-            pushl %ebp
-            int	 $0x33
-            popl %ebp
-            leave
-            ret
-         end;
-      end;
+Function Check_Mouse:Boolean;
+begin
+  asm
+        xorl    %eax,%eax
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+        cmpw    $0xffff,%ax
+        setz    %al
+        movb    %al,MouseFound
+        movb    %al,__RESULT
+  end;
+end;
 
-   procedure mouseon;
 
-     begin
-         asm
-            movw $1,%ax
-            pushl %ebp
-            int	 $0x33
-            popl %ebp
-         end;
-     end;
+procedure show_mouse;
+begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $1,%eax
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+  end;
+end;
 
-   procedure mouseoff;
+procedure hide_mouse;
+begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $2,%eax
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+  end;
+end;
 
-     begin
-        asm
-           movw $2,%ax
-           pushl %ebp
-           int	 $0x33
-           popl %ebp
-        end;
-     end;
-     
-   function mousex : longint;
+procedure read_mouse (var x,y,buttons:Longint);
+begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $3,%eax
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+        andl    $0xffff,%ecx
+        andl    $0xffff,%edx
+        movl    x,%eax
+        movl    %ecx,(%eax)
+        movl    y,%eax
+        movl    %edx,(%eax)
+        movl    buttons,%eax
+        movw    %bx,(%eax)
+  end;
+end;
 
-     begin
-        asm
-           movw $3,%ax
-           pushl %ebp
-           int	 $0x33
-           popl %ebp
-           movzwl %cx,%eax
-           leave
-           ret
-        end;
-     end;
-     
-   function mousey : longint;
+function mouse_press(var x,y:Longint;button:Longint):Longint;
+begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $5,%eax
+        movl    button,%ebx
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+        andl    $0xffff,%ecx
+        andl    $0xffff,%edx
+        movl    x,%ebx
+        movl    %ecx,(%ebx)
+        movl    y,%ebx
+        movl    %edx,(%ebx)
+        movl    %eax,__RESULT
+  end;
+end;
 
-     begin
-        asm
-           movw $3,%ax
-           pushl %ebp
-           int	 $0x33
-           popl %ebp
-           movzwl %dx,%eax
-           leave
-           ret
-        end;
-     end;
+function mouse_release (var row,col:Longint;button : Longint):integer;
+begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $6,%eax
+        movl    button,%ebx
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+        andl    $0xffff,%ecx
+        andl    $0xffff,%edx
+        movl    x,%ebx
+        movl    %ecx,(%ebx)
+        movl    y,%ebx
+        movl    %edx,(%ebx)
+        movl    %eax,__RESULT
+  end;
+end;
 
-   function mousebuttons : longint;
+procedure mouse_yrange (min,max:Longint);
+begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $8,%eax
+        movl    min,%ecx
+        movl    max,%edx
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+  end;
+end;
 
-     begin
-        asm
-           movw $3,%ax
-           pushl %ebp
-           int	 $0x33
-           popl %ebp
-           movl %ebx,%eax
-           andl $7,%eax
-           leave
-           ret
-        end;
-     end;
-     
-   procedure mousesetpos(x,y : longint);
-   
-     begin
-        asm
-           movw $4,%ax
-           movl 8(%ebp),%ecx
-           movl 12(%ebp),%edx
-           pushl %ebp
-           int	 $0x33
-           popl %ebp
-        end;
-     end;
-     
-   procedure mouserel(var x,y : longint);
+procedure mouse_xrange (min,max:Longint);
+begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $7,%eax
+        movl    min,%ecx
+        movl    max,%edx
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+  end;
+end;
 
-     begin
-        asm
-           movw $11,%ax
-           pushl %ebp
-           int	 $0x33
-           popl %ebp
-           movswl %cx,%ecx
-           movl 8(%ebp),%eax
-           movl %ecx,(%eax)
-           movswl %dx,%edx
-           movl 12(%ebp),%eax
-           movl %edx,(%eax)
-        end;
-     end;
-     
-end.
+Procedure Mouse_Cur(X,Y:Longint);
+Begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $4,%eax
+        movl    X,%ecx
+        movl    Y,%edx
+        shll    $3,%ecx
+        shll    $3,%edx
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+  End;
+End;
 
+Procedure Mouse_Shape(BackColor,ForColor,Ascii:LongInt);
+Begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        xorl    %ebx,%ebx
+        movl    $0xa,%ax
+        movl    $0xff,%ecx
+        xorl    %edx,%edx
+        movb    8(%ebp),%dh
+        shlb    $4,%dh
+        addb    ForColor,%dh
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+  End;
+End;
+
+Procedure Mouse_Ascii(Ascii:LongInt);
+Begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        xorl    %ebx,%ebx
+        movl    $0xa,%eax
+        movl    $0xff00,%ecx
+        xorl    %edx,%edx
+        movb    8(%ebp),%dl
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+  End;
+End;
+
+Procedure Unseen_Mouse(x1,y1,x2,y2:Longint);
+Begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $0x0010,%eax
+        movl    x1,%ecx
+        movl    y1,%edx
+        movl    x2,%esi
+        movl    y2,%edi
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+  end;
+End;
+
+Procedure Micky(Horizontal ,Vertical:Longint);
+Begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $0x0f,%eax
+        movl    Horizontal,%ecx
+        movl    Vertical,%edx
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+  end;
+End;
+
+Function IsRPressed:Boolean;
+Begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $3,%eax
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+        shrl    $1,%eax
+        andl    $1,%eax
+        movb    %al,__RESULT
+  end;
+end;
+
+Function IsLPressed:Boolean;
+Begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $3,%eax
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+        andl    $1,%eax
+        movb    %al,__RESULT
+  end;
+end;
+
+function MouseX:longint;
+begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $3,%eax
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+        movzwl  %cx,%eax
+        shrl    $3,%eax
+        incl    %eax
+        movl    %eax,__RESULT
+  end;
+end;
+
+function MouseY:longint;
+begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $3,%eax
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+        movzwl  %dx,%eax
+        shrl    $3,%eax
+        incl    %eax
+        movl    %eax,__RESULT
+  end;
+end;
+
+function MouseButtons:longint;
+begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  asm
+        movl    $3,%eax
+        pushl   %ebp
+        int     $0x33
+        popl    %ebp
+        movl    %ebx,%eax
+        andl    $7,%eax
+        movl    %eax,__RESULT
+  end;
+end;
+
+Procedure MWindow(x1,y1,x2,y2:Longint);
+Begin
+{$IFDEF MOUSECHECK}
+  If (Not MouseFound) Then Exit;
+{$ENDIF}
+  mouse_xrange(x1,x2);
+  mouse_yrange(y1,y2);
+End;
+
+Begin
+  Check_Mouse;
+End.
 {
   $Log$
-  Revision 1.1  1998-03-25 11:18:41  root
-  Initial revision
+  Revision 1.2  1998-03-26 12:25:22  peter
+    * integrated both mouse units
 
-  Revision 1.3  1998/01/26 11:56:50  michael
+  Revision 1.1.1.1  1998/03/25 11:18:41  root
+  * Restored version
+
+  Revision 1.4  1998/03/24 15:53:12  peter
+    * cleanup and doesn't give warnings when compiling
+
+  Revision 1.3  1998/01/26 11:56:24  michael
   + Added log at the end
 
-
-  
-  Working file: rtl/dos/mouse.pp
-  description:
-  ----------------------------
-  revision 1.2
-  date: 1997/12/01 12:15:47;  author: michael;  state: Exp;  lines: +12 -5
+  Revision 1.2
+  date: 1997/12/01 12:15:45;  author: michael;  state: Exp;  lines: +14 -12
   + added copyright reference in header.
-  ----------------------------
-  revision 1.1
-  date: 1997/11/27 08:33:50;  author: michael;  state: Exp;
+
+  Revision 1.1
+  date: 1997/11/27 08:33:49;  author: michael;  state: Exp;
   Initial revision
-  ----------------------------
-  revision 1.1.1.1
-  date: 1997/11/27 08:33:50;  author: michael;  state: Exp;  lines: +0 -0
+
+  Revision 1.1.1.1
+  date: 1997/11/27 08:33:49;  author: michael;  state: Exp;  lines: +0 -0
   FPC RTL CVS start
-  =============================================================================
 }
