6a5cfc8ade720d0aa69d4fa4d6834fbe656faffe
michael <michael@freepascal.org>
1998-03-26 10:46:49 +0000

    + lowercase filenames

---
+++ tests/ts010005.pp
@@ -0,0 +1,41 @@
+type
+   tclass1 = class
+      procedure a;virtual;
+      procedure b;virtual;
+   end;
+
+   tclass2 = class(tclass1)
+      procedure a;override;
+      procedure b;override;
+      procedure c;virtual;
+   end;
+
+
+  procedure tclass1.a;
+
+    begin
+    end;
+
+  procedure tclass1.b;
+
+    begin
+    end;
+
+  procedure tclass2.a;
+
+    begin
+    end;
+
+  procedure tclass2.b;
+
+    begin
+    end;
+
+
+  procedure tclass2.c;
+
+    begin
+    end;
+
+begin
+end.
