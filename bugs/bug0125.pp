fd838487e014453a6a7868b6fb81a11e0ca83168
carl <carl@freepascal.org>
1998-04-02 02:57:02 +0000

    * bug with -Rintel switch....

---
+++ bugs/bug0125.pp
@@ -0,0 +1,11 @@
+
+var
+ l : longint;
+begin
+ { problem here is that l is replaced by BP-offset     }
+ { relative to stack, and the parser thinks all wrong  }
+ { because of this.                                    }
+ asm
+   mov eax, [eax*4+l]    
+ end;
+end. 
