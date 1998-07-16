8fd5dde94c13832bf03ccea5f0f9c62749d49a91
michael <michael@freepascal.org>
1998-07-16 17:47:01 +0000

    + added pfinger example

---
+++ docs/sockex/pfinger.pp
@@ -0,0 +1,34 @@
+program pfinger;
+
+uses sockets,errors;
+
+Var Addr : TInetSockAddr;
+    S : Longint;
+    Sin,Sout : Text;
+    Line : string;
+    
+begin
+  Addr.family:=AF_INET;
+  { port 78 in network order }
+  Addr.port:=79 shl 8;
+  { localhost : 127.0.0.1 in network order }
+  Addr.addr:=((1 shl 24) or 127);
+  S:=Socket(AF_INET,SOCK_STREAM,0);
+  If Not Connect (S,ADDR,SIN,SOUT) Then
+    begin
+    Writeln ('Couldn''t connect to localhost');
+    Writeln ('Socket error : ',strerror(SocketError));
+    halt(1);
+    end;
+  rewrite (sout);
+  reset(sin);
+  writeln (sout,paramstr(1));
+  flush(sout);
+  while not eof(sin) do 
+    begin
+    readln (Sin,line);
+    writeln (line);
+    end;
+  close (sin);
+  close (sout);
+end.
