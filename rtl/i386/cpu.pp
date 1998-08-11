bd57e42086540e5b5aa25b13117917561e9f3e46
peter <peter@freepascal.org>
1998-08-11 00:04:46 +0000

    * $ifdef ver0_99_5 updates

--- rtl/i386/cpu.pp
+++ rtl/i386/cpu.pp
@@ -1,9 +1,11 @@
 {
     $Id$
     This file is part of the Free Pascal run time library.
-    Copyright (c) 1993,98 by Florian Klaempfl,
-    member of the Free Pascal development team.
+    Copyright (c) 1998 by Florian Klaempfl
 
+    This unit contains some routines to get informations about the
+    processor
+    
     See the file COPYING.FPC, included in this distribution,
     for details about the copyright.
 
@@ -12,11 +14,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
  **********************************************************************}
-{ this unit contains some routines to get informations about the
-  processor
-}
 unit cpu;
-{$I386_INTEL}
   interface
 
     { returns true, if the processor supports the cpuid instruction }
@@ -28,10 +26,17 @@ unit cpu;
     { returns the contents of the cr0 register }
     function cr0 : longint;
 
+
   implementation
 
-    function cpuid_support : boolean;assembler;
+{$ifdef VER0_99_5}
+  {$I386_INTEL}
+{$endif}
 
+{$ASMMODE INTEL}
+  
+
+    function cpuid_support : boolean;assembler;
       {
         Check if the ID-flag can be changed, if changed then CpuID is supported.
         Tested under go32v1 and Linux on c6x86 with CpuID enabled and disabled (PFV)
@@ -53,8 +58,8 @@ unit cpu;
          setnz   al
       end;
 
-    function cr0 : longint;assembler;
 
+    function cr0 : longint;assembler;
       asm
          DB 0Fh,20h,0C0h
          { mov eax,cr0
@@ -62,8 +67,8 @@ unit cpu;
   	        parsers }
       end;
 
-    function floating_point_emulation : boolean;
 
+    function floating_point_emulation : boolean;
       begin
          {!!!! I don't know currently the position of the EM flag }
          { $4 after Ralf Brown's list }
@@ -74,7 +79,10 @@ end.
 
 {
   $Log$
-  Revision 1.3  1998-05-25 10:51:27  pierre
+  Revision 1.4  1998-08-11 00:04:46  peter
+    * $ifdef ver0_99_5 updates
+
+  Revision 1.3  1998/05/25 10:51:27  pierre
     * CR0 works now (written using DB to allow to use it we INTEL and ATT output)
     * floating_emulation bit set correctly
 
