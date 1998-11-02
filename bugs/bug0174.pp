37badd43834cbe0a0c6b8aed54c303fe3a68be4a
peter <peter@freepascal.org>
1998-11-02 15:13:27 +0000

    + 174

---
+++ bugs/bug0174.pp
@@ -0,0 +1,19 @@
+{$ASMMODE ATT}
+
+type
+  tobj=object
+   l : longint;
+  end;
+var
+  t : tobj;
+
+procedure kl;assembler;
+asm
+  movl tobj.l,%eax        // tobj.l should return the offset of l in tobj 
+end;
+
+
+begin
+end.
+
+
