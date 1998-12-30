150ba339ac302f1353f4ddfb4a2c10b5fcad76f7
michael <michael@freepascal.org>
1998-12-30 22:05:41 +0000

    Added library example

---
+++ docs/refex/ex84.pp
@@ -0,0 +1,24 @@
+Library Greeting;
+
+{$Mode objpas}
+
+Procedure Hello;
+
+begin
+  Writeln ('Hello, World !');
+end;
+
+Function GetNumber (Max : longint) : Longint;
+
+begin
+  Repeat
+    Write ('Please enter a nuber between 0 and ',max,' : ');
+    ReadLn (Result);
+  Until (Result>=0) and (Result<=Max);
+end; 
+
+exports
+  Hello,
+  GetNumber;
+  
+end. 
