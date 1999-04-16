97b2b7f0b79463ecc136d2fb3179f9886cb78a21
pierre <pierre@freepascal.org>
1999-04-16 10:03:27 +0000

    + better test for IMUL and SHRD !

--- tests/tbs0123.pp
+++ tests/tbs0123.pp
@@ -3,5 +3,13 @@ begin
 {$asmmode intel}
    asm
       SHRD [ESI-8], EAX, CL
+      SHLD EBX,ECX,5
+      IMUL ECX,dword [EBP-8],5
+   end;
+{$asmmode att}
+   asm
+      shrdl %cl,%eax,-8(%esi)
+      shldl $5,%ecx,%ebx
+      imull $5,-8(%ebp),%ecx
    end;
 end.
