69c36744e14c936b26022e9b85047fe7330e8a44
peter <peter@freepascal.org>
1999-06-01 00:10:17 +0000

    * go32v2 fs only

--- tests/ts010025.pp
+++ tests/ts010025.pp
@@ -12,14 +12,18 @@ var
   r : trec;
 begin
   asm
+    xor esi,esi
     mov [esi+r],eax
-    mov fs:[2+trec.b],eax
+    lea esi,r
     mov [esi+2+trec.b],eax
     mov trec[esi].b,eax
-    mov fs:[046ch],eax
     mov eax,trec.b+2
     mov trec[esi].b+2,eax
     mov eax,Count
     mov eax,Count*100
+{$ifdef go32v2}
+    mov fs:[0468+trec.b],eax
+    mov fs:[046ch],eax
+{$endif}
   end;
 end.
