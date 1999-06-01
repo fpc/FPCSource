5e63dbceeba5014de064fc8cb89ed4fad128952f
pierre <pierre@freepascal.org>
1999-06-01 22:23:11 +0000

    + several tbs/tbf

---
+++ tests/tbs0218.pp
@@ -0,0 +1,25 @@
+Program Wrong_Output;
+{}
+Var r,rr:Extended; s:String;
+    code : word;
+{}
+Begin
+  Writeln('Size of Extended type (r)=',SizeOf(r),' bytes');
+  r:=0.000058184639;
+  Writeln('r=',r);
+  Writeln('r=',r:16:13);
+  Writeln('r=',r:15:12);
+  Writeln('r=',r:14:11);
+  Writeln('r=',r:13:10);
+  Writeln('r=',r:12:9);
+  Writeln('r=',r:11:8);
+  Writeln('r=',r:10:7);
+  Writeln('r=',r:9:6);
+  Writeln('r=',r:8:5);
+  Writeln('r=',r:7:4);
+  Str(r:7:4,s);
+  Writeln('r=',s,' (as string)')
+  str(r,s);
+  val(s,rr,code);
+  if r<>rr then halt(1);
+End.
---
+++ tests/tbs0255.pp
@@ -0,0 +1,9 @@
+
+function a: char;
+begin
+  a:='c';
+end;
+
+begin
+  if #12 in [a, a, a, a, a] then ; { <--- }
+end.
