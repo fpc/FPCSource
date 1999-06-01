be67d33ae6c9a99e5f31e7e3cfb98ec27ad3c586
peter <peter@freepascal.org>
1999-06-01 00:17:03 +0000

    * fixes to run correctly

--- tests/ts010024.pp
+++ tests/ts010024.pp
@@ -13,14 +13,22 @@ var
   r : trec;
 begin
   asm
+    leal r,%edi
+    leal r,%esi
     movl %es:46(%edi),%eax
     movl 2+trec.b(%esi),%eax
-    movl trec.b(,%esi,(2*4)),%eax
+    movl $1,%ebx
+    movl trec.b(%esi,%ebx,(2*4)),%eax
+    movl r(,%ebx,(2*4)),%eax
+    xorl %esi,%esi
     movl r.c(,%esi,(2*4)),%eax
-    movl %fs:(0x46c),%eax
     movl Count,%eax
     movl Count*100,%eax
     movl trec.b+2,%eax
+    leal r,%esi
     movl trec.b+2(%esi),%eax
+{$ifdef go32v2}
+    movl %fs:(0x46c),%eax
+{$endif}
   end;
 end.
