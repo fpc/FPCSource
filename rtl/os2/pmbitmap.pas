395ddade10f95346ba7f0ad513ba0201a3c480ac
Tomas Hajny <hajny@freepascal.org>
1999-06-02 16:01:29 +0000

    * changes by Ramon Bosque

--- rtl/os2/pmbitmap.pas
+++ rtl/os2/pmbitmap.pas
@@ -1,24 +1,23 @@
 {****************************************************************************
 
-                   Copyright (c) 1993,94 by Florian Kl„mpfl
+    $Id$
+
+                   Copyright (c) 1993,94,99 by FK, RB
                   
  ****************************************************************************}
 unit pmbitmap;
 
   interface
-  
-    uses
-       os2def;
 
 {$PACKRECORDS 1}
 
     type
        BITMAPINFOHEADER = record
-          cbFix : ULONG;
-          cx : USHORT;
-          cy : USHORT;
-          cPlanes : USHORT;
-          cBitCount : USHORT;
+          cbFix : cardinal;
+          cx : word;
+          cy : word;
+          cPlanes : word;
+          cBitCount : word;
        end;
 
        PBITMAPINFOHEADER = ^BITMAPINFOHEADER;
@@ -30,15 +29,120 @@ unit pmbitmap;
        end;
 
        BITMAPINFO = record
-          cbFix : ULONG;
-          cx : USHORT;
-          cy : USHORT;
-          cPlanes : USHORT;
-          cBitCount : USHORT;
+          cbFix : cardinal;
+          cx : word;
+          cy : word;
+          cPlanes : word;
+          cBitCount : word;
           argbColor : array[0..1-1] of RGB;
        end;
 
        PBITMAPINFO = ^BITMAPINFO;
+
+       BITMAPINFOHEADER2 = record
+          cbFix : cardinal;
+          cx : cardinal;
+          cy : cardinal;
+          cPlanes : word;
+          cBitCount : word;
+          ulCompression : cardinal;
+          cbImage : cardinal;
+          cxResolution : cardinal;
+          cyResolution : cardinal;
+          cclrUsed : cardinal;
+          cclrImportant : cardinal;
+          usUnits : word;
+          usReserved : word;
+          usRecording : word;
+          usRendering : word;
+          cSize1 : cardinal;
+          cSize2 : cardinal;
+          ulColorEncoding : cardinal;
+          ulIdentifier : cardinal;
+       end;
+
+       PBITMAPINFOHEADER2 = ^BITMAPINFOHEADER2;
+
+       RGB2 = record
+          bBlue : BYTE;
+          bGreen : BYTE;
+          bRed : BYTE;
+          fcOptions : BYTE;
+       end;
+
+       PRGB2 = ^RGB2;
+
+       BITMAPINFO2 = record
+          cbFix : cardinal;
+          cx : cardinal;
+          cy : cardinal;
+          cPlanes : word;
+          cBitCount : word;
+          ulCompression : cardinal;
+          cbImage : cardinal;
+          cxResolution : cardinal;
+          cyResolution : cardinal;
+          cclrUsed : cardinal;
+          cclrImportant : cardinal;
+          usUnits : word;
+          usReserved : word;
+          usRecording : word;
+          usRendering : word;
+          cSize1 : cardinal;
+          cSize2 : cardinal;
+          ulColorEncoding : cardinal;
+          ulIdentifier : cardinal;
+          argbColor : array[0..1-1] of RGB2;
+       end;
+
+       PBITMAPINFO2 = ^BITMAPINFO2;
+
+       BITMAPFILEHEADER = record
+          usType : word;
+          cbSize : cardinal;
+          xHotspot : integer;
+          yHotspot : integer;
+          offBits : cardinal;
+          bmp : BITMAPINFOHEADER;
+       end;
+
+       PBITMAPFILEHEADER = ^BITMAPFILEHEADER;
+
+       BITMAPARRAYFILEHEADER = record
+          usType : word;
+          cbSize : cardinal;
+          offNext : cardinal;
+          cxDisplay : word;
+          cyDisplay : word;
+          bfh : BITMAPFILEHEADER;
+       end;
+
+       PBITMAPARRAYFILEHEADER = ^BITMAPARRAYFILEHEADER;
+
+       BITMAPFILEHEADER2 = record
+          usType : word;
+          cbSize : cardinal;
+          xHotspot : integer;
+          yHotspot : integer;
+          offBits : cardinal;
+          bmp2 : BITMAPINFOHEADER2;
+       end;
+
+       PBITMAPFILEHEADER2 = ^BITMAPFILEHEADER2;
+
+       BITMAPARRAYFILEHEADER2 = record
+          usType : word;
+          cbSize : cardinal;
+          offNext : cardinal;
+          cxDisplay : word;
+          cyDisplay : word;
+          bfh2 : BITMAPFILEHEADER2;
+       end;
+
+       PBITMAPARRAYFILEHEADER2 = ^BITMAPARRAYFILEHEADER2;
+
+{$PACKRECORDS NORMAL}
+
 
     const
        CBD_COMPRESSION = 1;
@@ -59,118 +163,20 @@ unit pmbitmap;
        BCE_PALETTE = (-1);
        BCE_RGB = 0;
 
-    type
-       BITMAPINFOHEADER2 = record
-          cbFix : ULONG;
-          cx : ULONG;
-          cy : ULONG;
-          cPlanes : USHORT;
-          cBitCount : USHORT;
-          ulCompression : ULONG;
-          cbImage : ULONG;
-          cxResolution : ULONG;
-          cyResolution : ULONG;
-          cclrUsed : ULONG;
-          cclrImportant : ULONG;
-          usUnits : USHORT;
-          usReserved : USHORT;
-          usRecording : USHORT;
-          usRendering : USHORT;
-          cSize1 : ULONG;
-          cSize2 : ULONG;
-          ulColorEncoding : ULONG;
-          ulIdentifier : ULONG;
-       end;
-
-       PBITMAPINFOHEADER2 = ^BITMAPINFOHEADER2;
-
-       RGB2 = record
-          bBlue : BYTE;
-          bGreen : BYTE;
-          bRed : BYTE;
-          fcOptions : BYTE;
-       end;
-
-       PRGB2 = ^RGB2;
-
-       BITMAPINFO2 = record
-          cbFix : ULONG;
-          cx : ULONG;
-          cy : ULONG;
-          cPlanes : USHORT;
-          cBitCount : USHORT;
-          ulCompression : ULONG;
-          cbImage : ULONG;
-          cxResolution : ULONG;
-          cyResolution : ULONG;
-          cclrUsed : ULONG;
-          cclrImportant : ULONG;
-          usUnits : USHORT;
-          usReserved : USHORT;
-          usRecording : USHORT;
-          usRendering : USHORT;
-          cSize1 : ULONG;
-          cSize2 : ULONG;
-          ulColorEncoding : ULONG;
-          ulIdentifier : ULONG;
-          argbColor : array[0..1-1] of RGB2;
-       end;
-
-       PBITMAPINFO2 = ^BITMAPINFO2;
-
-       BITMAPFILEHEADER = record
-          usType : USHORT;
-          cbSize : ULONG;
-          xHotspot : SHORT;
-          yHotspot : SHORT;
-          offBits : ULONG;
-          bmp : BITMAPINFOHEADER;
-       end;
-
-       PBITMAPFILEHEADER = ^BITMAPFILEHEADER;
-
-       BITMAPARRAYFILEHEADER = record
-          usType : USHORT;
-          cbSize : ULONG;
-          offNext : ULONG;
-          cxDisplay : USHORT;
-          cyDisplay : USHORT;
-          bfh : BITMAPFILEHEADER;
-       end;
-
-       PBITMAPARRAYFILEHEADER = ^BITMAPARRAYFILEHEADER;
-
-       BITMAPFILEHEADER2 = record
-          usType : USHORT;
-          cbSize : ULONG;
-          xHotspot : SHORT;
-          yHotspot : SHORT;
-          offBits : ULONG;
-          bmp2 : BITMAPINFOHEADER2;
-       end;
-
-       PBITMAPFILEHEADER2 = ^BITMAPFILEHEADER2;
-
-       BITMAPARRAYFILEHEADER2 = record
-          usType : USHORT;
-          cbSize : ULONG;
-          offNext : ULONG;
-          cxDisplay : USHORT;
-          cyDisplay : USHORT;
-          bfh2 : BITMAPFILEHEADER2;
-       end;
-
-       PBITMAPARRAYFILEHEADER2 = ^BITMAPARRAYFILEHEADER2;
-
-    const
        BFT_ICON = $4349;
        BFT_BMAP = $4d42;
        BFT_POINTER = $5450;
        BFT_COLORICON = $4943;
        BFT_COLORPOINTER = $5043;
        BFT_BITMAPARRAY = $4142;
-{$PACKRECORDS NORMAL}
+
 
   implementation
 
 end.
+{
+  $Log$
+  Revision 1.3  1999-06-02 16:01:31  hajny
+    * changes by Ramon Bosque
+
+}
