8e6c15972e852d5fd55b7cd7a6398243b126d05b
peter <peter@freepascal.org>
1999-07-28 23:37:47 +0000

    * new illegal ref

--- bugs/bug0276.pp
+++ bugs/bug0276.pp
@@ -3,13 +3,21 @@ type
   trec = record
     ypos,
     xpos : longint;
-   end;
+  end;
+   
+  z80cont = record
+     page: array [0..11,0..16383] of byte;
+  end;
+  
 var
   rec : tRec;
+  myz80 : z80cont;
 begin
   asm
      lea     edi, Rec
      cmp   byte ptr [edi+tRec.Xpos], true
      cmp   byte ptr [edi].trec.Xpos, true
+     
+     mov dh,byte ptr myz80.page[ecx]
   end;
 end.
