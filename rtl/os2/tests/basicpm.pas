ee52225f00f5a2bc9d37c7546327a6496e1e099d
Tomas Hajny <hajny@freepascal.org>
1999-10-01 11:07:27 +0000

    * enhancements by Charlie/INQ

--- rtl/os2/tests/basicpm.pas
+++ rtl/os2/tests/basicpm.pas
@@ -1,7 +1,7 @@
 program BasicPM;
 
 uses
- Os2Def, PMWin, PMGpi;
+ Os2Def, PMWin;
 
 function ClientWindowProc (Window, Msg: cardinal; MP1, MP2: pointer): pointer;
                                                                  cdecl; export;
@@ -15,9 +15,9 @@ begin
  ClientWindowProc := nil;
  case Msg of
   wm_Paint: begin
-             PS := WinBeginPaint(Window, 0, nil);
-             GpiErase(PS);
-             WinEndPaint(PS);
+             PS := WinBeginPaint (Window, 0, @R);
+             WinFillRect (PS, @R, SYSCLR_WINDOW);
+             WinEndPaint (PS);
             end;
   else ClientWindowProc := WinDefWindowProc (Window, Msg, MP1, MP2);
  end;
@@ -37,12 +37,16 @@ begin
  Anchor := WinInitialize(0);
  { It might be beneficial to set the second parameter of the following }
  { call to something large, such as 1000.  The OS/2 documentation does }
- { not recommend this, however } MsgQue := WinCreateMsgQueue(Anchor, 0);
+ { not recommend this, however } MsgQue := WinCreateMsgQueue (Anchor, 0);
  if MsgQue = 0 then Halt (254);
- WinRegisterClass (Anchor, ClassName, proc (ClientWindowProc), cs_SizeRedraw,
+
+ WinMessageBox (HWND_DESKTOP, HWND_DESKTOP, 'FPC test', 'BASIC PM', 0,
+                                                      MB_OK or MB_INFORMATION);
+
+ WinRegisterClass (Anchor, ClassName, proc (@ClientWindowProc), cs_SizeRedraw,
                                                              SizeOf (pointer));
  Frame := WinCreateStdWindow (hwnd_Desktop, 0, WinFlags, ClassName,
-                                       'BASIC', 0, 0, idClientWindow, Client);
+                                     'BASIC PM', 0, 0, idClientWindow, Client);
  if (Frame <> 0) then
  begin
   WinSetWindowPos (Frame, 0, 0, WinQuerySysValue (hwnd_Desktop,
