c59cfa71151a44521702b2eafb021fd644755620
pierre <pierre@freepascal.org>
1999-12-13 13:36:51 +0000

    batch to complete go32v2 tests under win95

---
+++ tests/win95test.bat
@@ -0,0 +1,18 @@
+set LONGLOG=1
+make alltbf
+make tbs0to99
+make tbs100to199
+make tbs200to299
+make tbs300to399
+make tbsexec0to99
+make tbsexec100to199
+make tbsexec200to299
+make tbsexec300to399
+make allts alltf allto 
+make allwebtbf allwebtbs
+make allwebtbsexec
+make alltsexec
+
+
+
+  
