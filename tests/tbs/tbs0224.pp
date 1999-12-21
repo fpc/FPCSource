602b295aafc1f0a44d08d7cf57b11001793c58f2
peter <peter@freepascal.org>
1999-12-21 16:09:12 +0000

    * zipinstall fixes

--- tests/tbs/tbs0224.pp
+++ tests/tbs/tbs0224.pp
@@ -3,10 +3,17 @@ var f:text;
     i:integer;
 begin
   assign(f,'bug0224.txt');
+  rewrite(f);
+  write(f,'     ');
   reset(f);
 {$I-}
   readln(f,i);              { you can't avoid run-time error generation }
 {$I+}
-  if IOResult<>0 then writeln('error...')
-                 else close(f);
+  if IOResult<>0 then
+   writeln('error...');
+{$I-}
+  close(f);
+  erase(f);
+{$I+}
+  if IOResult<>0 then;
 end.
