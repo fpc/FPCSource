02081ba1a4b505348c9bd763a500af0e5f0b1e21
sg <sg>
1999-12-22 13:43:14 +0000

    * Improved messages in the case of failure: Now the test prints the results   it got from the XMLCfg unit

--- fcl/tests/cfgtest.pp
+++ fcl/tests/cfgtest.pp
@@ -7,6 +7,9 @@ program cfgtest;
 uses xmlcfg;
 var
   cfg: TXMLConfig;
+  i: Integer;
+  s: String;
+  b: Boolean;
 begin
 
   WriteLn('Writing a sample XML configuration to "testcfg.xml"...');
@@ -22,16 +25,26 @@ begin
   WriteLn('Ok; now I''ll try to read back all values...');
 
   cfg := TXMLConfig.Create('testcfg.xml');
-  if cfg.GetValue('cfgtest/MainWindow/Constraints/Width', 0) <> 600 then
-    WriteLn('Invalid value: cfgtest/MainWindow/Constraints/Width');
-  if cfg.GetValue('cfgtest/MainWindow/Constraints/Height', 400) <> 400 then
-    WriteLn('Invalid value: cfgtest/MainWindow/Constraints/Height');
-  if cfg.GetValue('cfgtest/MainWindow/Caption', '') <> 'TXMLConfig Test' then
-    WriteLn('Invalid value: cfgtest/MainWindow/Caption');
-  if cfg.GetValue('cfgtest/SomeForm/Presets/Preset1/Name', '') <> 'Example' then
-    WriteLn('Invalid value: cfgtest/SomeForm/Presets/Preset1/Name');
-  if cfg.GetValue('TipOfTheDay/Disabled', False) <> True then
-    WriteLn('Invalid value: TipOfTheDay/Disabled');
+
+  i := cfg.GetValue('cfgtest/MainWindow/Constraints/Width', 0);
+  if i <> 600 then
+    WriteLn('Invalid value: cfgtest/MainWindow/Constraints/Width, got ', i);
+
+  i := cfg.GetValue('cfgtest/MainWindow/Constraints/Height', 400);
+  if i <> 400 then
+    WriteLn('Invalid value: cfgtest/MainWindow/Constraints/Height, got ', i);
+
+  s := cfg.GetValue('cfgtest/MainWindow/Caption', '');
+  if s <> 'TXMLConfig Test' then
+    WriteLn('Invalid value: cfgtest/MainWindow/Caption, got "', s, '"');
+
+  s := cfg.GetValue('cfgtest/SomeForm/Presets/Preset1/Name', '');
+  if s <> 'Example' then
+    WriteLn('Invalid value: cfgtest/SomeForm/Presets/Preset1/Name, got "', s, '"');
+
+  b := cfg.GetValue('TipOfTheDay/Disabled', False);
+  if b <> True then
+    WriteLn('Invalid value: TipOfTheDay/Disabled, got ', b);
   cfg.Free;
 
   WriteLn('Done!');
@@ -40,7 +53,11 @@ end.
 
 {
   $Log$
-  Revision 1.1  1999-07-09 21:06:59  michael
+  Revision 1.2  1999-12-22 13:43:14  sg
+  * Improved messages in the case of failure: Now the test prints the results
+    it got from the XMLCfg unit
+
+  Revision 1.1  1999/07/09 21:06:59  michael
   + Initial implementation by sebastian Guenther
 
 }
