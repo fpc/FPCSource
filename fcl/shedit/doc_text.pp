5d0b7809a3707738d424d21935a44dedeaf775ec
sg <sg>
1999-12-30 21:02:14 +0000

    * Shortened copyright notice

--- fcl/shedit/doc_text.pp
+++ fcl/shedit/doc_text.pp
@@ -1,22 +1,15 @@
 {
-  $Id$
+    $Id$
 
-  "shedit" - Text editor with syntax highlighting
-  Copyright (C) 1999  Sebastian Guenther (sguenther@gmx.de)
+    "SHEdit" - Text editor with syntax highlighting
+    Copyright (C) 1999  Sebastian Guenther (sg@freepascal.org)
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 }
 
 // Generic text document class
@@ -268,7 +261,10 @@ end.
 
 {
   $Log$
-  Revision 1.3  1999-12-09 23:16:41  peter
+  Revision 1.4  1999-12-30 21:02:14  sg
+  * Shortened copyright notice
+
+  Revision 1.3  1999/12/09 23:16:41  peter
     * cursor walking is now possible, both horz and vert ranges are now
       adapted
     * filter key modifiers
