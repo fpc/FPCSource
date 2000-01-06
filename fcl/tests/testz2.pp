5488c7d8ab03a51e4baf3be8033f6c4c4bf7c3fd
peter <peter@freepascal.org>
2000-01-06 23:51:29 +0000

    * fixed EOL for non-linux

--- fcl/tests/testz2.pp
+++ fcl/tests/testz2.pp
@@ -2,17 +2,24 @@ Program testz2;
 
 uses zstream;
 
+const
+{$ifdef linux}
+  eol=#10;
+{$else}
+  eol=#10#13;
+{$endif}
+
 Var F : TGZfileStream;
     S : String;
     i :longint;
     c : char;
 begin
   Writeln ('Creating file.');
-  S:='This is a sentence'#10;
+  S:='This is a sentence'+eol;
   F:=TGZFileStream.create('test.gz',gzopenWrite);
-  For I:=1 to 10 do 
+  For I:=1 to 10 do
     F.Write(S[1],Length(S));
-  f.Free;  
+  f.Free;
   Writeln ('Done.');
   Writeln ('Reopening file for read.');
   F:=TGZFileStream.Create('test.gz',gzopenread);
@@ -20,6 +27,6 @@ begin
   While F.Read(C,SizeOf(c))<>0 do
     write(c);
   F.free;
-  Writeln('Done.');  
+  Writeln('Done.');
 end.
 
