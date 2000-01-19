291eb87a54dff57d8c4bbcb3ec5ca61ae5576687
Jonas Maebe <jonas@freepascal.org>
2000-01-19 13:26:28 +0000

    + bug0308 and bug0308a

---
+++ bugs/bug0308.pp
@@ -0,0 +1,5 @@
+uses bug0308a;
+
+begin
+  writeln(coursedb.name(60));
+end.
---
+++ bugs/bug0308a.pp
@@ -0,0 +1,26 @@
+unit bug0308a;
+
+interface
+
+type
+  tcourses = object
+    function index(cName: string): integer;
+    function name(cIndex: integer): string;
+  end;
+
+var coursedb: tcourses;
+    l: longint;
+
+implementation
+
+function tcourses.index(cName: string): integer;
+begin
+  index := byte(cName[0]);
+end;
+
+function tcourses.name(cIndex: integer): string;
+begin
+  name := char(byte(cIndex));
+end;
+
+end.
