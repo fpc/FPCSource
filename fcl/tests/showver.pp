3e5162f23d222bb5734d29452ef4c35323fd7293
michael <michael@freepascal.org>
2000-01-27 13:48:23 +0000

    + Added fileinfo object

---
+++ fcl/tests/showver.pp
@@ -0,0 +1,31 @@
+program showversion;
+
+{$mode fpc}
+{$R showver.res}
+
+uses sysutils,fileinfo;
+
+Var version : TFileVersionInfo;
+    I : longint;
+    
+begin
+  Version:=TFileVErsionInfo.create(Nil);
+  Version.FileName:=paramstr(1);
+  With Version do
+    begin
+    if Not FileExists(Paramstr(1)) then
+      begin
+      Writeln (Format('%s : No such file or directory',[Paramstr(1)]));
+      Halt(1);
+      end;
+    FileName:=Paramstr(1);
+    If VersionStrings.Count=0 then
+      begin
+      Writeln (Format('%s : No version information found.',[paramstr(1)]));
+      Halt(2);
+      end;
+    For i:=0 to VersionStrings.Count-1 do
+      Writeln (VersionCategories[I],'=',VersionStrings[i]);
+    free;
+    end;
+end.
