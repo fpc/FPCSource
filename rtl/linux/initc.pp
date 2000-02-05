83aecd8c937433e1f8d8ffe33517d141ba4f021f
peter <peter@freepascal.org>
2000-02-05 21:53:46 +0000

    * added initc unit dummy

---
+++ rtl/linux/initc.pp
@@ -0,0 +1,25 @@
+{
+    $Id$
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 1999-2000 by Michael Van Canneyt and Peter Vreman,
+    members of the Free Pascal development team
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+unit initc;
+interface
+{$linklib c}
+implementation
+end.
+{
+  $Log$
+  Revision 1.1  2000-02-05 21:53:46  peter
+    * added initc unit dummy
+
+}
