db3d97c2dcd8f827d5c38bc74a12c618409e0314
pierre <pierre@freepascal.org>
2000-03-01 23:24:26 +0000

    *** empty log message ***

---
+++ tests/webtbs/tbug848.pp
@@ -0,0 +1,30 @@
+{$ASMMODE INTEL}
+
+PROCEDURE a;
+VAR v,v2,v3:integer;
+
+  PROCEDURE b;
+  BEGIN
+    ASM
+      MOV AX,v
+      mov v2,AX
+      mov EDI,0
+      MOV AX,[EDI+v]
+      MOV AX,[EBP+OFFSET v]
+      MOV v3,AX
+    END;
+  END;
+
+BEGIN
+  v:=5;
+  v2:=4;
+  v3:=0;
+  b;
+  if (v2<>v) or (v3<>v) then
+    Halt(1);
+END;
+
+begin
+ a;
+ Writeln('Program works');
+end.
