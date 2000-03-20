3dd1e2bc6190b1e855f50c8d47f779282bbda4cf
pierre <pierre@freepascal.org>
2000-03-20 14:32:31 +0000

    test for 877

---
+++ tests/webtbs/tbug877.pp
@@ -0,0 +1,31 @@
+{$mode objfpc}
+
+program testlist;
+uses
+  Sysutils,
+  Classes;
+var
+  l: TList;
+  IsCaught: boolean;
+
+
+
+begin
+  L:= TList.Create;
+  IsCaught:=false;
+  Try
+  WriteLn(LongInt(L[0]));{L[0] not exist, ==> access violation}
+  L.Free;
+  Except
+    on eListError do
+       begin
+         Writeln('Exception caught');
+         IsCaught:=true;
+       end;
+  end;
+  If not IsCaught then
+    begin
+      Writeln('Error in TList');
+      Halt(1);
+    end;
+end.
