7a5fcf6931c3d3a7f1bb567cfa89eb9f46af5f00
pierre <pierre@freepascal.org>
2000-03-27 09:47:32 +0000

    some bug #896 related tests

---
+++ tests/webtbf/tbug896.pp
@@ -0,0 +1,16 @@
+
+var
+  dat : file;
+  j : longint;
+  Buffer : Array[0..2047] of byte;
+
+begin
+  for j:=0 to 2047 do
+    Buffer[j]:=j and $ff;
+  Assign(dat,'tbug896.txt');
+  Rewrite(dat,1);
+  for j:= 0 to 2047 do
+  { write should not be allowed for untyped files }
+    write (dat,Buffer[j]);
+  Close(dat);
+end.
