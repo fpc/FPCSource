7a861f6f57cb4061659374a786bbe23fc2ef8e12
pierre <pierre@freepascal.org>
2000-03-27 11:27:04 +0000

    check for 890 and 892

--- tests/webtbs/tbug890.pp
+++ tests/webtbs/tbug890.pp
@@ -1,4 +1,6 @@
-{$MODE TP }
+{$ifdef FPC}
+  {$MODE TP}
+{$endif FPC}
 
 unit tbug890;
 
@@ -6,11 +8,35 @@ INTERFACE
 
 procedure GetScreenLine(const x: Integer);
 
+function dummy(const x : integer) : integer;
+function dummy2(var x : integer) : integer;
+function dummystr(x : integer) : string;
+
 IMPLEMENTATION
 
-procedure GetScreenLine(x: Integer);
+
+procedure GetScreenLine;
 begin
- x:=5;
+end;
+
+function dummy2;
+begin
+  dummy2:=x;
+  x:=0;
+end;
+
+function dummystr;
+var
+  s : string;
+begin
+  str(x,s);
+  dummystr:=s;
+end;
+
+{ this one is refused by BP :( }
+function dummy : integer;
+begin
+  dummy:=x;
 end;
 
 begin
