f06a17c7c7a60a71799af6d3b1dc2d5b905229b3
pierre <pierre@freepascal.org>
2000-03-29 08:05:13 +0000

    * Require error 201 with range check on

--- tests/tbs/tbs0165.pp
+++ tests/tbs/tbs0165.pp
@@ -1,4 +1,8 @@
+{$R+}
 Program bug0165;
+
+uses
+  erroru;
 
 { No range check when -Cr given}
 
@@ -6,7 +10,9 @@ Type Directions = (North, East,South,West);
 
 Var Go : Directions;
 
+
 begin
+  Require_Error(201);
   Go:=North;
   Go:=Pred(Go); { must give run-time error }
   Go:=Pred(North); { must give compile time error }
