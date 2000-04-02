099f13ec929a6244de24f6e3742e3a01cfebd9f2
florian <florian@freepascal.org>
2000-04-02 12:10:11 +0000

    *** empty log message ***

---
+++ tests/testopt/readme.txt
@@ -0,0 +1,1 @@
+This directory contains some tests which test the optimizer
---
+++ tests/testopt/testreg1.pp
@@ -0,0 +1,26 @@
+{$minenumsize 1}
+
+
+type
+   tenum = (e1,e2,e3);
+
+procedure p1(e : tenum);forward;
+
+procedure p1;
+
+  begin
+     e:=tenum(byte(e)*byte(e));
+     case e of
+        e1 : ;
+     else
+       begin
+          writeln('error');
+          halt(1);
+       end;
+     end;
+  end;
+
+begin
+   p1(e1);
+end.
+
