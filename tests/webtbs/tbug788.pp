bbc901e863f07ebdc7518f918a16d1e781b0dcc3
Jonas Maebe <jonas@freepascal.org>
2000-04-20 08:05:38 +0000

    * fixed tbug788

--- tests/webtbs/tbug788.pp
+++ tests/webtbs/tbug788.pp
@@ -6,6 +6,6 @@ Var
 Begin
    For I := 0 To 359 Do
    Begin
-      WriteLn( RadToDeg(ArcSin( DegToRad(Real(I)))):3:18);
+      WriteLn( RadToDeg(ArcSin(Sin(DegToRad(Real(I))))):3:18);
    End
 End.
