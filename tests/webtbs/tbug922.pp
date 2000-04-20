5ee979254c069721668bef392b0e5c85e1e51352
pierre <pierre@freepascal.org>
2000-04-20 15:54:17 +0000

    some new ones

---
+++ tests/webtbs/tbug922.pp
@@ -0,0 +1,24 @@
+program test;
+
+{$ifdef win32}
+uses
+  windows;
+{$endif }
+
+procedure write1(  var charbuf:string);
+begin
+  Writeln(Charbuf);
+end;
+
+procedure write2(  var charbuf:string; attrbuf:array of word);
+begin
+  Writeln(Charbuf);
+end;
+
+var chars : String[82];
+    attrs : array [1..162] of word;
+begin
+  Chars := 'Das ist ein Test, den ich gerade schreibe';
+  write1(chars);
+  write2(chars,attrs);
+end.
