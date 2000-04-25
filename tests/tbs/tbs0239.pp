4ab63fea0ae925a0df5e1972fa3e802594e7b8d6
pierre <pierre@freepascal.org>
2000-04-25 21:41:35 +0000

    * avoid SIGSEGV

--- tests/tbs/tbs0239.pp
+++ tests/tbs/tbs0239.pp
@@ -1,5 +1,5 @@
 {$mode delphi}
-  uses 
+  uses
 {$ifdef go32v2}
     dpmiexcp,
 {$endif go32v2}
@@ -7,12 +7,41 @@
    type
      ttest=class
      end;
+     ttest2 = class(ttest)
+     end;
      ttestclass=class of ttest;
    var
-     i:ttest;
+     i,j:ttest;
      tt:tclass;
    begin
      tt:=ttest;
-     write(i is tt);
+     i:=ttest.create;
+     j:=ttest2.create;
+     Writeln('tt is a class of ttest initialized by "tt:=ttest"');
+     Writeln('i is a ttest class initialized by "i:=ttest.create"');
+     Writeln('j is a ttest class initialized by "j:=ttest2.create"');
+     writeln('i is tobject ',i is tobject);
+     if not(i is tobject) then
+       Halt(1);
+     writeln('i is tt ',i is tt);
+     if not(i is tt) then
+       Halt(1);
+     writeln('i is ttest ',i is ttest);
+     if not(i is ttest) then
+       Halt(1);
+     writeln('i is ttest2 ',i is ttest2);
+     if (i is ttest2) then
+       Halt(1);
+     writeln('j is tobject ',j is tobject);
+     if not(j is tobject) then
+       Halt(1);
+     writeln('j is tt ',j is tt);
+     if not(j is tt) then
+       Halt(1);
+     writeln('j is ttest ',j is ttest);
+     if not(j is ttest) then
+       Halt(1);
+     writeln('j is ttest2 ',j is ttest2);
+     if not(j is ttest2) then
+       Halt(1);
    end.
-
