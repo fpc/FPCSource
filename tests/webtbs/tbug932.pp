695f24d2bd38e6cfaf2cdf6083bb4dee3f6d1a82
pierre <pierre@freepascal.org>
2000-04-27 23:08:59 +0000

    new

---
+++ tests/webtbs/tbug932.pp
@@ -0,0 +1,16 @@
+program test;
+
+{$ASMMODE Intel }
+
+procedure TestProc;
+const
+  TestConst: String = 'Test';
+begin
+  asm
+    mov edi, offset TestConst
+  end;
+end;
+
+begin
+  TestProc;
+end.
