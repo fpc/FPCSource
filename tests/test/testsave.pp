366aece76e6f08a1628f831c7170996f8a030552
pierre <pierre@freepascal.org>
2000-05-03 22:58:41 +0000

    * saveregisters also save the one needed for return values

---
+++ tests/test/testsave.pp
@@ -0,0 +1,18 @@
+
+
+function x : longint;saveregisters;
+begin
+  x:=34;
+end;
+
+var
+  y : longint;
+begin
+  asm
+    movl $15,%eax
+  end;
+  y:=x;
+  Writeln(y);
+  if y<>34 then
+    halt(1);
+end.
