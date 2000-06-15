c4c93e0cd0c09fa995df0c86270d31b7a7e8fb9a
michael <michael@freepascal.org>
2000-06-15 19:05:55 +0000

    + Final changes for documentation

--- docs/progex/ctest2.c
+++ docs/progex/ctest2.c
@@ -6,18 +6,15 @@ int main()
     void *lib;
     char *s;
     int FromPos, ToPos;
-    char* (*SubStr)(const char*, int*, int*);
-    printf("arh %d\n",RTLD_LAZY);
-    lib = dlopen("./libcaseudf.so", RTLD_LAZY);
-    printf("Result from dlopen (library handle): 0x%08x\n", lib);
+    char* (*SubStr)(const char*, int, int);
+
+    lib = dlopen("./libsubs.so", RTLD_LAZY);
     SubStr = dlsym(lib, "SUBSTR");
-    printf("Address of SubStr = 0x%08x, last error code = %i\n",
-      SubStr, dlerror());
 
     s = strdup("Test");
     FromPos = 2;
     ToPos = 3;
-    printf("Result from SubStr: '%s'\n", (*SubStr)(s, &FromPos, &ToPos));
+    printf("Result from SubStr: '%s'\n", (*SubStr)(s, FromPos, ToPos));
     dlclose(lib);
     return 0;
 }
