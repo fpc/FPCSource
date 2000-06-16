3dac49524973e6433dfc30d4ca6fb8cc0ca23bef
pierre <pierre@freepascal.org>
2000-06-16 09:30:01 +0000

    * new files from Gabor

---
+++ ide/text/wconsts.pas
@@ -0,0 +1,38 @@
+{
+    $Id$
+    This file is part of the Free Pascal Integrated Development Environment
+    Copyright (c) 2000 by B‚rczi G bor
+
+    Strings for common utilities
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+{$i globdir.inc}
+unit wconsts;
+
+  interface
+
+  uses Dos;
+
+{$ifdef LANG_HUN}
+{$i wconstsh.inc}    { Hungarian language file }
+{$else}
+ {$ifdef LANG_GER}
+ {$i wconstsg.inc}    { German language file }
+ {$else}
+   {$i wconstse.inc}  { English language file }
+ {$endif}
+{$endif}
+
+  implementation
+
+end.
+{
+  $Log$
+  Revision 1.1  2000-06-16 09:30:01  pierre
+   * new files from Gabor
+
+}
