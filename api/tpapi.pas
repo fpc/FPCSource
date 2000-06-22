5997a4363b64c2e8274f7028f6a32be581164c0f
Tomas Hajny <hajny@freepascal.org>
2000-06-22 16:32:30 +0000

    * ApiComm instead of Common

--- api/tpapi.pas
+++ api/tpapi.pas
@@ -1,6 +1,36 @@
+{
+   $Id$
+   Helper for building all the units.
+
+   Copyright (c) 1999-2000 by FPC Developer Team
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+}
+
 program BuildAPI;
 uses
-  Common, FileSys, FileCtrl, Video, Keyboard, Mouse, CallSpec;
+  ApiComm, FileSys, FileCtrl, Video, Keyboard, Mouse, CallSpec;
 
 begin
 end.
+
+{
+  $Log$
+  Revision 1.2  2000-06-22 16:32:30  hajny
+    * ApiComm instead of Common
+
+
+}
