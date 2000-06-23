96900b2280d9c8e7d928c76c16d009e4ed0c55b7
peter <peter@freepascal.org>
2000-06-23 20:20:16 +0000

    + new

---
+++ tests/tbs/tbs0330.pp
@@ -0,0 +1,26 @@
+{$ifdef fpc}{$mode objfpc}{$endif}
+uses
+  Classes;
+
+type
+  TMyClass = class(TPersistent);
+
+var
+  MyVar: Integer;
+
+
+type
+  TMyClass2 = class(TObject)
+    procedure MyProc;
+  end;
+
+  TMyOtherClass = class(TPersistent);
+
+procedure TMyClass2.MyProc;
+var
+  MyImportantVar: Integer;
+begin
+end;
+
+begin
+end.
