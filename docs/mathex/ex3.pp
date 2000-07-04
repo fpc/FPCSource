b4e9f9cd04bd22914deea92eac311ed955e7f0cf
michael <michael@freepascal.org>
2000-07-04 21:24:14 +0000

    + Initial implementation

---
+++ docs/mathex/ex3.pp
@@ -0,0 +1,10 @@
+Program Example3;
+
+{ Program to demonstrate the arcosh function. }
+
+Uses math;
+
+begin
+  Writeln(arcosh(1));
+  Writeln(arcosh(2));
+end.
---
+++ docs/mathex/template.pp
@@ -0,0 +1,8 @@
+Program Example;
+
+{ Program to demonstrate the *** function. }
+
+Uses math;
+
+begin
+end.
