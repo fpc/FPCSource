2e0ef2edb75e8edcda4000d1caf10dbeed32e005
peter <peter@freepascal.org>
2000-07-06 19:27:41 +0000

    * bugs from mailinglist

---
+++ tests/tbs/tbs0333.pp
@@ -0,0 +1,16 @@
+var
+  a,b : comp;
+  s1,s2 : string;
+begin
+  a:=11384563;
+  b:=a*a;
+  str(a*a:0:0,s1);
+  str(b:0:0,s2);
+  writeln(s1);
+  writeln(s2);
+  if (s1<>'129608274700969') or (s2<>'129608274700969') then
+   begin
+     writeln('Error with comp type rounding');
+     halt(1);
+   end;
+end.
---
+++ tests/tbs/tbs0335.pp
@@ -0,0 +1,7 @@
+{$mode delphi}
+procedure f;stdcall export;
+asm
+end;
+
+begin
+end.
