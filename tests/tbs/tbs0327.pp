e514bed570f455b3e920257b8b154d5923eb6434
peter <peter@freepascal.org>
2000-08-02 19:47:40 +0000

    * added more tests

--- tests/tbs/tbs0327.pp
+++ tests/tbs/tbs0327.pp
@@ -1,4 +1,22 @@
 {$ifdef fpc}{$mode delphi}{$endif}
+unit tbs0327;
+interface
+
+type
+  tc=class
+    procedure l(i:integer);overload;
+    procedure l(s:string);overload;
+  end;
+
+implementation
+
+procedure tc.l(i:integer);
+begin
+end;
+
+procedure tc.l(s:string);
+begin
+end;
 
 procedure k(l:longint);overload;
 begin
