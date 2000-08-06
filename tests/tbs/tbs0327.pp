aa4be75abca5a8f97ef1a9a54b7b3ce243d22604
peter <peter@freepascal.org>
2000-08-06 14:14:25 +0000

    * more checks

--- tests/tbs/tbs0327.pp
+++ tests/tbs/tbs0327.pp
@@ -1,5 +1,5 @@
 {$ifdef fpc}{$mode delphi}{$endif}
-unit tbs0327;
+unit p;
 interface
 
 type
@@ -8,13 +8,35 @@ type
     procedure l(s:string);overload;
   end;
 
+    procedure l2(i:integer);overload;
+    procedure l2(s:string);overload;
+
 implementation
+
+    procedure l3(i:integer);forward;overload;
+    procedure l3(s:string);forward;overload;
 
 procedure tc.l(i:integer);
 begin
 end;
 
 procedure tc.l(s:string);
+begin
+end;
+
+procedure l2(i:integer);
+begin
+end;
+
+procedure l2(s:string);
+begin
+end;
+
+procedure l3(i:integer);
+begin
+end;
+
+procedure l3(s:string);
 begin
 end;
 
