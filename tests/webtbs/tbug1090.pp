7fbf34e60d2dd1c79cccf630538d300a5a440d3c
peter <peter@freepascal.org>
2000-08-06 10:27:52 +0000

    * init

---
+++ tests/webtbs/tbug1090.pp
@@ -0,0 +1,17 @@
+{$asmmode intel}
+const
+  Number = $7FFFFFF;
+  Shift  = 7;
+var
+  l : longint;
+begin
+  ASM
+        MOV EAX,(Number shr (Shift+3))
+        mov l,eax
+  End;
+  if l<>131071 then
+   begin
+     writeln('error in constant eval in intel reader');
+     halt(1);
+   end;
+end.
