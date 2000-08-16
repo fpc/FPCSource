f53823cb778dbbb86b2e2a2e7cdbfddf36e39a08
florian <florian@freepascal.org>
2000-08-16 13:02:33 +0000

    *** empty log message ***

---
+++ tests/webtbs/tbug1068.pp
@@ -0,0 +1,14 @@
+PROGRAM bug1068;
+VAR i: INT64;
+    s : string;
+BEGIN
+  i:=2147483648;
+  str(i,s);
+  if s<>'2147483648' then
+    begin
+       writeln(s);
+       halt(1);
+    end
+  else
+    halt(0);
+END.
