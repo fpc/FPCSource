bfdac9385b45b397a4a4041eb8f0dc2b4fc02991
michael <michael@freepascal.org>
2000-08-18 19:15:34 +0000

    + Implemented loading of dynamical libraries

---
+++ rtl/linux/dl.pp
@@ -0,0 +1,18 @@
+Unit dl;
+
+Interface
+
+Const  
+  LibDL = 'dl';
+
+  RTLD_LAZY         = $001;
+  RTLD_NOW          = $002;
+  RTLD_BINDING_MASK = $003;
+  
+Function dlopen(Name : PChar; Flags : longint) : Pointer; cdecl; external libdl;
+FUnction dlsym(Lib : Pointer; Name : Pchar) : Pointer; cdecl; external Libdl;  
+Function dlclose(Lib : Pointer) : Longint; cdecl; external libdl;
+
+implementation
+
+end.
