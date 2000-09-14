8085b5cab6227eb56de0ae7f790c6343c2c1699b
marco <marco@freepascal.org>
2000-09-14 13:38:25 +0000

    * Moved from Linux dir. now start of generic unix dir, from which the     really exotic features should be moved to the target specific dirs.

---
+++ rtl/unix/initc.pp
@@ -0,0 +1,32 @@
+{
+    $Id$
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 1999-2000 by Michael Van Canneyt and Peter Vreman,
+    members of the Free Pascal development team
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+unit initc;
+interface
+{$linklib c}
+implementation
+end.
+{
+  $Log$
+  Revision 1.1.2.1  2000-09-14 13:38:25  marco
+   * Moved from Linux dir. now start of generic unix dir, from which the
+      really exotic features should be moved to the target specific dirs.
+
+  Revision 1.1  2000/07/13 06:30:53  michael
+  + Initial import
+
+  Revision 1.1  2000/02/05 21:53:46  peter
+    * added initc unit dummy
+
+}
