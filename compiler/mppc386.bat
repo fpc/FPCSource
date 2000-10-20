9773d0089771833bef09dd70792387efeba55cb8
florian <florian@freepascal.org>
2000-10-20 22:00:32 +0000

    * -Fui386 added

--- compiler/mppc386.bat
+++ compiler/mppc386.bat
@@ -1,4 +1,4 @@
-ppc386 -O3p3 -Ch8000000 -dI386 -dGDB -dBROWSERLOG -Sg pp.pas %1 %2 %3 %4 %5 %6 %7 %8 %9
+ppc386 -O3p3 -Ch8000000 -Fui386 -dI386 -dGDB -dBROWSERLOG -Sg pp.pas %1 %2 %3 %4 %5 %6 %7 %8 %9
 if errorlevel 0 goto success
 goto failed
 :success
