fe3dffbaa62a94a28821809dee410a9b25bde9a3
sg <sg>
2000-11-17 13:40:53 +0000

    * Fixed header and log section

--- fcl/inc/ssockets.pp
+++ fcl/inc/ssockets.pp
@@ -1,3 +1,17 @@
+{
+    $Id$
+    This file is part of the Free Component Library (FCL)
+    Copyright (c) 1999-2000 by the Free Pascal development team
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
 unit ssockets;
 
 interface
@@ -93,7 +107,7 @@ type
   TUnixServer = Class(TSocketServer)
   Private 
     FUnixAddr : TUnixSockAddr;
-    FFileName : ShortString;
+    FFileName : String;
   Protected
     Function Accept : Longint;override;
     Function SockToStream (ASocket : Longint) : TSocketStream;Override;
@@ -479,8 +493,13 @@ begin
     Raise ESocketError.Create(seConnectFailed,[FFilename]);
 end;
                                           
-end.  $Log$
-end.  Revision 1.2  2000-07-13 11:33:00  michael
-end.  + removed logs
-end. 
+end.
+
+{
+  $Log$
+  Revision 1.3  2000-11-17 13:40:53  sg
+  * Fixed header and log section
+
+  Revision 1.2  2000/07/13 11:33:00  michael
+  + removed logs
 }
