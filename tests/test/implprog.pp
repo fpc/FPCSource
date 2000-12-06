35c8fab00c8d181bcf64095fe81e97e195df97fe
Jonas Maebe <jonas@freepascal.org>
2000-12-06 17:01:08 +0000

    + added implicit unit bug test (compile implprog twice without deleting     ppu files to reproduce)

---
+++ tests/test/implprog.pp
@@ -0,0 +1,16 @@
+uses impluni1;
+
+    Type
+      BEC_Single_Error = record
+         E : integer;
+         M : string [80];
+      end;
+
+    Const
+      BEC_Err_Msgs: array [0..1] of BEC_Single_Error =
+       ((E : impluni1.ICanUseThis;  M : '[1] No Error'),
+        (E : impluni2.ICantUseThis; M : '[10000] A Bug?'));
+
+begin
+end.
+
---
+++ tests/test/impluni1.pp
@@ -0,0 +1,13 @@
+unit impluni1;
+
+interface
+
+uses impluni2;
+
+const
+  ICanUseThis = 1;
+
+implementation
+
+end.
+
