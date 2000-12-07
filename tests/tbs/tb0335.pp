0d8825bc9891a578ab7c74146d779ca3f683929c
florian <florian@freepascal.org>
2000-12-07 22:29:12 +0000

    *** empty log message ***

---
+++ tests/tbs/tb0335.pp
@@ -0,0 +1,22 @@
+{$asmmode intel}
+
+var
+   a : array[0..5] of byte;
+
+function f : longint;assembler;
+
+  asm
+    mov ebx,offset a
+    mov ecx,0
+    mov al,[ebx+4*ecx]
+  end;
+
+begin
+   fillchar(a,5,255);
+   a[0]:=0;
+   if f<>0 then
+     begin
+        writeln('Scale factor problem in asmmode intel!');
+        halt(1);
+     end;
+end.
