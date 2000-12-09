6c5f5dc199ce3895cb16c900ba76d706cdd81184
florian <florian@freepascal.org>
2000-12-09 20:45:45 +0000

    no message

---
+++ tests/tbs/tb0336.pp
@@ -0,0 +1,40 @@
+var
+   l : longint;
+   d : dword;
+   s : string;
+   code : integer;
+
+procedure do_error(l : longint);
+
+  begin
+     writeln('Error near number ',l);
+     halt(1);
+  end;
+
+begin
+   s:='4294967295';
+   val(s,d,code);
+   if code<>0 then
+     do_error(1);
+   s:='4294967296';   
+   val(s,d,code);
+   if code=0 then
+     do_error(1);
+
+   s:='2147483647';
+   val(s,l,code);
+   if code<>0 then
+     do_error(3);
+   s:='2147483648';
+   val(s,l,code);
+   if code=0 then
+     do_error(4);
+   s:='-2147483648';
+   val(s,l,code);
+   if code<>0 then
+     do_error(5);
+   s:='-2147483649';
+   val(s,l,code);
+   if code=0 then
+     do_error(6);
+end.
