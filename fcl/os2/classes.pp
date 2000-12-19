ad054776880adab7740b92505948684303427ef6
Tomas Hajny <hajny@freepascal.org>
2000-12-19 00:43:07 +0000

    + FCL made compilable under OS/2

--- fcl/os2/classes.pp
+++ fcl/os2/classes.pp
@@ -24,6 +24,7 @@ unit Classes;
 interface
 
 uses
+  DosCalls, (* Needed here (i.e. before SysUtils) to avoid type clashes. *)
   strings,
   sysutils;
 
@@ -32,7 +33,6 @@ uses
 implementation
 
 uses
-  doscalls,
   typinfo;
 
 { OS - independent class implementations are in /inc directory. }
@@ -47,7 +47,10 @@ finalization
 end.
 {
   $Log$
-  Revision 1.3  2000-08-25 17:32:16  hajny
+  Revision 1.4  2000-12-19 00:43:07  hajny
+    + FCL made compilable under OS/2
+
+  Revision 1.3  2000/08/25 17:32:16  hajny
     * Cosmetic change (OS/2 instead of win32 in header)
 
   Revision 1.2  2000/07/13 11:33:01  michael
