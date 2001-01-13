a1f1b9c441b05e022feb21e84d0665a7a3933e31
peter <peter@freepascal.org>
2001-01-13 11:14:16 +0000

    * added

---
+++ tests/webtbs/tw1351.pp
@@ -0,0 +1,10 @@
+procedure Test(ParArr :array of const);
+begin
+   writeln(ParArr[0].vtype,' ',vtObject,' ',vtclass);
+   if ParArr[0].vtype<>vtObject then
+    halt(1);
+end;
+
+begin
+        Test([TObject.Create]);
+end.
