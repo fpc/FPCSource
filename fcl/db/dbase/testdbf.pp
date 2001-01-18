9a9ee075e5f4ccb153c4f4fe0b2db27cad718b8d
michael <michael@freepascal.org>
2001-01-18 22:19:17 +0000

    + Merged from fixbranch

---
+++ fcl/db/dbase/testdbf.pp
@@ -0,0 +1,45 @@
+program dumpdb;
+
+{$i+}
+
+uses db,dbf,sysutils;
+
+Procedure DumpTable (Const TN,FN : String);
+
+Var
+  I,Count : longint;
+  F : Text;
+  Buf : Array[1..1024*4] of byte;
+  
+begin
+  Assign(F,FN);
+  Rewrite(F);
+  SetTextBuf(F,Buf);
+  With TDBF.Create(Nil) do
+    begin
+    TableName:=TN;
+    Open;
+    While not EOF do
+      begin
+      Inc(Count);
+      For I:=0 to FieldCount-1 do
+        With Fields[i] do 
+          Writeln(F,FieldName:20,' : ',AsString);
+      Writeln(F,StringOfChar('=',72));
+      Next;
+      end;
+    end;
+  Writeln(F,'Dumped total of ',Count,' records.');
+  Close(F);
+end;
+
+Var i : longint;
+
+begin
+  If ParamCount<2 then
+    begin
+    Writeln('Usage: dumpdb tablename filename');
+    Halt(1);
+    end;
+  DumpTable(Paramstr(1),Paramstr(2));
+end.
