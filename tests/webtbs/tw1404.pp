bfb241352d929f2c5d6b06b86c94f2ae787abe24
peter <peter@freepascal.org>
2001-02-20 21:27:40 +0000

    * updated

--- tests/webtbs/tw1404.pp
+++ tests/webtbs/tw1404.pp
@@ -1,29 +1,5 @@
-program test_assembler;
-
-procedure test_att;
+uses
+  dos;
 begin
-{$asmmode att}
- asm
-   ret
-   lret
-   iret
-   iretw
- end;
-end;
-
-procedure test_intel;
-begin
-{$asmmode intel}
- asm
-   ret
-   retf
-   retn
-   iret
-   iretd
-   iretw
- end;
-end;
-
-begin
-  Writeln('This is just to test special assembler instructions');
+  chdir('');
 end.
