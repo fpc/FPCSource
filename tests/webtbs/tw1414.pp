c9d78b74323768a2f74f94b50bbdeba9949fe3ce
pierre <pierre@freepascal.org>
2001-02-21 21:37:39 +0000

    * generate some output on error

--- tests/webtbs/tw1414.pp
+++ tests/webtbs/tw1414.pp
@@ -1,5 +1,14 @@
 uses
   dos;
+ var
+   i : longint;
 begin
+{$i-}
   chdir('');
+  i:=ioresult;
+  if i<>0 then
+    begin
+      Writeln('chdir('''') fails wih error ',i);
+      Halt(i);
+    end;
 end.
