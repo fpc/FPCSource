1aafea6d824ca658a3448b65d290e1ecd15eeb88
peter <peter@freepascal.org>
2001-03-13 18:49:36 +0000

    * new bug from Pavel with property and indexes

---
+++ tests/tbs/tb0346.pp
@@ -0,0 +1,20 @@
+{$MODE DELPHI}
+type
+aClass=class
+ private
+  aa:longint;
+  procedure bb(index:integer;value:longint);
+ public
+  property cc:longint index 1 read aa write bb;
+end;
+procedure AClass.bb(index:integer;value:longint);
+ begin
+  aa:=value;
+ end;
+var
+ C:aClass;
+begin
+ C:=aClass.Create;
+ C.cc:=1;
+ writeln(C.cc);
+end.
