451b14c6b55448646736dd07ed9a555027aafefe
peter <peter@freepascal.org>
2001-04-13 22:22:59 +0000

    * call set_varstate for procvar calls

--- compiler/ncal.pas
+++ compiler/ncal.pas
@@ -678,6 +678,7 @@ implementation
          { procedure variable ? }
          if assigned(right) then
            begin
+              set_varstate(right,true);
               resulttypepass(right);
               if codegenerror then
                exit;
@@ -1615,7 +1616,10 @@ begin
 end.
 {
   $Log$
-  Revision 1.27  2001-04-13 01:22:08  peter
+  Revision 1.28  2001-04-13 22:22:59  peter
+    * call set_varstate for procvar calls
+
+  Revision 1.27  2001/04/13 01:22:08  peter
     * symtable change to classes
     * range check generation and errors fixed, make cycle DEBUG=1 works
     * memory leaks fixed
