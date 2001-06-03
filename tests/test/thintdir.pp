282c4d8f689538b09846128dd5b338450971dfc7
peter <peter@freepascal.org>
2001-06-03 21:58:24 +0000

    * hint directive test cases

---
+++ tests/test/thintdir.pp
@@ -0,0 +1,42 @@
+{ %version=1.1 }
+
+{$mode delphi}
+
+type
+  t1 = record
+  end platform;
+
+  t2 = class
+  end platform;
+  
+  t3 = object
+  end platform;
+  
+  t4 = record
+    w1 : word deprecated;
+    w2 : word deprecated
+  end;
+
+  t5 = record
+    w1 : word deprecated;
+  end;
+
+const
+  c1 : word = 2;
+  c2 = 21312 platform;
+
+var
+  v1 : word deprecated library;
+  l1,
+  l2 : word deprecated;
+
+procedure p1;platform deprecated;library;
+begin
+end;
+
+procedure p2;platform;deprecated;
+begin
+end;
+
+begin
+end.
