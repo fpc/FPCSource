e23694b03eb8746910794f997fd2b90963d64512
carl <carl@freepascal.org>
2001-07-14 04:25:17 +0000

    system unit testing : basic directory services

---
+++ tests/test/units/system/tdir.pp
@@ -0,0 +1,38 @@
+{ Program to test OS-specific features of the system unit }
+{ routines to test: 					  }
+{   mkdir()						  }
+{   chdir()						  }
+{ This program shoulf not be executed in a roto directory }
+{ Creates the following directory, and sets it as the     }
+{ current directory.					  }
+{    ../testdir						  }
+
+
+Program tdir;
+{$I+}
+
+var
+ s: string;
+Begin
+   Writeln('changing to parent directory...');
+   chdir('..');
+   Writeln('making directory...');
+   mkdir('testdir');
+   Writeln('going into the newly created directory...');
+   chdir('testdir');
+   Writeln('making directory...');
+   mkdir('testdir2');
+   WriteLn('removing directory ...');
+   rmdir('testdir2');
+   WriteLn('getting current directory...');
+   getdir(0,s);
+   WriteLn(s);
+end.
+
+{
+ $Log$
+ Revision 1.1  2001-07-14 04:25:17  carl
+ system unit testing : basic directory services
+
+}
+
