e6a8217133db7d2f370c8a3fe052f18a34970c9a
carl <carl@freepascal.org>
2001-07-14 04:24:20 +0000

    + system unit tests : paramstr() + paramcount()

---
+++ tests/test/units/system/tparam.pp
@@ -0,0 +1,17 @@
+program tparam;
+
+var
+ i: integer;
+Begin
+  for i:=0 to ParamCount do
+   Begin
+     WriteLn(paramStr(i));
+   End;
+end.
+
+{
+ $Log$
+ Revision 1.1  2001-07-14 04:24:20  carl
+ + system unit tests : paramstr() + paramcount()
+
+}
