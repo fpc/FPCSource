f399cef4e1738916b5a2bd878093f31a6526fa4f
peter <peter@freepascal.org>
2001-08-01 18:00:15 +0000

    * Test for result in assembler for delphi mode

---
+++ tests/tbs/tb0367.pp
@@ -0,0 +1,25 @@
+{$ifdef fpc}
+  {$mode delphi}
+  {$asmmode intel}
+{$endif}
+
+function LRot(Value:Byte) : Byte; assembler;
+asm
+        MOV     CL, Value
+        MOV     Result, CL
+        MOV     AL, 20
+end;
+
+
+var
+  i : Byte;
+begin
+  i:=LRot(10);
+  writeln('LRot(10) = ',i,' (should be 10)');
+  if i<>10 then
+   begin
+     writeln('ERROR!');
+     halt(1);
+   end;
+end.
+
