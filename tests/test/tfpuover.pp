4a4d7cd40db66e5453cc8bea2ca33162b71c1793
pierre <pierre@freepascal.org>
2001-09-10 23:06:45 +0000

    test for fpu overflow

---
+++ tests/test/tfpuover.pp
@@ -0,0 +1,28 @@
+{$define FAIL}
+
+var
+  x1,y1,z : double;
+
+  function x : double;
+  begin
+    x :=((x1+y1)*(x1-y1))*((x1+y1)*(x1-y1));
+  end;
+  function y : double;
+  begin
+    y :=((x1*y1)/(x1+y1)){$ifdef FAIL}*((x1+y1)*(x1-y1)){$endif};
+  end;
+
+begin
+  x1:=2;
+  y1:=3;
+{ Explanation a addnote needs the same number of fpu regs
+  that the max fpu need of left and right node, unless
+  these two numbers are equal:
+  this is the reason of the symetric form of this test code PM }
+  z:=((((x+y)*(x-y))+((x+y)*(x-y)))+(((x+y)*(x-y))+((x+y)*(x-y)))+
+      (((x+y)*(x-y))+((x+y)*(x-y)))+(((x+y)*(x-y))+((x+y)*(x-y))))+
+     ((((x+y)*(x-y))+((x+y)*(x-y)))+(((x+y)*(x-y))+((x+y)*(x-y)))+
+      (((x+y)*(x-y))+((x+y)*(x-y)))+(((x+y)*(x-y))+((x+y)*(x-y))));
+  Writeln('z = ',z);
+end.
+
