b41cfaef2b86f6a792339ed92c0de0151d926816
michael <michael@freepascal.org>
2001-09-12 22:39:49 +0000

    + Added examples 4 to 7

---
+++ docs/kbdex/ex4.pp
@@ -0,0 +1,26 @@
+program example4;
+
+{ This program demonstrates the PollKeyEvent function }
+
+uses keyboard, keybutil;
+
+Var
+  K : TKeyEvent;
+
+begin
+  InitKeyBoard;
+  Writeln('Press keys, press "q" to end.');
+  Repeat
+    K:=PollKeyEvent;
+    If k<>0 then
+      begin
+      K:=GetKeyEvent;
+      K:=TranslateKeyEvent(K);
+      writeln;
+      Writeln('Got key : ',KeyEventToString(K));
+      end
+    else
+      write('.');
+  Until (GetKeyEventChar(K)='q');
+  DoneKeyBoard;
+end.
---
+++ docs/kbdex/ex7.pp
@@ -0,0 +1,22 @@
+program example1;
+
+{ This program demonstrates the GetKeyEvent function }
+
+uses keyboard, keybutil;
+
+Var
+  K : TKeyEvent;
+
+begin
+  InitKeyBoard;
+  Writeln('Press keys, press "q" to end.');
+  Repeat
+    K:=GetKeyEvent;
+    K:=TranslateKeyEvent(K);
+    If IsFunctionKey(K) then
+      Writeln('Got function key : ',KeyEventToString(K))
+    else
+      Writeln('not a function key.');  
+  Until (GetKeyEventChar(K)='q');
+  DoneKeyBoard;
+end.
