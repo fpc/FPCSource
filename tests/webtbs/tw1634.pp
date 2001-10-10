2fc53dd0151a147f96fcfba7e8e5b6bc06332239
pierre <pierre@freepascal.org>
2001-10-10 22:23:20 +0000

    New test

---
+++ tests/webtbs/tw1634.pp
@@ -0,0 +1,22 @@
+{ Source provided for Free Pascal Bug Report 1634 }
+{ Submitted by "Igor Grigoriev" on  2001-10-06 }
+{ e-mail: igor@sch57.msk.ru }
+{$DEFINE A}
+program test;
+
+{$IFNDEF A}
+  procedure p1;
+  begin
+   (*{$IFDEF B}  {$ENDIF}*)
+  end;
+{$ENDIF}
+
+BEGIN
+END.
+
+// The above program gives error:
+// test.pas(13) Fatal: Unexpected end of file
+// But comment line changed to:
+// (* {$IFDEF B}  {$ENDIF}*)
+//   ( space after '(*' added )
+//gives successful compilation.
