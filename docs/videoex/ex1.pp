754e4047a6ba767e21002f7238f424c5a48f4f51
michael <michael@freepascal.org>
2001-10-10 22:13:43 +0000

    + Initial implementation

---
+++ docs/videoex/ex1.pp
@@ -0,0 +1,18 @@
+program testvideo;
+
+uses video,keyboard,vidutil;
+
+Var 
+  i : longint;
+  k : TkeyEvent;
+  
+begin
+  InitVideo;
+  InitKeyboard;
+  For I:=1 to 10 do
+    TextOut(i,i, 'Press any key to end');
+  UpdateScreen(False);
+  K:=GetKeyEvent;
+  DoneKeyBoard;
+  DoneVideo;
+end.  
---
+++ docs/videoex/template.pp
@@ -0,0 +1,12 @@
+Program Example;
+
+{ Program to demonstrate the *** function. }
+
+Uses video,keyboard,vidutil;
+
+begin
+  InitVideo;
+  InitKeyboard;
+  DoneKeyboard;
+  DoneVideo;
+end.
