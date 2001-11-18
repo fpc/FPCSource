f6d0c598bf0ea07d598265a47f67b8375afcc163
florian <florian@freepascal.org>
2001-11-18 18:27:15 +0000

    * small fixes to test really the int64 stuff

--- docs/typinfex/ex15.pp
+++ docs/typinfex/ex15.pp
@@ -14,12 +14,12 @@ begin
   O:=TMyTestObject.Create;
   Writeln('Int64 property : ');
   PI:=GetPropInfo(O,'Int64Field');
-  Writeln('Value            : ',O.RealField);
+  Writeln('Value            : ',O.Int64Field);
   Writeln('Get (name)       : ',GetInt64Prop(O,'Int64Field'));
   Writeln('Get (propinfo)   : ',GetInt64Prop(O,PI)); 
-  SetInt64Prop(O,'RealField',12345);
+  SetInt64Prop(O,'Int64Field',12345);
   Writeln('Set (name,12345)    : ',O.Int64Field);
-  SetFloatProp(O,PI,54321);
+  SetInt64Prop(O,PI,54321);
   Writeln('Set (propinfo,54321) : ',O.Int64Field);
   O.Free;
 end.
