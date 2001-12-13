b1a5ae6670616fda7c837a8ed19bbcba2c7684f3
michael <michael@freepascal.org>
2001-12-13 20:00:45 +0000

    + Added real2double example

---
+++ docs/refex/ex110.pp
@@ -0,0 +1,26 @@
+program Example110;
+
+{ Program to demonstrate the Real2Double function. }
+
+Var
+  i : integer;
+  R : Real48;
+  D : Double;
+  E : Extended;
+  F : File of Real48;
+  
+begin
+  Assign(F,'reals.dat');
+  Reset(f);
+  For I:=1 to 10 do
+    begin
+    Read(F,R);
+    D:=Real2Double(R);
+    Writeln('Real ',i,' : ',D);
+    D:=R;
+    Writeln('Real (direct to double)  ',i,' : ',D);
+    E:=R;
+    Writeln('Real (direct to Extended) ',i,' : ',E);
+    end;
+  Close(f);  
+end.
