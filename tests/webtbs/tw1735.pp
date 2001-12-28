69cfac21355217d2c2759148dc08307725800b03
Jonas Maebe <jonas@freepascal.org>
2001-12-28 14:10:08 +0000

    + bug #1735

---
+++ tests/webtbs/tw1735.pp
@@ -0,0 +1,22 @@
+{$mode delphi}
+function UpperCase(S: string): string;
+var 
+  i: integer; 
+begin 
+  for i := 1 to length(s) do 
+    case s[i] of 
+      'a' .. 'z': Dec(s[i], 32); 
+//      'a' .. 'z': s[i] := chr(ord(s[i])-32); 
+    end; 
+  result := s; 
+end; 
+
+var
+  s: string;
+begin
+  s := 'abcdef';
+  writeln(uppercase(s));
+  writeln(s);
+  if (s <> 'abcdef') then
+    halt(1);
+end.
