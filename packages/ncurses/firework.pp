6da3bb9300092b2bb740264bda64c15ec2ce2c03
peter <peter@freepascal.org>
2002-01-29 17:37:42 +0000

    * packages reorganization

--- packages/ncurses/firework.pp
+++
@@ -1,6 +0,0 @@
-{
-  $Log$
-  Revision 1.2  2000-07-13 11:33:27  michael
-  + removed logs
- 
-}
--- packages/uncgi/testcgi.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Setting environment variables:
-set REQUEST_METHOD=GET
-set QUERY_STRING=name=michael&address=home&city=heaven
-set HTTP_USER_AGENT=Shell script
-set HTTP_REFERER=The shell prompt
-echo Running program
-testcgi.exe
-
