3f639982d09ddd19d2cd75d971396101fcd9534c
carl <carl@freepascal.org>
2002-03-08 20:18:28 +0000

    * moved to correct directory

---
+++ tests/webtbf/tw1827.pp
@@ -0,0 +1,18 @@
+{ %FAIL }
+
+begin
+  ReadLn('SomeText');
+end.
+
+{
+  $Log$
+  Revision 1.1  2002-03-08 20:18:28  carl
+  * moved to correct directory
+
+  Revision 1.1  2002/03/07 21:44:14  carl
+  + bug report #1827 testing
+
+
+}  
+
+
--- tests/webtbf/tw1858.pp
+++ tests/webtbf/tw1858.pp
@@ -1,4 +1,4 @@
-{ %RESULTCODE=1 }
+{ %FAIL }
 { Source provided for Free Pascal Bug Report 1858 }
 { Submitted by "Mattias Gaertner" on  2002-03-07 }
 { e-mail: nc-gaertnma@netcologne.de }
@@ -23,7 +23,10 @@ end.
 
 {
   $Log$
-  Revision 1.2  2002-03-07 21:46:41  carl
+  Revision 1.1  2002-03-08 20:18:28  carl
+  * moved to correct directory
+
+  Revision 1.2  2002/03/07 21:46:41  carl
   + fix unit name
 
   Revision 1.1  2002/03/07 21:40:56  carl
