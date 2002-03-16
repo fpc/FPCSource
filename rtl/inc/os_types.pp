e31530f800adbbdfc8e6a69ad9ae75825bb0d01d
armin <armin>
2002-03-16 12:46:09 +0000

    + Netware added

--- rtl/inc/os_types.pp
+++ rtl/inc/os_types.pp
@@ -33,6 +33,11 @@ TYPE
   tOS_UINT = DWord;
 {$define OS_TYPES}
 {$endif}
+{$ifdef Netware}
+  tOS_INT  = LongInt;
+  tOS_UINT = DWord;
+{$define OS_TYPES}
+{$endif}
 {$ifdef WIN64}
   tOS_INT  = Comp;  { 8 byte signed ordinal }
   tOS_UINT = QWord; { 8 byte unsigned ordinal }{ possibly still needs to be defined }
@@ -65,7 +70,10 @@ IMPLEMENTATION
 end.
 
   $Log$
-  Revision 1.3  2000-11-13 13:40:04  marco
+  Revision 1.4  2002-03-16 12:46:09  armin
+  + Netware added
+
+  Revision 1.3  2000/11/13 13:40:04  marco
    * Renamefest
 
   Revision 1.2  2000/07/13 11:33:45  michael
