50159eb18243a4f330447e5feec423af1de66960
carl <carl@freepascal.org>
2002-04-13 08:05:38 +0000

    + web bug test #1907

---
+++ tests/webtbs/tw1907.pp
@@ -0,0 +1,17 @@
+{ Source provided for Free Pascal Bug Report 1907 }
+{ Submitted by "Michail A.Baikov" on  2002-03-31 }
+{ e-mail: russia@freepascal.org }
+{$MODE DELPHI}
+var
+    a:function (k:byte):Pointer; cdecl;
+    b:function:Pointer;
+begin
+    b:=a(0);
+end.
+
+{
+  $Log$
+  Revision 1.1  2002-04-13 08:05:38  carl
+  + web bug test #1907
+
+}
