451532aaed0213d251fcb7dc1e5362c14a7652f3
peter <peter@freepascal.org>
2002-04-23 19:16:09 +0000

    * add test for setlength with multiple parameters

--- tests/webtbs/tw1856.pp
+++ tests/webtbs/tw1856.pp
@@ -1,6 +1,8 @@
 { %version=1.1 }
 
+{$ifdef fpc}
 {$mode objfpc}
+{$endif}
 
 type
  TFlowItem = class
@@ -9,12 +11,21 @@ type
 
  TFlow = array of TFlowItem;
 
+ TFlow2 = array of array of TFlowItem;
+
 var
  flow: tflow;
+ flow2 : TFLow2;
 begin
  setlength(flow, 10);
  setlength(flow, 1);
+ flow[1]:=TFlowItem.Create;
+
  { release }
  setlength(flow, 0);
+
+ setlength(flow2, 10, 20);
+ setlength(flow2, 0);
+
  writeln('no crash');
 end.
