09ccea65cbcdd5c930cff26301ac3d9a3f530550
peter <peter@freepascal.org>
2002-05-10 07:48:04 +0000

    * FExpand bug

---
+++ tests/webtbs/tw1964.pp
@@ -0,0 +1,30 @@
+  uses  DOS;
+  var
+    error : boolean;
+
+  procedure Expand(FileMask: string);
+    var
+      FileMaskOut : string;
+    begin
+      WriteLn;
+      WriteLn('Expanding "',FileMask,'"');
+      FileMaskOut := FExpand(FileMask);
+      WriteLn('To become "',FileMaskOut,'"');
+      if FileMask<>FileMaskOut then
+       error:=true;
+    end;
+  begin
+{$ifdef Win32}
+    Expand('C:\Windows1\System');
+    Expand('\\.\C\Windows1\System');
+    Expand('C:\Windows1\System');
+    if error then
+     begin
+       Writeln('ERROR!');
+       Halt(1);
+     end;
+{$else}
+   Writeln('Win32 only test');
+{$endif}
+  end.
+  
