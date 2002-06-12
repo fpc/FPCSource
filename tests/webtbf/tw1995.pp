462ab6c72e4d77bcdf82db8fb7fcc5504178b991
Jonas Maebe <jonas@freepascal.org>
2002-06-12 18:01:06 +0000

    + added

---
+++ tests/webtbf/tw1995.pp
@@ -0,0 +1,44 @@
+{ %FAIL }
+unit tw1995;
+
+interface
+
+type
+  TMemoryManager = record
+    FreememSize : Function(p:pointer;Size:Longint):Longint;
+  end;
+
+  TMemoryManager2 = record
+    FreememSize : Function(var p:pointer;Size:Longint):Longint;
+  end;
+
+  Function CFreeMemSize(var p:pointer;Size:Longint):Longint;
+  Function CFreeMemSize2(p:pointer;Size:Longint):Longint;
+
+
+Const
+ CMemoryManager : TMemoryManager =
+    (
+      FreememSize : {$ifdef fpc}@{$endif}CFreememSize;
+    );
+
+ CMemoryManager2 : TMemoryManager2 =
+    (
+      FreememSize : {$ifdef fpc}@{$endif}CFreememSize2;
+    );
+
+implementation
+
+Function CFreeMemSize(var p:pointer;Size:Longint):Longint;
+begin
+  CFreeMemSize:=0;
+end;
+
+Function CFreeMemSize2(p:pointer;Size:Longint):Longint;
+begin
+  CFreeMemSize2:=0;
+end;
+
+end.
+
+
