6517a7c0d0916d1ee96933983ac5e9b720e041c1
florian <florian@freepascal.org>
2002-07-16 12:33:15 +0000

    *** empty log message ***

---
+++ tests/webtbs/tw2041.pp
@@ -0,0 +1,12 @@
+{ %version=1.1}
+{$mode objfpc}
+
+type
+  ITest = interface                                                                                       
+    procedure Put(Key: Integer; Item: IUnknown);                                                                        
+    function Get(Key: Integer): IUnknown;                                                                              
+    property Item[Key: Integer]: IUnknown read Get write Put; default;                                                 
+  end;
+
+begin
+end.
