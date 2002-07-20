ba1d11e7786256b2ae3076ceee5242fada76d058
florian <florian@freepascal.org>
2002-07-20 17:38:07 +0000

    no message

---
+++ tests/tbs/tb0398.pp
@@ -0,0 +1,13 @@
+{ %version=1.1}
+{$codepage cp850}
+begin
+   if ord(widechar(#196))<>9472 then
+     halt(1);
+   if ord(#0196))<>196 then
+     halt(1);
+   if ord(widechar(#$a6))<>170 then
+     halt(1);
+   if ord(#$0a6))<>166 then
+     halt(1);
+   halt(0);
+end.
