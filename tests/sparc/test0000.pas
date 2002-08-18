08a92a26b42cb42e5f6d233e1987ae7ab0aefa10
mazen <mazen@freepascal.org>
2002-08-18 21:53:17 +0000

    * fixing include path

---
+++ tests/sparc/test0000.pas
@@ -0,0 +1,18 @@
+{ $Id$ }
+{$UNITPATH ../../rtl/linux}
+{$INCLUDEPATH ../../rtl/unix;../../rtl/linux;../../rtl/sparc;../../rtl/inc;../../rtl/linux/sparc}
+PROGRAM SparcTest;
+BEGIN
+END.
+{
+  $Log$
+  Revision 1.2  2002-08-18 21:53:17  mazen
+  * fixing include path
+
+  Revision 1.1  2003/02/05 21:54:37  mazen
+  * changing names of test files to allow traçability and automated NRT
+
+  Revision 1.5  2002/12/06 08:35:50  mazen
+  + added just to test RTL compilation
+
+}
