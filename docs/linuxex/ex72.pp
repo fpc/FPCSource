abe97bf0da8409c925056ef1d1c4815125fa7840
michael <michael@freepascal.org>
2002-08-30 06:32:14 +0000

    + Added NanoSleep example

---
+++ docs/linuxex/ex72.pp
@@ -0,0 +1,27 @@
+program example72;
+
+{ Program to demonstrate the NanoSleep function. } 
+
+uses Linux;
+
+Var
+  Req,Rem : TimeSpec;
+  Res : Longint;
+  
+begin
+  With Req do
+    begin
+    tv_sec:=10;
+    tv_nsec:=100;
+    end;
+  Write('NanoSleep returned : ');
+  Flush(Output);
+  Res:=(NanoSleep(Req,rem);
+  Writeln(res);
+  If (res<>0) then
+    With rem do
+      begin
+      Writeln('Remaining seconds     : ',tv_sec);
+      Writeln('Remaining nanoseconds : ',tv_nsec);
+      end;
+end.
