b963ca1c2af0c14ec7b688d5e95c178d31bd0e1f
michael <michael@freepascal.org>
2002-08-30 06:37:00 +0000

    + Added Octstr example

---
+++ docs/refex/ex112.pp
@@ -0,0 +1,14 @@
+Program example112;
+
+{ Program to demonstrate the OctStr function }
+
+Const Value = 45678;
+
+Var I : longint;
+
+begin
+  For I:=1 to 10 do
+    Writeln (OctStr(Value,I));
+  For I:=1 to 16 do
+    Writeln (OctStr(I),3);
+end.
