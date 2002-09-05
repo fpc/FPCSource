aed84190ca835506de6fbcec7663aca96679bb04
carl <carl@freepascal.org>
2002-09-05 16:44:17 +0000

    * bug #2072

---
+++ tests/webtbs/tw2072.pp
@@ -0,0 +1,20 @@
+{ Source provided for Free Pascal Bug Report 2072 }
+{ Submitted by "Bill Rayer" on  2002-08-09 }
+{ e-mail: lingolanguage@hotmail.com }
+program tw2072;
+
+
+
+(*$ifdef FPC*)
+const name = 'FPC';
+(*$else *)(*Hello*)
+const name = 'Delphi';
+(*$endif *)
+
+
+
+begin
+  writeln ('Hello ', name);
+end.
+
+
