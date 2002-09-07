24720bfb5016556d19222cd4b47d327abb770b1d
carl <carl@freepascal.org>
2002-09-07 11:06:55 +0000

    * range checking code verification

---
+++ tests/tbf/tb0123.pp
@@ -0,0 +1,15 @@
+type
+   smallrange = 5..287;
+const
+   c1 : byte = succ(256);
+   c2 : byte = pred(0);
+   c3 : byte = succ(high(smallrange));
+   c4 : byte = pred(low(smallrange));
+   c5 = succ(high(smallrange));
+   c6 = pred(low(smallrange));
+var
+ b: byte;
+Begin
+ b:=succ(256);
+ b:=pred(0);
+end.
