99398071a79a4343023593e980eba77bad9c97ec
carl <carl@freepascal.org>
2002-09-07 19:09:15 +0000

    * new bug report fix

---
+++ tests/webtbs/tw2065.pp
@@ -0,0 +1,23 @@
+{ Source provided for Free Pascal Bug Report 2065 }
+{ Submitted by "Lavergne Thomas" on  2002-08-01 }
+{ e-mail: thomas.lavergne@laposte.net }
+program test;
+
+{$APPTYPE CONSOLE}
+{$LONGSTRINGS ON}
+
+type
+  TRec = record
+    S: string;
+    P: Pointer;
+  end;
+
+const
+  MAX_SIZE = 2;
+
+var
+  A: array[0..MAX_SIZE - 1] of TRec;
+
+begin
+  WriteLn(A[0].S);
+end.
