563a31edb441ca58b676533398cb5af5dbb45ad3
carl <carl@freepascal.org>
2002-09-08 07:00:30 +0000

    * bugfix report 2070

---
+++ tests/webtbf/tw2070.pp
@@ -0,0 +1,17 @@
+{ Source provided for Free Pascal Bug Report 2070 }
+{ Submitted by "MrNoop" on  2002-08-09 }
+{ e-mail: mrnoop@the-asw.com }
+type
+TPersistent = class (TObject)
+public
+constructor Create(Proprio:TPersistent);virtual;
+end;
+
+TServeur = class (TPersistent)
+public
+constructor Create(Proprio:TPersistent);override;
+end;
+
+constructor TServeur.Create(Proprio:TPersistent);
+begin
+inherited Create; {<- lethal error (see above)}
