fff4cf3b05aa0a2f16446e3c70c2ba885e60a107
peter <peter@freepascal.org>
2002-09-09 19:42:39 +0000

    * fix typo's

--- tests/tbs/tb0398.pp
+++ tests/tbs/tb0398.pp
@@ -3,11 +3,11 @@
 begin
    if ord(widechar(#196))<>9472 then
      halt(1);
-   if ord(#0196))<>196 then
+   if ord(#0196)<>196 then
      halt(1);
    if ord(widechar(#$a6))<>170 then
      halt(1);
-   if ord(#$0a6))<>166 then
+   if ord(#$0a6)<>166 then
      halt(1);
    halt(0);
 end.
