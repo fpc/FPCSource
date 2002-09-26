fa51835542f67cb78773dd5079935934849a4b7a
florian <florian@freepascal.org>
2002-09-26 11:38:38 +0000

    no message

--- tests/webtbs/tw1677.pp
+++ tests/webtbs/tw1677.pp
@@ -3,8 +3,22 @@
 { e-mail: anders.lindeberg@telia.com }
 program test;
 type trec = record i:integer; s:ansistring end;
-procedure p1(const r:trec); begin end;
-procedure p2(r:trec); begin end;
+procedure p1(const r:trec);
+  begin
+  end;
+
+procedure p2(r:trec); 
+  begin
+  end;
+
+procedure p3(const a:ansistring);
+  begin
+  end;
+
+procedure p4(a:ansistring);
+  begin
+  end;
+
 var r:trec; s:ansistring;
 begin
   s:=chr(ord('A')+random(26));
---
+++ tests/webtbs/tw1820.pp
@@ -0,0 +1,22 @@
+{ %version=1.1 }
+unit tb1820;
+
+interface
+
+{$mode Delphi}
+Uses Windows;
+
+type
+  IEnumWorkItems = interface (IUnknown)
+      ['{148BD528-A2AB-11CE-B11F-00AA00530503}']
+  end;
+
+
+const
+  IEnumTasks = IEnumWorkItems;
+
+  implementation
+
+end.
+
+
