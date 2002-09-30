d605448715b280e6a9eabd567649b8d9ec7cfe6b
florian <florian@freepascal.org>
2002-09-30 20:40:44 +0000

    no message

---
+++ tests/webtbs/tw1573.pas
@@ -0,0 +1,25 @@
+{ %version=1.1 }
+type
+	TCheck=class(TObject);
+
+
+var
+	vlA : TCheck;
+	vlB : TObject;
+
+procedure aa(const ParXX :array of TObject);
+begin
+        // writeln(cardinal(ParXX[0]),' ', cardinal(ParXX[1]));
+        if (ParXX[0]<>vlA) or (ParXX[1]<>vlB) then
+          begin
+             writeln('error');
+             halt(1);
+          end;
+end;
+
+begin
+	vlB := TObject.Create;
+	vlA := TCheck.Create;
+	aa([vlA,vlB]);
+end.
+
