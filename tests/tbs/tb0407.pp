3d19418cae0d286a78fde6d63d7170a7021cf4de
peter <peter@freepascal.org>
2002-10-04 23:17:14 +0000

    * inherited with overloaded constructors should not call anything

---
+++ tests/tbs/tb0407.pp
@@ -0,0 +1,45 @@
+{ %version=1.1 }
+
+{$ifdef fpc}{$mode objfpc}{$endif}
+
+var
+  err : boolean;
+
+type
+  tc1=class(tinterfacedobject)
+    constructor Create;overload;
+    constructor Create(s:string);overload;
+  end;
+
+  tc2=class(tc1)
+    constructor Create(l1,l2:longint);overload;
+  end;
+
+constructor tc1.create;
+begin
+  err:=true;
+end;
+
+constructor tc1.create(s:string);
+begin
+  err:=true;
+end;
+
+constructor tc2.create(l1,l2:longint);
+begin
+  { The next line should do nothing }
+  inherited;
+end;
+
+var
+  c : tc2;
+begin
+  err:=false;
+  c:=tc2.create(1,1);
+  c.free;
+  if err then
+   begin
+     writeln('Error!');
+     halt(1);
+   end;
+end.
