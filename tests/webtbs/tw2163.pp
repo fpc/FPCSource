7de44d58658aad55328e73124aaef178d06e0608
peter <peter@freepascal.org>
2002-10-05 15:23:29 +0000

    * Length(PChar)

---
+++ tests/webtbs/tw2163.pp
@@ -0,0 +1,20 @@
+{ %version=1.1 }
+
+type oo2 = pchar;
+
+var a: oo2;
+
+function len(a:pchar):longint;
+begin
+  len:=-1;
+end;
+
+begin
+  a:='0123456789';
+  if length(a)<>10 then
+   begin
+     writeln('Error!');
+     halt(1);
+   end;  
+end.
+   
