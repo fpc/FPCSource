c610d3d0ddfad63848e64bab2fcf941e65ee42d7
peter <peter@freepascal.org>
2002-10-06 21:01:00 +0000

    * also check output

--- tests/webtbs/tw1699.pp
+++ tests/webtbs/tw1699.pp
@@ -1,56 +1,28 @@
 { Source provided for Free Pascal Bug Report 1699 }
 { Submitted by "SHADOW" on  2001-11-25 }
 { e-mail: mdikm@hotmail.com }
-function bget(d:byte;p:byte):boolean;
-begin
-end;
-procedure bset(var d:byte;p:byte;v:boolean);
-begin
-end;
-function nget(d:byte;p,c:byte):byte;
-begin
-end;
+const
+  i : longint = 0;
+
+
 procedure nset(var d:byte;p,c:byte;v:byte);
 begin
+  i:=i or 1;
 end;
 
-function bget(d:word;p:byte):boolean;
-begin
-end;
-procedure bset(var d:word;p:byte;v:boolean);
-begin
-end;
-function nget(d:word;p,c:byte):word;
-begin
-end;
 procedure nset(var d:word;p,c:byte;v:word);
 begin
+  i:=i or 2;
 end;
 
-function bget(d:cardinal;p:byte):boolean;
-begin
-end;
-procedure bset(var d:cardinal;p:byte;v:boolean);
-begin
-end;
-function nget(d:cardinal;p,c:byte):cardinal;
-begin
-end;
 procedure nset(var d:cardinal;p,c:byte;v:cardinal);
 begin
+  i:=i or 4;
 end;
 
-function bget(d:qword;p:byte):boolean;
-begin
-end;
-procedure bset(var d:qword;p:byte;v:boolean);
-begin
-end;
-function nget(d:qword;p,c:byte):qword;
-begin
-end;
 procedure nset(var d:qword;p,c:byte;v:qword);
 begin
+  i:=i or 8;
 end;
 
 var
@@ -63,4 +35,9 @@ begin
   nset(w,0,0,0);
   nset(c,0,0,0);
   nset(q,0,0,0);
+  if i<>15 then
+   begin
+     Writeln('Error');
+     halt(1);
+   end;
 end.
