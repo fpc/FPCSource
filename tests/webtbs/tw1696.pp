eab3951304499f55d8fa9f7522b26c35235b6780
peter <peter@freepascal.org>
2002-10-06 20:00:46 +0000

    * New bug

---
+++ tests/webtbs/tw1696.pp
@@ -0,0 +1,28 @@
+{ %version=1.1 }
+
+{$ifdef Win32}
+Uses Windows;
+Var Font:HFONT;
+Begin
+   { Windows unit does not support Widechar correct }
+   Font:=CreateFont(16, { Height Of Font }
+                    0,   { Width Of Font  }
+                    0,   { Angle Of Escapement }
+                    0,   { Orientation Angle   }
+                    FW_BOLD, { Font Weight }
+                    0,   { Italic }
+                    0,   { Underline }
+                    0,   { Strikeout }
+                    ANSI_CHARSET,
+                    OUT_TT_PRECIS,
+                    CLIP_DEFAULT_PRECIS,
+                    PROOF_QUALITY,
+                    FF_DONTCARE Or DEFAULT_PITCH,
+                    'Verdana');
+{$else}
+begin
+  Writeln('Win32 only');
+{$endif}
+End.
+
+
