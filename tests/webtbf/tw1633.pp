d90b5855695a15378d8f16557b6bc91c6e64fb11
peter <peter@freepascal.org>
2002-10-07 18:09:35 +0000

    * new bug

---
+++ tests/webtbf/tw1633.pp
@@ -0,0 +1,42 @@
+{ %fail }
+
+{$ifdef fpc}
+  {$mode delphi}
+  {$asmmode intel}
+{$endif}
+
+type tscreen = class
+      x : Cardinal;
+      end;
+type ttestobj = class
+   screen : tscreen;
+   constructor create;
+   function testasmcall : tscreen;
+end;
+
+var
+  testobj : ttestobj;
+
+constructor ttestobj.create;
+begin
+  asm
+  mov screen.x,0
+  end;
+end;
+
+function ttestobj.testasmcall : tscreen;
+begin
+asm
+mov screen.x, 0
+ADD screen.x, 1
+end;
+result := screen;
+end;
+
+
+begin
+testobj := ttestobj.create;
+testobj.testasmcall;
+testobj.destroy;
+end.
+
