1de5ae84d36807ce69c76b0e4ba81bc5eecbc5b0
florian <florian@freepascal.org>
2002-10-09 20:24:08 +0000

    no message

---
+++ tests/tbs/tb0412.pp
@@ -0,0 +1,30 @@
+{ %version=1.1 }
+{$mode objfpc}
+{$r+}
+uses
+   sysutils;
+
+var
+   a : array of longint;
+
+begin
+   try
+     a[10]:=1;
+   except
+     setlength(a,3);
+     a[0]:=1;
+     a[1]:=1;
+     a[2]:=1;
+     try
+       a[3]:=1;
+     except
+       try
+         a[-1]:=1;
+       except
+         halt(0);
+       end;
+     end;
+   end;
+   writeln('Problem with dyn. array range checking');
+   halt(1);
+end.
