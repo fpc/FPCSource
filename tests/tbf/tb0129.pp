270c85450bbfa0fd903e40891ff57a3699a16af3
peter <peter@freepascal.org>
2002-10-14 12:14:16 +0000

    * threadvar in for loop

---
+++ tests/tbf/tb0129.pp
@@ -0,0 +1,15 @@
+{ %fail }
+{ %version=1.1 }
+
+{$ifdef fpc}
+  {$mode objfpc}
+  {$threading on}
+{$endif}  
+
+threadvar
+   thri : longint;
+   
+begin
+  { Delphi does not allow threadvars as for loop control variable }
+  for thri:=1 to 1000 do;
+end.
