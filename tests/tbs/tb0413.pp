f0acfdf631dfaa7c98e17fbc835cc4554b457267
florian <florian@freepascal.org>
2002-10-17 12:19:01 +0000

    no message

---
+++ tests/tbs/tb0413.pp
@@ -0,0 +1,20 @@
+var
+   s : ansistring;
+   ss : shortstring;
+   as : ansistring;
+   c : char;
+
+begin
+   as:='ansistring';
+   ss:='shortstring';
+   c:='C';
+   s:=s+as;
+   s:=s+c;
+   s:=s+ss;
+   s:=s+s;
+   if s<>'ansistringCshortstringansistringCshortstring' then
+     begin
+        writeln('Problem with ansistring appending');
+        halt(1);
+     end;
+end.
