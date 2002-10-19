1dfea79867a5bd4b4925c0d19397693f6a2b2a2c
carl <carl@freepascal.org>
2002-10-19 22:26:25 +0000

    * alignment testing

---
+++ tests/test/talign.pp
@@ -0,0 +1,34 @@
+{ This is just a small file used to verify the alignment of different
+  structures. Only the assembler output should be checked.
+}
+type
+  tmyclass = class 
+    s: ansistring;
+    f: real;
+  end;
+{$M+}  
+  tmyotherclass = class(tmyclass)
+  public
+    procedure tito(var Msg); message 'hello';
+    procedure titi(var Msg); message 12;
+  published 
+    procedure published_method;
+  end;
+  
+  procedure tmyotherclass.tito(var Msg);
+   begin
+   end;
+   
+  procedure tmyotherclass.titi(var Msg);
+   begin
+   end;
+
+  procedure tmyotherclass.published_method;
+   begin
+   end;
+   
+  
+var
+ c: tmyclass;
+Begin
+end.
