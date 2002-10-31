2f60fd3818f117dc769c660bccec271c6a3e9b56
carl <carl@freepascal.org>
2002-10-31 15:20:22 +0000

    * bugfix 2187 (already executes correctly, but crashed in 1.0.6)

---
+++ tests/webtbs/tw2187.pp
@@ -0,0 +1,13 @@
+{ Source provided for Free Pascal Bug Report 2187 }
+{ Submitted by "Artur Kornilowicz" on  2002-10-18 }
+{ e-mail: arturk@math.uwb.edu.pl }
+{$mode delphi}
+type x = record
+            s : string;
+            a : ^byte;
+         end; 
+
+var a: array [1..2] of x;
+
+begin
+end.
