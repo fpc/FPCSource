945bbee573ee4e16f6f9b565474ab805e0353c39
mazen <mazen@freepascal.org>
2002-11-09 11:31:27 +0000

    + simple function (1 param) added

--- tests/sparc/sparc.pas
+++ tests/sparc/sparc.pas
@@ -2,6 +2,10 @@ PROGRAM SparcTest;
 VAR
   x,y:Cardinal; 
   z:0..255;
+FUNCTION CopyMe(x:Cardinal):Cardinal;
+  BEGIN
+    CopyMe:=x;
+  END;
 FUNCTION Add(a,b:Cardinal):Cardinal;
   BEGIN
     Add:=a+b;
