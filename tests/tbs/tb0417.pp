e27d66d7bbbfe41eaf43d397bb4ce5b469e1f3a4
peter <peter@freepascal.org>
2002-11-15 16:33:47 +0000

    * include which test failed

--- tests/tbs/tb0417.pp
+++ tests/tbs/tb0417.pp
@@ -6,6 +6,7 @@ type
   charset=set of char;
 
   var
+     err : byte;
      tr,tr2    : charset;
 
 
@@ -17,13 +18,19 @@ type
     end;
 
   begin
+     err:=0;
      Test([#20..#32]);
-     if not(#32 in tr) or ([#33..#255]*tr <> []) or
-        (tr<>[#7..#10,#20..#32]) or
-        (tr<>tr2) then
+     if not(#32 in tr) then
+      err:=1;
+     if ([#33..#255]*tr <> []) then
+      err:=2;
+     if (tr<>[#7..#10,#20..#32]) then
+      err:=3;
+     if (tr<>tr2) then
+      err:=4;
+     if err<>0 then
        begin
-         Writeln('Bug in set handling');
+         Writeln('Bug in set handling, see err:=',err);
          halt(1);
        end;
-
   end.
