9a8c7712804be61b8e007ae8aeccaa792cb18737
pierre <pierre@freepascal.org>
2002-11-18 01:33:33 +0000

    + add check that args after are found

--- tests/tbs/tb0419.pp
+++ tests/tbs/tb0419.pp
@@ -1,9 +1,18 @@
 
+var
+  nc : integer;
+
 procedure array_test(b: integer; parr: array of word; c: integer);cdecl;
 begin
+  nc:=c;
 end;
 
 
 begin
-  array_test(0,[1,2,3,4],0);
+  nc:=5;
+  array_test(0,[1,2,3,4],56);
+  if nc<>56 then
+    begin
+      Writeln('Wrong code generated');
+    end;
 end.
