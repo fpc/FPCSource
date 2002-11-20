85728221192bf1d9630fd47565d76d55f958f6c5
peter <peter@freepascal.org>
2002-11-20 21:40:49 +0000

    * overloading byte->longint operator should be forbidden

---
+++ tests/tbf/tb0141.pp
@@ -0,0 +1,22 @@
+{ %fail }
+
+{ This should not be allowed, it creates an infinite loop.
+  The loop can be solved using word() typecasts. But it is
+  to dangerous code to be allowed }
+operator := (b:byte) l:longint;
+begin
+  if b=0 then
+   l:=-1
+  else
+   l:=0;  
+end;
+
+var
+  l : longint;
+  b : byte;
+begin
+  b:=0;
+  l:=b;
+  writeln(l);
+end.
+
