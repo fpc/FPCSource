03de4893b00ff38b135821e13a40d736d7f40182
florian <florian@freepascal.org>
2002-11-28 23:55:58 +0000

    no message

---
+++ tests/test/testsse2.pp
@@ -0,0 +1,18 @@
+{ %version=1.1}
+uses
+   mmx;
+
+{ only a small test to see if it works in principal }
+
+var
+   q : array[0..15] of byte;
+
+begin
+  if sse2_support then
+    asm
+       movdqa    %xmm1,%xmm2
+       movdqa    q,%xmm4
+       psubq     %xmm1,%xmm2
+       psubq     q,%xmm4
+    end;
+end.
