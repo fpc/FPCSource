d38880fb14b161b2fc482b740b22b5710df59879
carl <carl@freepascal.org>
2002-11-30 18:46:56 +0000

    + hint directive testing

---
+++ tests/tbs/tb0426.pp
@@ -0,0 +1,12 @@
+{ %VERSION=1.1 }
+uses ub0426;
+
+
+Begin
+ myroutine;
+ myroutine2;
+ myroutine3;
+ z:=0;
+end.
+
+
---
+++ tests/tbs/ub0426.pp
@@ -0,0 +1,39 @@
+{ %VERSION=1.1 }
+{$MODE OBJFPC}
+Unit ub0426;
+
+interface
+
+var
+ z: integer platform;
+
+
+procedure myroutine; platform;
+
+procedure myroutine2; deprecated;
+
+procedure myroutine3; unimplemented;
+
+
+implementation
+
+procedure myroutine; platform;
+begin
+end;
+
+procedure myroutine2; deprecated;
+begin
+end;
+
+procedure myroutine3;{$ifdef fpc}unimplemented;{$endif}
+begin
+end;
+
+Begin
+ myroutine;
+ myroutine2;
+ myroutine3;
+ z:=0;
+end.
+
+
