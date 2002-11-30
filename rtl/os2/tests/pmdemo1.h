25810d3deed43229716811f043af583622875ee8
Tomas Hajny <hajny@freepascal.org>
2002-11-30 18:43:02 +0000

    * fix for missing end of comment block

--- rtl/os2/tests/pmdemo1.h
+++ rtl/os2/tests/pmdemo1.h
@@ -7,6 +7,10 @@
 
 /*
   $Log$
-  Revision 1.2  2002-09-07 16:01:25  peter
+  Revision 1.3  2002-11-30 18:43:02  hajny
+    * fix for missing end of comment block
+
+  Revision 1.2  2002/09/07 16:01:25  peter
     * old logs removed and tabs fixed
 
+*/
