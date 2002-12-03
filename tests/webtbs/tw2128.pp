02b2929c94931fbb1ddaead421f912b769408f58
florian <florian@freepascal.org>
2002-12-03 22:24:13 +0000

    *** empty log message ***

--- tests/webtbs/tw2128.pp
+++ tests/webtbs/tw2128.pp
@@ -1,3 +1,5 @@
+{ %version=1.1}
+
 { Source provided for Free Pascal Bug Report 2128 }
 { Submitted by "Bill Rayer" on  2002-09-18 }
 { e-mail: lingolanguage@hotmail.com }
@@ -9,4 +11,6 @@ var
   c : comp;
 begin
   c := -9223372036854775809;
+  if c<>-9223372036854775809 then
+    halt(1);
 end.
