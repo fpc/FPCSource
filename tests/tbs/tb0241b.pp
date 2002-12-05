5a47ac1e8647339bca481593b8a7ac2dbca48dfc
pierre <pierre@freepascal.org>
2002-12-05 15:43:00 +0000

    + one more check for the long mangledname problem

---
+++ tests/tbs/tb0241b.pp
@@ -0,0 +1,42 @@
+{ %OPT=-al }
+{ this forces use of GNU as }
+{ Old file: tbs0282.pp }
+{ long mangledname problem with -Aas                   OK 0.99.13 (PFV) }
+
+
+type very____long_____string___identifier= string[200];
+
+procedure test(very__long_variable01: very____long_____string___identifier;
+               very__long_variable02: very____long_____string___identifier;
+               very__long_variable03: very____long_____string___identifier;
+               very__long_variable04: very____long_____string___identifier;
+               very__long_variable05: very____long_____string___identifier;
+               very__long_variable06: very____long_____string___identifier;
+               very__long_variable07: very____long_____string___identifier;
+               very__long_variable08: very____long_____string___identifier;
+               very__long_variable09: very____long_____string___identifier;
+               very__long_variable10: very____long_____string___identifier;
+               very__long_variable11: very____long_____string___identifier;
+               very__long_variable12: very____long_____string___identifier;
+               very__long_variable13: very____long_____string___identifier;
+               very__long_variable14: very____long_____string___identifier;
+               very__long_variable15: very____long_____string___identifier;
+               very__long_variable16: very____long_____string___identifier;
+               very__long_variable17: very____long_____string___identifier;
+               very__long_variable18: very____long_____string___identifier);
+begin
+  writeln('hi!');
+end;
+
+var
+  p : pointer;
+
+begin
+  writeln('vreemd!');
+  test('','','','','','','','','','',
+       '','','','','','','','');
+  p:=@test;
+end.
+
+
+
