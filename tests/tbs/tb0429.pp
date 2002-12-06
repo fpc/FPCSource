25739b34da192948ac578647beee4651f69dec2b
peter <peter@freepascal.org>
2002-12-06 15:29:41 +0000

    * runtime check added

--- tests/tbs/tb0429.pp
+++ tests/tbs/tb0429.pp
@@ -1,3 +1,5 @@
+var
+  err : boolean;
 
 procedure lowercase(c:char);overload;
 begin
@@ -6,6 +8,7 @@ end;
 procedure lowercase(c:shortstring);overload;
 begin
   writeln('short');
+  err:=false;
 end;
 procedure lowercase(c:ansistring);overload;
 begin
@@ -15,7 +18,13 @@ end;
 var
   w : widestring;
 begin
+  err:=true;
   { this should choosse the shortstring version }
   lowercase(w);
+  if err then
+   begin
+     writeln('ERROR!');
+     halt(1);
+   end;
 end.
 
