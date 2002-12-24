3e86b4d56e11b676c2a9bd44e0698f6264d90222
peter <peter@freepascal.org>
2002-12-24 16:52:29 +0000

    * runtime check

--- tests/tbs/tb0438.pp
+++ tests/tbs/tb0438.pp
@@ -1,13 +1,33 @@
-{ %version=1.1 }
-{$mode objfpc}
-procedure p(a : array of const);
+{$ifdef fpc}{$mode objfpc}{$endif}
 
+procedure p(a : array of const);
+  var
+    i : integer;
   begin
+    for i:=low(a) to high(a) do
+     begin
+       write(i,': ');
+       if (a[i].vtype=vtpchar) then
+        begin
+          writeln('"',a[i].vpchar,'"');
+          if (a[i].vpchar<>'test') then
+           begin
+             writeln('Wrong string content');
+             halt(1);
+           end;
+        end
+       else
+        begin
+          writeln('No string type (',a[i].vtype,')');
+          halt(1);
+        end;
+     end;
   end;
 
 var
    a : array[0..25] of char;
 
 begin
-   p([a]);
+   a:='test';
+   p([a,a]);
 end.
