50ec6f50b50c697b3f414549c1b5fa45a2c6c23b
peter <peter@freepascal.org>
2002-12-27 18:37:13 +0000

    * new bugs

---
+++ tests/webtbs/tw2220.pp
@@ -0,0 +1,28 @@
+{ Source provided for Free Pascal Bug Report 2220 }
+{ Submitted by "marco" on  2002-11-06 }
+{ e-mail: marcov@freepascal.org }
+{$H+}
+{$ifdef fpc}{$MODE DELPHI}{$endif}
+// NO OUTPUT, GDB SHOWS SIGSEGV IN DECR_REF
+
+type bla=class
+        freceivebuffer : array[0..4095] of char;
+        flastresponse  : String;
+        procedure themethod;
+        end;
+
+procedure bla.themethod;
+        var i : longint;
+        begin
+         i:=10;
+         flastresponse:=copy(freceivebuffer,1,I-1);
+         writeln('point 1');
+        end;
+
+var x : bla;
+
+begin
+  x:=bla.create;
+  x.themethod;
+  writeln('point 2');
+end.
