556435a4e1e38569e44d0c9498cc7e9004093f9b
peter <peter@freepascal.org>
2002-12-27 19:22:31 +0000

    * new bugs

---
+++ tests/webtbs/tw2280.pp
@@ -0,0 +1,10 @@
+{ Source provided for Free Pascal Bug Report 2280 }
+{ Submitted by "Yakov Sudeikin" on  2002-12-23 }
+{ e-mail: yashka@exebook.com }
+var
+ a: array of string;
+
+begin
+ a := nil;
+ if a = nil then;
+end.
---
+++ tests/webtbs/tw2289.pp
@@ -0,0 +1,16 @@
+{ Source provided for Free Pascal Bug Report 2289 }
+{ Submitted by "Mattias Gaertner" on  2002-12-27 }
+{ e-mail: nc-gaertnma@netcologne.de }
+program TestSkip;
+
+{$IFDEF NotDefined}
+
+//
+{$IFDEF Bogus}
+{$ENDIF}
+
+{$ENDIF}
+
+begin
+
+end.
