1d747fc5d3aafd2a0bf65c0bfa1a8b5f79cf4193
peter <peter@freepascal.org>
2002-12-29 17:51:52 +0000

    * delphi allows callign convention in implementation

---
+++ tests/tbs/tb0444.pp
@@ -0,0 +1,13 @@
+{$mode delphi}
+unit tb0444;
+interface
+
+function Trunc(const x : Single) : Integer;
+
+implementation
+
+function Trunc(const x : Single) : Integer; register;
+asm
+end;
+
+end.
