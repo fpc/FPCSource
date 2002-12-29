84cc5ebe7424481bd4767f0a48db8f7f865a30c8
florian <florian@freepascal.org>
2002-12-29 23:50:03 +0000

    no message

--- tests/webtbs/tw2214.pp
+++ tests/webtbs/tw2214.pp
@@ -10,10 +10,20 @@ function LongLatToDMS(longlat : longint; hemis : string):string;
 Var ldeg, lmin, lsec, lmsec : extended;
     hemi                    : char;
 begin
+  ldeg:=1;
+  lmin:=2;
+  lsec:=3;
+  lmsec:=4;
+  hemi:='n';
   result := Format('%d %02d %02d.%03d',
                [round(ldeg), round(lmin), round(lsec),
                 round(lmsec)]) + ' ' + hemi;
 end;
 
 begin
+   if LongLatToDMS(1,'a')<>'1  2  3.  4 n' then
+     begin
+       writeln('Problem with format');
+       halt(1);
+     end;
 end.
