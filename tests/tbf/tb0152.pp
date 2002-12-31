3f7df8d4023878033776d80cc22d21fb9fe57486
florian <florian@freepascal.org>
2002-12-31 13:20:46 +0000

    *** empty log message ***

---
+++ tests/tbf/tb0152.pp
@@ -0,0 +1,14 @@
+{$mode objfpc}
+type
+   TMainForm = class
+   end;
+
+function FreeFPgtkObjects (Data:pointer) : longbool; Cdecl;
+
+begin
+  With (Data as TMainForm) do // <<--- this is the one
+    ;
+end;
+
+begin
+end.
