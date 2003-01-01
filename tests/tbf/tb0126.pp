06921236401c0e1619cd63dbac093a3effdc1ffd
peter <peter@freepascal.org>
2003-01-01 21:09:49 +0000

    * delphi mode only

--- tests/tbf/tb0126.pp
+++ tests/tbf/tb0126.pp
@@ -2,24 +2,24 @@
 { This program should fail compilation, since
   the declaration is not the same as the actual
   implementation, checked against Delphi 3 -
-  the class keyword is missing in front 
+  the class keyword is missing in front
   of procedure tmyclass.myproc
 }
 {$ifdef fpc}
-{$mode objfpc}
+{$mode delphi}
 {$endif}
 type
    tmyclass = class
     class procedure myproc;virtual;
    end;
-   
+
    { missing class keyword in front of procedure }
    procedure tmyclass.myproc;
     begin
     end;
-    
+
 
 Begin
 end.
-    
+
   
