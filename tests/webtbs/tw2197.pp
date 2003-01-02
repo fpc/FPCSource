e73a2261336c33e041db4ff41841b97bb16f3e4f
peter <peter@freepascal.org>
2003-01-02 16:17:07 +0000

    * updated runtime check

--- tests/webtbs/tw2197.pp
+++ tests/webtbs/tw2197.pp
@@ -1,4 +1,4 @@
-
+{ %RESULT=123 }
 { Source provided for Free Pascal Bug Report 2197 }
 { Submitted by "Pavel V.Ozerski" on  2002-10-23 }
 { e-mail: pavel@insect.mail.iephb.ru }
@@ -12,5 +12,7 @@
      writeln('Before call');
      Average([1,2,3]);
      writeln('After call');
+     { We need to be sure that the following exitcode is 
+       returned }
+     runerror(123);
  END.
-
