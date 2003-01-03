5400c36e4f07333067c27e715337796de2003eca
florian <florian@freepascal.org>
2003-01-03 01:03:03 +0000

    *** empty log message ***

---
+++ tests/test/testda1.pp
@@ -0,0 +1,18 @@
+{ %VERSION=1.1}
+var
+   a1 : array of longint;
+   i : longint;
+
+begin  
+   setlength(a1,10);
+   setlength(a1,0);
+   setlength(a1,10);
+   setlength(a1,100);
+   setlength(a1,10);
+   for i:=0 to 9 do
+     a1[i]:=i*i;
+   setlength(a1,1000);
+   setlength(a1,10);
+   for i:=0 to 9 do
+     writeln(a1[i]);
+end.
