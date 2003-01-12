bd06dd902ccfeb8f825f8683331af211cdb4fc44
peter <peter@freepascal.org>
2003-01-12 17:15:59 +0000

    * runtime check

--- tests/tbs/tb0448.pp
+++ tests/tbs/tb0448.pp
@@ -1,15 +1,26 @@
 {$mode delphi}
 
+var
+  err : boolean;
+
 procedure p1(s:string);overload;
 begin
 end;
 
 procedure p1(l:longint);overload;
 begin
+  err:=false;
 end;
 
 var
   pv : procedure(l:longint);
 begin
+  err:=true;
   pv:=p1;
+  pv(1);
+  if err then
+   begin
+     writeln('Error!');
+     halt(1);
+   end;
 end.
