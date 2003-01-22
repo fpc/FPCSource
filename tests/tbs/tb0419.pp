b230154c32a7666940724f34b2c4e380f7d8fe3d
pierre <pierre@freepascal.org>
2003-01-22 00:36:40 +0000

    * also check that array can be accessed correctly

--- tests/tbs/tb0419.pp
+++ tests/tbs/tb0419.pp
@@ -1,17 +1,20 @@
 
 var
   nc : integer;
+  test_w : word;
 
 procedure array_test(b: integer; parr: array of word; c: integer);cdecl;
 begin
   nc:=c;
+  test_w:=parr[2];
 end;
 
 
 begin
   nc:=5;
+  test_w:=$abcd;
   array_test(0,[1,2,3,4],56);
-  if nc<>56 then
+  if (nc<>56) or (test_w<>3) then
     begin
       Writeln('Wrong code generated');
     end;
