f1e708e29873d149ebb45b9014ad22bb82d45e89
pierre <pierre@freepascal.org>
2003-02-01 15:39:30 +0000

    * check that there is no internal error anymore

---
+++ tests/webtbf/tw1306.pp
@@ -0,0 +1,19 @@
+{ %FAIL }
+{ Source provided for Free Pascal Bug Report 1306 }
+{ Submitted by "Sergey Kosarevsky" on  2000-12-22 }
+{ e-mail: freecommander@aport2000.ru }
+UNIT OPERAT;
+INTERFACE
+
+USES DOS;
+
+OPERATOR [] (A,B:SEARCHREC) R:BOOLEAN;
+
+IMPLEMENTATION
+
+OPERATOR [] (A,B:SEARCHREC) R:BOOLEAN;
+BEGIN
+END;
+
+BEGIN
+END.
