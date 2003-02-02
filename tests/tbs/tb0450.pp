4841a83d44b945a4158339c7f60e5e66cfa993fc
carl <carl@freepascal.org>
2003-02-02 14:37:45 +0000

    + round returning int64 problem

---
+++ tests/tbs/tb0450.pp
@@ -0,0 +1,16 @@
+
+var
+ i: single;
+Begin
+  case round(i) of
+  0: WriteLn;
+  1: WriteLn;
+  end;
+end.
+
+{
+  $Log$
+  Revision 1.1  2003-02-02 14:37:45  carl
+   + round returning int64 problem
+
+}  
