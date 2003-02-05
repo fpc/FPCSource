76cd13108c46c8c1851534b554cb3718973ecfe5
mazen <mazen@freepascal.org>
2003-02-05 21:54:37 +0000

    * changing names of test files to allow traçability and automated NRT

--- tests/sparc/test0000.pas
+++ tests/sparc/test0000.pas
@@ -1,15 +1,12 @@
 { $Id$ }
 {$UNITPATH ../../rtl/linux}
-{$INCLUDEPATH ../../rtl/unix;../../rtl/linux;../../rtl/sparc;../../rtl/inc;../../rtl/linux/sparc}
+{$INCLUDEPATH ../../rtl/unix;../../rtl/inc;../../rtl/unix;../../rtl/sparc}
 PROGRAM SparcTest;
 BEGIN
 END.
 {
   $Log$
-  Revision 1.2  2002-08-18 21:53:17  mazen
-  * fixing include path
-
-  Revision 1.1  2003/02/05 21:54:37  mazen
+  Revision 1.1  2003-02-05 21:54:37  mazen
   * changing names of test files to allow traçability and automated NRT
 
   Revision 1.5  2002/12/06 08:35:50  mazen
