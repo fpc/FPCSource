22a60095c95e0d3bcbcf4e5d5abbbff8588ae322
mazen <mazen@freepascal.org>
2003-02-09 19:02:01 +0000

    * removed function to make focus on main prog parms

--- tests/sparc/test0001.pas
+++ tests/sparc/test0001.pas
@@ -3,18 +3,7 @@
 PROGRAM SparcTest;
 VAR
   x,y:Cardinal; 
-  z:0..255;
-FUNCTION CopyMe(x:Cardinal):Cardinal;
-  BEGIN
-    CopyMe:=x;
-  END;
-FUNCTION Add(a,b:Cardinal):Cardinal;
-  BEGIN
-    Add:=a+b;
-  END;
 BEGIN
   y:=0;
   z:=0;
-  x:=1+y;
-  x:=Add(x,y);
 END.
