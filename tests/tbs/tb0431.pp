ff2d3b7d06230a4b909a399896067e493a606f87
pierre <pierre@freepascal.org>
2003-02-11 12:03:47 +0000

    + also check shr 0 and shl 0

--- tests/tbs/tb0431.pp
+++ tests/tbs/tb0431.pp
@@ -4,9 +4,23 @@
   but on m68K it generated a asl.l #0,%d2
   which is not correct ... }
 
+const
+  has_errors : boolean = false;
 var
   x : longint;
 begin
   x:=5;
   x:=x*1;
+  if x<>5 then
+    has_errors:=true;
+  x:=5;
+  x:=x shl 0;
+  if x<>5 then
+    has_errors:=true;
+  x:=5;
+  x:=x shr 0;
+  if x<>5 then
+    has_errors:=true;
+  if has_errors then
+    halt(1);
 end.
