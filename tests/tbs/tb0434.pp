168690c8338410c991037826e6419c3a86d48d62
pierre <pierre@freepascal.org>
2003-02-11 11:58:31 +0000

    * enhance check

--- tests/tbs/tb0434.pp
+++ tests/tbs/tb0434.pp
@@ -1,7 +1,19 @@
+
+function dummy : longint;
+begin
+  dummy:=1;
+end;
+
 var
  x:function:longint;
+ x2:function:longint;
  y:pointer absolute x;
+ y2:pointer absolute x2; 
 begin
-  if y<>nil then
-   halt(1);
+  x2:=@dummy;
+  if (y<>nil) or (y2<>pointer(@dummy)) then
+   begin
+     Writeln('Wrong code generated for absolute to procvarsmy');
+     halt(1);
+   end;
 end.
