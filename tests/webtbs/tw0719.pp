6365e9bfa8ab19dfd592678bdc9c78077d98c83b
pierre <pierre@freepascal.org>
2003-03-16 23:31:18 +0000

    * check if fileAge returns -1

--- tests/webtbs/tw0719.pp
+++ tests/webtbs/tw0719.pp
@@ -9,8 +9,12 @@ begin
 r:=FindFirst('*.*',faAnyFile,SR);
 while r=0 do
   begin
-    S:=DateTimeToStr(FileDateToDateTime(FileAge(SR.Name)));
-    Writeln(SR.Name,' has Date ',S);
+    r:=FileAge(SR.Name);
+    if r<>-1 then
+      begin
+        S:=DateTimeToStr(FileDateToDateTime(r));
+        Writeln(SR.Name,' has Date ',S);
+      end;
     r:=FindNext(SR);
   end;
 FindClose(SR);
