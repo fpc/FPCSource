8caf047fa81cf788ef756a8a1eb06afde9249e38
pierre <pierre@freepascal.org>
2003-03-16 23:42:02 +0000

    * fix error as reported in webbug 2373

--- docs/sysutex/ex45.pp
+++ docs/sysutex/ex45.pp
@@ -8,13 +8,13 @@ Uses sysutils;
 Var Dirs : Array[0..127] of pchar;
     I,Count : longint;
     Dir,NewDir : String;
- 
+
 Begin
   Dir:=GetCurrentDir;
   Writeln ('Dir : ',Dir);
   NewDir:='';
   count:=GetDirs(Dir,Dirs);
-  For I:=0 to Count do
+  For I:=0 to Count-1 do
     begin
     NewDir:=NewDir+'/'+StrPas(Dirs[I]);
     Writeln (NewDir);
