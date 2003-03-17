45910eed90624f6b4184685b90734c4722d337b2
michael <michael@freepascal.org>
2003-03-17 08:25:53 +0000

    + Convert shorthand syntax notation to full notation

---
+++ docs/syntax/changeit.pp
@@ -0,0 +1,21 @@
+program changeit;
+
+uses sysutils;
+
+var 
+  S : String;
+
+begin
+  While not eof do
+    begin
+    ReadLn(S);
+    S:=StringReplace(S,'\[','\begin{stack}\\',[rfReplaceAll]);
+    S:=StringReplace(S,'\]','\end{stack}',[rfReplaceAll]);
+    S:=StringReplace(S,'\(','\begin{stack}',[rfReplaceAll]);
+    S:=StringReplace(S,'\)','\end{stack}',[rfReplaceAll]);
+    S:=StringReplace(S,'\<','\begin{rep}',[rfReplaceAll]);
+    S:=StringReplace(S,'\>','\end{rep}',[rfReplaceAll]);
+    Writeln(S);
+    end;
+end.
+ 
