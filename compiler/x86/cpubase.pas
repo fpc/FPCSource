98d09473e4121f3d49372c4596117004173f643d
florian <florian@freepascal.org>
2003-04-25 11:12:09 +0000

    * merged i386/cpubase and x86_64/cpubase to x86/cpubase;     different stuff went to cpubase.inc

--- compiler/x86/cpubase.pas
+++ compiler/x86/cpubase.pas
@@ -128,34 +128,6 @@ uses
 
       {Super register numbers:}
    const
-{$ifdef x86_64}
-      RS_SPECIAL    = $00;      {Special register}
-      RS_RAX        = $01;      {EAX}
-      RS_RBX        = $02;      {EBX}
-      RS_RCX        = $03;      {ECX}
-      RS_RDX        = $04;      {EDX}
-      RS_RSI        = $05;      {ESI}
-      RS_RDI        = $06;      {EDI}
-      RS_RBP        = $07;      {EBP}
-      RS_RSP        = $08;      {ESP}
-      RS_R8         = $09;      {R8}
-      RS_R9         = $0a;      {R9}
-      RS_R10        = $0b;      {R10}
-      RS_R11        = $0c;      {R11}
-      RS_R12        = $0d;      {R12}
-      RS_R13        = $0e;      {R13}
-      RS_R14        = $0f;      {R14}
-      RS_R15        = $10;      {R15}
-      { create aliases to allow code sharing between x86-64 and i386 }
-      RS_EAX        = RS_RAX;
-      RS_EBX        = RS_RBX;
-      RS_ECX        = RS_RCX;
-      RS_EDX        = RS_RDX;
-      RS_ESI        = RS_RSI;
-      RS_EDI        = RS_RDI;
-      RS_EBP        = RS_RBP;
-      RS_ESP        = RS_RSP;
-{$else x86_64}
       RS_SPECIAL    = $00;      {Special register}
       RS_EAX        = $01;      {EAX}
       RS_EBX        = $02;      {EBX}
@@ -165,7 +137,14 @@ uses
       RS_EDI        = $06;      {EDI}
       RS_EBP        = $07;      {EBP}
       RS_ESP        = $08;      {ESP}
-{$endif x86_64}
+      RS_R8         = $09;      {R8}
+      RS_R9         = $0a;      {R9}
+      RS_R10        = $0b;      {R10}
+      RS_R11        = $0c;      {R11}
+      RS_R12        = $0d;      {R12}
+      RS_R13        = $0e;      {R13}
+      RS_R14        = $0f;      {R14}
+      RS_R15        = $10;      {R15}
 
 
       {Number of first and last superregister.}
@@ -486,6 +465,9 @@ uses
       ALL_REGISTERS = [firstreg..lastreg];
       ALL_INTREGISTERS = [1..255];
 
+      general_registers = [R_EAX,R_EBX,R_ECX,R_EDX];
+      general_superregisters = [RS_EAX,RS_EBX,RS_ECX,RS_EDX];
+
       {# low and high of the available maximum width integer general purpose }
       { registers                                                            }
       LoGPReg = R_EAX;
@@ -510,6 +492,12 @@ uses
 
       maxintregs = 4;
       intregs = [R_EAX..R_BL]-[R_ESI,R_SI];
+{$ifdef newra}
+      usableregsint = [first_imreg..last_imreg];
+{$else}
+      usableregsint = [RS_EAX,RS_EBX,RS_ECX,RS_EDX];
+{$endif}
+      c_countusableregsint = 4;
 
       maxfpuregs = 8;
       fpuregs = [R_ST0..R_ST7];
@@ -519,6 +507,25 @@ uses
       mmregs = [R_MM0..R_MM7];
       usableregsmm = [R_MM0..R_MM7];
       c_countusableregsmm  = 8;
+
+      maxaddrregs = 1;
+      addrregs    = [R_ESI];
+      usableregsaddr = [RS_ESI];
+      c_countusableregsaddr = 1;
+
+      maxvarregs = 4;
+      varregs : array[1..maxvarregs] of Toldregister =
+         (R_EBX,R_EDX,R_ECX,R_EAX);
+
+      maxfpuvarregs = 8;
+
+      {# Registers which are defined as scratch and no need to save across
+         routine calls or in assembler blocks.
+      }
+{$ifndef newra}
+      max_scratch_regs = 1;
+      scratch_regs : array[1..max_scratch_regs] of Tsuperregister = (RS_EDI);
+{$endif}
 
 {*****************************************************************************
                             CPU Dependent Constants
@@ -707,14 +714,7 @@ implementation
 end.
 {
   $Log$
-  Revision 1.3  2002-04-25 20:15:40  florian
-    * block nodes within expressions shouldn't release the used registers,
-      fixed using a flag till the new rg is ready
-
-  Revision 1.2  2002/04/25 16:12:09  florian
-    * fixed more problems with cpubase and x86-64
-
-  Revision 1.1  2003/04/25 11:12:09  florian
+  Revision 1.1  2003-04-25 11:12:09  florian
     * merged i386/cpubase and x86_64/cpubase to x86/cpubase;
       different stuff went to cpubase.inc
 
