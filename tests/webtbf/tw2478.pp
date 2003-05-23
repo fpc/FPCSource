79a53ba0c83a615d494f54e10f3bd3b5a81126e1
peter <peter@freepascal.org>
2003-05-23 15:01:30 +0000

    * new bugs

---
+++ tests/webtbf/tw2478.pp
@@ -0,0 +1,36 @@
+{ Source provided for Free Pascal Bug Report 2478 }
+{ Submitted by "Louis Jean-Richard" on  2003-04-30 }
+{ e-mail: Ljean_richard@compuserve.com }
+UNIT tw2478;
+{$HINTS OFF}
+INTERFACE
+TYPE
+        aByteArray = ARRAY[1 .. 100] OF byte;
+        aCardinalArray = ARRAY[4 .. 100] OF byte;
+        aType1 = ARRAY[12 .. 100] OF byte;
+TYPE
+        anObject =
+                OBJECT
+                        PUBLIC
+                                PROCEDURE Method( CONST aba: aByteArray );
+                                PROCEDURE Method( CONST clt: aType1 );
+                        PRIVATE
+                                PROCEDURE Method( CONST aba: aByteArray; CONST clt: aType1 );
+                                PROCEDURE Method( CONST aca: aCardinalArray; CONST clt: aType1 );
+                                PROCEDURE Method( cp, cc: cardinal; CONST clt: aType1 );
+                END
+                ;
+{       *************************************************************** }
+IMPLEMENTATION
+PROCEDURE anObject.Method( CONST aba: aByteArray );
+BEGIN END;
+PROCEDURE anObject.Method( CONST aba: aByteArray; CONST clt: aType1 );
+BEGIN END;
+PROCEDURE anObject.Method( CONST aca: aCardinalArray; CONST clt: aType1 );
+BEGIN END;
+PROCEDURE anObject.Method( CONST clt: aType1 );
+BEGIN END;
+INITIALIZATION
+FINALIZATION
+END
+.
---
+++ tests/webtbs/tw2480.pp
@@ -0,0 +1,26 @@
+{ Source provided for Free Pascal Bug Report 2480 }
+{ Submitted by "Sergey Kosarevsky" on  2003-05-01 }
+{ e-mail: netsurfer@au.ru }
+{$mode objfpc}
+{$static on}
+
+Type tSingleton=Class
+       Private
+        _Instance:tSingleton;Static;
+       Public
+        Constructor Init;
+        Class Function Instance:tSingleton;
+     End;
+
+Function tSingleton.Instance:tSingleton;
+Begin
+   If _Instance=Nil Then _Instance:=tSingleton.Init;
+   Exit(_Instance);
+End;
+
+Constructor tSingleton.Init;
+Begin
+End;
+
+Begin
+End.
