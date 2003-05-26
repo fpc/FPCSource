b05a217962e38440e67bff000beac846bead91a0
mazen <mazen@freepascal.org>
2003-05-26 22:10:19 +0000

    * making program assembling first as assembling the   RTL may fail. It should take log time before it succed

--- tests/sparc/asld.sh
+++ tests/sparc/asld.sh
@@ -3,12 +3,12 @@ DoExitAsm ()
 { echo "An error occurred while assembling $1"; exit 1; }
 DoExitLink ()
 { echo "An error occurred while linking $1"; exit 1; }
+echo Assembling $1
+/usr/local/bin/sparc-linux/as -o "$1.o" "$1.s"
+if [ $? != 0 ]; then DoExitAsm system; fi
 echo Assembling system
 /usr/local/bin/sparc-linux/as -o system.o system.s
-if [ $? != 0 ]; then DoExitAsm system; fi
-echo Assembling sparctest
-/usr/local/bin/sparc-linux/as -o test0001.o test0001.s
 if [ $? != 0 ]; then DoExitAsm sparctest; fi
 echo Linking sparctest
-/usr/local/bin/sparc-linux/ld    -s -L. -o test0001 link.res
+/usr/local/bin/sparc-linux/ld    -s -L. -o "$1" link.res
 if [ $? != 0 ]; then DoExitLink sparctest; fi
