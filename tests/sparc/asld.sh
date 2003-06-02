ae80816ac2df1e8ce6759b9a245a307c3777528f
mazen <mazen@freepascal.org>
2003-06-02 22:05:56 +0000

    + adding assmbling of prt0.as to the script

--- tests/sparc/asld.sh
+++ tests/sparc/asld.sh
@@ -8,6 +8,7 @@ echo Assembling $1
 if [ $? != 0 ]; then DoExitAsm system; fi
 echo Assembling system
 /usr/local/bin/sparc-linux/as -o system.o system.s
+/usr/local/bin/sparc-linux/as -o prt0.o ../../rtl/linux/sparc/prt0.as
 if [ $? != 0 ]; then DoExitAsm sparctest; fi
 echo Linking sparctest
 /usr/local/bin/sparc-linux/ld    -s -L. -o "$1" link.res
