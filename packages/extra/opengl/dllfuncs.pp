36c34e73534aaa9ebcb43523412f4241890cec68
marco <marco@freepascal.org>
2003-08-25 18:16:38 +0000

    * BSD fix

--- packages/extra/opengl/dllfuncs.pp
+++ packages/extra/opengl/dllfuncs.pp
@@ -23,10 +23,15 @@ const
   RTLD_NOW          = $002;
   RTLD_BINDING_MASK = $003;
 
+{$ifdef Linux}
 function dlopen(Name: PChar; Flags: LongInt) : Pointer; cdecl; external 'dl';
 function dlsym(Lib: Pointer; Name: PChar) : Pointer; cdecl; external 'dl';
 function dlclose(Lib: Pointer): LongInt; cdecl; external 'dl';
-
+{$else}
+function dlopen(Name: PChar; Flags: LongInt) : Pointer; cdecl; external 'c';
+function dlsym(Lib: Pointer; Name: PChar) : Pointer; cdecl; external 'c';
+function dlclose(Lib: Pointer): LongInt; cdecl; external 'c';
+{$endif}
 
 function LoadLibrary(Name: PChar): THandle;
 begin
@@ -51,7 +56,10 @@ end.
 
 {
   $Log$
-  Revision 1.1  2002-10-13 13:57:30  sg
+  Revision 1.2  2003-08-25 18:16:38  marco
+   * BSD fix
+
+  Revision 1.1  2002/10/13 13:57:30  sg
   * Finally, the new units are available: Match the C headers more closely;
     support for OpenGL extensions, and much more. Based on the Delphi units
     by Tom Nuydens of delphi3d.net
