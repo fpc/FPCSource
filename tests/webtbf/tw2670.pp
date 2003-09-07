f32f95a58c298772ab5e11e68278bd86a0f15df1
florian <florian@freepascal.org>
2003-09-07 14:12:43 +0000

    no message

---
+++ tests/webtbf/tw2670.pp
@@ -0,0 +1,16 @@
+{ %version=1.1}
+{ Source provided for Free Pascal Bug Report 2670 }
+{ Submitted by "marco" on  2003-09-07 }
+{ e-mail: marco@freepascal.org }
+program bugje;
+{$mode delphi}
+
+Uses Classes;
+
+type            theenum = (hello,hi, hoi);
+                testme= class(TComponent)
+                           field : theenum;
+                           end;
+
+begin
+end.
