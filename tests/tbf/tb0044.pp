d0b694e633ac4097ca2964d779ecb18409b76154
peter <peter@freepascal.org>
2003-10-20 16:55:26 +0000

    * 1.0.x test only

--- tests/tbf/tb0044.pp
+++ tests/tbf/tb0044.pp
@@ -1,6 +1,12 @@
+{ %maxversion=1.0.99 }
 { %FAIL }
 { Old file: tbf0230.pp }
 { several strange happen on the ln function: ln(0): no FPE and writeln can't write non numeric values Gives out an exception on compiling because of zero div OK 0.99.11 (PM) }
+
+{ 
+  This test is only for 1.0.x.
+  1.1+ supports Nan and Inf
+}
 
 var
    e : extended;
