37762b957fe2a94c1242a1eaf890814f6bce449f
peter <peter@freepascal.org>
2003-10-21 18:20:05 +0000

    * delphi mode   * i386 only

--- tests/webtbs/tw2666.pp
+++ tests/webtbs/tw2666.pp
@@ -1,6 +1,11 @@
+{ %cpu=i386 }
+
 { Source provided for Free Pascal Bug Report 2666 }
 { Submitted by "marcov" on  2003-09-06 }
 { e-mail: marco@freepascal.org }
+
+{$mode delphi}
+
 function GetDynamicMethod(AClass: TClass; Index: Integer): Pointer; assembler;
 asm
         CALL    System.@Halt
