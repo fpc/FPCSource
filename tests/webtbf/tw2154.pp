e77d88c06e22f314ff3f707cea9df03e00a6931f
florian <florian@freepascal.org>
2003-10-26 14:20:32 +0000

    no message

---
+++ tests/webtbf/tw2154.pp
@@ -0,0 +1,17 @@
+{ %fail }
+{ Source provided for Free Pascal Bug Report 2154 }
+{ Submitted by "Sergey Kosarevsky" on  2002-10-01 }
+{ e-mail: netsurfer@au.ru }
+Var glCombinerOutputNV:Procedure(pname,
+                                 pname,
+                                 pname,
+                                 pname,
+                                 pname,
+                                 pname,
+                                 pname:Longint;
+                                 pbool,
+                                 pbool,
+                                 pbool:Boolean);
+
+Begin
+End.
