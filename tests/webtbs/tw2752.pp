738dd0f3e06e7c6247ecff9b75f83ab097cac17d
peter <peter@freepascal.org>
2003-10-30 17:43:33 +0000

    * new bug

---
+++ tests/webtbs/tw2752.pp
@@ -0,0 +1,27 @@
+{ %opt=-Sew }
+
+{ Source provided for Free Pascal Bug Report 2752 }
+{ Submitted by "Micha" on  2003-10-26 }
+{ e-mail:  }
+program Construct;
+
+{$mode delphi}
+
+type
+
+  tb = class
+  public
+    constructor Create(a: tobject);
+  end;
+
+constructor tb.create(a: tobject);
+begin
+end;
+
+var
+  b,c: tb;
+
+begin
+  b := tb.create(b);
+end.
+
