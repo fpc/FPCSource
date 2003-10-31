f51646dfd5b097f9c7dc4864b79853f5d205152b
peter <peter@freepascal.org>
2003-10-31 16:15:26 +0000

    * test exitcode 217

--- tests/tbs/tb0449.pp
+++ tests/tbs/tb0449.pp
@@ -1,15 +1,12 @@
-{ %NORUN }
-
+{ %RESULT=217 }
 
 {$ifdef fpc}
 {$mode objfpc}
 {$endif}
-uses sysutils;
+uses SysUtils;
 
 type
-
   EWbcError = class of Exception;
-  
   
 Begin
   raise EwbcError.create('Hello');
@@ -17,7 +14,10 @@ end.
 
 {
   $Log$
-  Revision 1.1  2003-02-02 13:46:06  carl
+  Revision 1.2  2003-10-31 16:15:26  peter
+    * test exitcode 217
+
+  Revision 1.1  2003/02/02 13:46:06  carl
    + exception testing
 
 }  
