3e019737ff1ce829a51c3f3be09553867affe1e0
peter <peter@freepascal.org>
2003-10-31 16:19:35 +0000

    * new bug

---
+++ tests/webtbs/tw2481.pp
@@ -0,0 +1,37 @@
+{ Source provided for Free Pascal Bug Report 2481 }
+{ Submitted by "Eero Tanskanen" on  2003-05-04 }
+{ e-mail: yendor@nic.fi }
+
+{$mode objfpc}
+
+Type
+  TMoo = Class
+
+    A : Real;
+    Constructor Init;
+
+  End;
+
+Var
+  Moo : TMoo;
+
+  Operator := (V : Real) B : TMoo;
+  Begin
+
+    B.A := V;
+
+  End;
+
+Constructor TMoo.Init;
+Begin
+
+  A := 0;
+
+End;
+
+Begin
+
+  Moo := TMoo.Init;
+  Moo := 0.2;
+
+End.
