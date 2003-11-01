211eb846caaf783269949dbbb20d013137bed5f6
peter <peter@freepascal.org>
2003-11-01 16:24:27 +0000

    * new bug

---
+++ tests/webtbs/tw2645.pp
@@ -0,0 +1,34 @@
+{ %version=1.1 }
+
+{$mode objfpc}
+{$inline on}
+
+type
+  c = class
+    l : longint;
+    procedure p;inline;
+    procedure p2;
+  end;
+
+    procedure c.p;inline;
+    begin
+      writeln(l);
+      inc(l,10);
+    end;
+
+    procedure c.p2;
+    begin
+      l:=10;
+      p;
+      if l<>20 then
+        halt(1);
+    end;
+
+var
+  o : c;
+begin
+  o:=c.create;
+  o.p2;
+  o.free;
+end.
+ 
