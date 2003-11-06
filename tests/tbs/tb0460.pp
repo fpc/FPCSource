e861eaa4c0460b7d89de2f62dc18bf7ca506106c
florian <florian@freepascal.org>
2003-11-06 22:46:45 +0000

    no message

--- tests/tbs/tb0460.pp
+++ tests/tbs/tb0460.pp
@@ -2,5 +2,22 @@ const
   MinCurrency : Currency = -922337203685477.5807;
   MaxCurrency : Currency = 922337203685477.5807;
 
+var
+  s : string;
+
 begin
+  str(MinCurrency:0:4,s);
+  { output according to delphi }
+  if s<>'-922337203685477.5810' then
+    begin
+      writeln(s);
+      halt(1);
+    end;
+  str(MaxCurrency:0:4,s);
+  { output according to delphi }
+  if s<>'922337203685477.5810' then
+    begin
+      writeln(s);
+      halt(1);
+    end;
 end.
