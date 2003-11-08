621b93dee19b0996744639b5fdbf5c6b20c72de7
florian <florian@freepascal.org>
2003-11-08 08:55:10 +0000

    no message

---
+++ tests/tbs/tb0462.pp
@@ -0,0 +1,16 @@
+{ %version=1.1 }
+type
+  RR = record
+    RA : WideString;
+  end;
+
+const
+  Z : RR = (RA: 'B');
+  
+begin
+  if z.ra<>'B' then
+    begin
+      writeln('error');
+      halt(1);
+    end; 
+end.
