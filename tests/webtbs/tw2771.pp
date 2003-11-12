1a3d62baa3a5e78598ab8db6c0c1a03b6376703c
peter <peter@freepascal.org>
2003-11-12 16:37:29 +0000

    * new bug

---
+++ tests/webtbs/tw2771.pp
@@ -0,0 +1,11 @@
+{ Source provided for Free Pascal Bug Report 2771 }
+{ Submitted by "Michalis Kamburelis" on  2003-11-08 }
+{ e-mail: michalis@camelot.homedns.org }
+{$macro on}
+
+{$define MAC:=procedure P}
+
+MAC; begin end;
+
+begin
+end.
