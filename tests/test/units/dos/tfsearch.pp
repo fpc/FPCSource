25e1ee995fc0ea0dd9b3b4874d3a242d30b865a7
Tomas Hajny <hajny@freepascal.org>
2003-11-16 17:27:20 +0000

    + test for FSearch added

---
+++ tests/test/units/dos/tfsearch.pp
@@ -0,0 +1,62 @@
+{
+  $Id$
+    This file is part of the Free Pascal test suite.
+    Copyright (c) 1999-2003 by the Free Pascal development team.
+
+    Test for possible bugs in Dos.FSearch
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
+program TFSearch;
+
+uses
+ Dos;
+
+var
+ S: string;
+ F: file;
+ Err: boolean;
+
+const
+ TestDir: string = 'TESTDIR';
+ TestFile: string = 'testfile';
+
+begin
+ Err := false;
+ MkDir (TestDir);
+ S := FSearch (TestDir, '');
+ if S <> '' then
+ begin
+  WriteLn ('FSearch should only find files, not directories!!');
+  WriteLn ('Returned value = ', S);
+  Err := true;
+ end;
+ Assign (F, TestDir + DirectorySeparator + TestFile);
+ Rewrite (F);
+ Close (F);
+ S := FSearch (TestFile, TestDir);
+ if S <> TestDir + DirectorySeparator + TestFile then
+ begin
+  WriteLn ('FSearch didn''t find the test file!!');
+  WriteLn ('Returned value = ', S);
+  Err := true;
+ end;
+ Erase (F);
+ RmDir (TestDir);
+ if Err then Halt (1);
+end.
+
+{
+  $Log$
+  Revision 1.1  2003-11-16 17:27:20  hajny
+    + test for FSearch added
+
+
+}
