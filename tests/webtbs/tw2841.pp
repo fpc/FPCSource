8b4a122d49d305a8a59614e2ca600bf8af40b33e
peter <peter@freepascal.org>
2003-12-12 15:43:41 +0000

    * new bug

---
+++ tests/webtbs/tw2841.pp
@@ -0,0 +1,17 @@
+{ %opt=-Sew }
+
+{ Source provided for Free Pascal Bug Report 2841 }
+{ Submitted by "Mattias Gaertner" on  2003-12-09 }
+{ e-mail: mattias@freepascal.org }
+program ShortStringNotInitialized;
+
+{$mode objfpc}{$H+}
+
+procedure DoSomething;
+var s: shortstring;
+begin
+  s[1]:=chr(3);
+end;
+
+begin
+end.
