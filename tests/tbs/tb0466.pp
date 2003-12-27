b7a19dd8470222feb9fb54b6fd0d100293afb442
peter <peter@freepascal.org>
2003-12-27 22:27:05 +0000

    * new bug

---
+++ tests/tbs/tb0466.pp
@@ -0,0 +1,13 @@
+var
+  outf : file of byte;
+  w : word;
+begin
+   assign(outf, 'tb0466.tmp');
+   rewrite(outf);
+   {only explicit typecasting helps: byte(10)}
+   write(outf, 10);
+   w:=20;
+   write(outf, w);
+   close(outf);
+end.
+
