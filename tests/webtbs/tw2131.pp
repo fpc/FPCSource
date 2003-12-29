cbfa84638791b3bffd15cc2ccfb0e9ede8a618da
Jonas Maebe <jonas@freepascal.org>
2003-12-29 18:53:18 +0000

    * fixed for systems where extended = double

--- tests/webtbs/tw2131.pp
+++ tests/webtbs/tw2131.pp
@@ -2,11 +2,16 @@ program bug2131;
 
 var
   TestStr : string;
+  correct : string;
 begin
+  if sizeof(extended) > sizeof(double) then
+    correct := ' 1.8E+0012'
+  else
+    correct := ' 1.8E+012';
   Str (1.789e12:1, TestStr);
-  if (teststr <> ' 1.8E+0012') then
+  if (teststr <> correct) then
     begin
-      writeln('error, got "',teststr,'" expected " 1.8E+0012"');
+      writeln('error, got "',teststr,'" expected "',correct,'"');
       halt(1);
     end
 end.
