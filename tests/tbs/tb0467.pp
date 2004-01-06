8c0e6c89437a92e89347a54e9ea5f2996c5ecd60
florian <florian@freepascal.org>
2004-01-06 12:45:12 +0000

    no message

--- tests/tbs/tb0467.pp
+++ tests/tbs/tb0467.pp
@@ -1,3 +1,7 @@
+{ %version=1.1 }
+{$mode objfpc}
+uses
+  typinfo;
 type
   tmyobject = class
       fs : single;
@@ -5,5 +9,11 @@ type
       property s : single read fs write fs default 3.1415;
   end;
 
+var
+  myobject : tmyobject;
+
 begin
+  myobject:=tmyobject.create;
+  writeln(GetFloatProp(myobject,'S'));
+  myobject.free;
 end.
