f46d787ab6ff77b0814e6f865007c95a6c6a5656
daniel <daniel@freepascal.org>
2004-01-10 21:32:02 +0000

    * @ operator does not work on labels in current compiler

---
+++ tests/tbs/tb0468.pp
@@ -0,0 +1,35 @@
+{ %OPT=-Sg }
+program jumptab;
+
+{$asmmode intel}
+
+procedure x;
+
+label a;
+
+var b:pointer;
+
+begin
+  b:=@a;
+a:
+end;
+
+procedure jumptabproc; assembler;
+
+label a,b,c,d;
+
+const jumptable:array[0..3] of pointer=(@a,@b,@b,@d);
+
+asm
+a:
+ nop
+b:
+ nop
+c:
+ nop
+d:
+ nop
+end;
+
+begin
+end.
