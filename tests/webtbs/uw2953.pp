326da54b3af782e268f93c0d586420e4a168d9d3
peter <peter@freepascal.org>
2004-02-13 15:44:43 +0000

    * helper unit

---
+++ tests/webtbs/uw2953.pp
@@ -0,0 +1,12 @@
+unit uw2953;
+{$mode Delphi}
+
+interface
+
+type
+     aclasswithfields = class
+                          protected
+                            field1:integer;
+                            end;
+implementation
+end.
