e14c3608380b399ba63d0f81f3462edc642dccac
florian <florian@freepascal.org>
2004-02-19 17:32:14 +0000

    no message

---
+++ tests/tbs/tb0470.pp
@@ -0,0 +1,20 @@
+procedure proc1(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20 : longint);
+  begin
+  end;
+
+procedure proc2;
+  var
+    l : longint;
+  begin
+    l:=$deadbeef;
+    proc1(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+    if l<>$deadbeef then
+      begin
+        writeln('error');
+        halt(1);
+      end;
+  end;
+
+begin
+  proc2;
+end.
