3e698772223bce692657aac5766f18bb336c147f
pierre <pierre@freepascal.org>
2004-02-20 07:26:40 +0000

    several tests related to webbug 2853

---
+++ tests/webtbf/tw2853a.pp
@@ -0,0 +1,21 @@
+{ %FAIL }
+{ %OPT=-dLOOP }
+{ Test variant showing another failure }
+{ added by Pierre }
+{ Source provided for Free Pascal Bug Report 2853 }
+{ Submitted by "Bj”rn Hendriks" on  2003-12-18 }
+{ e-mail: bjoern.hendriks.ext_ese@ts.siemens.de }
+program test;
+
+begin
+   WriteLn('abc');
+{$ifdef LOOP}
+   WriteLn('def');
+{
+ *)
+
+   WriteLn('ghi');
+{$endif LOOP}
+   WriteLn('test');
+end.
+
