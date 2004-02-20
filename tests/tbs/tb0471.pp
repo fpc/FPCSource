32b66da46519254f2053f12e55aa69e5427e5eca
peter <peter@freepascal.org>
2004-02-20 20:01:30 +0000

    * overload with procvar and result of procvar function

---
+++ tests/tbs/tb0471.pp
@@ -0,0 +1,30 @@
+const
+  err : boolean = true;
+  
+type
+  tf = function:longint;
+procedure p1(l:longint);overload;
+begin
+  writeln('longint');
+end;
+
+  
+procedure p1(f:tf);overload;
+begin
+  writeln('procvar');
+  err:=false;
+end;
+
+function vf:longint;
+begin
+  vf:=10;
+end;
+  
+var
+  v : tf;
+begin
+  v:=vf;
+  p1(v);
+  if err then
+    halt(1);
+end.
