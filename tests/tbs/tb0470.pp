d668c06dc9b81a2133937e218d9d1a70bc217b44
florian <florian@freepascal.org>
2004-02-20 20:56:28 +0000

    * fixed test

--- tests/tbs/tb0470.pp
+++ tests/tbs/tb0470.pp
@@ -4,7 +4,7 @@ procedure proc1(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p
 
 procedure proc2;
   var
-    l : longint;
+    l : dword;
   begin
     l:=$deadbeef;
     proc1(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
