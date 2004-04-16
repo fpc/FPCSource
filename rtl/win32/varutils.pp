0d9f209c26bf04d8b37df2904e81af02d776e396
michael <michael@freepascal.org>
2004-04-16 10:07:44 +0000

    + Case change (bug 3050)

--- rtl/win32/varutils.pp
+++ rtl/win32/varutils.pp
@@ -60,7 +60,7 @@ function SafeArrayDestroy(psa: PVarArray): HRESULT; stdcall;external oleaut;
 function SafeArrayDestroyData(psa: PVarArray): HRESULT; stdcall;external oleaut;
 function SafeArrayDestroyDescriptor(psa: PVarArray): HRESULT; stdcall;external oleaut;
 function SafeArrayGetDim(psa: PVarArray): Integer; stdcall;external oleaut;
-function SafeArrayGetElemSize(psa: PVarArray): LongWord; stdcall;external oleaut;
+function SafeArrayGetElemsize(psa: PVarArray): LongWord; stdcall;external oleaut;
 function SafeArrayGetElement(psa: PVarArray; Indices: PVarArrayCoorArray;  Data: Pointer): HRESULT; stdcall;external oleaut;
 function SafeArrayGetLBound(psa: PVarArray; Dim: Integer;  var LBound: Integer): HRESULT; stdcall;external oleaut;
 function SafeArrayGetUBound(psa: PVarArray; Dim: Integer;  var UBound: Integer): HRESULT; stdcall;external oleaut;
@@ -76,7 +76,10 @@ end.
 
 {
   $Log$
-  Revision 1.4  2002-10-10 19:20:02  florian
+  Revision 1.5  2004-04-16 10:07:44  michael
+  + Case change (bug 3050)
+
+  Revision 1.4  2002/10/10 19:20:02  florian
     * unit is now smartlinked by default to avoid problems with an old oleauth.dll
 
   Revision 1.3  2002/09/07 16:01:29  peter
