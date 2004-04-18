2d31d6f75e852d874472e30b13ded7a8e7b7aa09
florian <florian@freepascal.org>
2004-04-18 07:56:51 +0000

    no message

---
+++ tests/webtbs/tw3048.pp
@@ -0,0 +1,9 @@
+{ Source provided for Free Pascal Bug Report 3048 }
+{ Submitted by "GBD" on  2004-04-15 }
+{ e-mail:  }
+{$mode delphi}
+var a ,b : array of word;
+
+begin
+ if a<>b then writeln('ok');
+end.
