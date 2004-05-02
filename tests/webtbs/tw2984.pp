0f15bfeb7c4c8c4b33143069967f0b7f47f92a12
peter <peter@freepascal.org>
2004-05-02 15:21:20 +0000

    * System() function scope problem

---
+++ tests/webtbs/tw2984.pp
@@ -0,0 +1,7 @@
+uses
+  uw2984;
+
+begin
+  uw2984.System('test');
+end.
+
---
+++ tests/webtbs/uw2984.pp
@@ -0,0 +1,13 @@
+unit uw2984;
+interface
+
+procedure System(const s:string);
+
+implementation
+
+procedure System(const s:string);
+begin
+  writeln(s);
+end;
+
+end.
