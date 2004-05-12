40c6690206d415fb77050099839ed9924344fcfa
olle <olle>
2004-05-12 10:47:59 +0000

    + Added test

---
+++ tests/tbf/tb0163.pp
@@ -0,0 +1,12 @@
+{ %fail }
+unit Testunit;	
+{This doesn't fail although it should due to non resolved forward. OR}
+
+interface
+
+implementation
+
+procedure x; forward;
+
+//begin   {If the comment is removed, it fails as it should.}
+end.
