aa9fbc040eb8dc33366a8597c42953f3009bb25c
olle <olle>
2004-05-18 10:35:15 +0000

    + test that the compiler should not allow non local goto

---
+++ tests/tbf/tb0164.pp
@@ -0,0 +1,27 @@
+{ %fail }
+
+program tb0164;
+
+{ Non local goto cannot be handled properly by the compiler,
+  and should therefore not be allowed.}
+
+{$GOTO ON}
+
+  procedure foo;
+  
+    label 999;
+  
+    procedure bar;
+  
+    begin
+      goto 999
+    end;
+  
+  begin
+    bar;
+    999:
+  end;
+  
+begin
+  foo;
+end.
