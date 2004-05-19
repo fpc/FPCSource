726b980f38fa131c27784acffc7c1c2414c46dc9
Jonas Maebe <jonas@freepascal.org>
2004-05-19 22:27:18 +0000

    + added

---
+++ tests/webtbs/tw3104.pp
@@ -0,0 +1,19 @@
+unit tw3104;
+
+interface
+
+implementation
+
+{$S-}
+procedure MizExitProc;
+begin
+end;
+{$S+}
+
+procedure InitExitProc;
+begin end;
+
+begin
+end.
+
+
---
+++ tests/webtbs/uw3103.pp
@@ -0,0 +1,30 @@
+unit uw3103;
+
+interface
+
+const MaxArt = 2000;
+
+type
+   WordArr = array[1..MaxArt] of word;
+
+var 
+   RefList  : array[1..MaxArt] of string;
+   Theorems : array[- 1..MaxArt,1..2] of byte;
+
+implementation
+
+procedure P;
+var  lThNbr : WordArr;
+     sArr   : array[- 1..MaxArt] of Integer;
+     pctTh  : array[- 1..MaxArt] of real;
+     k   : integer;
+     lThArr : array[- 1..MaxArt] of Integer;
+ begin
+    sArr[k] := k;
+    if RefList[k] = 'A' then lThArr[- 1] := lThNbr[k];
+    pctTh[k] := Theorems[k,1];
+  end;
+
+begin
+end.
+
