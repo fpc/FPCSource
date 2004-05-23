53c7e9acb4afab79d3f962508a9edbcbebbd01d0
peter <peter@freepascal.org>
2004-05-23 16:22:03 +0000

    * new bug

---
+++ tests/webtbf/tw3114.pp
@@ -0,0 +1,20 @@
+{ %fail }
+
+{ Source provided for Free Pascal Bug Report 3114 }
+{ Submitted by "Maarten Bekers" on  2004-05-23 }
+{ e-mail: fpc@elexer.com }
+unit tw3114;
+
+{$mode delphi}
+
+interface
+
+function whee();
+
+implementation
+
+function whee();
+begin
+end;
+
+end.
