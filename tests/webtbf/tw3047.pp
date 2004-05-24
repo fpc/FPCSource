5cd52d5a42deb62b322d8ee4d4be55cf74c085ca
florian <florian@freepascal.org>
2004-05-24 20:34:22 +0000

    no message

---
+++ tests/webtbf/tw3047.pp
@@ -0,0 +1,16 @@
+{ Source provided for Free Pascal Bug Report 3047 }
+{ Submitted by "GBD" on  2004-04-12 }
+{ e-mail:  }
+{ $mode delphi}
+
+function JenkinsHashBuffer(const buffer; length : Integer; initVal : Integer) : longword;
+begin
+end;
+
+function JenkinsHashString(const s : String) : longword;
+begin
+                result := JenkinsHashBuffer(PChar(s), Length(s), 0)
+end;
+
+begin
+end.
