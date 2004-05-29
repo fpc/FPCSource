1d1df129f6f893dabec15ba8248b1b50c820b3a7
florian <florian@freepascal.org>
2004-05-29 15:52:33 +0000

    *** empty log message ***

--- tests/tbs/tb0467.pp
+++ tests/tbs/tb0467.pp
@@ -1,9 +1,11 @@
 { %version=1.1 }
 {$mode objfpc}
+{$M+}
 uses
   typinfo;
 type
   tmyobject = class
+    protected
       fs : single;
     published
       property s : single read fs write fs default 3.1415;
@@ -14,6 +16,11 @@ var
 
 begin
   myobject:=tmyobject.create;
-  writeln(GetFloatProp(myobject,'S'));
+  SetFloatProp(myobject,'s',3);
+  if GetFloatProp(myobject,'s')<>3 then
+    begin
+      writeln('error');
+      halt(1);
+    end;
   myobject.free;
 end.
