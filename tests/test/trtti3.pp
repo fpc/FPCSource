5a37581b299ca737e750a1908f93d8102fc5a66a
florian <florian@freepascal.org>
2004-05-29 15:20:02 +0000

    *** empty log message ***

--- tests/test/trtti3.pp
+++ tests/test/trtti3.pp
@@ -12,12 +12,18 @@ begin
    a:=c1+c2;
    finalize(a);
    if length(a)<>0 then
-     halt(1);
+     begin
+       writeln('length of a: ',length(a),' instead of 0');
+       halt(1);
+     end;
    for i:=1 to 10 do
      aa[i]:=c1+c2;
    finalize(aa[1],10);
    for i:=1 to 10 do
      if length(aa[i])<>0 then
-       halt(1);
+       begin
+         writeln('error at element: ',i,' contents: ''',aa[i],'''  instead of ''''');
+         halt(1);
+       end;
 end.
 
