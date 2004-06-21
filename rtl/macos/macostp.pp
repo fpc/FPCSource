0331789fad858190dc645e1379e9a3c4614b2917
olle <olle>
2004-06-21 19:16:47 +0000

    + added more entries

---
+++ rtl/macos/macostp.pp
@@ -0,0 +1,29 @@
+{
+    $Id$
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 2003 - 2004 by Olle Raab
+
+    A selection of the MacOS API for FreePascal, written
+    in the Turbo Pascal dialect.
+    It is primarily for internal use in the rtl, please
+    do not expect it to remain the same over time,
+    it will be subject to changes.
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
+unit macostp;
+
+interface
+
+{$I macostp.inc}
+
+implementation
+
+end.
