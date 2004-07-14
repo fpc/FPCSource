9a9c17f078fec5103139b4f92774d236e95b427b
Jonas Maebe <jonas@freepascal.org>
2004-07-14 14:46:05 +0000

    + tw3210

---
+++ tests/webtbs/tw3210.pp
@@ -0,0 +1,18 @@
+Const
+    N	= 10000;
+
+Var
+    count	: array[0..200] of int64;
+    i , tot	: longint;
+
+Begin
+    fillchar(count , sizeof(count) , 0);
+    for i := 1 to N do
+      begin
+          inc(count[random(201)]);
+      end;
+    tot := 0;
+    for i := 0 to 200 do inc(tot , count[i]);
+    if (tot <> N) then
+      halt(1);
+End.
