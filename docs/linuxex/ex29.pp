92ff8042a1746b3298b81dbe2e9d21b4a4e48a45
marco <marco@freepascal.org>
2004-07-17 22:58:25 +0000

    * 1.9.x updates

--- docs/linuxex/ex29.pp
+++ docs/linuxex/ex29.pp
@@ -2,7 +2,7 @@ program example29;
 
 { Program to demonstrate the LStat function. }
 
-uses linux;
+uses BaseUnix,Unix;
     
 var f : text;    
     i : byte;
@@ -15,49 +15,49 @@ begin
   for i:=1 to 10 do writeln (f,'Testline # ',i);
   close (f);
   { Do the call on made file. }
-  if not fstat ('test.fil',info) then 
+  if fpstat ('test.fil',info)<>0 then 
      begin
-     writeln('Fstat failed. Errno : ',linuxerror);
+     writeln('Fstat failed. Errno : ',fpgeterrno);
      halt (1);
      end;
   writeln;
-  writeln ('Result of fstat on file ''test.fil''.');
-  writeln ('Inode   : ',info.ino);
-  writeln ('Mode    : ',info.mode);
-  writeln ('nlink   : ',info.nlink);
-  writeln ('uid     : ',info.uid);
-  writeln ('gid     : ',info.gid);
-  writeln ('rdev    : ',info.rdev);
-  writeln ('Size    : ',info.size);
-  writeln ('Blksize : ',info.blksze);
-  writeln ('Blocks  : ',info.blocks);
-  writeln ('atime   : ',info.atime);
-  writeln ('mtime   : ',info.mtime);
-  writeln ('ctime   : ',info.ctime);
+  writeln ('Result of stat on file ''test.fil''.');
+  writeln ('Inode   : ',info.st_ino);
+  writeln ('Mode    : ',info.st_mode);
+  writeln ('nlink   : ',info.st_nlink);
+  writeln ('uid     : ',info.st_uid);
+  writeln ('gid     : ',info.st_gid);
+  writeln ('rdev    : ',info.st_rdev);
+  writeln ('Size    : ',info.st_size);
+  writeln ('Blksize : ',info.st_blksize);
+  writeln ('Blocks  : ',info.st_blocks);
+  writeln ('atime   : ',info.st_atime);
+  writeln ('mtime   : ',info.st_mtime);
+  writeln ('ctime   : ',info.st_ctime);
 
-  If not SymLink ('test.fil','test.lnk') then
-    writeln ('Link failed ! Errno :',linuxerror);
+  If  fpSymLink ('test.fil','test.lnk')<>0 then
+    writeln ('Link failed ! Errno :',fpgeterrno);
 
-  if not lstat ('test.lnk',info) then 
+  if  fplstat ('test.lnk',@info)<>0 then 
      begin
-     writeln('LStat failed. Errno : ',linuxerror);
+     writeln('LStat failed. Errno : ',fpgeterrno);
      halt (1);
      end;
   writeln;
   writeln ('Result of fstat on file ''test.lnk''.');
-  writeln ('Inode   : ',info.ino);
-  writeln ('Mode    : ',info.mode);
-  writeln ('nlink   : ',info.nlink);
-  writeln ('uid     : ',info.uid);
-  writeln ('gid     : ',info.gid);
-  writeln ('rdev    : ',info.rdev);
-  writeln ('Size    : ',info.size);
-  writeln ('Blksize : ',info.blksze);
-  writeln ('Blocks  : ',info.blocks);
-  writeln ('atime   : ',info.atime);
-  writeln ('mtime   : ',info.mtime);
-  writeln ('ctime   : ',info.ctime);
+  writeln ('Inode   : ',info.st_ino);
+  writeln ('Mode    : ',info.st_mode);
+  writeln ('nlink   : ',info.st_nlink);
+  writeln ('uid     : ',info.st_uid);
+  writeln ('gid     : ',info.st_gid);
+  writeln ('rdev    : ',info.st_rdev);
+  writeln ('Size    : ',info.st_size);
+  writeln ('Blksize : ',info.st_blksize);
+  writeln ('Blocks  : ',info.st_blocks);
+  writeln ('atime   : ',info.st_atime);
+  writeln ('mtime   : ',info.st_mtime);
+  writeln ('ctime   : ',info.st_ctime);
   { Remove file and link }  
   erase (f);
-  unlink ('test.lnk');    
+  fpunlink ('test.lnk');    
 end.
