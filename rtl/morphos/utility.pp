53ccf69f9defa5209e3ccd7c1017b92426280630
Károly Balogh <karoly@freepascal.org>
2004-08-03 14:26:10 +0000

    * initial revision

---
+++ rtl/morphos/utility.pp
@@ -0,0 +1,45 @@
+{
+    $Id$
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 2004 Karoly Balogh for Genesi S.a.r.l. <www.genesi.lu>
+
+    utility.library interface unit for MorphOS/PowerPC
+
+    MorphOS port was done on a free Pegasos II/G4 machine 
+    provided by Genesi S.a.r.l. <www.genesi.lu>
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
+unit utility;
+
+interface
+
+uses
+  exec;
+
+var
+  UtilityBase: Pointer;
+
+{$include utild1.inc}
+{$include utild2.inc}
+{$include utilf.inc}
+
+implementation
+
+begin
+  UtilityBase:=MOS_UtilityBase;
+end.
+
+{
+  $Log$
+  Revision 1.1  2004-08-03 14:26:10  karoly
+    * initial revision
+
+}
