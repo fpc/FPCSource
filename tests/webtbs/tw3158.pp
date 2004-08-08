6272064a5b8d15a2b0063df312621a1a0844a0da
florian <florian@freepascal.org>
2004-08-08 15:59:55 +0000

    no message

---
+++ tests/webtbs/tw3158.pp
@@ -0,0 +1,11 @@
+{ Source provided for Free Pascal Bug Report 3158 }
+{ Submitted by "Michalis Kamburelis" on  2004-06-11 }
+{ e-mail: michalis@camelot.homedns.org }
+{$R+}
+{$Q+}
+
+var A:Single;
+begin
+ A:=Exp(800);
+ Writeln(A);
+end.
