dde59bdf16d3400075fcb1f641d26bb766ef5ec6
florian <florian@freepascal.org>
2004-08-08 15:21:28 +0000

    no message

---
+++ tests/webtbf/tw3126.pp
@@ -0,0 +1,12 @@
+{ Source provided for Free Pascal Bug Report 3126 }
+{ Submitted by "Christian Iversen" on  2004-05-30 }
+{ e-mail: chrivers@iversen-net.dk }
+Program test;
+
+Var
+  P : PChar;
+Begin
+  Case P^ Of
+    'A'-'Z' : ;
+  End;
+End.
