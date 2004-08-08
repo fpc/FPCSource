c78b52579dd02bea13c289722004e26b2cadc809
florian <florian@freepascal.org>
2004-08-08 12:06:07 +0000

    no message

---
+++ tests/webtbs/tw3176.pp
@@ -0,0 +1,28 @@
+{ Source provided for Free Pascal Bug Report 3176 }
+{ Submitted by "Marcel Martin" on  2004-06-21 }
+{ e-mail: mm10@ellipsa.net }
+program test;
+{$MODE OBJFPC}
+{$GOTO ON}
+
+label DONE1;
+var i : Longint;
+begin
+  i := 0;
+  try
+    if i < 0 then goto DONE1;
+    i := 1;
+
+    //...
+
+DONE1:
+  finally
+    i := 2;
+  end;
+  if i<>2 then
+    halt(1);
+end.
+
+//DONE1:
+//    begin end; <- inserting this line fixes the problem
+//  finally
