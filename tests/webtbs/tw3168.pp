cd8d64e5110287c40feeae7ac25dcc4f8707a848
florian <florian@freepascal.org>
2004-08-08 12:36:47 +0000

    no message

---
+++ tests/webtbs/tw3168.pp
@@ -0,0 +1,15 @@
+{ Source provided for Free Pascal Bug Report 3168 }
+{ Submitted by "Henri Gourvest" on  2004-06-15 }
+{ e-mail: hgourvest@progdigy.com }
+program Untitled1;
+{ $MODE objfpc}
+type
+  TClazz = class
+  private
+    f: procedure(x: Integer); cdecl;
+  public
+    x: integer;
+  end;
+
+begin
+end.
