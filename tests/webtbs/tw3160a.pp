e966c1d79dc3375dcb4a39ff1bacc5ba817cedc1
florian <florian@freepascal.org>
2004-08-08 09:36:48 +0000

    no message

---
+++ tests/webtbs/tw3160a.pp
@@ -0,0 +1,16 @@
+{ %RESULT=207 }
+{ Source provided for Free Pascal Bug Report 3160 }
+{ Submitted by "Michalis Kamburelis" on  2004-06-12 }
+{ e-mail: michalis@camelot.homedns.org }
+var A:Extended;
+begin
+ { All lines below will raise runtime error 207
+   (Invalid floating point operation, converted to EInvalidOp exception)
+   under Win32.
+
+   But under Linux they raise runtime error 216
+   (General protection fault, converted to EAccessViolation). }
+ A:=0;   Writeln(0 / A);
+ A:=-3;  Writeln(Ln(A));
+ A:=-3;  Writeln(Sqrt(A));
+end.
