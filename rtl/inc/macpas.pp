c883d661b988df8713690a3ff475a825c75a3d8a
olle <olle>
2004-08-16 16:21:23 +0000

    + Added this file, to be used for stuff specific for mode macpas

---
+++ rtl/inc/macpas.pp
@@ -0,0 +1,27 @@
+{
+    $Id$
+    This file is part of the Free Pascal Run time library.
+    Copyright (c) 2004 by Olle Raab
+
+    This unit contain procedures specific for mode MacPas.
+    It should be platform independant.
+
+    See the file COPYING.FPC, included in this distribution,
+    For details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
+unit MacPas;
+
+interface
+
+type
+  LongDouble = ValReal;
+
+implementation
+
+end.
