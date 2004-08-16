b9d636fac713cc8f13d4cad74eec55d3fe96bc99
peter <peter@freepascal.org>
2004-08-16 16:29:49 +0000

    * fail added

--- tests/webtbf/tw3126.pp
+++ tests/webtbf/tw3126.pp
@@ -1,3 +1,5 @@
+{ %fail }
+
 { Source provided for Free Pascal Bug Report 3126 }
 { Submitted by "Christian Iversen" on  2004-05-30 }
 { e-mail: chrivers@iversen-net.dk }
--- tests/webtbf/tw3158.pp
+++ tests/webtbf/tw3158.pp
@@ -1,3 +1,5 @@
+{ %fail }
+
 { Source provided for Free Pascal Bug Report 3158 }
 { Submitted by "Michalis Kamburelis" on  2004-06-11 }
 { e-mail: michalis@camelot.homedns.org }
