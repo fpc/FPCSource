e23f68ca903b11a3764b446348df6e43c8b6b921
peter <peter@freepascal.org>
2004-10-31 18:52:45 +0000

    * updates from ik_5

--- packages/extra/x11/xi.pp
+++ packages/extra/x11/xi.pp
@@ -1,5 +1,5 @@
 {
- $Xorg: XI.h,v 1.4 2001/02/09 02:03:23 xorgcvs Exp $
+ $Xorg: XI.h,v 1.4 2001/02/09 02:03:23 xorgcvs Exp $ 
 
 ************************************************************
 
@@ -27,7 +27,7 @@ in this Software without prior written authorization from The Open Group.
 
 Copyright 1989 by Hewlett-Packard Company, Palo Alto, California.
 
-                        All Rights Reserved
+			All Rights Reserved
 
 Permission to use, copy, modify, and distribute this software and its
 documentation for any purpose and without fee is hereby granted,
@@ -46,15 +46,17 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 SOFTWARE.
 
 ********************************************************/
- $XFree86: xc/include/extensions/XI.h,v 1.5 2001/12/14 19:53:28 dawes Exp $
+ $XFree86: xc/include/extensions/XI.h,v 1.5 2001/12/14 19:53:28 dawes Exp $ 
 
  Definitions used by the server, library and client
-
-        Pascal Convertion was made by Ido Kannner - kanerido@actcom.net.il
-
+ 
+	Pascal Convertion was made by Ido Kannner - kanerido@actcom.net.il
+ 
 Histroy:
-        2004/10/07 - Removed the "uses X;" line. The unit does not need it.
-        2004/10/03 - Conversion from C header to Pascal unit.
+	2004/10/15 - Fixed a bug of accessing second based records by removing "paced record" and chnaged it to 
+	             "reocrd" only.
+	2004/10/07 - Removed the "uses X;" line. The unit does not need it.
+	2004/10/03 - Conversion from C header to Pascal unit.
 }
 {$PACKRECORDS C} {$MACRO ON} {$DEFINE MACROS}
 {$MODE OBJFPC}
@@ -62,234 +64,234 @@ unit xi;
 interface
 
 const
-        sz_xGetExtensionVersionReq           =  8;
-        sz_xGetExtensionVersionReply         = 32;
-        sz_xListInputDevicesReq              =  4;
-        sz_xListInputDevicesReply            = 32;
-        sz_xOpenDeviceReq                    =  8;
-        sz_xOpenDeviceReply                  = 32;
-        sz_xCloseDeviceReq                   =  8;
-        sz_xSetDeviceModeReq                 =  8;
-        sz_xSetDeviceModeReply               = 32;
-        sz_xSelectExtensionEventReq          = 12;
-        sz_xGetSelectedExtensionEventsReq    =  8;
-        sz_xGetSelectedExtensionEventsReply  = 32;
-        sz_xChangeDeviceDontPropagateListReq = 12;
-        sz_xGetDeviceDontPropagateListReq    =  8;
-        sz_xGetDeviceDontPropagateListReply  = 32;
-        sz_xGetDeviceMotionEventsReq         = 16;
-        sz_xGetDeviceMotionEventsReply       = 32;
-        sz_xChangeKeyboardDeviceReq          =  8;
-        sz_xChangeKeyboardDeviceReply        = 32;
-        sz_xChangePointerDeviceReq           =  8;
-        sz_xChangePointerDeviceReply         = 32;
-        sz_xGrabDeviceReq                    = 20;
-        sz_xGrabDeviceReply                  = 32;
-        sz_xUngrabDeviceReq                  = 12;
-        sz_xGrabDeviceKeyReq                 = 20;
-        sz_xGrabDeviceKeyReply               = 32;
-        sz_xUngrabDeviceKeyReq               = 16;
-        sz_xGrabDeviceButtonReq              = 20;
-        sz_xGrabDeviceButtonReply            = 32;
-        sz_xUngrabDeviceButtonReq            = 16;
-        sz_xAllowDeviceEventsReq             = 12;
-        sz_xGetDeviceFocusReq                =  8;
-        sz_xGetDeviceFocusReply              = 32;
-        sz_xSetDeviceFocusReq                = 16;
-        sz_xGetFeedbackControlReq            =  8;
-        sz_xGetFeedbackControlReply          = 32;
-        sz_xChangeFeedbackControlReq         = 12;
-        sz_xGetDeviceKeyMappingReq           =  8;
-        sz_xGetDeviceKeyMappingReply         = 32;
-        sz_xChangeDeviceKeyMappingReq        =  8;
-        sz_xGetDeviceModifierMappingReq      =  8;
-        sz_xSetDeviceModifierMappingReq      =  8;
-        sz_xSetDeviceModifierMappingReply    = 32;
-        sz_xGetDeviceButtonMappingReq        =  8;
-        sz_xGetDeviceButtonMappingReply      = 32;
-        sz_xSetDeviceButtonMappingReq        =  8;
-        sz_xSetDeviceButtonMappingReply      = 32;
-        sz_xQueryDeviceStateReq              =  8;
-        sz_xQueryDeviceStateReply            = 32;
-        sz_xSendExtensionEventReq            = 16;
-        sz_xDeviceBellReq                    =  8;
-        sz_xSetDeviceValuatorsReq            =  8;
-        sz_xSetDeviceValuatorsReply          = 32;
-        sz_xGetDeviceControlReq              =  8;
-        sz_xGetDeviceControlReply            = 32;
-        sz_xChangeDeviceControlReq           =  8;
-        sz_xChangeDeviceControlReply         = 32;
+	sz_xGetExtensionVersionReq           =  8;
+	sz_xGetExtensionVersionReply         = 32;
+	sz_xListInputDevicesReq              =  4;
+	sz_xListInputDevicesReply            = 32;
+	sz_xOpenDeviceReq                    =  8;
+	sz_xOpenDeviceReply                  = 32;
+	sz_xCloseDeviceReq                   =  8;
+	sz_xSetDeviceModeReq                 =  8;
+	sz_xSetDeviceModeReply               = 32;
+	sz_xSelectExtensionEventReq          = 12;
+	sz_xGetSelectedExtensionEventsReq    =  8;
+	sz_xGetSelectedExtensionEventsReply  = 32;
+	sz_xChangeDeviceDontPropagateListReq = 12;
+	sz_xGetDeviceDontPropagateListReq    =  8;
+	sz_xGetDeviceDontPropagateListReply  = 32;
+	sz_xGetDeviceMotionEventsReq         = 16;
+	sz_xGetDeviceMotionEventsReply       = 32;
+	sz_xChangeKeyboardDeviceReq          =  8;
+	sz_xChangeKeyboardDeviceReply        = 32;
+	sz_xChangePointerDeviceReq           =  8;
+	sz_xChangePointerDeviceReply         = 32;
+	sz_xGrabDeviceReq                    = 20;
+	sz_xGrabDeviceReply                  = 32;
+	sz_xUngrabDeviceReq                  = 12;
+	sz_xGrabDeviceKeyReq                 = 20;
+	sz_xGrabDeviceKeyReply               = 32;
+	sz_xUngrabDeviceKeyReq               = 16;
+	sz_xGrabDeviceButtonReq              = 20;
+	sz_xGrabDeviceButtonReply            = 32;
+	sz_xUngrabDeviceButtonReq            = 16;
+	sz_xAllowDeviceEventsReq             = 12;
+	sz_xGetDeviceFocusReq                =  8;
+	sz_xGetDeviceFocusReply              = 32;
+	sz_xSetDeviceFocusReq                = 16;
+	sz_xGetFeedbackControlReq            =  8;
+	sz_xGetFeedbackControlReply          = 32;
+	sz_xChangeFeedbackControlReq         = 12;
+	sz_xGetDeviceKeyMappingReq           =  8;
+	sz_xGetDeviceKeyMappingReply         = 32;
+	sz_xChangeDeviceKeyMappingReq        =  8;
+	sz_xGetDeviceModifierMappingReq      =  8;
+	sz_xSetDeviceModifierMappingReq      =  8;
+	sz_xSetDeviceModifierMappingReply    = 32;
+	sz_xGetDeviceButtonMappingReq        =  8;
+	sz_xGetDeviceButtonMappingReply      = 32;
+	sz_xSetDeviceButtonMappingReq        =  8;
+	sz_xSetDeviceButtonMappingReply      = 32;
+	sz_xQueryDeviceStateReq              =  8;
+	sz_xQueryDeviceStateReply            = 32;
+	sz_xSendExtensionEventReq            = 16;
+	sz_xDeviceBellReq                    =  8;
+	sz_xSetDeviceValuatorsReq            =  8;
+	sz_xSetDeviceValuatorsReply          = 32;
+	sz_xGetDeviceControlReq              =  8;
+	sz_xGetDeviceControlReply            = 32;
+	sz_xChangeDeviceControlReq           =  8;
+	sz_xChangeDeviceControlReply         = 32;
 
 const
-        INAME  = 'XInputExtension';
+	INAME  = 'XInputExtension';
 
 const
-        XI_KEYBOARD    = 'KEYBOARD';
-        XI_MOUSE       = 'MOUSE';
-        XI_TABLET      = 'TABLET';
-        XI_TOUCHSCREEN = 'TOUCHSCREEN';
-        XI_TOUCHPAD    = 'TOUCHPAD';
-        XI_BARCODE     = 'BARCODE';
-        XI_BUTTONBOX   = 'BUTTONBOX';
-        XI_KNOB_BOX    = 'KNOB_BOX';
-        XI_ONE_KNOB    = 'ONE_KNOB';
-        XI_NINE_KNOB   = 'NINE_KNOB';
-        XI_TRACKBALL   = 'TRACKBALL';
-        XI_QUADRATURE  = 'QUADRATURE';
-        XI_ID_MODULE   = 'ID_MODULE';
-        XI_SPACEBALL   = 'SPACEBALL';
-        XI_DATAGLOVE   = 'DATAGLOVE';
-        XI_EYETRACKER  = 'EYETRACKER';
-        XI_CURSORKEYS  = 'CURSORKEYS';
-        XI_FOOTMOUSE   = 'FOOTMOUSE';
+	XI_KEYBOARD    = 'KEYBOARD';
+	XI_MOUSE       = 'MOUSE';
+	XI_TABLET      = 'TABLET';
+	XI_TOUCHSCREEN = 'TOUCHSCREEN';
+	XI_TOUCHPAD    = 'TOUCHPAD';
+	XI_BARCODE     = 'BARCODE';
+	XI_BUTTONBOX   = 'BUTTONBOX';
+	XI_KNOB_BOX    = 'KNOB_BOX';
+	XI_ONE_KNOB    = 'ONE_KNOB';
+	XI_NINE_KNOB   = 'NINE_KNOB';
+	XI_TRACKBALL   = 'TRACKBALL';
+	XI_QUADRATURE  = 'QUADRATURE';
+	XI_ID_MODULE   = 'ID_MODULE';
+	XI_SPACEBALL   = 'SPACEBALL';
+	XI_DATAGLOVE   = 'DATAGLOVE';
+	XI_EYETRACKER  = 'EYETRACKER';
+	XI_CURSORKEYS  = 'CURSORKEYS';
+	XI_FOOTMOUSE   = 'FOOTMOUSE';
 
 const
-        Dont_Check                      = 0;
-        XInput_Initial_Release          = 1;
-        XInput_Add_XDeviceBell          = 2;
-        XInput_Add_XSetDeviceValuators  = 3;
-        XInput_Add_XChangeDeviceControl = 4;
+	Dont_Check                      = 0;
+	XInput_Initial_Release          = 1;
+	XInput_Add_XDeviceBell          = 2;
+	XInput_Add_XSetDeviceValuators  = 3;
+	XInput_Add_XChangeDeviceControl = 4;
 
 const
-        XI_Absent  = 0;
-        XI_Present = 1;
+	XI_Absent  = 0;
+	XI_Present = 1;
 
 const
-        XI_Initial_Release_Major = 1;
-        XI_Initial_Release_Minor = 0;
+	XI_Initial_Release_Major = 1;
+	XI_Initial_Release_Minor = 0;
 
 const
-        XI_Add_XDeviceBell_Major = 1;
-        XI_Add_XDeviceBell_Minor = 1;
+	XI_Add_XDeviceBell_Major = 1;
+	XI_Add_XDeviceBell_Minor = 1;
 
 const
-        XI_Add_XSetDeviceValuators_Major = 1;
-        XI_Add_XSetDeviceValuators_Minor = 2;
+	XI_Add_XSetDeviceValuators_Major = 1;
+	XI_Add_XSetDeviceValuators_Minor = 2;
 
 const
-        XI_Add_XChangeDeviceControl_Major = 1;
-        XI_Add_XChangeDeviceControl_Minor = 3;
+	XI_Add_XChangeDeviceControl_Major = 1;
+	XI_Add_XChangeDeviceControl_Minor = 3;
 
 const
-        DEVICE_RESOLUTION = 1;
+	DEVICE_RESOLUTION = 1;
 
 const
-        NoSuchExtension = 1;
+	NoSuchExtension = 1;
 
 const
-        COUNT  = 0;
-        CREATE = 1;
+	COUNT  = 0;
+	CREATE = 1;
 
 const
-        NewPointer  = 0;
-        NewKeyboard = 1;
+	NewPointer  = 0;
+	NewKeyboard = 1;
 
 const
-        XPOINTER  = 0;
-        XKEYBOARD = 1;
+	XPOINTER  = 0;
+	XKEYBOARD = 1;
 
 const
-        UseXKeyboard = $FF;
+	UseXKeyboard = $FF;
 
 const
-        IsXPointer         = 0;
-        IsXKeyboard        = 1;
-        IsXExtensionDevice = 2;
+	IsXPointer         = 0;
+	IsXKeyboard        = 1;
+	IsXExtensionDevice = 2;
 
 const
-        AsyncThisDevice   = 0;
-        SyncThisDevice    = 1;
-        ReplayThisDevice  = 2;
-        AsyncOtherDevices = 3;
-        AsyncAll          = 4;
-        SyncAll           = 5;
+	AsyncThisDevice   = 0;
+	SyncThisDevice    = 1;
+	ReplayThisDevice  = 2;
+	AsyncOtherDevices = 3;
+	AsyncAll          = 4;
+	SyncAll           = 5;
 
 const
-        FollowKeyboard = 3;
+	FollowKeyboard = 3;
 {$ifndef RevertToFollowKeyboard}
-        {$IFDEF MACROS}
-                {$define RevertToFollowKeyboard := 3}
-        {$ELSE}
-                RevertToFollowKeyboard = 3;
-        {$ENDIF}
+	{$IFDEF MACROS}
+		{$define RevertToFollowKeyboard := 3}
+	{$ELSE}
+		RevertToFollowKeyboard = 3;
+	{$ENDIF}
 {$ENDIF}
 
 const
-        DvAccelNum   = Cardinal(1) shl 0;
-        DvAccelDenom = Cardinal(1) shl 1;
-        DvThreshold  = Cardinal(1) shl 2;
+	DvAccelNum   = Cardinal(1) shl 0;
+	DvAccelDenom = Cardinal(1) shl 1;
+	DvThreshold  = Cardinal(1) shl 2;
 
 const
-        DvKeyClickPercent = Cardinal(1) shl 0;
-        DvPercent         = Cardinal(1) shl 1;
-        DvPitch           = Cardinal(1) shl 2;
-        DvDuration        = Cardinal(1) shl 3;
-        DvLed             = Cardinal(1) shl 4;
-        DvLedMode         = Cardinal(1) shl 5;
-        DvKey             = Cardinal(1) shl 6;
-        DvAutoRepeatMode  = Cardinal(1) shl 7;
+	DvKeyClickPercent = Cardinal(1) shl 0;
+	DvPercent         = Cardinal(1) shl 1;
+	DvPitch           = Cardinal(1) shl 2;
+	DvDuration        = Cardinal(1) shl 3;
+	DvLed             = Cardinal(1) shl 4;
+	DvLedMode         = Cardinal(1) shl 5;
+	DvKey             = Cardinal(1) shl 6;
+	DvAutoRepeatMode  = Cardinal(1) shl 7;
 
 const
-        DvString = Cardinal(1) shl 0;
+	DvString = Cardinal(1) shl 0;
 
 const
-        DvInteger = Cardinal(1) shl 0;
+	DvInteger = Cardinal(1) shl 0;
 
 const
-        DeviceMode         = Cardinal(1) shl 0;
-        Relative           =  0;
-        Absolute           =  1;
+	DeviceMode         = Cardinal(1) shl 0;
+	Relative           =  0;
+	Absolute           =  1;
 { Merged from Metrolink tree for XINPUT stuff }
-        TS_Raw             = 57;
-        TS_Scaled          = 58;
-        SendCoreEvents     = 59;
-        DontSendCoreEvents = 60;
-{ End of merged section }
+	TS_Raw             = 57;
+	TS_Scaled          = 58;
+	SendCoreEvents     = 59;
+	DontSendCoreEvents = 60;
+{ End of merged section	}
 
 const
-        ProximityState = Cardinal(1) shl 1;
-        InProximity    = Cardinal(0) shl 1;
-        OutOfProximity = Cardinal(1) shl 1;
+	ProximityState = Cardinal(1) shl 1;
+	InProximity    = Cardinal(0) shl 1;
+	OutOfProximity = Cardinal(1) shl 1;
 
 const
-        AddToList      = 0;
-        DeleteFromList = 1;
+	AddToList      = 0;
+	DeleteFromList = 1;
 
 const
-        KeyClass       = 0;
-        ButtonClass    = 1;
-        ValuatorClass  = 2;
-        FeedbackClass  = 3;
-        ProximityClass = 4;
-        FocusClass     = 5;
-        OtherClass     = 6;
+	KeyClass       = 0;
+	ButtonClass    = 1;
+	ValuatorClass  = 2;
+	FeedbackClass  = 3;
+	ProximityClass = 4;
+	FocusClass     = 5;
+	OtherClass     = 6;
 
 const
-        KbdFeedbackClass     = 0;
-        PtrFeedbackClass     = 1;
-        StringFeedbackClass  = 2;
-        IntegerFeedbackClass = 3;
-        LedFeedbackClass     = 4;
-        BellFeedbackClass    = 5;
+	KbdFeedbackClass     = 0;
+	PtrFeedbackClass     = 1;
+	StringFeedbackClass  = 2;
+	IntegerFeedbackClass = 3;
+	LedFeedbackClass     = 4;
+	BellFeedbackClass    = 5;
 
 const
-        _devicePointerMotionHint = 0;
-        _deviceButton1Motion     = 1;
-        _deviceButton2Motion     = 2;
-        _deviceButton3Motion     = 3;
-        _deviceButton4Motion     = 4;
-        _deviceButton5Motion     = 5;
-        _deviceButtonMotion      = 6;
-        _deviceButtonGrab        = 7;
-        _deviceOwnerGrabButton   = 8;
-        _noExtensionEvent        = 9;
+	_devicePointerMotionHint = 0;
+	_deviceButton1Motion     = 1;
+	_deviceButton2Motion     = 2;
+	_deviceButton3Motion     = 3;
+	_deviceButton4Motion     = 4;
+	_deviceButton5Motion     = 5;
+	_deviceButtonMotion      = 6;
+	_deviceButtonGrab        = 7;
+	_deviceOwnerGrabButton   = 8;
+	_noExtensionEvent        = 9;
 
 const
-        XI_BadDevice  = 0;
-        XI_BadEvent   = 1;
-        XI_BadMode    = 2;
-        XI_DeviceBusy = 3;
-        XI_BadClass   = 4;
+	XI_BadDevice  = 0;
+	XI_BadEvent   = 1;
+	XI_BadMode    = 2;
+	XI_DeviceBusy = 3;
+	XI_BadClass   = 4;
 
 { Make XEventClass be a CARD32 for 64 bit servers.  Don't affect client
   definition of XEventClass since that would be a library interface change.
@@ -297,9 +299,9 @@ const
 }
 type
 {$ifdef _XSERVER64}
-        XEventClass = CARD32;
+	XEventClass = CARD32;
 {$ELSE}
-        XEventClass = Longword;
+	XEventClass = Longword;
 {$ENDIF}
 
 (*******************************************************************
@@ -309,12 +311,12 @@ type
  *)
 
 type
-        PXExtensionVersion = ^TXExtensionVersion;
-        TXExtensionVersion = packed record
-                              present       : Smallint;
-                              major_version : Smallint;
-                              minor_version : Smallint;
-                             end;
+	PXExtensionVersion = ^TXExtensionVersion;
+	TXExtensionVersion = record
+	                      present       : Smallint;
+	                      major_version : Smallint;
+	                      minor_version : Smallint;
+	                     end;
 
 implementation
 
