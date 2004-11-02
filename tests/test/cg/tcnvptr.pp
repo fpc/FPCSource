a9163ca6494d9cd1f675261e0373317273d5b46a
peter <peter@freepascal.org>
2004-11-02 12:26:19 +0000

    * inc(ptr) should not give warnings

---
+++ tests/test/cg/tcnvptr.pp
@@ -0,0 +1,15 @@
+{ %opt=-Sew }
+
+var
+  p : pointer;
+  b : byte;
+  w : word;
+  a : array[0..10] of char;
+begin
+  b:=1;
+  w:=1;
+  p:=@a;
+  // This should work without warnings
+  inc(p,b);
+  inc(p,w);
+end.
