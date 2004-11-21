a827c692e264a79c3e5cf11b3539b99e7ea39b4a
peter <peter@freepascal.org>
2004-11-21 21:32:01 +0000

    * new bugs

---
+++ tests/webtbs/tw3131.pp
@@ -0,0 +1,19 @@
+{ Source provided for Free Pascal Bug Report 3131 }
+{ Submitted by "Arnstein Prytz" on  2004-06-02 }
+{ e-mail: Arnstein.Prytz@jcu.edu.au }
+program tmp;
+
+procedure l;
+  label l1;
+  begin
+    l1 : WRITELN( 'Label L1' );
+  end;
+
+procedure a; assembler;
+  asm
+    @_quit : ;
+  end;
+
+begin
+end.
+
---
+++ tests/webtbs/tw3393.pp
@@ -0,0 +1,12 @@
+{ Source provided for Free Pascal Bug Report 3393 }
+{ Submitted by "David Emerson" on  2004-11-20 }
+{ e-mail: demerson3p@angelbase.com }
+var
+  array_of_longint : array [0..5] of longint;
+  longint_file : file of longint;
+
+begin
+  for i := 0 to 5 do
+    write (longint_file, array_of_longint[i]);
+end.
+
