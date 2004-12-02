15e2b2167192e9f3626d1087e09249bf411afea6
peter <peter@freepascal.org>
2004-12-02 17:51:40 +0000

    * crash with empty strigns

---
+++ tests/webtbs/tw3423.pp
@@ -0,0 +1,17 @@
+{ Source provided for Free Pascal Bug Report 3423 }
+{ Submitted by "Bram Kuijvenhoven" on  2004-12-02 }
+{ e-mail: kuifware@hotmail.com }
+{$MODE OBJFPC}{$H+}
+
+var
+  a:ansistring;
+  s:shortstring;
+
+begin
+
+  a:='';
+  s:='';
+  a+=s;
+
+end.
+
