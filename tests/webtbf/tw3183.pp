164acd59d20b162e83f2cb09b9a8efb1ad2431f1
florian <florian@freepascal.org>
2004-12-05 22:28:59 +0000

    no message

---
+++ tests/webtbf/tw3183.pp
@@ -0,0 +1,22 @@
+{$ifdef fpc}{$mode delphi}{$endif}
+
+type
+  IA=interface
+   function copy:String;
+  end;
+
+  IB=interface(IA)
+   function copy:integer;
+  end;
+
+  to1 = class(tinterfacedobject,ia,ib)
+    function copy:string;
+  end;
+
+function to1.copy:string;
+  begin
+  end;
+
+begin
+end.
+
