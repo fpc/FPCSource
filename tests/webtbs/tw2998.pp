addb9693551ae59c1f60834825bb2f4aa5243093
florian <florian@freepascal.org>
2004-12-12 00:31:06 +0000

    no message

---
+++ tests/webtbs/tw2998.pp
@@ -0,0 +1,22 @@
+{ %cpu=i386 }
+
+{ Source provided for Free Pascal Bug Report 2998 }
+{ Submitted by "bartek" on  2004-03-02 }
+{ e-mail: bbartek@gmx.net }
+
+{$asmmode intel}
+program SSE_test;
+uses
+  mmx;
+type
+        vector4 = array[0..3] of single;
+var
+        a,b,c :vector4;
+begin
+  if is_sse_cpu then
+    asm
+      movups xmm0, [a]
+      addps xmm0,[b]
+      movups [c], xmm0
+    end;
+end.
