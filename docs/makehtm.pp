23c3b52d7f453372f826c7e710a4da58651d199f
michael <michael@freepascal.org>
2004-12-14 21:28:51 +0000

    + Added header

--- docs/makehtm.pp
+++ docs/makehtm.pp
@@ -1,3 +1,18 @@
+{
+    $Id$
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 1999-2000 by the Free Pascal development team
+
+    Convert .html to htm files, together with links.
+    
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
 {$mode objfpc}
 {$h+}
 program makehtm;
