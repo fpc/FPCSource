855ccb7ac8135ee44b6692948b8616b6a0c7cc99
peter <peter@freepascal.org>
2004-12-22 16:48:33 +0000

    * remove wrong dword ptr

---
+++ tests/webtbs/tw3460.pp
@@ -0,0 +1,34 @@
+{ %cpu=i386,x86_64 }
+
+{ Source provided for Free Pascal Bug Report 3460 }
+{ Submitted by "Danny Milosavljevic" on  2004-12-22 }
+{ e-mail: danny_milo@yahoo.com }
+
+{$ifdef fpc}{$mode delphi}{$endif}
+
+type
+  TGValue=array[0..10] of byte;
+  PGValue=^TGValue;
+  
+function x: Integer;
+asm
+  mov edx,0
+  add edx, sizeof(Integer)
+  mov Result, edx
+end;
+
+function SignalHandlerNextParam(param: PGValue): PGValue;
+asm
+  mov edx, param
+  add edx, sizeof(TGValue)  
+  mov Result, edx    
+end;
+
+var
+  a : array[0..10] of TGValue;
+begin
+  SignalHandlerNextParam(@a[0]);
+  writeln(x);
+  if x<>sizeof(integer) then
+    halt(1);
+end.
