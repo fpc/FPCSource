c30d55ce5eee87fb3be3e279197fba406931050a
peter <peter@freepascal.org>
2004-12-26 16:28:03 +0000

    * new bug

---
+++ tests/webtbf/tw3480.pp
@@ -0,0 +1,21 @@
+{ %fail }
+
+{ Source provided for Free Pascal Bug Report 3480 }
+{ Submitted by "Danny Milosavljevic" on  2004-12-26 }
+{ e-mail: danny_milo@yahoo.com }
+program z;
+
+type
+  IBla = interface
+  class function X: Boolean;
+  end;
+
+  TBla = class(TInterfacedObject, IBla, IInterface)
+  public
+  class function X: Boolean;
+  end;
+
+begin
+  TBla.X;
+end.
+
