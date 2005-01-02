c70c501571da4f6270009f6baabee484b8e5439b
peter <peter@freepascal.org>
2005-01-02 17:33:15 +0000

    new bug

---
+++ tests/webtbs/tw3490.pp
@@ -0,0 +1,14 @@
+{ %OPT=-Sew }
+
+{ Source provided for Free Pascal Bug Report 3490 }
+{ Submitted by "Tomas Hajny" on  2004-12-29 }
+{ e-mail:  }
+var
+ A: byte;
+ B: cardinal;
+
+begin
+ A := 3;
+ B := 0;
+ B := B + A * A;
+end.
