afd6156f08333138fec36f4333eb791bd32f5629
florian <florian@freepascal.org>
2005-01-07 21:16:48 +0000

    no message

---
+++ tests/webtbs/tw3494.pp
@@ -0,0 +1,18 @@
+{ Source provided for Free Pascal Bug Report 3494 }
+{ Submitted by "Danny Milosavljevic" on  2004-12-31 }
+{ e-mail: danny_milo@yahoo.com }
+program ivar;
+uses variants;
+
+type
+  ti = interface(iinterface)
+  end;
+
+var
+  v: Variant;
+  i: ti;
+begin
+  i := nil;
+  v := i;
+  i := v;
+end.
