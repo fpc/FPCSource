3a9740ab1248680cae3f7c22ff606cdd4c0d6de4
peter <peter@freepascal.org>
2005-01-16 10:50:08 +0000

    * unreachable code warning

---
+++ tests/webtbf/tw3553.pp
@@ -0,0 +1,23 @@
+{ %fail }
+{ %opt=-Sew }
+
+{ Source provided for Free Pascal Bug Report 3553 }
+{ Submitted by "Naj Kejah" on  2005-01-15 }
+{ e-mail: universario@hotmail.com }
+
+var K : integer;
+
+BEGIN
+  while FALSE do
+    K:=2;{ NO warning "unreachable code" from User.txt  }
+
+  if FALSE then
+    K:=2
+  else
+    K:=3;
+
+  if TRUE then
+    K:=2
+  else
+    K:=3;
+END.
