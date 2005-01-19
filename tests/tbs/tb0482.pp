97bbacd0a8a0e29e1c7787ac7f25982db6a54834
florian <florian@freepascal.org>
2005-01-19 23:33:13 +0000

    no message

--- tests/tbs/tb0482.pp
+++ tests/tbs/tb0482.pp
@@ -5,5 +5,18 @@ uses
 resourcestring sMyNewErrorMessage = 'Illegal value: %d';
 
 begin
-  raise Exception.CreateResFmt(@sMyNewErrorMessage, [-1]);
+  try
+    raise Exception.CreateResFmt(@sMyNewErrorMessage, [-1]);
+  except
+    on e : exception do
+      begin
+        if e.message='Illegal value: -1' then
+          halt(0)
+        else
+          begin
+            writeln('error : ',e.message);
+            halt(1);
+          end;
+      end;
+  end;
 end.
