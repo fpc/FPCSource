4c59b499fab884dcc561ddc89d4e149d1b3eac56
florian <florian@freepascal.org>
2005-01-19 20:03:50 +0000

    no message

---
+++ tests/webtbs/tw3576.pp
@@ -0,0 +1,14 @@
+{ Source provided for Free Pascal Bug Report 3576 }
+{ Submitted by "Simon Kissel" on  2005-01-19 }
+{ e-mail: scamp@untergrund.net }
+
+{$mode delphi}
+{$asmmode intel}
+
+function Q_CompStr(const S1, S2: string): Integer;assembler;
+asm
+end;
+
+begin
+  Q_CompStr('','');
+end.
