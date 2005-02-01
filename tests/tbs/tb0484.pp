cc7220d3db4e0409030cb5786877ef8e629ec558
florian <florian@freepascal.org>
2005-02-01 23:17:46 +0000

    no message

--- tests/tbs/tb0484.pp
+++ tests/tbs/tb0484.pp
@@ -1,7 +1,20 @@
 type
-  r = record
+  r1 = record
     p : procedure stdcall;
     i : longint;
+  end;
+
+  r2 = record
+    p : procedure;
+    i : longint;
+  end;
+
+  r3 = record
+    p : procedure
+  end;
+
+  r4 = record
+    p : procedure stdcall
   end;
 
 begin
