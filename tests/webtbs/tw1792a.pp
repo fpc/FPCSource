2ccc7d566c782f0d734df6deb565978c97c17ba6
florian <florian@freepascal.org>
2005-02-08 20:24:52 +0000

    *** empty log message ***

--- tests/webtbs/tw1792a.pp
+++ tests/webtbs/tw1792a.pp
@@ -1,4 +1,23 @@
-{ %RESULT=207 }
+uses
+  math;
+var
+  s : string;
 Begin
- WriteLn(power(2,63));
+{$ifdef FPC_HAS_TYPE_EXTENDED}
+ str(intpower(2,63),s);
+ if s<>' 9.2233720368547758E+0018' then
+   begin
+     WriteLn(intpower(2,63));
+     halt(1);
+   end;
+{$endif FPC_HAS_TYPE_EXTENDED}
+
+{$ifdef FPC_HAS_TYPE_DOUBLE}
+ str(double(intpower(2,63)),s);
+ if s<>' 9.223372036854776E+018' then
+   begin
+     WriteLn(double(intpower(2,63)));
+     halt(1);
+   end;
+{$endif FPC_HAS_TYPE_DOUBLE}
 end.
