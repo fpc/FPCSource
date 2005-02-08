72f24474fb77000409e095a01e562e12c6bdc365
olle <olle>
2005-02-08 22:30:32 +0000

    + added SIZE resource facilities

--- compiler/systems/t_macos.pas
+++ compiler/systems/t_macos.pas
@@ -109,7 +109,7 @@ end;
 Function TLinkerMPW.WriteResponseFile(isdll:boolean) : Boolean;
 Var
   linkres      : TLinkRes;
-  s: string;
+  s,heapsizestr: string;
 
 begin
   WriteResponseFile:=False;
@@ -154,10 +154,24 @@ begin
 
       Add('Exit If "{Status}" != 0');
 
+      if heapsize = 0 then
+        heapsizestr:= HexStr(384000, 8)
+      else
+        heapsizestr:= HexStr(heapsize, 8);
+
+      {Add a SIZE resource on the fly. It controls:
+         * backgrounding is enabled, to facilitate debuging with Power Mac Debugger
+         * it is signaled it is a 32 bit app. (perhaps not nessecary on PowerPC)
+         * heapsize  }
+      if apptype <> app_tool then
+        Add('Echo "data ''SIZE'' (-1) '#182'{ $'#182'"1080 ' + heapsizestr + ' ' + heapsizestr + 
+                                         #182'" '#182'};" | Rez -a -o ' + ScriptFixFileName(current_module.exefilename^));
+      Add('Exit If "{Status}" != 0');
+
       {Add mac resources}
       if apptype = app_cui then
         begin
-          Add('Rez -append "{RIncludes}"SIOW.r -o ' + ScriptFixFileName(current_module.exefilename^));
+          Add('Rez -a "{RIncludes}"SIOW.r -o ' + ScriptFixFileName(current_module.exefilename^));
           Add('Exit If "{Status}" != 0');
         end;
 
@@ -165,9 +179,9 @@ begin
         begin
           s := Current_module.ResourceFiles.GetFirst;
           if Copy(s,Length(s)-1,Length(s)) = '.r' then
-            Add('Rez -append ' + s + ' -o ' + ScriptFixFileName(current_module.exefilename^))
+            Add('Rez -a ' + s + ' -o ' + ScriptFixFileName(current_module.exefilename^))
           else
-            Add('DeRez ' + s + ' | Rez -append -o ' + ScriptFixFileName(current_module.exefilename^));
+            Add('DeRez ' + s + ' | Rez -a -o ' + ScriptFixFileName(current_module.exefilename^));
           Add('Exit If "{Status}" != 0');
         end;
 
@@ -258,7 +272,10 @@ initialization
 end.
 {
   $Log$
-  Revision 1.19  2005-01-24 17:53:12  olle
+  Revision 1.20  2005-02-08 22:30:32  olle
+    + added SIZE resource facilities
+
+  Revision 1.19  2005/01/24 17:53:12  olle
     + Mac style resource files can now be included in MacOS
 
   Revision 1.18  2005/01/09 16:35:41  olle
