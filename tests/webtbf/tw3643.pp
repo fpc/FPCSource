7c944ee49e6a16890a60507c942434d2cf8c3780
peter <peter@freepascal.org>
2005-02-11 15:39:08 +0000

    * new bugs

---
+++ tests/webtbf/tw3643.pp
@@ -0,0 +1,20 @@
+{ %fail }
+{ %opt=-Sew }
+
+{ Source provided for Free Pascal Bug Report 3643 }
+{ Submitted by "Naj Kejah" on  2005-02-08 }
+{ e-mail: universario@hotmail.com }
+type
+  recDep = record
+    cuec, cuek: byte
+  end;
+var
+  Dep1Rec : recDep;
+BEGIN
+  WITH Dep1Rec DO
+    begin
+      if cuek = 4 then ;
+      if cuek = 4 then ;
+    end;
+END.
+
--- tests/webtbs/tw3628.pp
+++ tests/webtbs/tw3628.pp
@@ -1,11 +1,17 @@
 { Source provided for Free Pascal Bug Report 3628 }
 { Submitted by "rimga" on  2005-02-03 }
 { e-mail: rimga@ktl.mii.lt }
+{$ifdef fpc}
 {$mode delphi}
+{$else}
+type
+  ptrint = longint;
+{$endif}  
 var
   s: string='12223445';
 begin
-  s:=string(@s[3]); //internal error 200410231
-  WriteLn(ptrint(@s[3]));   //runtime error 203
+  s:=string(@s[3]);
+  WriteLn(ptrint(@s));
+  WriteLn(ptrint(@s[3]));
 end.
 
