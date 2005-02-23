627420ae566ac50d132d6b1053b3a5de54e50241
florian <florian@freepascal.org>
2005-02-23 19:18:23 +0000

    no message

---
+++ tests/tbs/tb0486.pp
@@ -0,0 +1,32 @@
+{ $mode delphi}
+type
+  tprocedure = procedure;
+  pprocedure = ^tprocedure;
+
+var
+  l : longint;
+
+function _f1 : plongint;
+  begin
+    result:=@l;
+  end;
+
+var
+  f1 : function : plongint;
+  f2 : function : pprocedure;
+
+procedure p;
+  begin
+    l:=2;
+  end;
+
+begin
+  f1^:=1;
+  if l<>1 then
+    halt(1);
+  f2^:=p;
+  f2^;
+  if l<>2 then
+    halt(1);
+  writeln('ok');
+end.
