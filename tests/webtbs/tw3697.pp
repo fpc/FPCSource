c55870ebb53da91c242a0685cebc5fdde02ec0a8
florian <florian@freepascal.org>
2005-02-26 10:20:39 +0000

    no message

---
+++ tests/webtbs/tw3697.pp
@@ -0,0 +1,16 @@
+{ Source provided for Free Pascal Bug Report 3697 }
+{ Submitted by "Matthias Hryniszak" on  2005-02-26 }
+{ e-mail: matthias@hryniszak.de }
+uses
+  SysUtils;
+
+var
+  S: WideString;
+
+begin
+  S := WideFormat('Test %s', ['string']);
+  if s<>'Test string' then
+    halt(1);
+  
+  writeln('ok');
+end.
