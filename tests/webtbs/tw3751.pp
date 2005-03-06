d72af08d6ac73389c3f81d854e1b091b36d64d27
florian <florian@freepascal.org>
2005-03-06 15:22:57 +0000

    *** empty log message ***

---
+++ tests/webtbs/tw3751.pp
@@ -0,0 +1,32 @@
+{ Source provided for Free Pascal Bug Report 3751 }
+{ Submitted by "Nicola Lugato" on  2005-03-05 }
+{ e-mail: msx_80@hotmail.com }
+{$mode delphi}
+program turn;
+
+uses cthreads, classes, sysutils;
+
+type
+
+Tmy = class(TThread)
+constructor Create(suspended:boolean);
+Procedure Execute;override;
+end;
+
+constructor TMy.Create(suspended:boolean);
+begin
+FreeOnTerminate:=true;
+inherited Create(suspended);
+end;
+
+Procedure TMy.Execute;
+begin
+writeln('Just print and exit!');
+end;
+
+var m:TMy;
+begin
+m:=Tmy.create(true);
+m.resume;
+sleep(1000);
+end.
