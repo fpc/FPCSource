97d4723f454b8ee378195ea024ab37a588663244
florian <florian@freepascal.org>
2005-03-06 21:23:37 +0000

    no message

--- tests/webtbs/tw3470.pp
+++ tests/webtbs/tw3470.pp
@@ -1,6 +1,8 @@
 { Source provided for Free Pascal Bug Report 3470 }
 { Submitted by "Danny Milosavljevic" on  2004-12-25 }
 { e-mail: danny_milo@yahoo.com }
+
+{$mode objfpc}
 program a;
 uses variants;
 
