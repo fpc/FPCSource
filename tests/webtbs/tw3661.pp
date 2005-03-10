ca10eecc47b7b10277d734b17e8dd31dda7bf4be
florian <florian@freepascal.org>
2005-03-10 20:37:29 +0000

    no message

---
+++ tests/webtbs/tw3661.pp
@@ -0,0 +1,14 @@
+{ %OPT=-glhc }
+{ %TARGET=win32,linux,go32v2 }
+{ Source provided for Free Pascal Bug Report 3661 }
+{ Submitted by "Martin Schreiber" on  2005-02-16 }
+{ e-mail:  }
+program project1;
+
+{$mode objfpc}{$H+}
+uses
+  Classes;
+
+begin
+ writeln('abc');
+end.
