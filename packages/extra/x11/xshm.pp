da3929ed16586681ba6c029310f2a6b0dfac505e
florian <florian@freepascal.org>
2005-03-13 09:34:08 +0000

    * ctypes patch from Nikolay Nikolov

--- packages/extra/x11/xshm.pp
+++ packages/extra/x11/xshm.pp
@@ -1,7 +1,7 @@
 unit xshm;
 interface
 uses
-  x,xlib;
+  ctypes,x,xlib;
 
 {$ifndef os2}
   {$LinkLib c}
@@ -41,45 +41,48 @@ const
 type
 
    PShmSeg = ^TShmSeg;
-   TShmSeg = dword;
+   TShmSeg = culong;
 
    PXShmCompletionEvent = ^TXShmCompletionEvent;
    TXShmCompletionEvent = record
-        _type : longint;
-        serial : dword;
+        _type : cint;
+        serial : culong;
         send_event : TBool;
         display : PDisplay;
         drawable : TDrawable;
-        major_code : longint;
-        minor_code : longint;
+        major_code : cint;
+        minor_code : cint;
         shmseg : TShmSeg;
-        offset : dword;
+        offset : culong;
      end;
 
    PXShmSegmentInfo = ^TXShmSegmentInfo;
    TXShmSegmentInfo = record
         shmseg : TShmSeg;
-        shmid : longint;
+        shmid : cint;
         shmaddr : Pchar;
         readOnly : TBool;
      end;
 
 function XShmQueryExtension(para1:PDisplay):TBool;cdecl;external libX11;
-function XShmQueryVersion(para1:PDisplay; para2:Plongint; para3:Plongint; para4:PBool):TBool;cdecl;external libX11;
-function XShmPixmapFormat(para1:PDisplay):longint;cdecl;external libX11;
+function XShmGetEventBase(para1:PDisplay):cint;cdecl;external libX11;
+function XShmQueryVersion(para1:PDisplay; para2:Pcint; para3:Pcint; para4:PBool):TBool;cdecl;external libX11;
+function XShmPixmapFormat(para1:PDisplay):cint;cdecl;external libX11;
 function XShmAttach(para1:PDisplay; para2:PXShmSegmentInfo):TStatus;cdecl;external libX11;
 function XShmDetach(para1:PDisplay; para2:PXShmSegmentInfo):TStatus;cdecl;external libX11;
-function XShmPutImage(para1:PDisplay; para2:TDrawable; para3:TGC; para4:PXImage; para5:longint;
-           para6:longint; para7:longint; para8:longint; para9:dword; para10:dword;
+function XShmPutImage(para1:PDisplay; para2:TDrawable; para3:TGC; para4:PXImage; para5:cint;
+           para6:cint; para7:cint; para8:cint; para9:cuint; para10:cuint;
            para11:TBool):TStatus;cdecl;external libX11;
-function XShmGetImage(para1:PDisplay; para2:TDrawable; para3:PXImage; para4:longint; para5:longint;
-           para6:dword):TStatus;cdecl;external libX11;
-function XShmCreateImage(para1:PDisplay; para2:PVisual; para3:dword; para4:longint; para5:Pchar;
-           para6:PXShmSegmentInfo; para7:dword; para8:dword):PXImage;cdecl;external libX11;
-function XShmCreatePixmap(para1:PDisplay; para2:TDrawable; para3:Pchar; para4:PXShmSegmentInfo; para5:dword;
-           para6:dword; para7:dword):TPixmap;cdecl;external libX11;
+function XShmGetImage(para1:PDisplay; para2:TDrawable; para3:PXImage; para4:cint; para5:cint;
+           para6:culong):TStatus;cdecl;external libX11;
+function XShmCreateImage(para1:PDisplay; para2:PVisual; para3:cuint; para4:cint; para5:Pchar;
+           para6:PXShmSegmentInfo; para7:cuint; para8:cuint):PXImage;cdecl;external libX11;
+function XShmCreatePixmap(para1:PDisplay; para2:TDrawable; para3:Pchar; para4:PXShmSegmentInfo; para5:cuint;
+           para6:cuint; para7:cuint):TPixmap;cdecl;external libX11;
 
 implementation
 
 
 end.
+
+
