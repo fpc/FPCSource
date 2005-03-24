fea853245d8c30105119740b030ea6866bedb4c8
peter <peter@freepascal.org>
2005-03-24 23:06:53 +0000

    * new bug

---
+++ tests/webtbs/tw3764.pp
@@ -0,0 +1,12 @@
+{ Source provided for Free Pascal Bug Report 3764 }
+{ Submitted by "carl" on  2005-03-09 }
+{ e-mail: cecodere@yahoo.ca }
+
+begin
+  repeat
+    break;
+  until (true);
+  while false do
+    continue;
+end.
+
