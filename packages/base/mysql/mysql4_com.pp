859ef71337ec86bcc97c98cf80dd268cb401d218
joost <joost@cnoc.nl>
2005-04-01 13:38:16 +0000

    - Statical linked MySQL 4.0 now also works on win32

--- packages/base/mysql/mysql4_com.pp
+++ packages/base/mysql/mysql4_com.pp
@@ -10,11 +10,21 @@ interface
     mysqlclient
     mysql_com.ph
 }
+{$mode objfpc}{$H+}
+{$MACRO on}
+
 {$PACKRECORDS C}
 
-const
-  External_library='mysqlclient'; {Setup as you need}
-
+{$IFDEF Unix}
+  {$DEFINE extdecl:=cdecl}
+  const
+    External_library = 'libmysqlclient.so';
+{$ENDIF}
+{$IFDEF Win32}
+  {$DEFINE extdecl:=stdcall}
+  const
+    External_library = 'libmysql.dll';
+{$ENDIF}
 
 { Pointers to basic pascal types, inserted by h2pas conversion program.}
 Type
@@ -44,24 +54,24 @@ Type
      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  }
 
 
-  function my_net_init(net:PNET; vio:PVio):longint;cdecl;external External_library name 'my_net_init';
+  function my_net_init(net:PNET; vio:PVio):longint;extdecl;external External_library name 'my_net_init';
 
-  procedure net_end(net:PNET);cdecl;external External_library name 'net_end';
+  procedure net_end(net:PNET);extdecl;external External_library name 'net_end';
 
-  procedure net_clear(net:PNET);cdecl;external External_library name 'net_clear';
+  procedure net_clear(net:PNET);extdecl;external External_library name 'net_clear';
 
-  function net_flush(net:PNET):longint;cdecl;external External_library name 'net_flush';
-
-(* Const before type ignored *)
-  function my_net_write(net:PNET; packet:Pchar; len:dword):longint;cdecl;external External_library name 'my_net_write';
+  function net_flush(net:PNET):longint;extdecl;external External_library name 'net_flush';
 
 (* Const before type ignored *)
-  function net_write_command(net:PNET; command:byte; packet:Pchar; len:dword):longint;cdecl;external External_library name 'net_write_command';
+  function my_net_write(net:PNET; packet:Pchar; len:dword):longint;extdecl;external External_library name 'my_net_write';
 
 (* Const before type ignored *)
-  function net_real_write(net:PNET; packet:Pchar; len:dword):longint;cdecl;external External_library name 'net_real_write';
+  function net_write_command(net:PNET; command:byte; packet:Pchar; len:dword):longint;extdecl;external External_library name 'net_write_command';
 
-  function my_net_read(net:PNET):dword;cdecl;external External_library name 'my_net_read';
+(* Const before type ignored *)
+  function net_real_write(net:PNET; packet:Pchar; len:dword):longint;extdecl;external External_library name 'net_real_write';
+
+  function my_net_read(net:PNET):dword;extdecl;external External_library name 'my_net_read';
 
   { The following function is not meant for normal usage  }
   {
@@ -71,41 +81,41 @@ Type
    }
 
 
-  procedure randominit(_para1:Prand_struct; seed1:dword; seed2:dword);cdecl;external External_library name 'randominit';
+  procedure randominit(_para1:Prand_struct; seed1:dword; seed2:dword);extdecl;external External_library name 'randominit';
 
-  function rnd(_para1:Prand_struct):double;cdecl;external External_library name 'rnd';
-
-(* Const before type ignored *)
-  procedure make_scrambled_password(_to:Pchar; password:Pchar);cdecl;external External_library name 'make_scrambled_password';
+  function rnd(_para1:Prand_struct):double;extdecl;external External_library name 'rnd';
 
 (* Const before type ignored *)
-  procedure get_salt_from_password(res:Pdword; password:Pchar);cdecl;external External_library name 'get_salt_from_password';
-
-  procedure make_password_from_salt(_to:Pchar; hash_res:Pdword);cdecl;external External_library name 'make_password_from_salt';
+  procedure make_scrambled_password(_to:Pchar; password:Pchar);extdecl;external External_library name 'make_scrambled_password';
 
 (* Const before type ignored *)
-(* Const before type ignored *)
-  function scramble(_to:Pchar; message:Pchar; password:Pchar; old_ver:my_bool):Pchar;cdecl;external External_library name 'scramble';
+  procedure get_salt_from_password(res:Pdword; password:Pchar);extdecl;external External_library name 'get_salt_from_password';
+
+  procedure make_password_from_salt(_to:Pchar; hash_res:Pdword);extdecl;external External_library name 'make_password_from_salt';
 
 (* Const before type ignored *)
 (* Const before type ignored *)
-  function check_scramble(_para1:Pchar; message:Pchar; salt:Pdword; old_ver:my_bool):my_bool;cdecl;external External_library name 'check_scramble';
-
-  function get_tty_password(opt_message:Pchar):Pchar;cdecl;external External_library name 'get_tty_password';
+  function scramble(_to:Pchar; message:Pchar; password:Pchar; old_ver:my_bool):Pchar;extdecl;external External_library name 'scramble';
 
 (* Const before type ignored *)
-  procedure hash_password(result:Pdword; password:Pchar);cdecl;external External_library name 'hash_password';
+(* Const before type ignored *)
+  function check_scramble(_para1:Pchar; message:Pchar; salt:Pdword; old_ver:my_bool):my_bool;extdecl;external External_library name 'check_scramble';
+
+  function get_tty_password(opt_message:Pchar):Pchar;extdecl;external External_library name 'get_tty_password';
+
+(* Const before type ignored *)
+  procedure hash_password(result:Pdword; password:Pchar);extdecl;external External_library name 'hash_password';
 
   { Some other useful functions  }
-  procedure my_init;cdecl;external External_library name 'my_init';
+  procedure my_init;extdecl;external External_library name 'my_init';
 
 (* Const before type ignored *)
 (* Const before type ignored *)
-  procedure load_defaults(conf_file:Pchar; groups:PPchar; argc:Plongint; argv:PPPchar);cdecl;external External_library name 'load_defaults';
+  procedure load_defaults(conf_file:Pchar; groups:PPchar; argc:Plongint; argv:PPPchar);extdecl;external External_library name 'load_defaults';
 
-  function my_thread_init:my_bool;cdecl;external External_library name 'my_thread_init';
+  function my_thread_init:my_bool;extdecl;external External_library name 'my_thread_init';
 
-  procedure my_thread_end;cdecl;external External_library name 'my_thread_end';
+  procedure my_thread_end;extdecl;external External_library name 'my_thread_end';
 
 
 function packet_error : longint;
