913efc5b832c876af889b43375006e2920478615
peter <peter@freepascal.org>
2005-04-15 15:47:07 +0000

    * new bug

---
+++ tests/webtbs/tw3669.pp
@@ -0,0 +1,18 @@
+{ %opt=-Sew }
+
+{ Source provided for Free Pascal Bug Report 3669 }
+{ Submitted by "Mattias Gaertner" on  2005-02-19 }
+{ e-mail: mattias@freepascal.org }
+program UninitializedArrayBounds;
+
+{$mode objfpc}{$H+}
+
+uses
+  Classes, SysUtils;
+
+var
+  a: array[1..2] of integer;
+  i: Integer;
+begin
+  for i:=Low(a) to High(a) do a[i]:=0;
+end.
