70d3cfae075e7afb12e75e2b94f38919eb78a6c3
peter <peter@freepascal.org>
2005-04-15 15:26:12 +0000

    * new bug

---
+++ tests/webtbs/tw3893.pp
@@ -0,0 +1,21 @@
+{ Source provided for Free Pascal Bug Report 3893 }
+{ Submitted by "George Bakhtadze" on  2005-04-14 }
+{ e-mail: mirage@avagames.net }
+program test;
+
+function GetPropertyValue: Pointer;
+begin
+end;
+
+var
+  Size, Size2: Single;
+  d: Integer;
+  p: Pointer;
+
+begin
+  Size := 9.11;
+//  d := Integer(GetPropertyValue());   // All commented code works
+//  p := GetPropertyValue();
+//  Size2 := single(p);
+  Size2 := single(GetPropertyValue());
+end.
