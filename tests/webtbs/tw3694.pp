18c6645bd84f1d119c5c581f75665aa8fa8db40f
florian <florian@freepascal.org>
2005-04-25 07:50:55 +0000

    * fixed for 64 bit CPUs

--- tests/webtbs/tw3694.pp
+++ tests/webtbs/tw3694.pp
@@ -6,7 +6,15 @@ begin
 end;
 
 var
+{$ifdef CPU64}
+  s : double;
+{$else}
   s : single;
+{$endif CPU64}
 begin
+{$ifdef CPU64}
+  s := double(f);
+{$else CPU64}
   s := single(f);
+{$endif CPU64}
 end.
