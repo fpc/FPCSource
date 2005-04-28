942c4ea025df384d95dadbe619bee773c2da7d42
olle <olle>
2005-04-28 18:36:18 +0000

    - Removed dependency on unit DOS   * Made filename valid length for macos

--- tests/tbs/tb0312.pp
+++ tests/tbs/tb0312.pp
@@ -2,16 +2,19 @@
   inside reset(file)
   we call reset(file,longint)
   but we also emit a call to iocheck after and this is wrong !!  PM }
-
 program getret;
-
- uses dos;
 
   var
       ppfile : file;
 
 begin
+{$ifndef macos}
        assign(ppfile,'this_file_probably_does_not_exist&~"#');
+{$else}
+       {Max 32 chars in macos fielnames}
+       assign(ppfile,'this_file_probably_&~"#');
+{$endif}
+
 {$I-}
        reset(ppfile,1);
        if ioresult=0 then
