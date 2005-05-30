9da46ff50a844baa72e2a4c47b6eac0de7dcfded
florian <florian@freepascal.org>
2005-05-30 20:47:34 +0000

    * more descriptive error message in case of a missing variant unit added * variant unit uses now the math unit for ** implementation
    git-svn-id: trunk@158 -

---
+++ tests/webtbs/tw4013.pp
@@ -0,0 +1,13 @@
+{ Source provided for Free Pascal Bug Report 4013 }
+{ Submitted by "Miguel Garcia-Blanco" on  2005-05-25 }
+{ e-mail: mgbonline@hotmail.com }
+PROGRAM Test;
+
+uses
+  variants;
+
+BEGIN
+
+    writeln( 2.0**5.0 );
+
+END.
