153c6a5b20b546e21ef84540a35798e13eab3f41
fpc <fpc@idefix.freepascal.org>
2005-05-30 20:38:28 +0000

    Merged revisions 156 via svnmerge from /trunk
    git-svn-id: branches/fixes_2_0@157 -

---
+++ tests/webtbs/tw3913.pp
@@ -0,0 +1,25 @@
+{ $mode objfpc}
+uses
+  sysutils;
+var
+  d : tdatetime;
+begin
+  try
+    d:=strtodate('03'+dateseparator+'03'+dateseparator+'2033');
+  except
+    halt(1);
+  end;
+  writeln(1);
+  try
+    d:=strtodate('2.2/2');
+    halt(1);
+  except
+  end;
+  writeln(2);
+  try
+    d:=strtodate('33'+dateseparator+'33'+dateseparator+'33');
+    halt(1);
+  except
+  end;
+  writeln('ok');
+end.
