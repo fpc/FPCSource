3fc30bb05a552e1a82fbca3ed35053ca98ba4adf
florian <florian@freepascal.org>
2005-06-05 14:17:05 +0000

    This change has been merge back to avoid having always an outdated fpcmake in releases which create corrupted makefiles Merged revisions 216 via svnmerge from /trunk
    git-svn-id: branches/fixes_2_0@217 -

--- utils/fpcm/fpcmmain.pp
+++ utils/fpcm/fpcmmain.pp
@@ -69,7 +69,8 @@ interface
       TOS=(
         o_linux,o_go32v2,o_win32,o_os2,o_freebsd,o_beos,o_netbsd,
         o_amiga,o_atari, o_solaris, o_qnx, o_netware, o_openbsd,o_wdosx,
-        o_palmos,o_macos,o_darwin,o_emx,o_watcom,o_morphos,o_netwlibc
+        o_palmos,o_macos,o_darwin,o_emx,o_watcom,o_morphos,o_netwlibc,
+        o_win64,o_wince
       );
 
       TTargetSet=array[tcpu,tos] of boolean;
@@ -86,13 +87,15 @@ interface
       OSStr : array[TOS] of string=(
         'linux','go32v2','win32','os2','freebsd','beos','netbsd',
         'amiga','atari','solaris', 'qnx', 'netware','openbsd','wdosx',
-        'palmos','macos','darwin','emx','watcom','morphos','netwlibc'
+        'palmos','macos','darwin','emx','watcom','morphos','netwlibc',
+        'win64','wince'
       );
 
       OSSuffix : array[TOS] of string=(
         '_linux','_go32v2','_win32','_os2','_freebsd','_beos','_netbsd',
         '_amiga','_atari','_solaris', '_qnx', '_netware','_openbsd','_wdosx',
-        '_palmos','_macos','_darwin','_emx','_watcom','_morphos','_netwlibc'
+        '_palmos','_macos','_darwin','_emx','_watcom','_morphos','_netwlibc',
+        '_win64','_wince'
       );
 
       { This table is kept OS,Cpu because it is easier to maintain (PFV) }
@@ -118,7 +121,9 @@ interface
         { emx }     ( true,  false, false, false, false, false),
         { watcom }  ( true,  false, false, false ,false, false),
         { morphos } ( false, false, true,  false ,false, false),
-        { netwlibc }( true,  false, false, false, false, false)
+        { netwlibc }( true,  false, false, false, false, false),
+        { win64   } ( false, false, false, false, true,  false),
+        { wince    }( false, false, false, false, false, true )
       );
 
     type
