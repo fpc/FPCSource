3e97ec6295f488c963896bdbcbf7015dc8ad45ae
peter <peter@freepascal.org>
2005-06-30 14:44:21 +0000

    * add %fail
    git-svn-id: trunk@545 -

--- tests/webtbf/tw4103.pp
+++ tests/webtbf/tw4103.pp
@@ -1,3 +1,5 @@
+{ %fail }
+
 { Source provided for Free Pascal Bug Report 4103 }
 { Submitted by "Daniël Mantione" on  2005-06-21 }
 { e-mail: daniel@freepascal.org }
--- tests/webtbf/tw4144.pp
+++ tests/webtbf/tw4144.pp
@@ -1,3 +1,5 @@
+{ %fail }
+
 { Source provided for Free Pascal Bug Report 4144 }
 { Submitted by "Wiktor Sywula" on  2005-06-30 }
 { e-mail: wswiktor@poczta.fm }
