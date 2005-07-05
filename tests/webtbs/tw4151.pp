4222e2442276fc88a2f01219aa1b3fb37378c740
peter <peter@freepascal.org>
2005-07-05 06:11:20 +0000

    * fix preprocessor support for empty set constants
    git-svn-id: trunk@584 -

---
+++ tests/webtbs/tw4151.pp
@@ -0,0 +1,13 @@
+{ Source provided for Free Pascal Bug Report 4151 }
+{ Submitted by "Christian Iversen" on  2005-07-03 }
+{ e-mail: chrivers@iversen-net.dk }
+program foo;
+type
+  t = (a, b, c);
+const bar = [];
+
+{$IF a in bar}{$MESSAGE ERROR '1'}{$IFEND}
+{$IF a in []}{$MESSAGE ERROR '2'}{$IFEND}
+
+begin
+end.
