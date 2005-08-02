d7b89351080a1f4244ecf589dfa6a5945a38255f
Jonas Maebe <jonas@freepascal.org>
2005-08-02 18:32:56 +0000

    Merged revisions 779 via svnmerge from /trunk
    git-svn-id: branches/fixes_2_0@780 -

--- compiler/htypechk.pas
+++ compiler/htypechk.pas
@@ -1117,7 +1117,7 @@ implementation
                       (hp.resulttype.def.deftype=stringdef) then
                      result:=true
                  else
-                   if valid_const in opts then
+                   if ([valid_const,valid_addr] * opts = [valid_const]) then
                      result:=true
                  else
                   CGMessagePos(hp.fileinfo,errmsg);
