d7a51b2fa2f8b73a119251f2e98f13a686005e60
peter <peter@freepascal.org>
2005-08-16 07:12:53 +0000

    Merged revisions 764 via svnmerge from /trunk
    git-svn-id: branches/fixes_2_0@871 -

---
+++ tests/webtbf/tw3931b.pp
@@ -0,0 +1,16 @@
+{ %fail }
+{ %cpu=i386 }
+
+{$asmmode intel}
+
+procedure SmallForwardMove_3; assembler;
+asm
+jmp dword ptr [@@FwdJumpTable+@@FwdJumpTable+ecx*4]
+nop {Align Jump Table}
+@@Done:
+@@FwdJumpTable:
+dd @@Done {Removes need to test for zero size move}
+end;
+
+begin
+end.
