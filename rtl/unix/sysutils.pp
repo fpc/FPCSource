ee9862d231ab68663c233f1f1c03996a4e731551
florian <florian@freepascal.org>
2005-09-17 22:21:30 +0000

    + signal hooking dummies
    git-svn-id: trunk@1103 -

--- rtl/unix/sysutils.pp
+++ rtl/unix/sysutils.pp
@@ -36,11 +36,49 @@ uses
 
 Procedure AddDisk(const path:string);
 
+{ the following is Kylix compatibility stuff, it should be moved to a
+  special compatibilty unit (FK) }
+  const
+    RTL_SIGINT     = 0;
+    RTL_SIGFPE     = 1;
+    RTL_SIGSEGV    = 2;
+    RTL_SIGILL     = 3;
+    RTL_SIGBUS     = 4;
+    RTL_SIGQUIT    = 5;
+    RTL_SIGLAST    = RTL_SIGQUIT;
+    RTL_SIGDEFAULT = -1;
+
+  type
+    TSignalState = (ssNotHooked, ssHooked, ssOverridden);
+
+function InquireSignal(RtlSigNum: Integer): TSignalState;
+procedure AbandonSignalHandler(RtlSigNum: Integer);
+procedure HookSignal(RtlSigNum: Integer);
+procedure UnhookSignal(RtlSigNum: Integer; OnlyIfHooked: Boolean = True);
 
 implementation
 
 Uses
   {$ifdef FPC_USE_LIBC}initc{$ELSE}Syscall{$ENDIF}, Baseunix, unixutil;
+
+function InquireSignal(RtlSigNum: Integer): TSignalState;
+  begin
+  end;
+
+
+procedure AbandonSignalHandler(RtlSigNum: Integer);
+  begin
+  end;
+
+
+procedure HookSignal(RtlSigNum: Integer);
+  begin
+  end;
+
+
+procedure UnhookSignal(RtlSigNum: Integer; OnlyIfHooked: Boolean = True);
+  begin
+  end;
 
 {$Define OS_FILEISREADONLY} // Specific implementation for Unix.
 
