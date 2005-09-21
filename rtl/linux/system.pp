6d17d83b71fcd7cc93505173bffead34fdcc3a53
peter <peter@freepascal.org>
2005-09-21 08:25:10 +0000

    Merged revisions 543 via svnmerge from /trunk
    git-svn-id: branches/fixes_2_0@1152 -

--- rtl/linux/system.pp
+++ rtl/linux/system.pp
@@ -233,6 +233,7 @@ end;
 
 
 Begin
+  SysResetFPU;
   IsConsole := TRUE;
   IsLibrary := FALSE;
   StackLength := InitialStkLen;
---
+++ tests/webtbs/tw4100.pp
@@ -0,0 +1,21 @@
+{ %result=217 }
+
+{ Source provided for Free Pascal Bug Report 4100 }
+{ Submitted by "Markus Roberts" on  2005-06-20 }
+{ e-mail: Markus@reality.com }
+{$linklib c}
+
+    uses SysUtils;
+    var
+        X,Y,Z : Extended;
+        I     : integer;
+    begin
+        X := 0.0;
+        Y := 1.0;
+        Z := Y/X;
+        I := 7;
+        writeln('test');
+        writeln(StrToFloat('1e7'));
+        writeln(Z);
+        end.
+
