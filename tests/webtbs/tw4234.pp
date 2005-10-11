c48c42b995fa79bbbf33455113fb82b6ffbed55f
florian <florian@freepascal.org>
2005-10-11 21:41:37 +0000

    + "reverse merge" from 2.1
    git-svn-id: trunk@1356 -

---
+++ tests/webtbs/tw4234.pp
@@ -0,0 +1,38 @@
+{ %cpu=i386 }
+{ %opt=-O2 }
+
+{$mode objfpc}
+
+type
+  TFPColor = record
+    red,green,blue,alpha : word;
+  end;
+  TColorData = qword;
+
+  tcl=class
+    function ColorColorAlpha16 (CD:TColorData) : TFPColor;
+  end;
+
+function tcl.ColorColorAlpha16 (CD:TColorData) : TFPColor;
+var c : qword;
+begin
+with result do
+begin
+red := CD and $FFFF;
+c := qword($FFFF0000);
+green := (CD and c) shr 16;
+c := c shl 16;
+blue := (CD and c) shr 32;
+c := c shl 16;
+alpha := (CD and c) shr 48;
+end;
+end;
+
+var
+  cd  : tcolordata;
+  c : tcl;
+begin
+  cd:=$1234;
+  c:=tcl.create;
+  c.colorcoloralpha16(cd);
+end.
