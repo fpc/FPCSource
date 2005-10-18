f3365350354215975fe41d2c5f15231daa8d2dc7
florian <florian@freepascal.org>
2005-10-18 20:12:54 +0000

    Merged revisions 1479 via svnmerge from http://svn.freepascal.org/svn/fpc/trunk
    r1479 (florian)
    + proper init rtti for proc. vars
    
    git-svn-id: branches/fixes_2_0@1480 -

---
+++ tests/webtbs/tw4239.pp
@@ -0,0 +1,21 @@
+{ Source provided for Free Pascal Bug Report 4239 }
+{ Submitted by "Lars" on  2005-07-30 }
+{ e-mail: L@z505.com }
+program Project1;
+
+{$mode objfpc}{$H+}
+
+var
+  MyProc: array of procedure(s:string);
+
+procedure testing(s:string);
+begin
+  writeln(s);
+end;
+
+begin
+  setlength(myproc,1);
+  MyProc[0]:=@testing;
+  MyProc[0]('Test me');
+  readln; //watch for error on close
+end.
