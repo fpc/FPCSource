f18e4aa66458a0b3159c705b4d34362b2599d7cc
Jonas Maebe <jonas@freepascal.org>
2005-10-18 07:23:48 +0000

    Merged revisions 1458 via svnmerge from svn+ssh://jonas@svn.freepascal.org/FPC/svn/fpc/trunk
    r1458 (jonas)
      * fixed test for x86
    
    git-svn-id: branches/fixes_2_0@1460 -

--- tests/webtbs/tw1792a.pp
+++ tests/webtbs/tw1792a.pp
@@ -14,7 +14,11 @@ Begin
 
 {$ifdef FPC_HAS_TYPE_DOUBLE}
  str(double(intpower(2,63)),s);
+{$ifdef FPC_HAS_TYPE_EXTENDED}
+ if s<>' 9.223372036854776E+018' then
+{$else FPC_HAS_TYPE_EXTENDED}
  if s<>' 9.22337203685478E+018' then
+{$endif FPC_HAS_TYPE_EXTENDED}
    begin
      WriteLn(double(intpower(2,63)));
      halt(1);
