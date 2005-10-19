46c9d03d82332103a827239706790519f5ce7c44
florian <florian@freepascal.org>
2005-10-19 19:25:49 +0000

    * initial revision
    git-svn-id: trunk@1538 -

---
+++ tests/webtbs/tw3700.pp
@@ -0,0 +1,11 @@
+{ Source provided for Free Pascal Bug Report 3700 }
+{ Submitted by "Roman" on  2005-02-26 }
+{ e-mail: roman@csltd.com.ua }
+uses
+  variants;
+var
+  v : olevariant;
+  b : boolean;
+begin
+  b:=VarIsArray(V);
+end.
