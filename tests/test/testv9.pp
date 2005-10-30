08d0ea156bf4c84c644c2a49bd21973b3a2fb9ff
florian <florian@freepascal.org>
2005-10-30 20:48:27 +0000

    * fixed multi dimensional dyn. array to variant array
    git-svn-id: trunk@1605 -

--- tests/test/testv9.pp
+++ tests/test/testv9.pp
@@ -6,7 +6,7 @@ var
   a : array of longint;
   a2 : array of array of longint;
   v : variant;
-  i : longint;
+  i,j : longint;
 begin
   setlength(a,1000);
   for i:=0 to high(a) do
@@ -21,4 +21,16 @@ begin
   writeln('simple test ok');
 
   setlength(a2,10,30);
+  for i:=0 to high(a2) do
+    for j:=0 to high(a2[i]) do
+      a2[i,j]:=i*j;
+  v:=a2;
+  for i:=0 to high(a2) do
+    for j:=0 to high(a2[i]) do
+    if v[i,j]<>i*j then
+      begin
+        writeln('v[',i,',',j,']=',v[i,j]);
+        halt(1);
+      end;
+  writeln('complex test ok');
 end.
