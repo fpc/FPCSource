f16581d9d17b583493b88f9e5e452349970adb45
peter <peter@freepascal.org>
2005-11-03 08:29:13 +0000

    Merged revisions 1614,1618,1625-1626,1630-1633 via svnmerge from http://peter@svn.freepascal.org/svn/fpc/trunk
    ........
    r1614 | marco | 2005-10-31 16:16:50 +0100 (Mon, 31 Oct 2005) | 2 lines
    
     * streams freed to fix 4478
    
    ........
    r1618 | tom_at_work | 2005-11-01 22:08:07 +0100 (Tue, 01 Nov 2005) | 1 line
    
    - fixed TFDSet type for 64 bit compatibility
    ........
    r1625 | tom_at_work | 2005-11-01 23:40:23 +0100 (Tue, 01 Nov 2005) | 2 lines
    
    * fixed some typecasts of pointers to Longint for 64 bit architectures (Linux and generic parts of the RTL only)
    * 64 bit sockets unit fixes
    ........
    r1626 | florian | 2005-11-01 23:41:25 +0100 (Tue, 01 Nov 2005) | 2 lines
    
    * started to fix var. array -> dyn. array
    
    ........
    r1630 | marco | 2005-11-02 15:52:05 +0100 (Wed, 02 Nov 2005) | 2 lines
    
     * stacksize fixes for freebsd
    
    ........
    r1631 | jonas | 2005-11-02 16:11:17 +0100 (Wed, 02 Nov 2005) | 2 lines
    
      + test for new inlining (fails currently)
    
    ........
    r1632 | tom_at_work | 2005-11-02 20:55:57 +0100 (Wed, 02 Nov 2005) | 1 line
    
    * fixes error introduced in sscanf() when looking for 64 bit unsafe pointer casts
    ........
    r1633 | tom_at_work | 2005-11-02 20:59:17 +0100 (Wed, 02 Nov 2005) | 1 line
    
    * fixed webbug 4201 which does not properly clean up all threads, causing (program) freeze
    ........
    
    git-svn-id: branches/fixes_2_0@1634 -

---
+++ tests/test/tinline5.pp
@@ -0,0 +1,43 @@
+{$inline on}
+{$mode objfpc}
+
+type
+  tc = class
+    lf: longint;
+    procedure t(const l: longint); inline;
+  end;
+
+var
+  a: longint;
+
+procedure tc.t(const l: longint); inline;
+begin
+  lf := 10;
+  if (l <> 5) then
+    begin
+      writeln('error class');
+      halt(1);
+    end;
+end;
+
+
+procedure t(const l: longint); inline;
+begin
+  a := 10;
+  if (l <> 5) then
+    begin
+      writeln('error proc');
+      halt(1);
+    end;
+end;
+
+var
+  c: tc;
+
+begin
+  c := tc.create;
+  c.lf := 5;
+  c.t(c.lf);
+  a := 5;
+  t(a);
+end.
