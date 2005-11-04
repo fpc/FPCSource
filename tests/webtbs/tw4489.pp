ba22b0b3af76e8472d8e9b9b32b2c2c827eb7d57
florian <florian@freepascal.org>
2005-11-04 19:01:33 +0000

    + format("%d",[<qword>]); fixes 4489
    git-svn-id: trunk@1640 -

---
+++ tests/webtbs/tw4489.pp
@@ -0,0 +1,25 @@
+{ Source provided for Free Pascal Bug Report 4489 }
+{ Submitted by "Vincent Snijders" on  2005-11-04 }
+{ e-mail: vsnijders@quicknet.nl }
+program Project1;
+
+{$mode objfpc}{$H+}
+
+uses
+  Classes, SysUtils
+  { add your units here };
+
+var
+  s: string;
+  u: SizeUInt;
+  d: qword;
+
+begin
+   d:=11111111;
+   u:=1111;
+   s := format('pid=%d', [u]);
+   writeln(s);
+   s := format('pid=%d', [d]);
+   writeln(s);
+end.
+    
