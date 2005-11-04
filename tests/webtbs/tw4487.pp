c1e968abf90e9e65c7c8f54751faeffa0a67f590
florian <florian@freepascal.org>
2005-11-04 21:05:18 +0000

    + binary operator support for booleans in variants
    git-svn-id: trunk@1642 -

---
+++ tests/webtbs/tw4487.pp
@@ -0,0 +1,38 @@
+{ Source provided for Free Pascal Bug Report 4487 }
+{ Submitted by "Phil H." on  2005-11-02 }
+{ e-mail: pjhess@purdue.edu }
+program TestVarBug;
+
+{$IFDEF FPC}
+{$mode objfpc}
+uses
+  Variants;
+{$ENDIF}
+
+type
+  TMyClass = class
+  private
+    function GetValue(AsInt : Boolean) : Variant;
+  public
+    property Value[AsInt : Boolean] : Variant read GetValue;
+  end;
+  
+function TMyClass.GetValue(AsInt : Boolean) : Variant;
+begin
+  if AsInt then
+    Result := 1
+  else
+    Result := True;
+end;
+
+var
+  AClass : TMyClass;  
+begin
+  AClass := TMyClass.Create;
+  if (AClass.Value[True] = 1) and
+     AClass.Value[False] then  //Throws exception with FPC (requires "= True")
+    WriteLn('Value is True')
+  else
+    WriteLn('Value is False');
+end.
+
