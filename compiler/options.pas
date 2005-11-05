ac73b72f2739e5ecb9abf71da1934a0e95518e91
michael <michael@freepascal.org>
2005-11-05 23:31:01 +0000

    + Merged revision 1669
    git-svn-id: branches/fixes_2_0@1670 -

--- compiler/options.pas
+++ compiler/options.pas
@@ -883,14 +883,15 @@ begin
            'o' :
              begin
                if More<>'' then
+                 begin
 {$IFDEF USE_SYSUTILS}
-               begin
-                 d := SplitPath(More);
-                 OutputFile := SplitFileName(More);
-               end
+                 OutputExeDir:=SplitPath(More);
+                 OutputFile:=SplitFileName(More);
+                 OutputExtension:=SplitExtension(More);
 {$ELSE USE_SYSUTILS}
-                 Fsplit(More,d,OutputFile,e)
+                 FSplit(More,OutputExeDir,OutputFile,OutputExtension);
 {$ENDIF USE_SYSUTILS}
+                 end
                else
                  IllegalPara(opt);
              end;
