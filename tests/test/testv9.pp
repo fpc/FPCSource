07228ddf91a40936e7a4e936fdfcda83442fff3e
florian <florian@freepascal.org>
2005-11-22 22:10:40 +0000

    + extended test
    git-svn-id: trunk@1804 -

--- tests/test/testv9.pp
+++ tests/test/testv9.pp
@@ -1,11 +1,13 @@
+{$ifdef fpc}
 {$mode objfpc}
+{$endif fpc}
 uses
   Variants;
 
 var
-  a : array of longint;
-  a2 : array of array of longint;
-  v : variant;
+  a,av : array of longint;
+  a2,av2 : array of array of longint;
+  v,v2 : variant;
   i,j : longint;
 begin
   setlength(a,1000);
@@ -24,13 +26,27 @@ begin
   for i:=0 to high(a2) do
     for j:=0 to high(a2[i]) do
       a2[i,j]:=i*j;
-  v:=a2;
+  writeln(length(a2));
+  writeln(length(a2[0]));
+  v2:=a2;
   for i:=0 to high(a2) do
     for j:=0 to high(a2[i]) do
-    if v[i,j]<>i*j then
+    if v2[i,j]<>i*j then
       begin
-        writeln('v[',i,',',j,']=',v[i,j]);
+        writeln('v2[',i,',',j,']=',v2[i,j]);
         halt(1);
       end;
   writeln('complex test ok');
+
+  av:=v;
+  writeln('1d dyn. array:=var. array ok');
+
+
+  av2:=v2;
+  if high(av2)<>VarArrayHighBound(v2,1) then
+   halt(1);
+  if high(av2[0])<>VarArrayHighBound(v2,2) then
+   halt(1);
+  writeln('2d dyn. array:=var. array ok');
+  writeln('ok');
 end.
