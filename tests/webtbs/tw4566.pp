3a81473a051460ebc68d0b820534bb534f691cb9
florian <florian@freepascal.org>
2005-12-04 18:29:29 +0000

    * initial revision
    git-svn-id: trunk@1877 -

---
+++ tests/webtbs/tw4566.pp
@@ -0,0 +1,15 @@
+{ Source provided for Free Pascal Bug Report 4566 }
+{ Submitted by "Vincent Snijders" on  2005-12-04 }
+{ e-mail: vsnijders@quicknet.nl }
+program bug4566;
+
+{$mode objfpc}{$H+}
+
+var
+  s: string;
+  
+begin
+  //accidently use #1310 instead of #13#10
+  s := 'Message Text' + #1310 + #1310 + 'More Text';
+end.
+                   
