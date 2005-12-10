b3b104130d0212e71cf84645613484a2814fa0ed
Jonas Maebe <jonas@freepascal.org>
2005-12-10 20:37:17 +0000

    * compiler discerns between +0.0 and -0.0   + test for the above
    git-svn-id: trunk@1917 -

---
+++ tests/webtbs/tw4534.pp
@@ -0,0 +1,26 @@
+type
+  pbyte = ^byte;
+
+procedure checksigns(a,b: extended);
+var
+  p1, p2: pbyte;
+  i: longint;
+begin
+  p1 := @a;
+  p2 := @b;
+  for i := 1 to sizeof(a) do
+    begin
+      if (p1^ xor p2^) = $80 then
+        halt(0);
+      halt(1);
+    end;
+end;
+
+var x,y:extended;
+
+Begin
+    x:=-0.0;
+    y:=0.0;
+    checksigns(x,y);
+End.
+
