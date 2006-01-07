c81af0b2f822c58445c9da7429c9121434724e33
Jonas Maebe <jonas@freepascal.org>
2006-01-07 15:51:36 +0000

    Merged revisions 2191 via svnmerge from svn+ssh://jonas@svn.freepascal.org/FPC/svn/fpc/trunk
    r2191 (jonas)
      * test for register variables and goto
    
    git-svn-id: branches/fixes_2_0@2207 -

---
+++ tests/test/opt/tgotoreg.pp
@@ -0,0 +1,39 @@
+{% OPT=-Or}
+
+{$goto on}
+
+var
+  global: longint;
+
+procedure t1;
+begin
+  { make sure it's not put in a register in t }
+  global := 1;
+end;
+
+procedure t;
+var
+  l: longint;
+  s1,s2,s3: ansistring;
+label lab;
+begin
+  t1;
+  l := 1;
+  s1 := 'ABC';
+  s2 := 'DEF';
+  s3 := '';
+lab:
+  inc(l);
+  inc(global);
+  if global > 10 then
+    halt(1);
+  if l = 10 then
+    s3 := 'ABCDEF';
+  if s1+s2 <> s3 then
+    goto lab;
+end;
+
+begin
+  t;
+end.
+ 
