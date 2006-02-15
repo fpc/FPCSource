949f4fcaad99ee01f85e8abd2e5ceb65316e8f18
peter <peter@freepascal.org>
2006-02-15 07:29:46 +0000

    * added files
    git-svn-id: branches/fixes_2_0@2580 -

---
+++ tests/webtbf/tw4781a.pp
@@ -0,0 +1,15 @@
+{ %fail }
+{ %OPT=-Sew -vw}
+program tw4781a;
+var U : integer;
+
+procedure B; 
+begin 
+if U = 0 then; 
+if U = 0 then; 
+end;
+
+begin
+   if  U = 1 then writeln('UNdetected');
+end.
+
---
+++ tests/webtbf/tw4781b.pp
@@ -0,0 +1,14 @@
+{ %fail }
+{ %OPT=-Sew -vw}
+program tw4781b;
+var U : integer;
+
+procedure B; 
+begin 
+if U = 0 then; 
+if U = 0 then; 
+end;
+
+begin
+end.
+
