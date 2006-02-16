a977fc06ccf878734a742ee4f3ee357faa71ec09
Jonas Maebe <jonas@freepascal.org>
2006-02-16 16:03:33 +0000

    + added
    git-svn-id: trunk@2609 -

---
+++ tests/webtbf/tw4809.pp
@@ -0,0 +1,17 @@
+{ %fail }
+
+{$r+}
+Type
+  MySet = (msOne,msTwo,msThree,msFour,msFive);
+
+  Var
+    aVar1: array[msOne..msFive] of LongInt = (1,2,3,4,5);
+    aVar2: array[msTwo..msFive] of LongInt = (2,3,4,5);
+    mVar: MySet;
+    v1,v2: LongInt;
+
+   begin
+     longint(mVar) := 100;
+     v1 := aVar1[mVar];
+  end.
+
