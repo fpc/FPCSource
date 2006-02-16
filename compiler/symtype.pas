7734e41734dd49783d26e333971dab438f1f1870
Jonas Maebe <jonas@freepascal.org>
2006-02-16 19:46:08 +0000

    Merged revisions 2612 via svnmerge from svn+ssh://jonas@svn.freepascal.org/FPC/svn/fpc/trunk
    ........
    r2612 | jonas | 2006-02-16 20:43:20 +0100 (Thu, 16 Feb 2006) | 2 lines
    
      * fixed deref/resolve of sl_vec symlistitems
    
    ........
    
    git-svn-id: branches/fixes_2_0@2613 -

--- compiler/symtype.pas
+++ compiler/symtype.pas
@@ -778,10 +778,11 @@ implementation
              sl_load,
              sl_subscript :
                hp^.sym:=tsym(hp^.symderef.resolve);
-             sl_vec,
              sl_absolutetype,
              sl_typeconv :
                hp^.tt.resolve;
+             sl_vec:
+               hp^.valuett.resolve;
              else
               internalerror(200110205);
            end;
@@ -803,10 +804,11 @@ implementation
              sl_load,
              sl_subscript :
                hp^.symderef.build(hp^.sym);
-             sl_vec,
              sl_absolutetype,
              sl_typeconv :
                hp^.tt.buildderef;
+             sl_vec:
+               hp^.valuett.buildderef;
              else
               internalerror(200110205);
            end;
