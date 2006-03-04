9f8faf0a1de4e3c81c09b618146cd44ce50678d8
Jonas Maebe <jonas@freepascal.org>
2006-03-04 20:09:41 +0000

    Merged revisions 2738 via svnmerge from svn+ssh://jonas@svn.freepascal.org/FPC/svn/fpc/trunk
    ........
    r2738 | jonas | 2006-03-04 21:04:35 +0100 (Sat, 04 Mar 2006) | 2 lines
    
      * fixed web bug #4872
    
    ........
    
    git-svn-id: branches/fixes_2_0@2739 -

--- compiler/ncgmem.pas
+++ compiler/ncgmem.pas
@@ -745,16 +745,7 @@ implementation
                  objectlibrary.getlabel(falselabel);
                end;
               secondpass(right);
-
-              if cs_check_range in aktlocalswitches then
-               begin
-                 if left.resulttype.def.deftype=arraydef then
-                   rangecheck_array;
-               end;
-
-              { if mulsize = 1, we won't have to modify the index }
-              location_force_reg(exprasmlist,right.location,OS_ADDR,(mulsize = 1));
-
+              
               if isjump then
                begin
                  truelabel:=otl;
@@ -762,6 +753,16 @@ implementation
                end
               else if (right.location.loc = LOC_JUMP) then
                 internalerror(2006010801);
+
+              { if mulsize = 1, we won't have to modify the index }
+              location_force_reg(exprasmlist,right.location,OS_ADDR,(mulsize = 1));
+
+              { only range check now, we can't range check loc_flags/loc_jump }
+              if cs_check_range in aktlocalswitches then
+               begin
+                 if left.resulttype.def.deftype=arraydef then
+                   rangecheck_array;
+               end;
 
             { produce possible range check code: }
               if cs_check_range in aktlocalswitches then
