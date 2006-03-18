9dbd69c30727acf77b289ae0c1271a23ac7772ec
Jonas Maebe <jonas@freepascal.org>
2006-03-18 22:02:37 +0000

    * don't give range check hints/warnings for conversions of     realconstnodes to types with less precision than the default (bug 4898)
    git-svn-id: trunk@2957 -

---
+++ tests/webtbs/tw4898.pp
@@ -0,0 +1,12 @@
+{ %OPT=-Sewh -vwh}
+
+{ Source provided for Free Pascal Bug Report 4898 }
+{ Submitted by "Naj Kejah" on  2006-03-13 }
+{ e-mail: universario@hotmail.com }
+program aFP211p; 
+var R : real;
+begin
+  R:=0.0;
+  writeln(r);
+end.
+
