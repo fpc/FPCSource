f26a655ceacd6bbe3454e73a997b3fd0735b8006
Jonas Maebe <jonas@freepascal.org>
2006-04-09 19:23:51 +0000

    Merged revisions 3185 via svnmerge from svn+ssh://jonas@svn.freepascal.org/FPC/svn/fpc/trunk
    ........
    r3185 | jonas | 2006-04-09 21:18:31 +0200 (Sun, 09 Apr 2006) | 3 lines
    
      * fixed another bug in fpc_ansistr_append_ansistring in case source = dest
        (web bug #4999)
    
    ........
    
    git-svn-id: branches/fixes_2_0@3186 -

---
+++ tests/webtbs/tw4999.pp
@@ -0,0 +1,12 @@
+program fpc203fish;
+{$mode objfpc}{$H+}
+uses  Classes;
+var    s:String;
+       i:integer;
+begin
+ s:='123123sdfjkysdjfklsdn3m,w45';
+ for i:=0 to 22 do begin
+  s:=s+s;
+  writeln(i);
+ end;
+end.  
