74f9acd0d87abfb7c1568170f847e192d6b675ef
Jonas Maebe <jonas@freepascal.org>
2006-04-12 13:25:39 +0000

    + added
    git-svn-id: trunk@3198 -

---
+++ tests/tbs/tb0500.pp
@@ -0,0 +1,15 @@
+{ %OPT=-Seh }
+program test;
+uses math;
+
+{ compiler claims that math isn't used, while the ** operator is used from it }
+
+function f(r: real; i: integer): int64;
+begin
+  f:= trunc(r**i);
+end;
+
+begin
+  writeln(f(2.0,2));
+end.
+
