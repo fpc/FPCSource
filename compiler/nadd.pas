85c3e64942b56cfcc65de11dc8210f2556b512b6
florian <florian@freepascal.org>
2006-05-13 20:12:33 +0000

    Merged revisions 3505 via svnmerge from http://svn.freepascal.org/svn/fpc/trunk
    r3505 (florian)
    * fixed s<'', fixes 4925
    
    git-svn-id: branches/fixes_2_0@3506 -

--- compiler/nadd.pas
+++ compiler/nadd.pas
@@ -1517,6 +1517,8 @@ implementation
 
 
     function taddnode.first_addstring: tnode;
+      const
+        swap_relation: array [ltn..unequaln] of Tnodetype=(gtn, gten, ltn, lten, equaln, unequaln);
       var
         p: tnode;
       begin
@@ -1547,6 +1549,7 @@ implementation
                       p := left;
                       left := right;
                       right := p;
+                      nodetype:=swap_relation[nodetype];
                     end;
                   if is_shortstring(left.resulttype.def) or
                      (nodetype in [gtn,gten,ltn,lten]) then
