ea86114f96c6ce9baf6454185635c2aadb8cfddf
Jonas Maebe <jonas@freepascal.org>
2006-05-24 15:16:38 +0000

    * check zero-extending of parameters < sizeof(aint)
    git-svn-id: trunk@3656 -

---
+++ tests/test/cg/tcalval10.pp
@@ -0,0 +1,45 @@
+{ %target=darwin }
+
+type
+  tr= packed  record
+    b1, b2: byte;
+  end;
+
+procedure test(b: tr);mwpascal;
+begin
+  if b.b2 <> 5 then
+    halt(1);
+end;
+
+procedure t;
+var
+  r: tr;
+begin
+  r.b2 := 5;
+  test(r);
+end;
+
+procedure t2(b: byte); mwpascal;
+var
+  p: plongint;
+begin
+  p := plongint(@b);
+  if p^ <> 1 then
+    halt(2);
+end;
+
+procedure t3;
+var
+  b,b2,b3,b4: byte;
+begin
+  b := 1;
+  b2 := 2;
+  b3 := 3;
+  b4 := 4;
+  t2(b);
+end;
+
+begin
+  t;
+  t3;
+end.
