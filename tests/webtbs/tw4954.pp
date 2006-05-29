316c4d631344697b3053c8c3c73e4b6516b928aa
Jonas Maebe <jonas@freepascal.org>
2006-05-29 09:54:42 +0000

    * fixed web bug tw4954 (warning when reading from a typed file)   + test for the above (submitted test program minus childish remarks)
    git-svn-id: trunk@3733 -

---
+++ tests/webtbs/tw4954.pp
@@ -0,0 +1,26 @@
+{ %OPT=-Seh }
+
+program aFP211r;  { false hints on varrec }
+
+  type
+    t = record
+      d:integer
+    end;
+
+  var
+   f: file of t;
+
+  procedure P1;
+  var
+    varrec : t ;
+  begin
+    READ( f , varrec )
+  end{ P1 };
+
+begin 
+  assign( f , 'fname');
+  reset(f);
+  P1;
+end.
+
+
