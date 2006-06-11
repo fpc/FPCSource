eb8db1e460111547a939d15b01b7fb1cbd837c9b
peter <peter@freepascal.org>
2006-06-11 10:51:27 +0000

    * fix internal alias parameters for incr_ref
    git-svn-id: trunk@3844 -

---
+++ tests/webtbs/tw7006.pp
@@ -0,0 +1,19 @@
+program av;
+{$ifdef FPC}{$mode objfpc}{$h+}{$INTERFACES CORBA}{$endif}
+{$ifdef mswindows}{$apptype console}{$endif}
+uses
+ {$ifdef FPC}{$ifdef linux}cthreads,{$endif}{$endif}sysutils;
+type
+ testrecty = record
+  str: widestring;
+  int: integer;
+ end;
+
+var
+ tr1,tr2: testrecty;
+ 
+begin
+ tr1.str:= 'abc';
+ tr1.int:= 0;
+ tr2:= tr1;
+end.
