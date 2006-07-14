1609b068eec2d9c6630222a8f8ff52ecacd4305b
Tomas Hajny <hajny@freepascal.org>
2006-07-14 00:19:55 +0000

    Merged revisions 4166 via svnmerge from http://hajny@svn.freepascal.org/svn/fpc/trunk
    ........
    r4166 | hajny | 2006-07-13 23:08:05 +0000 (Thu, 13 Jul 2006) | 1 line
    
      * show full compiler version in About dialog
    ........
    
    git-svn-id: branches/fixes_2_0@4173 -

--- ide/fpintf.pas
+++ ide/fpintf.pas
@@ -32,6 +32,7 @@ procedure SetPrimaryFile(const fn:string);
 function LinkAfter : boolean;
 {$ifdef USE_EXTERNAL_COMPILER}
 function version_string : string;
+function full_version_string : string;
 {$endif USE_EXTERNAL_COMPILER}
 
 
@@ -227,12 +228,32 @@ end;
 function version_string : string;
   begin
     if not ExecuteRedir(ExternalCompilerExe,'-iV','','ppc___.out','ppc___.err') then
-      version_string:=version.version_string;
-    Assign(CompilerOut,'ppc___.out');
-    Reset(CompilerOut);
-    Readln(CompilerOut,s);
-    Close(CompilerOut);
-    version_string:=s;
+      version_string:=version.version_string
+    else
+     begin
+      Assign(CompilerOut,'ppc___.out');
+      Reset(CompilerOut);
+      Readln(CompilerOut,s);
+      Close(CompilerOut);
+      version_string:=s;
+     end;
+  end;
+
+function full_version_string : string;
+  begin
+    if not ExecuteRedir(ExternalCompilerExe,'-iW','','ppc___.out','ppc___.err') then
+      full_version_string:=version.full_version_string
+    else
+     begin
+      Assign(CompilerOut,'ppc___.out');
+      Reset(CompilerOut);
+      Readln(CompilerOut,s);
+      Close(CompilerOut);
+      if Pos ('-iW', S) <> 0 then
+(* Unknown option - full version not supported! *)
+       S := Version_String;
+      full_version_string:=s;
+     end;
   end;
 {$endif USE_EXTERNAL_COMPILER}
 
