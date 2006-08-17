73d9beecffea7a30f98cba7d02174e1bd936c0c2
tom_at_work <tom_at_work@idefix.freepascal.org>
2006-08-17 21:37:06 +0000

    * fix for bug web bug #7276: the code to read '*' format specifiers failed errorneously on Int64 and QWord input values. Based on code by mftq75 * test program for above changes
    git-svn-id: trunk@4434 -

---
+++ tests/webtbs/tw7276.pp
@@ -0,0 +1,18 @@
+uses Sysutils;
+
+var
+  s : AnsiString;
+
+begin
+  s := 'Hello';
+  // tests whether the * in the format specifier allows all
+  // kind of integers; note that the * in the format specifier here
+  // is an incomplete format specification, i.e. it does not change
+  // the length of the result string
+  Format('%*s', [Integer(length(s)), s]);
+  // this is only seemingly equivalent to above, but on 64 bit
+  // machines the default integer type is Int64
+  Format('%*s', [length(s), s])
+  // also test QWord
+  Format('%*s', [QWord(length(s)), s]);  
+end.
