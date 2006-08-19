cb01ea581ccfd9bbdd4cc0efe7d235599ae637f7
florian <florian@freepascal.org>
2006-08-19 10:08:43 +0000

    + copyright header
    git-svn-id: trunk@4439 -

--- rtl/win32/initc.pp
+++ rtl/win32/initc.pp
@@ -1,5 +1,16 @@
 {
-}
+    This file is part of the Free Pascal run time library.
+    This unit implements cygwin initialization
+    Copyright (c) 1999-2006 by the Free Pascal development team.
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
 unit initc;
 
 interface
