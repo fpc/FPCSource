2d76bbf4dbfad75ee2f130e709337cd5afd7615b
Jonas Maebe <jonas@freepascal.org>
2006-09-09 18:43:34 +0000

    * changed so it still fails after today's changes
    git-svn-id: trunk@4587 -

--- tests/test/tparray1.pp
+++ tests/test/tparray1.pp
@@ -3,15 +3,15 @@
 {$mode macpas}
 
 type
-  tba = array[0..7] of boolean;
+  tba = packed array[0..7] of boolean;
   tkeymap = packed array[0..3] of tba;
 
-procedure test(var b: tba);
+procedure test(var b: boolean);
 begin
 end;
 
 var
   km: tkeymap;
 begin
-  test(km[1]);
+  test(km[1][2]);
 end.
