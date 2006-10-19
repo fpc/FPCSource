ecbe0b801745115c49241e6b2d5efcc546b669ed
michael <michael@freepascal.org>
2006-10-19 19:59:38 +0000

    + Added LGPL header
    git-svn-id: trunk@4981 -

--- fcl/web/htmlelements.pp
+++ fcl/web/htmlelements.pp
@@ -1,3 +1,16 @@
+{
+    $Id: header,v 1.1 2000/07/13 06:33:45 michael Exp $
+    This file is part of the Free Component Library (FCL)
+    Copyright (c) 1999-2000 by the Free Pascal development team
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
 unit htmlelements;
 
 {$mode objfpc}{$H+}
