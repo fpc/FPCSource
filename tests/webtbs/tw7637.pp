5213992110338608de696b0ff606348c46592e81
Jonas Maebe <jonas@freepascal.org>
2006-10-21 17:11:06 +0000

    * fixed mantis  tw7637
    git-svn-id: trunk@4995 -

---
+++ tests/webtbs/tw7637.pp
@@ -0,0 +1,13 @@
+uses sysutils;
+
+var
+  s: ansistring;
+begin
+  s := format('%e',[1.72]);
+{$ifdef FPC_HAS_TYPE_EXTENDED}
+  if s <> '1.7200000000000000E+000' then
+{$else}
+  if s <> '1.72000000000000E+000' then
+{$endif}
+    halt(1);
+end.
