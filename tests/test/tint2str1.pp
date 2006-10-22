9f5a2063a1f3b4fd2960f0a147d88ec14e178e6c
Jonas Maebe <jonas@freepascal.org>
2006-10-22 18:17:47 +0000

    * extended test
    git-svn-id: trunk@5005 -

--- tests/test/tint2str1.pp
+++ tests/test/tint2str1.pp
@@ -1,14 +1,22 @@
 {$mode macpas}
 
-procedure test(s: string);
+procedure test(const s: string);
 begin
   if s <> 'abcd' then
     halt(1);
 end;
+
+procedure test2(c1,c2,c3,c4: char);
+begin
+  if (c1 <> 'a') or (c2 <> 'b') or (c3 <> 'c') or (c4 <> 'd') then
+    halt(2);
+end;
+
 
 var
   l: longint;
 begin
   l := 'abcd';
   test(l);
+  test2(char(l shr 24),char(l shr 16),char(l shr 8),char(l));
 end.
