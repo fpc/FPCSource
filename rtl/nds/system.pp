9e6d19a4946de131ee84e9de1074da73502d4c50
Legolas <legolas@idefix.freepascal.org>
2006-12-14 17:34:51 +0000

    * rtl part of first Nintendo DS port
    git-svn-id: trunk@5593 -

---
+++ rtl/nds/system.pp
@@ -0,0 +1,172 @@
+{
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 2006 by Francesco Lombardi.
+
+    System unit for Nintendo DS
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
+unit System;
+
+interface
+
+{$define FPC_IS_SYSTEM}
+
+{$i ndsbiosh.inc}
+
+{$I systemh.inc}
+
+{$define fpc_softfpu_interface}
+{$i softfpu.pp}
+{$undef fpc_softfpu_interface}
+
+
+const
+ LineEnding = #10;
+ LFNSupport = true;
+ CtrlZMarksEOF: boolean = false; 
+ DirectorySeparator = '/';
+ DriveSeparator = ':';
+ PathSeparator = ';';
+ FileNameCaseSensitive = false;
+ maxExitCode = 255;
+ MaxPathLen = 255;
+
+
+ sLineBreak : string[1] = LineEnding;
+ DefaultTextLineBreakStyle : TTextLineBreakStyle = tlbsCRLF;
+  
+const
+  UnusedHandle    = $ffff;
+  StdInputHandle  = 0;
+  StdOutputHandle = 1;
+  StdErrorHandle  = $ffff;
+
+
+var
+  argc: LongInt = 0;
+  argv: PPChar;
+  envp: PPChar;
+  errno: integer;
+  fake_heap_start: pchar; cvar;
+  fake_heap_end: pchar; cvar;
+
+implementation
+
+{$define fpc_softfpu_implementation}
+{$i softfpu.pp}
+{$undef fpc_softfpu_implementation}
+
+{ we get these functions and types from the softfpu code }
+{$define FPC_SYSTEM_HAS_float64}
+{$define FPC_SYSTEM_HAS_float32}
+{$define FPC_SYSTEM_HAS_flag}
+{$define FPC_SYSTEM_HAS_extractFloat64Frac0}
+{$define FPC_SYSTEM_HAS_extractFloat64Frac1}
+{$define FPC_SYSTEM_HAS_extractFloat64Exp}
+{$define FPC_SYSTEM_HAS_extractFloat64Frac}
+{$define FPC_SYSTEM_HAS_extractFloat64Sign}
+{$define FPC_SYSTEM_HAS_ExtractFloat32Frac}
+{$define FPC_SYSTEM_HAS_extractFloat32Exp}
+{$define FPC_SYSTEM_HAS_extractFloat32Sign}
+
+{$I system.inc}
+
+{$i ndsbios.inc}
+
+
+function GetProcessID: SizeUInt;
+begin
+end;
+
+
+
+{*****************************************************************************
+                       Misc. System Dependent Functions
+*****************************************************************************}
+procedure System_exit;
+begin
+end;
+
+
+
+{*****************************************************************************
+                             ParamStr/Randomize
+*****************************************************************************}
+
+{ number of args }
+function paramcount : longint;
+begin
+  paramcount:=0;
+end;
+
+{ argument number l }
+function paramstr(l : longint) : string;
+begin
+  paramstr:='';
+end;
+
+{ set randseed to a new pseudo random value }
+procedure randomize;
+begin
+end;
+
+procedure SysInitStdIO;
+begin
+  OpenStdIO(Input,fmInput,StdInputHandle);
+  OpenStdIO(Output,fmOutput,StdOutputHandle);
+  OpenStdIO(StdOut,fmOutput,StdOutputHandle);
+end;
+
+
+function CheckInitialStkLen(stklen : SizeUInt) : SizeUInt;
+begin
+  result := stklen;
+end;
+
+
+procedure InitHeap;
+begin
+{
+  FillChar(freelists_fixed,sizeof(tfreelists),0);
+  FillChar(freelists_free_chunk,sizeof(freelists_free_chunk),0);
+
+  freelist_var:=nil;
+  freeoslistcount:=1;
+  freeoslist:=pointer($2040000);
+  fillchar(freeoslist^,sizeof(freeoslist^),0);
+  freeoslist^.size:=$40000; 
+  fillchar(internal_status,sizeof(internal_status),0);
+}
+end;
+
+
+begin
+  StackLength := CheckInitialStkLen(InitialStkLen);
+  ///StackBottom := Sptr - StackLength;
+  StackBottom := StackTop - StackLength;
+{ OS specific startup }
+  fake_heap_start := pchar(0);
+  fake_heap_end := pchar(0);
+{ Set up signals handlers }
+
+{ Setup heap }
+  InitHeap;
+  //SysInitExceptions;
+{ Setup stdin, stdout and stderr }
+  //SysInitStdIO;
+{ Reset IO Error }
+  InOutRes := 0;
+{ Arguments }
+
+  //InitSystemThreads;
+  //initvariantmanager;
+  //initwidestringmanager;
+end.
---
+++ rtl/nds/sysutils.pp
@@ -0,0 +1,284 @@
+{
+
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 2004 by Karoly Balogh
+
+    Sysutils unit for Nintendo DS.
+    This unit is based on the MorphOS one and is adapted for Nintendo DS
+    simply by stripping out all stuff inside funcs and procs. 
+    Copyright (c) 2006 by Francesco Lombardi
+
+    Based on Amiga version by Carl Eric Codere, and other
+    parts of the RTL
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
+unit sysutils;
+
+interface
+
+{$MODE objfpc}
+{ force ansistrings }
+{$H+}
+
+{ Include platform independent interface part }
+{$i sysutilh.inc}
+
+
+implementation
+
+uses 
+  dos, sysconst;
+
+{ Include platform independent implementation part }
+{$i sysutils.inc}
+
+
+{****************************************************************************
+                              File Functions
+****************************************************************************}
+function FileOpen(const FileName: string; Mode: Integer): LongInt;
+begin
+  result := -1;
+end;
+
+
+function FileGetDate(Handle: LongInt) : LongInt;
+begin
+  result := -1;
+end;
+
+
+function FileSetDate(Handle, Age: LongInt) : LongInt;
+begin
+  result := -1;
+end;
+
+
+function FileCreate(const FileName: string) : LongInt;
+begin
+  result := -1;
+end;
+
+
+function FileCreate(const FileName: string; Mode: integer): LongInt;
+begin
+  result := -1;
+end;
+
+
+function FileRead(Handle: LongInt; var Buffer; Count: LongInt): LongInt;
+begin
+  result := -1;
+end;
+
+
+function FileWrite(Handle: LongInt; const Buffer; Count: LongInt): LongInt;
+begin
+  result := -1;
+end;
+
+
+function FileSeek(Handle, FOffset, Origin: LongInt) : LongInt;
+begin
+  result := -1;
+end;
+
+function FileSeek(Handle: LongInt; FOffset: Int64; Origin: Longint): Int64;
+begin
+  result := -1;
+end;
+
+
+procedure FileClose(Handle: LongInt);
+begin
+end;
+
+
+function FileTruncate(Handle, Size: LongInt): Boolean;
+begin
+  result := false;
+end;
+
+
+function DeleteFile(const FileName: string) : Boolean;
+begin
+  result := false;
+end;
+
+
+function RenameFile(const OldName, NewName: string): Boolean;
+begin
+  result := false;
+end;
+
+
+(****** end of non portable routines ******)
+
+
+Function FileAge (Const FileName : String): Longint;
+begin
+  result := -1;
+end;
+
+
+Function FileExists (Const FileName : String) : Boolean;
+Begin
+  result := false;
+end;
+
+
+
+Function FindFirst (Const Path : String; Attr : Longint; Out Rslt : TSearchRec) : Longint;
+begin
+  result := -1;
+end;
+
+
+Function FindNext (Var Rslt : TSearchRec) : Longint;
+begin
+  result := -1;
+end;
+
+Procedure FindClose (Var F : TSearchrec);
+begin
+end;
+
+Function FileGetAttr (Const FileName : String) : Longint;
+begin
+  result := -1;
+end;
+
+
+Function FileSetAttr (Const Filename : String; Attr: longint) : Longint;
+begin
+  result := -1;
+end;
+
+
+
+{****************************************************************************
+                              Disk Functions
+****************************************************************************}
+
+Procedure AddDisk(const path:string);
+begin
+
+end;
+
+
+
+Function DiskFree(Drive: Byte): int64;
+Begin
+  result := -1;
+End;
+
+
+Function DiskSize(Drive: Byte): int64;
+Begin
+  result := -1;
+End;
+
+
+Function GetCurrentDir : String;
+begin
+  result := '';
+end;
+
+
+Function SetCurrentDir (Const NewDir : String) : Boolean;
+begin
+  result := false;
+end;
+
+
+Function CreateDir (Const NewDir : String) : Boolean;
+begin
+  result := false;
+end;
+
+
+Function RemoveDir (Const Dir : String) : Boolean;
+begin
+  result := false;
+end;
+
+
+function DirectoryExists(const Directory: string): Boolean;
+begin
+  result := false;
+end;
+
+
+
+{****************************************************************************
+                              Misc Functions
+****************************************************************************}
+
+procedure Beep;
+begin
+end;
+
+
+{****************************************************************************
+                              Locale Functions
+****************************************************************************}
+
+Procedure GetLocalTime(var SystemTime: TSystemTime);
+begin
+end ;
+
+
+function SysErrorMessage(ErrorCode: Integer): String;
+begin
+{  Result:=StrError(ErrorCode);}
+  result := '';
+end;
+
+{****************************************************************************
+                              OS utility functions
+****************************************************************************}
+
+Function GetEnvironmentVariable(Const EnvVar : String) : String;
+begin
+  result := '';
+end;
+
+Function GetEnvironmentVariableCount : Integer;
+begin
+  result := -1;
+end;
+
+Function GetEnvironmentString(Index : Integer) : String;
+begin
+  result := '';
+end;
+
+function ExecuteProcess (const Path: AnsiString; const ComLine: AnsiString): integer;
+begin
+  result := -1;
+end;
+
+function ExecuteProcess (const Path: AnsiString;
+                                  const ComLine: array of AnsiString): integer;
+begin
+  result := -1;
+end;
+
+
+{****************************************************************************
+                              Initialization code
+****************************************************************************}
+
+Initialization
+  InitExceptions;
+Finalization
+  DoneExceptions;
+end.
