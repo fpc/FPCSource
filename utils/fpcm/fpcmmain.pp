46edbf33528f86e6edd421ad34ef9f015b4e075e
Legolas <legolas@idefix.freepascal.org>
2006-12-15 16:48:52 +0000

    * Changes for Nintendo DS target
    git-svn-id: trunk@5606 -

--- utils/fpcm/fpcmmain.pp
+++ utils/fpcm/fpcmmain.pp
@@ -70,7 +70,7 @@ interface
         o_linux,o_go32v2,o_win32,o_os2,o_freebsd,o_beos,o_netbsd,
         o_amiga,o_atari, o_solaris, o_qnx, o_netware, o_openbsd,o_wdosx,
         o_palmos,o_macos,o_darwin,o_emx,o_watcom,o_morphos,o_netwlibc,
-        o_win64,o_wince,o_gba
+        o_win64,o_wince,o_gba,o_nds
       );
 
       TTargetSet=array[tcpu,tos] of boolean;
@@ -88,14 +88,14 @@ interface
         'linux','go32v2','win32','os2','freebsd','beos','netbsd',
         'amiga','atari','solaris', 'qnx', 'netware','openbsd','wdosx',
         'palmos','macos','darwin','emx','watcom','morphos','netwlibc',
-        'win64','wince','gba'
+        'win64','wince','gba','nds'
       );
 
       OSSuffix : array[TOS] of string=(
         '_linux','_go32v2','_win32','_os2','_freebsd','_beos','_netbsd',
         '_amiga','_atari','_solaris', '_qnx', '_netware','_openbsd','_wdosx',
         '_palmos','_macos','_darwin','_emx','_watcom','_morphos','_netwlibc',
-        '_win64','_wince','_gba'
+        '_win64','_wince','_gba','_nds'
       );
 
       { This table is kept OS,Cpu because it is easier to maintain (PFV) }
@@ -124,7 +124,8 @@ interface
         { netwlibc }( true,  false, false, false, false, false, false),
         { win64   } ( false, false, false, false, true,  false, false),
         { wince    }( true,  false, false, false, false, true,  false),
-        { gba    }  ( false, false, false, false, false, true,  false)
+        { gba    }  ( false, false, false, false, false, true,  false),
+        { nds    }  ( false, false, false, false, false, true,  false)
       );
 
     type
