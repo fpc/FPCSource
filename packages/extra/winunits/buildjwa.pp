5938f9ed6cacfdd4edd438c086a30fe046ab3880
marco <marco@freepascal.org>
2006-12-29 20:43:52 +0000

    * here also shfolder added on Vincents request
    git-svn-id: trunk@5740 -

--- packages/extra/winunits/buildjwa.pp
+++ packages/extra/winunits/buildjwa.pp
@@ -22,7 +22,7 @@ interface
 
 uses
     winver, mmsystem, comobj, ole2, activex, shellapi, shlobj, oleserver,
-    jwawintype, jwawinbase, jwawinnt,
+    jwawintype, jwawinbase, jwawinnt, shfolder,
     jwalmerr, jwalmmsg, jwaaclui, jwaadsdb, jwalmerrlog, jwalmjoin, jwaauthz,
     jwabits, jwalmremutl, jwalmrepl, jwalmserver, jwalmshare, jwalmsname,
     jwalmstats, jwaaccctrl, jwaaclapi, jwacderr, jwacpl, jwaactiveds, jwadbt, jwadde,
