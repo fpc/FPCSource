227ec1012ddc3d79089e58d477a9791317ffd087
Jonas Maebe <jonas@freepascal.org>
2007-01-12 18:37:27 +0000

    + another test for previous commit (mantis #7179)
    git-svn-id: trunk@5928 -

---
+++ tests/webtbs/tw7179.pp
@@ -0,0 +1,27 @@
+{ %opt=-Seh }
+
+{$mode delphi}
+program TestMismatch;
+
+type
+  TTestClass = class(TObject)
+  public
+    procedure AProc(AValue : Single);
+  end;
+
+procedure TTestClass.AProc(AValue : Single);
+begin
+  WriteLn(AValue);
+end;
+
+var
+  AnObj : TTestClass;
+  ASingle : Single;
+  
+begin
+  AnObj := TTestClass.Create;
+  ASingle := 1;
+  AnObj.AProc(ASingle * 2); //Does not generate warning
+  AnObj.AProc(ASingle * 2.0); //Generates warning with -Cr or -vh
+end.
+
