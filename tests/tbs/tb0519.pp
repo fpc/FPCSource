7afbe22031375d2e96138d6a6e2f527ea3fe46c5
Jonas Maebe <jonas@freepascal.org>
2007-01-12 10:38:15 +0000

    * extended test
    git-svn-id: trunk@5917 -

--- tests/tbs/tb0519.pp
+++ tests/tbs/tb0519.pp
@@ -1,8 +1,30 @@
 var
   e: extended;
+  d: double;
+  s: single;
 begin
   e := pi;
+  d := pi;
+  s := pi;
+  if (sizeof(double) < sizeof(extended)) and
+     (d = e) then
+    halt(1);
+  if (sizeof(double) < sizeof(extended)) and
+     (s = e) then
+    halt(2);
+  if (sizeof(double) < sizeof(extended)) and
+     (double(e) = e) then
+    halt(3);
   if (sizeof(single) < sizeof(extended)) and
      (single(e) = e) then
-    halt(1);
+    halt(4);
+
+  d := 1.1;
+  s := 1.1;
+  if (sizeof(single) < sizeof(double)) and
+     (d = s) then
+   halt(5);
+  if (sizeof(single) < sizeof(double)) and
+     (d = single(d)) then
+   halt(6);
 end.
