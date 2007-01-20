5234486213298025dcbce81568663c45e4b20007
tom_at_work <tom_at_work@idefix.freepascal.org>
2007-01-20 22:18:08 +0000

    * fixed test for 64 bit platforms
    git-svn-id: trunk@6093 -

--- tests/webtbs/tw8141.pp
+++ tests/webtbs/tw8141.pp
@@ -2,9 +2,18 @@
 uses
   Classes, SysUtils;
 
+type
+  ARec = packed record
+  {$ifdef cpu64}
+    hi, lo : DWord;
+  {$else}
+    hi, lo : Word;
+  {$endif}
+  end;
+
 var
   List : TList;
 begin
-  with LongRec(Pointer(List.Last)) do
+  with ARec(Pointer(List.Last)) do
     lo := 1;
 end.
