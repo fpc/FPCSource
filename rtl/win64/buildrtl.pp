614f4894a3f8891135ece9528722149a444bac95
florian <florian@freepascal.org>
2007-02-17 19:46:13 +0000

    + buildrtl unit for win64
    git-svn-id: trunk@6528 -

---
+++ rtl/win64/buildrtl.pp
@@ -0,0 +1,18 @@
+unit buildrtl;
+
+  interface
+
+    uses
+      ctypes, strings,
+      lineinfo, lnfodwrf, heaptrc, matrix,
+      windows, winsock, winsock2, cmem, dynlibs,
+      dos, crt, objects, messages,
+      rtlconsts, sysconst, sysutils, math, types,
+      strutils, dateutils, varutils, variants, typinfo, fgl, classes,
+      convutils, stdconvs, charset, ucomplex, getopts,
+      winevent, sockets, printer,
+      video, mouse, keyboard, fmtbcd;
+
+  implementation
+
+end.
