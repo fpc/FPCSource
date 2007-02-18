5fc4257482e95513ac9921b6121587a4b2a5e640
pierre <pierre@freepascal.org>
2007-02-18 00:25:31 +0000

    * range check explicitly disabled
    git-svn-id: trunk@6537 -

--- tests/tbs/tb0062.pp
+++ tests/tbs/tb0062.pp
@@ -1,6 +1,11 @@
 { Old file: tbs0068.pp }
 {  Shows incorrect type of ofs()                        OK 0.99.1 (PFV and FK) }
 
+{ As l is only a smallint, this
+  test will almost always
+  trigger a range check error,
+  disable range check explicitly }
+{$R-}
 var
   p : pointer;
   l  : smallint;
