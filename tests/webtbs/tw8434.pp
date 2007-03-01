e44a9ca4cb563e0c1a45069bdd664dc368aac8ff
Jonas Maebe <jonas@freepascal.org>
2007-03-01 14:30:55 +0000

    * fixed mantis 8434 (wrong precision used in case of |number| < 1,     which when multiplied by 10^x goes from 0.9y to 1.0z, and if a     specific presision is specified)
    git-svn-id: trunk@6692 -

---
+++ tests/webtbs/tw8434.pp
@@ -0,0 +1,11 @@
+uses sysutils;
+
+var
+  x: double;
+begin
+  x := 0.099991;
+  if (Format('%5.2f', [x]) <> ' 0.10') then
+    halt(1);
+  if (Format('%6.3f', [x]) <> ' 0.100') then
+    halt(2);
+end.
