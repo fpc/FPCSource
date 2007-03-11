5784674f2080061291fa8ff2cfd87437c9843eec
daniel <daniel@freepascal.org>
2007-03-11 18:56:02 +0000

    + Test for append on devices.
    git-svn-id: trunk@6787 -

---
+++ tests/tbs/tb0532.pp
@@ -0,0 +1,23 @@
+program tb0532;
+
+{Append was the recommended way to open devices in TP.
+ A pitfall is that you cannot seek to the end of a device.
+
+ It has to work on modern platforms too, because:
+  - Rewrite will destroy the device on platforms where devices are
+    files.
+  - Reset doesn't allow writing to the device.
+}
+
+var null:text;
+
+begin
+{$ifdef Unix}
+  assign(null,'/dev/null');
+{$else}
+  assign(null,'NUL');
+{$endif}
+  append(null);
+  writeln(null,'Text disappearing into the black hole.');
+  close(null);
+end.
