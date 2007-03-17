aa1500c13ef502a27d424bc38b454813b6978744
Jonas Maebe <jonas@freepascal.org>
2007-03-17 22:13:57 +0000

    Merged revisions 6904,6906 via svnmerge from svn+ssh://jonas@svn.freepascal.org/FPC/svn/fpc/trunk
    ........
    r6904 | jonas | 2007-03-17 22:57:45 +0100 (Sat, 17 Mar 2007) | 3 lines
    
      * don't warn about uninitialised open array out parameters when
        only getting their length
    
    ........
    r6906 | jonas | 2007-03-17 23:08:31 +0100 (Sat, 17 Mar 2007) | 3 lines
    
      * test for wrong warning when accessing length of unwritten
        open array out-parameters
    
    ........
    
    git-svn-id: branches/fixes_2_2@6908 -

--- compiler/ninl.pas
+++ compiler/ninl.pas
@@ -1588,7 +1588,8 @@ implementation
               in_length_x:
                 begin
                   if ((left.resultdef.typ=arraydef) and
-                      not is_special_array(left.resultdef)) or
+                      (not is_special_array(left.resultdef) or
+                       is_open_array(left.resultdef))) or
                      (left.resultdef.typ=orddef) then
                     set_varstate(left,vs_read,[])
                   else
