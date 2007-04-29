8561175b019519c6dc69b05a9e28b6de6e8fc585
joost <joost@cnoc.nl>
2007-04-29 17:14:49 +0000

    Merged revisions 7167-7168 via svnmerge from svn+ssh://joost@svn.freepascal.org/FPC/svn/fpc/trunk
    ........
    r7167 | florian | 2007-04-24 10:53:59 +0200 (Tue, 24 Apr 2007) | 2 lines
    
    * don't write def for constant widestrings
    
    ........
    r7168 | florian | 2007-04-24 22:19:25 +0200 (Tue, 24 Apr 2007) | 2 lines
    
    * don't generate pointer checks for newly initialized by new pointers, resolves #8757
    
    ........
    
    git-svn-id: branches/fixes_2_2@7192 -

---
+++ tests/webtbs/tw8757.pp
@@ -0,0 +1,15 @@
+{ %OPT=-ghc }
+//test.pp
+type o=object
+      constructor init;
+     end;
+
+constructor o.init; begin end;
+
+var o1 : ^o;
+
+begin
+  New(o1,init);
+  // New(o1); o1^.init; <- no error
+  dispose(o1);
+end.
