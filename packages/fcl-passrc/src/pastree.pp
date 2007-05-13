2212c78609abce66a83e6ef4ef402345e7cfc4c9
michael <michael@freepascal.org>
2007-05-13 17:44:00 +0000

    * Fixed description of array type without index (open array)
    git-svn-id: trunk@7325 -

--- packages/fcl-passrc/src/pastree.pp
+++ packages/fcl-passrc/src/pastree.pp
@@ -1115,7 +1115,10 @@ end;
 
 function TPasArrayType.GetDeclaration (full : boolean) : string;
 begin
-  Result:='Array['+IndexRange+'] of ';
+  Result:='Array';
+  If (IndexRange<>'') then
+    Result:=Result+'['+IndexRange+']';
+  Result:=Result+' of ';
   If IsPacked then
      Result := 'packed '+Result;      // 12/04/04 Dave - Added
   If Assigned(Eltype) then
