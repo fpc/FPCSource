0c79628c8acd97ceee03d4cb9b71867aa49b10f3
Jonas Maebe <jonas@freepascal.org>
2007-06-16 08:48:13 +0000

    * ifdef linux -> ifdef unix for cthreads
    git-svn-id: trunk@7678 -

--- tests/webtbs/tw7006.pp
+++ tests/webtbs/tw7006.pp
@@ -2,7 +2,7 @@ program av;
 {$ifdef FPC}{$mode objfpc}{$h+}{$INTERFACES CORBA}{$endif}
 {$ifdef mswindows}{$apptype console}{$endif}
 uses
- {$ifdef FPC}{$ifdef linux}cthreads,{$endif}{$endif}sysutils;
+ {$ifdef FPC}{$ifdef unix}cthreads,{$endif}{$endif}sysutils;
 type
  testrecty = record
   str: widestring;
