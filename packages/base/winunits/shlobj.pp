9d7e2c203f4ee3ffb9e7b8a3f67db1bbb7c3eb1a
florian <florian@freepascal.org>
2007-06-16 17:04:15 +0000

    + some types added
    git-svn-id: trunk@7687 -

--- packages/base/winunits/shlobj.pp
+++ packages/base/winunits/shlobj.pp
@@ -1,4 +1,17 @@
 {
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 2007 by Florian Klaempfl
+    member of the Free Pascal development team.
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    Original copyright statement follows.
+
 }
 {$mode objfpc}
 unit shlobj;
@@ -34,6 +47,27 @@ unit shlobj;
         ['{00bb2762-6a77-11d0-a535-00c04fd7d062}']
         function Init(hwndEdit: HWND; punkACL: IUnknown; pwszRegKeyPath: LPCWSTR; pwszQuickComplete: LPCWSTR): HRESULT; stdcall;
         function Enable(fEnable: BOOL): HRESULT; stdcall;
+      end;
+
+    const
+      CLSID_AutoComplete: TGUID = '{00BB2763-6A77-11D0-A535-00C04FD7D062}';
+
+    const
+      { IAutoComplete2 options }
+      ACO_NONE           = 0;
+      ACO_AUTOSUGGEST    = $1;
+      ACO_AUTOAPPEND     = $2;
+      ACO_SEARCH         = $4;
+      ACO_FILTERPREFIXES = $8;
+      ACO_USETAB         = $10;
+      ACO_UPDOWNKEYDROPSLIST = $20;
+      ACO_RTLREADING     = $40;
+
+    type
+      IAutoComplete2 = interface(IAutoComplete)
+        ['{EAC04BC0-3791-11d2-BB95-0060977B464C}']
+        function SetOptions(dwFlag: DWORD): HRESULT; stdcall;
+        function GetOptions(var dwFlag: DWORD): HRESULT; stdcall;
       end;
 
   implementation
