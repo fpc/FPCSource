4cabd0e45190b0e87da9ab792866a63793e55728
peter <peter@freepascal.org>
2007-06-17 14:45:18 +0000

    Merged revisions 7703 via svnmerge from http://svn.freepascal.org/svn/fpc/trunk
    ........
    r7703 | peter | 2007-06-17 16:17:30 +0200 (Sun, 17 Jun 2007) | 2 lines
    
      * test for wrong first match of operator overloading
    
    ........
    
    git-svn-id: branches/fixes_2_2@7712 -

---
+++ tests/test/toperator6.pp
@@ -0,0 +1,35 @@
+{$mode objfpc}
+{$R+}
+
+type  Tconstexprint=record
+        overflow:boolean;
+        case signed:boolean of
+          false:
+            (uvalue:qword);
+          true:
+            (svalue:int64);
+      end;
+
+operator := (const u:qword):Tconstexprint;
+begin
+  result.overflow:=false;
+  result.signed:=false;
+  result.uvalue:=u;
+end;
+
+operator := (const s:int64):Tconstexprint;
+begin
+  result.overflow:=false;
+  result.signed:=true;
+  result.svalue:=s;
+end;
+
+
+var
+  value : tconstexprint;
+begin
+  // Here it should choose the int64 code instead of qword
+  value:=-128;
+  // Here it should choose the qword
+  value:=high(int64)+100;
+end.
