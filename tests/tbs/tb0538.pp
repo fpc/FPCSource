e94e32d0056bce7bb9e1ea5757be40671de89d3b
florian <florian@freepascal.org>
2007-06-24 14:33:28 +0000

    + new test
    git-svn-id: trunk@7792 -

---
+++ tests/tbs/tb0538.pp
@@ -0,0 +1,19 @@
+{$mode delphi}
+type
+  to1 = class
+    procedure p1;
+    procedure p2;
+  end;
+
+procedure to1.p1;
+  begin
+  end;
+
+procedure to1.p2;
+  const
+    p1 : pointer = nil;
+  begin
+  end;
+
+begin
+end.
