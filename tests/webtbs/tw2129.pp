8527ed6a8da71b021fcf7b5b24549d0916b02a2d
Vincent Snijders <vincents@freepascal.org>
2007-07-13 21:37:20 +0000

    * fixed test for win64; it doesn't have 80 bit floating point, even if it runs on a x86_64 processor
    git-svn-id: trunk@8042 -

--- tests/webtbs/tw2129.pp
+++ tests/webtbs/tw2129.pp
@@ -36,14 +36,14 @@ begin
   comp1:=swap(comp1);
 {$endif FPC_DOUBLE_HILO_SWAPPED}
   str(comp1,s);
-{$if defined(cpui386) or defined(cpux86_64)}
+{$ifdef FPC_HAS_EXTENDED}
   if s<>'-6.53142228756617E+0018' then
-{$else cpui386}
+{$else FPC_HAS_EXTENDED}
   { this constant has been verified and is correct (FK) }
   { doubles have slightly different precision on processors <> x86, because }
   { intermediate calculations are not performed in 80 bit there (JM)        }
   if Copy(s,1,Length(s)-1)<>'-653142228756617021' then
-{$endif cpui386}
+{$endif FPC_HAS_EXTENDED}
     begin
       writeln(s);
       writeln('error');
