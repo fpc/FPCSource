432f75d8914a54762517acc4edfa4fd66502a2fd
yury <jura@cp-lab.com>
2007-07-14 10:13:09 +0000

    Merged revisions 8047 via svnmerge from http://svn.freepascal.org/svn/fpc/trunk
    ........
      r8047 | yury | 2007-07-14 13:09:37 +0300 (Сб, 14 июл 2007) | 1 line
      
      * fixed unary minus in softfloat mode (bug #8737).
    ........
    
    git-svn-id: branches/fixes_2_2@8048 -

--- compiler/nmat.pas
+++ compiler/nmat.pas
@@ -712,8 +712,8 @@ implementation
                     internalerror(2005082801);
                 end;
                 result:=ctypeconvnode.create_internal(ccallnode.createintern(procname,ccallparanode.create(
-                  ctypeconvnode.create_internal(crealconstnode.create(0,resultdef),fdef),
-                  ccallparanode.create(ctypeconvnode.create_internal(left,fDef),nil))),resultdef);
+                  ctypeconvnode.create_internal(left,fDef),
+                  ccallparanode.create(ctypeconvnode.create_internal(crealconstnode.create(0,resultdef),fdef),nil))),resultdef);
               end
             else
               begin
