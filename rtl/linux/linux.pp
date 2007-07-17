d418c3dbba9c40ee410d9154eee3c7707c2c6909
joost <joost@cnoc.nl>
2007-07-17 21:26:43 +0000

    * Reverted r8082
    git-svn-id: branches/fixes_2_2@8087 -

--- rtl/linux/linux.pp
+++ rtl/linux/linux.pp
@@ -197,12 +197,6 @@ function epoll_wait(epfd: cint; events: pepoll_event; maxevents, timeout: cint):
 
 implementation
 
-// FUTEX_OP is a macro, doesn't exist in libC as function
-function FUTEX_OP(op, oparg, cmp, cmparg: cint): cint; {$ifdef SYSTEMINLINE}inline;{$endif}
-begin
-  FUTEX_OP := ((op and $F) shl 28) or ((cmp and $F) shl 24) or ((oparg and $FFF) shl 12) or (cmparg and $FFF);
-end;
-
 {$ifndef FPC_USE_LIBC}
 Uses Syscall;
 
@@ -291,6 +285,11 @@ begin
   end;
   *)
 {$endif cpum68k}
+end;
+
+function FUTEX_OP(op, oparg, cmp, cmparg: cint): cint;
+begin
+  FUTEX_OP := ((op and $F) shl 28) or ((cmp and $F) shl 24) or ((oparg and $FFF) shl 12) or (cmparg and $FFF);
 end;
 
 function epoll_create(size: cint): cint;
