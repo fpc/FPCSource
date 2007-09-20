37695af02b10e8a902cca05f4f3a8532aaed0d81
tom_at_work <tom_at_work@idefix.freepascal.org>
2007-09-20 20:35:35 +0000

    * added missing assignment check when searching for a symbol in a class, fixing bug #9673 * test program for above
    git-svn-id: trunk@8579 -

--- compiler/symtable.pas
+++ compiler/symtable.pas
@@ -1658,7 +1658,7 @@ implementation
       begin
         result:=false;
         hashedid.id:=s;
-        if assigned(current_procinfo.procdef) then
+        if assigned(current_procinfo) and assigned(current_procinfo.procdef) then
           currentclassh:=current_procinfo.procdef._class
         else
           currentclassh:=nil;
---
+++ tests/webtbs/tw9673.pp
@@ -0,0 +1,20 @@
+unit tw9673;
+
+interface
+{$mode objfpc}
+
+type
+  Testclass = class
+  
+  type
+    TList = array of byte;
+	
+  end;
+	    
+var
+  b : Testclass.TList;
+	      
+	    
+implementation
+begin
+end.
