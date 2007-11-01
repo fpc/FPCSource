220a37b4430296dfe8834e2f1182b7ccfb3bb620
yury <jura@cp-lab.com>
2007-11-01 16:15:29 +0000

    * Do not fill generic upcase and lowcase conversion tables for windows targets, because generic ANSI routines are not used here.
    git-svn-id: trunk@9049 -

--- rtl/wince/sysutils.pp
+++ rtl/wince/sysutils.pp
@@ -485,29 +485,6 @@ end;
                               Locale Functions
 ****************************************************************************}
 
-Procedure InitAnsi;
-Var
-  i : longint;
-begin
-  {  Fill table entries 0 to 127  }
-  for i := 0 to 96 do
-    UpperCaseTable[i] := chr(i);
-  for i := 97 to 122 do
-    UpperCaseTable[i] := chr(i - 32);
-  for i := 123 to 191 do
-    UpperCaseTable[i] := chr(i);
-  Move (CPISO88591UCT,UpperCaseTable[192],SizeOf(CPISO88591UCT));
-
-  for i := 0 to 64 do
-    LowerCaseTable[i] := chr(i);
-  for i := 65 to 90 do
-    LowerCaseTable[i] := chr(i + 32);
-  for i := 91 to 191 do
-    LowerCaseTable[i] := chr(i);
-  Move (CPISO88591LCT,UpperCaseTable[192],SizeOf(CPISO88591UCT));
-end;
-
-
 function GetLocaleStr(LID, LT: Longint; const Def: string): ShortString;
 var
   L: Integer;
@@ -614,7 +591,6 @@ begin
   InitInternationalGeneric;
   SysLocale.MBCS:=GetSystemMetrics(SM_DBCSENABLED)<>0;
   SysLocale.RightToLeft:=GetSystemMetrics(SM_MIDEASTENABLED)<>0;
-  InitAnsi;
   GetFormatSettings;
 end;
 
