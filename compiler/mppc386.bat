676f477ddb54ec643b2271ca1c8cf259965949a8
peter <peter@freepascal.org>
2007-11-02 21:21:56 +0000

    * remove obsolete scripts
    git-svn-id: trunk@9068 -

--- compiler/mppc386.bat
+++
@@ -1,6 +0,0 @@
-ppc386 -O3p3 -Ch8000000 -FE. -Fui386 -dI386 -dGDB -dBROWSERLOG -Sg pp.pas %1 %2 %3 %4 %5 %6 %7 %8 %9
-if errorlevel 0 goto success
-goto failed
-:success
-copy pp.exe ppc386.exe
-:failed
