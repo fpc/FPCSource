2ffc310ebbb0f2c60fbc70dab840e90416d5b4cd
Jonas Maebe <jonas@freepascal.org>
2007-12-16 10:41:18 +0000

    * fixed AnsiStrLComp and AnsiStrLIComp: they also have to ignore     embedded #0's apparently + adapted test
    git-svn-id: trunk@9471 -

--- tests/test/units/sysutils/tastrcmp.pp
+++ tests/test/units/sysutils/tastrcmp.pp
@@ -95,7 +95,7 @@ begin
   check (ansistrlcomp ('abce', 'abc', 3) = 0, 11);  { Count = length. }
   check (ansistrlcomp ('abcd', 'abce', 4) < 0, 12);  { Nudging limit. }
   check (ansistrlcomp ('abc', 'def', 0) = 0, 13);   { Zero count. }
-  check (ansistrlcomp ('abc'#0'e', 'abc'#0'd', 99) = 0, 14);
+  check (ansistrlcomp ('abc'#0'e', 'abc'#0'd', 99) > 0, 14);
 end;
 
 
@@ -150,7 +150,7 @@ begin
   check(ansistrlicomp('AbC', 'abcd', 4) < 0, 18);
   check(ansistrlicomp('ADC', 'abcd', 1) = 0, 19);
   check(ansistrlicomp('ADC', 'abcd', 2) > 0, 20);
-  check(ansistrlicomp('abc'#0'e', 'abc'#0'd', 99) = 0, 21);
+  check(ansistrlicomp('abc'#0'e', 'abc'#0'd', 99) > 0, 21);
 end;
 
 
