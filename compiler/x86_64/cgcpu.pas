3d89822bc41c850a1a5843d042776dd4bb756e89
Jonas Maebe <jonas@freepascal.org>
2007-12-25 23:47:44 +0000

    * free saved_standard_registers and saved_mm_registers in     done_register_allocators
    git-svn-id: trunk@9527 -

--- compiler/x86_64/cgcpu.pas
+++ compiler/x86_64/cgcpu.pas
@@ -35,6 +35,8 @@ unit cgcpu;
     type
       tcgx86_64 = class(tcgx86)
         procedure init_register_allocators;override;
+        procedure done_register_allocators;override;
+
         procedure g_proc_exit(list : TAsmList;parasize:longint;nostackframe:boolean);override;
         procedure g_intf_wrapper(list: TAsmList; procdef: tprocdef; const labelname: string; ioffset: longint);override;
 
@@ -91,6 +93,14 @@ unit cgcpu;
         rg[R_MMREGISTER]:=trgcpu.create(R_MMREGISTER,R_SUBWHOLE,[RS_XMM0,RS_XMM1,RS_XMM2,RS_XMM3,RS_XMM4,RS_XMM5,RS_XMM6,RS_XMM7,
           RS_XMM8,RS_XMM9,RS_XMM10,RS_XMM11,RS_XMM12,RS_XMM13,RS_XMM14,RS_XMM15],first_mm_imreg,[]);
         rgfpu:=Trgx86fpu.create;
+      end;
+
+
+    procedure Tcgx86_64.done_register_allocators;
+      begin
+        inherited done_register_allocators;
+        setlength(saved_standard_registers,0);
+        setlength(saved_mm_registers,0);
       end;
 
 
