f83a5b0afccdceafea5c737fee69c9fed383b091
Jonas Maebe <jonas@freepascal.org>
2008-01-22 13:54:09 +0000

    - reverted merge of 9418 because it causes testsuite regressions and it     is not a fix for anything
    git-svn-id: branches/fixes_2_2@9864 -

--- compiler/nadd.pas
+++ compiler/nadd.pas
@@ -479,7 +479,7 @@ implementation
                 end
             end;
 
-        { Add,Sub,Mul with constant 0, 1 or -1?  }
+        { Add,Sub,Mul with constant 0 or 1?  }
         if is_constintnode(right) and is_integer(left.resultdef) then
           begin
             if tordconstnode(right).value = 0 then
@@ -496,13 +496,6 @@ implementation
                 case nodetype of
                   muln:
                    result := left.getcopy;
-                end;
-              end
-            else if tordconstnode(right).value = -1 then
-              begin
-                case nodetype of
-                  muln:
-                   result := cunaryminusnode.create(left.getcopy);
                 end;
               end;
             if assigned(result) then
@@ -526,13 +519,6 @@ implementation
                 case nodetype of
                   muln:
                    result := right.getcopy;
-                end;
-              end
-            else if tordconstnode(left).value = -1 then
-              begin
-                case nodetype of
-                  muln:
-                   result := cunaryminusnode.create(right.getcopy);
                 end;
               end;
             if assigned(result) then
