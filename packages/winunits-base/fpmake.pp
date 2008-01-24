dc3985bb3606db6d581903a527a23d0e45293a82
peter <peter@freepascal.org>
2008-01-24 23:49:49 +0000

    * new fpmake.pp
    git-svn-id: trunk@9913 -

--- packages/winunits-base/fpmake.pp
+++ packages/winunits-base/fpmake.pp
@@ -1,3 +1,4 @@
+{$ifndef ALLPACKAGES}
 {$mode objfpc}{$H+}
 program fpmake;
 
@@ -5,13 +6,39 @@ uses fpmkunit;
 
 Var
   T : TTarget;
-
+  P : TPackage;
 begin
-  With Installer do 
+  With Installer do
     begin
-    { Base packages }
-    {$i fpmake.inc}
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('winunits-base');
+{$ifdef ALLPACKAGES}
+    P.Directory:='winunits-base';
+{$endif ALLPACKAGES}
+    P.Version:='2.2.1';
+    P.OSes:=[win32,win64];
+
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('buildwinutilsbase.pp');
+      T.Install:=False;
+    T:=P.Targets.AddImplicitUnit('activex.pp');
+    T:=P.Targets.AddImplicitUnit('comconst.pp');
+    T:=P.Targets.AddImplicitUnit('commctrl.pp');
+    T:=P.Targets.AddImplicitUnit('comobj.pp');
+    T:=P.Targets.AddImplicitUnit('flatsb.pp');
+    T:=P.Targets.AddImplicitUnit('mmsystem.pp');
+    T:=P.Targets.AddImplicitUnit('ole2.pp');
+    T:=P.Targets.AddImplicitUnit('oleserver.pp');
+    T:=P.Targets.AddImplicitUnit('richedit.pp');
+    T:=P.Targets.AddImplicitUnit('shellapi.pp');
+    T:=P.Targets.AddImplicitUnit('shfolder.pp');
+    T:=P.Targets.AddImplicitUnit('shlobj.pp');
+    T:=P.Targets.AddImplicitUnit('winver.pp');
+
+{$ifndef ALLPACKAGES}
     Run;
     end;
 end.
-
+{$endif ALLPACKAGES}
