875168c56bf5e3291535933d591327772e6cddd7
peter <peter@freepascal.org>
2008-01-24 23:37:32 +0000

    * new fpmake.pp
    git-svn-id: trunk@9911 -

---
+++ packages/fcl-xml/fpmake.pp
@@ -0,0 +1,122 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  T : TTarget;
+  P : TPackage;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('fcl-xml');
+{$ifdef ALLPACKAGES}
+    P.Directory:='fcl-xml';
+{$endif ALLPACKAGES}
+    P.Version:='2.2.1';
+
+    P.Dependencies.Add('paszlib');
+    P.Dependencies.Add('fcl-base');
+    P.Dependencies.Add('fcl-xml');
+
+    P.SourcePath.Add('src');
+    P.IncludePath.Add('src');
+
+    T:=P.Targets.AddUnit('dom_html.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('dom');
+        end;
+    T:=P.Targets.AddUnit('dom.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('xmlutils');
+        end;
+    T:=P.Targets.AddUnit('htmldefs.pp');
+    T:=P.Targets.AddUnit('htmlelements.pp');
+      with T.Dependencies do
+        begin
+          AddInclude('tagsintf.inc');
+          AddInclude('tagsimpl.inc');
+          AddUnit('dom');
+          AddUnit('htmldefs');
+        end;
+    T:=P.Targets.AddUnit('htmlwriter.pp');
+      with T.Dependencies do
+        begin
+          AddInclude('wtagsintf.inc');
+          AddInclude('wtagsimpl.inc');
+          AddUnit('dom');
+          AddUnit('htmlelements');
+          AddUnit('htmldefs');
+        end;
+    T:=P.Targets.AddUnit('htmwrite.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('dom');
+          AddUnit('htmldefs');
+        end;
+    T:=P.Targets.AddUnit('sax_html.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('sax');
+          AddUnit('dom');
+          AddUnit('dom_html');
+          AddUnit('htmldefs');
+        end;
+    T:=P.Targets.AddUnit('sax.pp');
+    T:=P.Targets.AddUnit('xhtml.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('dom');
+          AddUnit('dom_html');
+        end;
+    T:=P.Targets.AddUnit('xmlcfg.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('dom');
+          AddUnit('xmlread');
+          AddUnit('xmlwrite');
+        end;
+    T:=P.Targets.AddUnit('xmlconf.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('dom');
+          AddUnit('xmlread');
+          AddUnit('xmlwrite');
+        end;
+    T:=P.Targets.AddUnit('xmlread.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('dom');
+          AddUnit('xmlutils');
+        end;
+    T:=P.Targets.AddUnit('xmlstreaming.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('dom');
+        end;
+    T:=P.Targets.AddUnit('xmlutils.pp');
+      with T.Dependencies do
+        begin
+          AddInclude('names.inc');
+        end;
+    T:=P.Targets.AddUnit('xmlwrite.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('dom');
+        end;
+    T:=P.Targets.AddUnit('xpath.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('dom');
+        end;
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
