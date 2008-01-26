853f5a84d93e8257f053bc0099614f3ad3be80b7
marco <marco@freepascal.org>
2008-01-26 21:58:23 +0000

    * matroska moved
    git-svn-id: trunk@9982 -

---
+++ packages/matroska/fpmake.pp
@@ -0,0 +1,29 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('matroska');
+{$ifdef ALLPACKAGES}
+    P.Directory:='matroska';
+{$endif ALLPACKAGES}
+    P.Version:='2.0.0';
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('matroska.pas');
+
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
