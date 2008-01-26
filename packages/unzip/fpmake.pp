b7abf7ff1f407442195f9a3309806de3944fc630
marco <marco@freepascal.org>
2008-01-26 15:04:58 +0000

    * Unzip moved
    git-svn-id: trunk@9931 -

---
+++ packages/unzip/fpmake.pp
@@ -0,0 +1,33 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('unzip');
+{$ifdef ALLPACKAGES}
+    P.Directory:='unzip';
+{$endif ALLPACKAGES}
+    P.Version:='2.0.0';
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('unzip.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('ziptypes');
+        end;
+    T:=P.Targets.AddUnit('ziptypes.pp');
+//    T:=P.Targets.AddUnit('unzipdll.pp',[emx,os2]);
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
