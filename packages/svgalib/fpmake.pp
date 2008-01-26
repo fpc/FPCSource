e78bd0038fefeef45eea5fc86f7b74daa83931cd
marco <marco@freepascal.org>
2008-01-26 23:53:01 +0000

    * moved svgalib
    git-svn-id: trunk@10011 -

---
+++ packages/svgalib/fpmake.pp
@@ -0,0 +1,29 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('svgalib');
+{$ifdef ALLPACKAGES}
+    P.Directory:='svgalib';
+{$endif ALLPACKAGES}
+    P.Version:='2.0.0';
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('svgalib.pp');
+    T:=P.Targets.AddUnit('vgamouse.pp');
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
