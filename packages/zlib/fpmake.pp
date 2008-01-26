7a3e3e6c5c533cf921bef14f271cfb219d932c3d
marco <marco@freepascal.org>
2008-01-26 15:12:56 +0000

    * zlib moved
    git-svn-id: trunk@9932 -

---
+++ packages/zlib/fpmake.pp
@@ -0,0 +1,29 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('ibase');
+{$ifdef ALLPACKAGES}
+    P.Directory:='ibase';
+{$endif ALLPACKAGES}
+    P.Version:='2.0.0';
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('zlib.pp');
+
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
