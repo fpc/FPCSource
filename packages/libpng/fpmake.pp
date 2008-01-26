092ffc121db3f8d4fe6141ddee6b49903a3fc9ce
marco <marco@freepascal.org>
2008-01-26 18:24:29 +0000

    * libpng moved for Florian
    git-svn-id: trunk@9952 -

---
+++ packages/libpng/fpmake.pp
@@ -0,0 +1,33 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('libpng');
+{$ifdef ALLPACKAGES}
+    P.Directory:='libpng';
+{$endif ALLPACKAGES}
+    P.Version:='2.0.0';
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('png.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('zlib');
+        end;
+
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
