32693d1d3e0a63c99c2521507d33d04429d4f4a8
marco <marco@freepascal.org>
2008-01-27 10:52:27 +0000

    * lua moved
    git-svn-id: trunk@10031 -

---
+++ packages/lua/fpmake.pp
@@ -0,0 +1,40 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('lua');
+{$ifdef ALLPACKAGES}
+    P.Directory:='lua';
+{$endif ALLPACKAGES}
+    P.Version:='2.0.0';
+    P.SourcePath.Add('src');
+//    P.Dependencies.Add('x11');
+
+    T:=P.Targets.AddUnit('lauxlib.pas');
+      with T.Dependencies do
+        begin
+          AddUnit('lua');
+        end;
+    T:=P.Targets.AddUnit('lualib.pas');
+      with T.Dependencies do
+        begin
+          AddUnit('lua');
+        end;
+    T:=P.Targets.AddUnit('lua.pas');
+
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
