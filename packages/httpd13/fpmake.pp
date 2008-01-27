934d1a92174e2b55c86d9544d843245b7c804b0d
marco <marco@freepascal.org>
2008-01-27 17:22:29 +0000

    * httpd moved, splitted, decapitated and rejoined
    git-svn-id: trunk@10061 -

---
+++ packages/httpd13/fpmake.pp
@@ -0,0 +1,48 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('httpd13');
+{$ifdef ALLPACKAGES}
+    P.Directory:='httpd13';
+{$endif ALLPACKAGES}
+    P.Version:='2.2.0';
+    P.SourcePath.Add('src');
+//    P.Dependencies.Add('x11');
+
+    T:=P.Targets.AddUnit('httpd.pas');
+      with T.Dependencies do
+        begin
+          AddInclude('httpd.inc');
+          AddInclude('ap_config.inc');
+          AddInclude('ap_mmn.inc');
+          AddInclude('hsregex.inc');
+          AddInclude('ap_alloc.inc');
+          AddInclude('readdir.inc');
+          AddInclude('buff.inc');
+          AddInclude('ap.inc');
+          AddInclude('util_uri.inc');
+          AddInclude('http_config.inc');
+          AddInclude('http_core.inc');
+          AddInclude('http_log.inc');
+          AddInclude('http_main.inc');
+          AddInclude('http_protocol.inc');
+          AddInclude('http_request.inc');
+          AddInclude('http_vhost.inc');
+        end;
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
