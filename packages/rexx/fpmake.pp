2b8631d722bfd6aaf37633d8bb60ae9c60c59cf5
marco <marco@freepascal.org>
2008-01-27 10:18:45 +0000

    * rexx moved
    git-svn-id: trunk@10019 -

---
+++ packages/rexx/fpmake.pp
@@ -0,0 +1,28 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('rexx');
+{$ifdef ALLPACKAGES}
+    P.Directory:='rexx';
+{$endif ALLPACKAGES}
+    P.Version:='2.0.0';
+    P.SourcePath.Add('src');
+    P.Oses:=[emx,os2];
+//    P.Dependencies.Add('x11');
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
