3a4b8ea4d5afca0dff9eb7f2f02a756ee7f34322
marco <marco@freepascal.org>
2008-01-27 10:28:44 +0000

    * ggi moved
    git-svn-id: trunk@10022 -

---
+++ packages/ggi/fpmake.pp
@@ -0,0 +1,39 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('ggi');
+{$ifdef ALLPACKAGES}
+    P.Directory:='ggi';
+{$endif ALLPACKAGES}
+    P.Version:='2.0.0';
+    P.SourcePath.Add('src');
+//    P.Dependencies.Add('x11');
+
+    T:=P.Targets.AddUnit('ggi2d.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('ggi');
+        end;
+    T:=P.Targets.AddUnit('ggi.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('gii');
+        end;
+    T:=P.Targets.AddUnit('gii.pp');
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
