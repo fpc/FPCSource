983e723262e6281e1e5ecebbbf113935ff7b5cfa
joost <joost@cnoc.nl>
2008-02-16 22:07:44 +0000

    * Implemented Port property (bug 10533)
    git-svn-id: trunk@10344 -

--- packages/fcl-db/src/sqldb/sqldb.pp
+++ packages/fcl-db/src/sqldb/sqldb.pp
@@ -85,7 +85,9 @@ type
     FHostName            : string;
     FCharSet             : string;
     FRole                : String;
-
+    
+    function GetPort: cardinal;
+    procedure Setport(const AValue: cardinal);
   protected
     FConnOptions         : TConnOptions;
     procedure GetDBInfo(const SchemaType : TSchemaType; const SchemaObjectName, ReturnField : string; List: TStrings);
@@ -119,6 +121,7 @@ type
     function GetSchemaInfoSQL(SchemaType : TSchemaType; SchemaObjectName, SchemaPattern : string) : string; virtual;
     procedure LoadBlobIntoBuffer(FieldDef: TFieldDef;ABlobBuf: PBufBlobField; cursor: TSQLCursor; ATransaction : TSQLTransaction); virtual; abstract;
     function RowsAffected(cursor: TSQLCursor): TRowsCount; virtual;
+    property port: cardinal read GetPort write Setport;
   public
     property Handle: Pointer read GetHandle;
     destructor Destroy; override;
@@ -541,6 +544,19 @@ begin
   finally;
     DeAllocateCursorHandle(Cursor);
   end;
+end;
+
+function TSQLConnection.GetPort: cardinal;
+begin
+  result := StrToIntDef(Params.Values['Port'],0);
+end;
+
+procedure TSQLConnection.Setport(const AValue: cardinal);
+begin
+  if AValue<>0 then
+    params.Values['Port']:=IntToStr(AValue)
+  else with params do if IndexOfName('Port') > -1 then
+    Delete(IndexOfName('Port'));
 end;
 
 procedure TSQLConnection.GetDBInfo(const SchemaType : TSchemaType; const SchemaObjectName, ReturnField : string; List: TStrings);
