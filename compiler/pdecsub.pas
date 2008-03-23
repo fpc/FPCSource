5da1a86d946d30efba71667bad71ac8bd4e77436
peter <peter@freepascal.org>
2008-03-23 12:02:36 +0000

    Merged revisions 10473,10475 via svnmerge from http://svn.freepascal.org/svn/fpc/trunk
    ........
      r10473 | peter | 2008-03-11 18:57:24 +0100 (Tue, 11 Mar 2008) | 3 lines
      
        * give warning for disabling inline if assembler
          is detected
    ........
      r10475 | jonas | 2008-03-12 11:48:19 +0100 (Wed, 12 Mar 2008) | 2 lines
      
        * fixed "array of const" check in r10473 (+ fixed indentation)
    ........
    
    git-svn-id: branches/fixes_2_2@10539 -

--- compiler/pdecsub.pas
+++ compiler/pdecsub.pas
@@ -341,31 +341,6 @@ implementation
       end;
 
 
-    procedure check_inline_para(p:TObject;arg:pointer);
-      var
-        pd : tabstractprocdef absolute arg;
-      begin
-        if not(po_inline in pd.procoptions) or
-           (tsym(p).typ<>paravarsym) then
-         exit;
-        with tparavarsym(p) do
-         begin
-           case vardef.typ of
-             arraydef :
-               begin
-                 if is_array_constructor(vardef) or
-                    is_variant_array(vardef) then
-                   begin
-                     Message1(parser_w_not_supported_for_inline,'array of const');
-                     Message(parser_w_inlining_disabled);
-                     pd.proccalloption:=pocall_default;
-                   end;
-               end;
-           end;
-         end;
-      end;
-
-
     procedure set_addr_param_regable(p:TObject;arg:pointer);
       begin
         if (tsym(p).typ<>paravarsym) then
@@ -1200,12 +1175,6 @@ begin
 end;
 
 
-procedure pd_inline(pd:tabstractprocdef);
-begin
-  { Check if there are parameters that can't be inlined }
-  pd.parast.SymList.ForEachCall(@check_inline_para,pd);
-end;
-
 procedure pd_internconst(pd:tabstractprocdef);
 begin
   if pd.typ<>procdef then
@@ -1730,7 +1699,7 @@ const
     ),(
       idtok:_INLINE;
       pd_flags : [pd_interface,pd_implemen,pd_body,pd_notobjintf];
-      handler  : @pd_inline;
+      handler  : nil;
       pocall   : pocall_none;
       pooption : [po_inline];
       mutexclpocall : [];
