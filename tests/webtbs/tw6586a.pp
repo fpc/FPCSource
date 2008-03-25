61df38ae9b97a55f9ee9bb25f2d0a70c192bbb8d
Jonas Maebe <jonas@freepascal.org>
2008-03-25 15:44:30 +0000

    + tests from mantis #6586, already work after the previous shared     library fixes
    git-svn-id: trunk@10559 -

---
+++ tests/webtbs/tw6586a.pp
@@ -0,0 +1,19 @@
+{ %target=win32,win64,wince,darwin,linux,freebsd,solaris,beos }
+{ %norun }
+
+library tw6586a;
+{$H+}{$MODE OBJFPC}
+
+uses cmem;
+
+procedure ExportTest1(input: longint); stdcall;
+begin
+ input:= 5;
+end;
+
+exports
+  ExportTest1;
+
+begin
+end.
+
---
+++ tests/webtbs/tw6586b.pp
@@ -0,0 +1,22 @@
+{ %target=win32,win64,wince,darwin,linux,freebsd,solaris,beos }
+{ %needlibrary }
+
+program project1;
+
+{$mode objfpc}{$H+}
+
+uses cmem;
+
+const
+{$ifdef windows}
+  libname='tw6586a.dll';
+{$else}
+  libname='tw6586a';
+  {$linklib tw6586a}
+{$endif}
+
+procedure ExportTest1(input: longint); stdcall; external libname;
+
+begin
+  writeln('Watch for Seg fault on closing');
+end.
