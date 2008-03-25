8a98d4659494cd06235cc2c2c4b70876ab5807b5
Jonas Maebe <jonas@freepascal.org>
2008-03-25 12:36:10 +0000

    * fixed test
    git-svn-id: trunk@10556 -

--- tests/webtbs/uw6822a.pp
+++ tests/webtbs/uw6822a.pp
@@ -5,10 +5,16 @@ interface
 
 implementation
 
+var
+  t: text;
+
 initialization
   writeln('Unit 1');
   writeln('initialization');
 finalization
   writeln('Unit 1'); // problem
   writeln('finalization'); 
+  assign(t,'uw6822a.txt');
+  rewrite(t);
+  close(t);
 end.
