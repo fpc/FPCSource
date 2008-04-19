9a5125f93e685f6d4220e6002bddc47b1a7f58a5
yury <jura@cp-lab.com>
2008-04-19 08:23:24 +0000

    Merged revisions 10691-10692 via svnmerge from http://svn.freepascal.org/svn/fpc/trunk
    ........
      r10691 | yury | 2008-04-18 14:31:12 +0300 (Пт, 18 апр 2008) | 2 lines
      
      * Fixed int to int conversion in ARM code generator.
      + Added new test to detect more bugs in int to int conversion.
    ........
      r10692 | yury | 2008-04-18 14:46:39 +0300 (Пт, 18 апр 2008) | 1 line
      
      * Small optimization.
    ........
    
    git-svn-id: branches/fixes_2_2@10709 -

---
+++ tests/test/cg/tcnvint6.pp
@@ -0,0 +1,87 @@
+
+procedure error(n: longint);
+begin
+  writeln('Test failed: ', n);
+  Halt(n);
+end;
+
+var
+  b: byte;
+  w: word;
+  c: cardinal;
+  shi: shortint;
+  si: smallint;
+begin
+  b:=$ff;
+  Inc(b,$ff);
+  if shortint(b)<>-2 then
+    error(1);
+  if smallint(b)<>$fe then
+    error(2);
+  if word(b)<>$fe then
+    error(3);
+  if longint(b)<>$fe then
+    error(4);
+  if cardinal(b)<>$fe then
+    error(5);
+  if int64(b)<>$fe then
+    error(6);
+{$ifdef FPC}
+  if qword(b)<>$fe then
+    error(7);
+{$endif FPC}
+
+  w:=$8000;
+  if shortint(w)<>0 then
+    error(8);
+
+  w:=$ffff;
+  Inc(w,$ffff);
+  if shortint(w)<>-2 then
+    error(11);
+  if byte(w)<>$fe then
+    error(12);
+  if smallint(w)<>-2 then
+    error(13);
+  if longint(w)<>$fffe then
+    error(14);
+  if cardinal(w)<>$fffe then
+    error(15);
+  if int64(w)<>$fffe then
+    error(16);
+{$ifdef FPC}
+  if qword(w)<>$fffe then
+    error(17);
+{$endif FPC}
+
+  c:=$ffffffff;
+  Inc(c,$ffffffff);
+  if int64(c)<>$fffffffe then
+    error(21);
+{$ifdef FPC}
+  if qword(c)<>$fffffffe then
+    error(22);
+{$endif FPC}
+
+  shi:=-1;
+  if word(shi)<>$ffff then
+    error(31);
+  if cardinal(shi)<>$ffffffff then
+    error(32);
+{$ifdef FPC}
+  if qword(shi)<>$ffffffffffffffff then
+    error(33);
+{$endif FPC}
+
+  si:=-1;
+  if word(si)<>$ffff then
+    error(34);
+  if cardinal(si)<>$ffffffff then
+    error(35);
+{$ifdef FPC}
+  if qword(si)<>$ffffffffffffffff then
+    error(36);
+{$endif FPC}
+
+  writeln('Test OK.');
+end.
