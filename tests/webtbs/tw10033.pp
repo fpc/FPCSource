553b524c0addd143578cb0c6794c6fe439e78cfc
tom_at_work <tom_at_work@idefix.freepascal.org>
2008-04-20 20:02:23 +0000

    * also fix rtti for named enumerations * updated test program to test both named and unnamed enumerations
    git-svn-id: trunk@10741 -

--- tests/webtbs/tw10033.pp
+++ tests/webtbs/tw10033.pp
@@ -2,13 +2,17 @@
 // writing and reading of rtti for enums, both "dense" and 
 // "sparse" enumerations (different rtti is generated and 
 // different code used for generating and reading)
-{$mode objfpc}
 type
+  // "dense" unnamed enumeration
+  Txx = set of (_one, _two, _three);
+  // "sparse" unnamed enumeration
+  Tyy = set of (_zero := 0, _ten := 10, _twenty := 20);
+
   // "dense" enumeration
   Tx = (one,two,three);
   Txxx = set of Tx;
   // "sparse" enumeration
-  Ty =(zero := 0, ten := 10, twenty := 20);
+  Ty = (zero := 0, ten := 10, twenty := 20);
   Tyyy = set of Ty;
 
 procedure error(number : longint);
@@ -21,8 +25,17 @@ var
   x : txxx;
   y : tyyy;
   err : word;
+  
+  _x : txx;
+  _y : tyy;
 
 begin
+  writeln(low(_x));
+  writeln(high(_x));
+
+  writeln(low(_y));
+  writeln(high(_y));
+
   writeln(low(x));
   writeln(high(x));
 
