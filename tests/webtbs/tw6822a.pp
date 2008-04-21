c0e760ef3c17875630900445401cf797cdbbde14
yury <jura@cp-lab.com>
2008-04-21 12:06:10 +0000

    * Do not use console I/O in DLL on wince.
    git-svn-id: trunk@10747 -

--- tests/webtbs/tw6822a.pp
+++ tests/webtbs/tw6822a.pp
@@ -6,5 +6,7 @@ uses
   uw6822a;
 
 begin
+{$ifndef wince}
   writeln('hello from library');
+{$endif wince}
 end.
--- tests/webtbs/uw6822a.pp
+++ tests/webtbs/uw6822a.pp
@@ -9,11 +9,15 @@ var
   t: text;
 
 initialization
+{$ifndef wince}
   writeln('Unit 1');
   writeln('initialization');
+{$endif wince}
 finalization
+{$ifndef wince}
   writeln('Unit 1'); // problem
   writeln('finalization'); 
+{$endif wince}
   assign(t,'uw6822a.txt');
   rewrite(t);
   close(t);
