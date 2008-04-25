629eb29da3df6cad2115a505fc2102ffa79efabc
yury <jura@cp-lab.com>
2008-04-25 20:59:10 +0000

    * Manually fixed results of bad merge in r8536.
    git-svn-id: branches/fixes_2_2@10795 -

--- compiler/arm/narmcon.pas
+++ compiler/arm/narmcon.pas
@@ -66,8 +66,7 @@ interface
         location_reset(location,LOC_CREFERENCE,def_cgsize(resultdef));
         lastlabel:=nil;
         realait:=floattype2ait[tfloatdef(resultdef).floattype];
-        hiloswapped:=(current_settings.fputype in [fpu_fpa,fpu_fpa10,fpu_fpa11]) and
-          not(cs_fp_emulation in current_settings.moduleswitches);
+        hiloswapped:=is_double_hilo_swapped;
         { const already used ? }
         if not assigned(lab_real) then
           begin
