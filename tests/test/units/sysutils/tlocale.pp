fc4b4317689009ddeeb91c40962e0df2032235d6
Jonas Maebe <jonas@freepascal.org>
2008-05-08 19:55:48 +0000

    * slightly reformatted output
    git-svn-id: trunk@10912 -

--- tests/test/units/sysutils/tlocale.pp
+++ tests/test/units/sysutils/tlocale.pp
@@ -19,45 +19,45 @@ begin
   for i := 1 to 7 do
      writeln('short day ',i:5,' ',shortdaynames[i]:25,' - ',longdaynames[i]);
 
-  writeln('Dateseparator :', dateseparator);
+  writeln('Dateseparator : ', dateseparator);
   {$ifdef localedebug}
-  writeln('orgshortdate  :', orgformatsettings.shortdateformat);
+  writeln('orgshortdate  : ', orgformatsettings.shortdateformat);
   {$endif}
-  writeln('short date    :', shortdateformat);
+  writeln('short date    : ', shortdateformat);
   {$ifdef localedebug}
-  writeln('orglongdate   :', orgformatsettings.longdateformat);
+  writeln('orglongdate   : ', orgformatsettings.longdateformat);
   {$endif}
-  writeln('long  date    :', longdateformat);
+  writeln('long  date    : ', longdateformat);
  { Time stuff }
-  writeln('TimeSeparator :', timeseparator);
-  writeln('TimeAMstring  :', timeamstring);
-  writeln('TimePMstring  :', timepmstring);  
+  writeln('TimeSeparator : ', timeseparator);
+  writeln('TimeAMstring  : ', timeamstring);
+  writeln('TimePMstring  : ', timepmstring);  
   {$ifdef localedebug}
-  writeln('orgshorttime  :', orgformatsettings.shorttimeformat);
+  writeln('orgshorttime  : ', orgformatsettings.shorttimeformat);
   {$endif}
 
  // No support for 12 hour stuff at the moment...
-  writeln('short time    :', shorttimeformat);
+  writeln('short time    : ', shorttimeformat);
   {$ifdef localedebug}
-  writeln('orglongtime   :', orgformatsettings.longtimeformat);
+  writeln('orglongtime   : ', orgformatsettings.longtimeformat);
   {$endif}
-  writeln('long  time    :', longtimeformat);
+  writeln('long  time    : ', longtimeformat);
 
  { Currency stuff }
 
   {$ifdef localedebug}
-  writeln('currency1     :', orgformatsettings.currencystring1);
-  writeln('currency2     :', orgformatsettings.currencystring2);
+  writeln('currency1     : ', orgformatsettings.currencystring1);
+  writeln('currency2     : ', orgformatsettings.currencystring2);
   {$endif}
 
-  writeln('currencystring  :', currencystring);
+  writeln('currencystring  : ', currencystring);
 
-  writeln('currencyformat  :', currencyformat);
-  writeln('negcurrformat   :', negcurrformat);
+  writeln('currencyformat  : ', currencyformat);
+  writeln('negcurrformat   : ', negcurrformat);
 
-  writeln('decimalseparator :', decimalseparator);
-  writeln('thousandseparator:', thousandseparator);
-  writeln('currencydecimals :', currencydecimals);
+  writeln('decimalseparator : ', decimalseparator);
+  writeln('thousandseparator: ', thousandseparator);
+  writeln('currencydecimals : ', currencydecimals);
 end;
 
 begin
