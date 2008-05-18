38df5b4eae9c65ca88a6e7d5edd22ac3a9f6edc9
florian <florian@freepascal.org>
2008-05-18 19:05:45 +0000

    + needs library
    git-svn-id: trunk@11011 -

--- tests/test/tlib1.pp
+++ tests/test/tlib1.pp
@@ -1,4 +1,5 @@
 { %opt=-gl }
+{ %needlibrary }
 {$goto on}
 
 { test lineinfo in libraries }
@@ -30,7 +31,7 @@ w:
   GetLineInfo(PtrUInt(@w),s1,s2,l);
   writeln({ 'Func: ',s1,'} 'Source: ',s2,' Line: ',l);
   { GetLineInfo of dwarf doesn't return the function name }
-  if { (s1<>'P') or } (s2<>'tlib1.pp') or (l<>19) then
+  if { (s1<>'P') or } (s2<>'tlib1.pp') or (l<>20) then
     halt(1);
   
   writeln('ok');
