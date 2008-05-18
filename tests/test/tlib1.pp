1fe749c90c8020bc7e691c812a9381b763c6086d
florian <florian@freepascal.org>
2008-05-18 18:26:17 +0000

    + test for proper line info retrival from dlls/sos
    git-svn-id: trunk@11007 -

---
+++ tests/test/tlib1.pp
@@ -0,0 +1,20 @@
+{ %opt=-gl }
+{$ifdef unix}
+uses
+  dl;
+{$endif unix}
+
+procedure p(var a : pointer);external 'tlib1a' name 'p';
+
+var
+  s1,s2 : string;
+  l : longint;
+  a : pointer;
+begin
+  p(a);
+  GetLineInfo(PtrUInt(a),s1,s2,l);
+  writeln('Func: ',s1,' Source: ',s2,' Line: ',l);
+  if (s1<>'P') or (s2<>'tlib1a.pp') or (l<>7) then
+    halt(1);
+  writeln('ok');
+end.
---
+++ tests/test/tlib1a.pp
@@ -0,0 +1,16 @@
+{ %norun }
+{$goto on}
+library tlib1a;
+
+  procedure p(var a : pointer);
+    label
+      we;
+    begin
+    we:
+      a:=@we;
+    end;
+
+  exports p;
+
+begin
+end.
