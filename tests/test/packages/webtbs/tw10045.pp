216e468cd9c704b3773a8c2b944a6df2b0c115a5
florian <florian@freepascal.org>
2008-05-23 21:23:10 +0000

    + test for #10045
    git-svn-id: trunk@11065 -

---
+++ tests/test/packages/webtbs/tw10045.pp
@@ -0,0 +1,31 @@
+uses regexpr;
+
+var
+  engine : tRegexprEngine;
+  source, dest : ansistring;
+  count : longint;
+begin
+  if not GenerateRegExprEngine( 'foo', [], engine) then
+  begin
+    writeln( 'Failed to generate regex. engine.' );
+    halt(1)
+  end;
+
+  source := 'foo bur a';
+  count := RegExprReplaceAll(engine, source, '@', dest);
+  if (count<>1) or (dest<>'@ bur a') then
+    halt(1);
+
+  source := 'xfoo bur a';
+  count := RegExprReplaceAll(engine, source, '@', dest);
+  if (count<>1) or (dest<>'x@ bur a') then
+    halt(1);
+
+  source := 'foo bur a';
+  count := RegExprReplaceAll(engine, source, '@', dest);
+  if (count<>1) or (dest<>'@ bur a') then
+    halt(1);
+
+  DestroyRegExprEngine( engine );
+  writeln('ok');
+end.
