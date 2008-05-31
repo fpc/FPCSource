bfe5f54c7cb5aa931796216ded01368f5892c6d5
mazen <mazen@freepascal.org>
2008-05-31 21:56:27 +0000

    * Changed version to 2.2.2rc1 : code freezed in this branch.
    git-svn-id: branches/fixes_2_2@11147 -

--- compiler/version.pas
+++ compiler/version.pas
@@ -29,7 +29,7 @@ interface
        { version string }
        version_nr = '2';
        release_nr = '2';
-       patch_nr   = '1';
+       patch_nr   = '2';
        minorpatch = 'rc1';
 
        { word version for ppu file }
