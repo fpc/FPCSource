272673bfe13059bd18ac6aa997c082951d90e85b
giulio <giulio@idefix.freepascal.org>
2008-06-21 14:06:12 +0000

    .rodata* sections must end up in .data section on go32v2, since the go32v2 stub only handles .text, .data and .bss sections. Solves bug #11509.
    git-svn-id: trunk@11257 -

--- compiler/aggas.pas
+++ compiler/aggas.pas
@@ -317,6 +317,10 @@ implementation
           (target_info.system=system_i386_win32) then
           secname:='.tls';
 
+        if (atype in [sec_rodata,sec_rodata_norel]) and
+          (target_info.system=system_i386_go32v2) then
+          secname:='.data';
+
         { For bss we need to set some flags that are target dependent,
           it is easier to disable it for smartlinking. It doesn't take up
           filespace }
