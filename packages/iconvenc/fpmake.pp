0ea254295f53a4c1e262adf2b1e3e4892949ba77
marco <marco@freepascal.org>
2008-07-19 10:58:53 +0000

    * iconvenc packages, first version. Tested on Linux/x86 and FreeBSD/x86
    git-svn-id: trunk@11407 -

---
+++ packages/iconvenc/fpmake.pp
@@ -0,0 +1,29 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('iconvenc');
+{$ifdef ALLPACKAGES}
+    P.Directory:='iconvenc';
+{$endif ALLPACKAGES}
+    P.Version:='2.0.0';
+    P.SourcePath.Add('src');
+    P.IncludePath.Add('src');
+
+    T:=P.Targets.AddUnit('iconvenc.pas');
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
