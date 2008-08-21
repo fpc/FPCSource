b7339852e15ded10539148c02718f3b09e408959
Tomas Hajny <hajny@freepascal.org>
2008-08-21 23:58:07 +0000

    * reverted previous changes, proper fix still to be found :-(
    git-svn-id: trunk@11626 -

--- compiler/systems/t_os2.pas
+++ compiler/systems/t_os2.pas
@@ -358,7 +358,7 @@ end;
           ImportLibrary : TImportLibrary;
           ImportSymbol  : TImportSymbol;
       begin
-        LibName:=FixFileName(ChangeFileExt(Current_Module.ObjFileName^,Target_Info.StaticCLibExt));
+        LibName:=FixFileName(Current_Module.RealModuleName^ + Target_Info.StaticLibExt);
         seq_no:=1;
         current_module.linkotherstaticlibs.add(libname,link_always);
         assign(out_file,current_module.outputpath^+libname);
