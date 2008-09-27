17e7c221fdb431b7dae5a869492258d1df63c71e
florian <florian@freepascal.org>
2008-09-27 12:37:30 +0000

    * typo fixed
    git-svn-id: trunk@11829 -

--- rtl/linux/buildrtl.pp
+++ rtl/linux/buildrtl.pp
@@ -4,7 +4,7 @@ unit buildrtl;
   interface
 
     uses
-      system, unixtype, ctypes, baseunix, strings, objpas,b macpas, syscall, unixutil,
+      system, unixtype, ctypes, baseunix, strings, objpas,macpas, syscall, unixutil,
       fpintres, heaptrc, lineinfo, lnfodwrf,
       termio, unix, linux, initc, cmem, mmx,
       crt, printer, linuxvcs,
