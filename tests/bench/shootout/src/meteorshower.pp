1c98cda100e29b210a73893bd1d6bb7b8cae9863
Vincent Snijders <vincents@freepascal.org>
2008-10-03 08:40:37 +0000

    * fixed compilation on x86_64
    git-svn-id: trunk@11858 -

--- tests/bench/shootout/src/meteorshower.pp
+++ tests/bench/shootout/src/meteorshower.pp
@@ -6,7 +6,7 @@ program meteorshower;
 
  mostly based on Ben St. John's implementation.}
 
-{$Q-}
+{$Q-}{$PACKSET 4}
 
 uses dos;
 
@@ -705,7 +705,7 @@ begin
        isOdd:=parity(row and 1);
 
        if not(cardinal(last_row) in alwaysBad[parity(row and 1)]) then
-         if boardVec*bitvec(cardinal(TOP_ROW) shl N_COL*3)=[] then
+         if boardVec*bitvec(cardinal(cardinal(TOP_ROW) shl N_COL*3))=[] then
            begin
              isClosed:=fixed(row>6); {because we track 3 rows}
                if not(cardinal(last_row) in is_known[isClosed,isOdd]) then
