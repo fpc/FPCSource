80ff09c6f4161bb3a79f1939c61063f3d0c9d460
florian <florian@freepascal.org>
2008-11-18 16:21:58 +0000

    * correct handling of constant operands of aad/aam
    git-svn-id: trunk@12158 -

--- compiler/x86/aasmcpu.pas
+++ compiler/x86/aasmcpu.pas
@@ -997,7 +997,8 @@ implementation
               top_const :
                 begin
                   { allow 2nd or 3rd operand being a constant and expect no size for shuf* etc. }
-                  if (opsize=S_NO) and not(i in [1,2]) then
+                  { further, allow AAD and AAM with imm. operand }
+                  if (opsize=S_NO) and not((i in [1,2]) or ((i=0) and (opcode in [A_AAD,A_AAM]))) then
                     message(asmr_e_invalid_opcode_and_operand);
                   if (opsize<>S_W) and (aint(val)>=-128) and (val<=127) then
                     ot:=OT_IMM8 or OT_SIGNED
