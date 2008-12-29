0d4c4a213e6f7a190834fc4bd72a2ed8de751f50
Jonas Maebe <jonas@freepascal.org>
2008-12-29 11:18:26 +0000

    * only excute tests on platforms for which -gc is supported
    git-svn-id: trunk@12453 -

--- tests/webtbs/tw3661.pp
+++ tests/webtbs/tw3661.pp
@@ -1,3 +1,5 @@
+{ %cpu=i386,powerpc}
+{ %target=linux,win32,go32v2,os2,beos,haiku,morphos }
 { %OPT=-glhc }
 { Source provided for Free Pascal Bug Report 3661 }
 { Submitted by "Martin Schreiber" on  2005-02-16 }
