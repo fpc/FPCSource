b136b85cb4df5aa4b58385484178e24d98f23aa8
ivost <ivost@idefix.freepascal.org>
2009-01-18 17:38:46 +0000

    * added fpmake.pp for librsvg package
    git-svn-id: trunk@12565 -

---
+++ packages/librsvg/fpmake.pp
@@ -0,0 +1,37 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('rsvg');
+{$ifdef ALLPACKAGES}
+    P.Directory:='librsvg';
+{$endif ALLPACKAGES}
+    P.Version:='2.2.2-0';
+    P.SourcePath.Add('src');
+    P.IncludePath.Add('src');
+
+  T:=P.Targets.AddUnit('rsvg.pas');
+  with T.Dependencies do
+    begin
+    end;
+
+    // 'Makefile
+    // 'Makefile.fpc
+    // 'test1.xml
+    // 'test2.xml
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
