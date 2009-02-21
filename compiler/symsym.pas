6911b70dc45eb236921a36b102456aeecf630db3
florian <florian@freepascal.org>
2009-02-21 22:59:58 +0000

    + write/read alignment to ppu for explicit parameter locations
    git-svn-id: trunk@12765 -

--- compiler/symsym.pas
+++ compiler/symsym.pas
@@ -1299,6 +1299,7 @@ implementation
          paraloc[callerside].init;
          if vo_has_explicit_paraloc in varoptions then
            begin
+             paraloc[callerside].alignment:=ppufile.getbyte;
              b:=ppufile.getbyte;
              if b<>sizeof(paraloc[callerside].location^) then
                internalerror(200411154);
@@ -1328,6 +1329,7 @@ implementation
          if vo_has_explicit_paraloc in varoptions then
            begin
              paraloc[callerside].check_simple_location;
+             ppufile.putbyte(sizeof(paraloc[callerside].alignment));
              ppufile.putbyte(sizeof(paraloc[callerside].location^));
              ppufile.putdata(paraloc[callerside].location^,sizeof(paraloc[callerside].location^));
            end;
