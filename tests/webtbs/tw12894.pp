6a813b8e7b85dfe08afc395207736d0c8f3794f4
Jonas Maebe <jonas@freepascal.org>
2009-03-22 18:26:34 +0000

    * fixed rounding error in DateTimeToUnix (mantis #12894 and #12936)   * fixed errors in Inc*() routines regarding skipping the black     home in the data-time continuum around 0, and the same for the     *Between() functions   + tests, test...
    git-svn-id: trunk@12958 -

---
+++ tests/webtbs/tw12894.pp
@@ -0,0 +1,88 @@
+program Project1;
+
+uses
+ Classes, SysUtils, DateUtils;
+
+var
+ utime : longword;
+ sec : word;
+ currentDt, convertedDt : TDateTime;
+ times: longint;
+ s1, s2: ansistring;
+
+begin
+ for sec := 0 to 59 do
+   begin
+     currentDt := EncodeDateTime(1989, 9, 16, 12, 0, sec, 0);
+     utime := DateTimeToUnix(currentDt);
+     convertedDt := UnixToDateTime(utime);
+     s1:=FormatDateTime('mm/dd/yyyy HH:nn:ss', currentDt);
+     s2:=FormatDateTime('mm/dd/yyyy HH:nn:ss', convertedDt);
+     writeln(s1 + ' = ' + IntToStr(utime) + ' = ' + s2);
+     if (s1<>s2) then
+       halt(1);
+ end;
+ for times:=-10000 to 10000 do
+   if times<>datetimetounix(unixtodatetime(times)) then
+     begin
+       writeln('error for ',times,', becomes ',datetimetounix(unixtodatetime(times)));
+       halt(2);
+     end;
+
+ // check some borderline cases
+ currentDt := EncodeDateTime(1899, 12, 29, 6, 0, 0, 0);
+ convertedDt := EncodeDateTime(1899, 12, 30, 6, 0, 0, 0);
+ writeln(currentDt:0:4,' - ',convertedDt:0:4);
+ s1:=FormatDateTime('mm/dd/yyyy HH:nn:ss', currentDt);
+ s2:=FormatDateTime('mm/dd/yyyy HH:nn:ss', convertedDt);
+ writeln(s1);
+ writeln(s2);
+ if (currentDt<>-1.25) or
+    (convertedDt<>0.25) or
+    (s1<>'12-29-1899 06:00:00') or
+    (s2<>'12-30-1899 06:00:00') or
+    (DaysBetween(currentDt,convertedDt)<>1) or
+    (HoursBetween(currentDt,convertedDt)<>24) or
+    (MinutesBetween(currentDt,convertedDt)<>24*60) or
+    (SecondsBetween(currentDt,convertedDt)<>24*60*60) or
+    (MilliSecondsBetween(currentDt,convertedDt)<>24*60*60*1000) then
+   begin
+     writeln('between ',s1,' and ',s2);
+     writeln(DaysBetween(currentDt,convertedDt));
+     writeln(HoursBetween(currentDt,convertedDt));
+     writeln(MinutesBetween(currentDt,convertedDt));
+     writeln(SecondsBetween(currentDt,convertedDt));
+     writeln(MilliSecondsBetween(currentDt,convertedDt));
+     halt(3);
+   end;
+ currentDt := EncodeDateTime(1899, 12, 30, 6, 0, 0, 0);
+ convertedDt := EncodeDateTime(1899, 12, 29, 6, 0, 0, 0);
+ if (DaysBetween(currentDt,convertedDt)<>1) or
+    (HoursBetween(currentDt,convertedDt)<>24) or
+    (MinutesBetween(currentDt,convertedDt)<>24*60) or
+    (SecondsBetween(currentDt,convertedDt)<>24*60*60) or
+    (MilliSecondsBetween(currentDt,convertedDt)<>24*60*60*1000) then
+   halt(4);
+ currentDt := EncodeDateTime(1898, 12, 30, 6, 0, 0, 0);
+ convertedDt := EncodeDateTime(1899, 12, 30, 6, 0, 0, 0);
+ if (YearsBetween(currentDt,convertedDt)<>1) or
+    (MonthsBetween(currentDt,convertedDt)<>12) or
+    (DaysBetween(currentDt,convertedDt)<>365) or
+    (HoursBetween(currentDt,convertedDt)<>365*24) or
+    (MinutesBetween(currentDt,convertedDt)<>365*24*60) or
+    (SecondsBetween(currentDt,convertedDt)<>365*24*60*60) or
+    (MilliSecondsBetween(currentDt,convertedDt)<>365*24*60*60*1000) then
+   halt(5);
+ currentDt := EncodeDateTime(1898, 12, 29, 6, 0, 0, 0);
+ convertedDt := EncodeDateTime(1899, 12, 30, 6, 0, 0, 0);
+ if (YearsBetween(currentDt,convertedDt)<>1) or
+    (MonthsBetween(currentDt,convertedDt)<>12) or
+    (DaysBetween(currentDt,convertedDt)<>366) or
+    (HoursBetween(currentDt,convertedDt)<>366*24) or
+    (MinutesBetween(currentDt,convertedDt)<>366*24*60) or
+    (SecondsBetween(currentDt,convertedDt)<>366*24*60*60) or
+    (MilliSecondsBetween(currentDt,convertedDt)<>366*24*60*60*1000) then
+   halt(6);
+// convertedDt:=incseconds(currentDt,
+
+end.
