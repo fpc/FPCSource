ea356808b170fd1aa131bcaea6eea06c17a6d07c
Jonas Maebe <jonas@freepascal.org>
2009-05-16 10:30:28 +0000

    * tests have to fail   * "fixed" tests so they also fail on 32 bit platforms
    git-svn-id: trunk@13149 -

--- tests/tbf/tb0210.pp
+++ tests/tbf/tb0210.pp
@@ -1,7 +1,10 @@
+{ %fail }
+
 // check whether enums can NOT be casted to object references; this 
 // should NOT work in objfpc mode (see also tbs/tb0554.pp)
 {$mode objfpc}
 
+{$packenum 2}
 type
   TEnum = (a, b, c);
   
