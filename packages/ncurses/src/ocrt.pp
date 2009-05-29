6fa94ad9d8ef92bdfae2e4436733e1da5476a783
michael <michael@freepascal.org>
2009-05-29 07:30:46 +0000

    * Fix from Ken Wright for broken FPC changes
    git-svn-id: trunk@13211 -

--- packages/ncurses/src/ocrt.pp
+++ packages/ncurses/src/ocrt.pp
@@ -144,6 +144,10 @@ Unit oCrt;
           | 1) Added nReadScr, nReadScrStr, nReadScrColor, nWriteScrStr,
           | nGrabScreen, nPopScreen, nReleaseScreen.
           | 2) Fixed some trouble with PrevWn accuracy.
+  2.16.01 | 05/26/2009 | kjw |
+          | 1) Corrected error with tnWindow.PutFrame and wattr_get. Recent
+          | updates to ncurses and ocrt by the FreePascal team introduced an
+          | error with tnWindow.PutFrame's use of wattr_get.
 ------------------------------------------------------------------------------
 }
 Interface
@@ -858,10 +862,11 @@ Var
    x,y,
    mx,my,
    atts : longint;
+   junk : smallint;
    
 Begin
    wbkgd(win,COLOR_PAIR(nSetColorPair(att)));
-   atts := wattr_get(win,nil,Nil,nil);
+   wattr_get(win,@atts,@junk,nil);
    If nisbold(att) then wattr_on(win,atts or A_BOLD,Nil);
    box(win,ACS_VLINE,ACS_HLINE);
    framecolor := att;
