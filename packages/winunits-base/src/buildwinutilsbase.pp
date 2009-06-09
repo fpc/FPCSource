3c0c4fc96622c8643b000b2f683d7da8ad07cdbb
marco <marco@freepascal.org>
2009-06-09 20:07:07 +0000

    * copyright and added to buildunit
    git-svn-id: trunk@13251 -

--- packages/winunits-base/src/buildwinutilsbase.pp
+++ packages/winunits-base/src/buildwinutilsbase.pp
@@ -23,7 +23,7 @@ interface
 uses
     flatsb, winver, mmsystem, comconst, commctrl, comobj, commdlg,
     ole2, activex, shellapi, shlobj, oleserver,  shfolder, richedit,
-    imagehlp, wininet, uxtheme, dwmapi, multimon;
+    imagehlp, wininet, uxtheme, dwmapi, multimon, htmlhelp;
 
 implementation
 
--- packages/winunits-base/src/htmlhelp.pp
+++ packages/winunits-base/src/htmlhelp.pp
@@ -1,3 +1,26 @@
+{
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 2009 by Marco van de Voort
+    member of the Free Pascal development team.
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    Original copyright statement follows.
+
+/****************************************************************************
+*                                                                           *
+* HtmlHelp.h                                                                *
+*                                                                           *
+* Copyright (c) 1996-1997, Microsoft Corp. All rights reserved.             *
+*                                                                           *
+****************************************************************************/
+}
+
 unit htmlhelp;
 
 interface
