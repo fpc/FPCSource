ee49e8acb67395216cf0b5776a6eeed0d80ec502
florian <florian@freepascal.org>
2009-07-16 21:12:45 +0000

    * SScanf uses StrToFloat to convert the string to a float so it has to use DecimalSeparator to parse the string * test adapted to use DecimalSeparator to create the test string
    git-svn-id: trunk@13399 -

--- tests/test/units/sysutils/tsscanf.pp
+++ tests/test/units/sysutils/tsscanf.pp
@@ -7,7 +7,7 @@ var
   s : string;
   l : longint;
 begin
-  sscanf('asdf 1.2345 1234','%s %f %d',[@s,@e,@l]);
+  sscanf('asdf 1'+DecimalSeparator+'2345 1234','%s %f %d',[@s,@e,@l]);
   if (e<>1.2345) or
     (l<>1234) or
     (s<>'asdf') then
