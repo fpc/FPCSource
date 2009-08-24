6d2a9dd173559e503253df1f3dfa7efbfd8b7bf5
florian <florian@freepascal.org>
2009-08-24 17:22:25 +0000

    + file forgotten to add in r13588
    git-svn-id: trunk@13589 -

---
+++ utils/fppkg/buildfppkg.pp
@@ -0,0 +1,12 @@
+unit buildfppkg;
+
+  interface
+
+    uses
+      fprepos,fpxmlrep,pkgoptions,pkgglobals,pkgmessages,pkghandler,pkgmkconv,pkgdownload,pkgfpmake,pkgcommands,pkgrepos,pkgwget,pkglnet;
+
+  implementation
+
+end.
+
+
