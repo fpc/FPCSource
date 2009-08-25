1a3449299043ff1e3640d7ba250e10b3a7d7edae
Jonas Maebe <jonas@freepascal.org>
2009-08-25 19:10:18 +0000

    * fixed test after merge in r13458 (selector() -> objcselector())
    git-svn-id: branches/objc@13596 -

--- tests/test/tobjc1.pp
+++ tests/test/tobjc1.pp
@@ -8,11 +8,11 @@ var
   a: NSObject;
 begin
   a:=NSObject(NSObject(NSObject.alloc).init);
-  if a.respondstoselector_(selector('isKindOfClass:')) then
+  if a.respondstoselector_(objcselector('isKindOfClass:')) then
     writeln('ok string selector!')
   else
     halt(1);
-  if a.respondstoselector_(selector(NSObject.init)) then
+  if a.respondstoselector_(objcselector(NSObject.init)) then
     writeln('ok method selector!')
   else
     halt(2);
