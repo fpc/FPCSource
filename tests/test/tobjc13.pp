3b1e285f7058b465f75769688b8656a4dc02a42a
Jonas Maebe <jonas@freepascal.org>
2009-09-08 16:00:15 +0000

    * fixed number of colons in (fake) message names
    git-svn-id: branches/objc@13677 -

--- tests/test/tobjc13.pp
+++ tests/test/tobjc13.pp
@@ -7,8 +7,8 @@
 
 type
   ta = objcclass(NSObject)
-    procedure test(l: longint; a: array of const); message 'class';
-    procedure test2(l: longint); varargs; message 'class';
+    procedure test(l: longint; a: array of const); message 'class:';
+    procedure test2(l: longint); varargs; message 'class:';
   end; external name 'NSObject';
 
 begin
