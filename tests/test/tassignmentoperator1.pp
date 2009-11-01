c2e749797b1d81c88af1c4046acb23a91dd8717d
paul <paul@idefix.freepascal.org>
2009-11-01 14:42:52 +0000

    + test
    git-svn-id: trunk@13999 -

---
+++ tests/test/tassignmentoperator1.pp
@@ -0,0 +1,16 @@
+program tassignmentoperator1;
+
+{$mode objfpc}
+
+operator := (S1, S2: String): Integer;
+begin
+  Result := Length(S1);
+end;
+
+var
+  S: String;
+  V: Integer;
+begin
+  V := S;
+end.
+
