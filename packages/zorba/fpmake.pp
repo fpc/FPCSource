7ac46442890eb5015fc81a9de702d80e4df850ea
ivost <ivost@idefix.freepascal.org>
2009-11-24 12:10:12 +0000

    * splittet zorba(dyn).pas into xqc.pas and zorba(dyn).pas
    git-svn-id: trunk@14265 -

--- packages/zorba/fpmake.pp
+++ packages/zorba/fpmake.pp
@@ -20,21 +20,25 @@ begin
     P.SourcePath.Add('src');
     P.IncludePath.Add('src');
 
+    T:=P.Targets.AddUnit('xqc.pas');
+      with T.Dependencies do
+        begin
+          AddInclude('xqc_error.inc');
+          AddInclude('xqc_static_context_consts.inc');
+        end;
     T:=P.Targets.AddUnit('zorbadyn.pas');
       with T.Dependencies do
         begin
+          AddUnit('xqc.pas');
           AddInclude('zorba.inc');
-          AddInclude('zorba_error.inc');
           AddInclude('zorba_options.inc');
-          AddInclude('zorba_static_context_consts.inc');
         end;
     T:=P.Targets.AddUnit('zorba.pas');
       with T.Dependencies do
         begin
+          AddUnit('xqc.pas');
           AddInclude('zorba.inc');
-          AddInclude('zorba_error.inc');
           AddInclude('zorba_options.inc');
-          AddInclude('zorba_static_context_consts.inc');
         end;
 
 //    P.ExamplePath.Add('tests/');
