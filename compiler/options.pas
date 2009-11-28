7a21770ead97fcc6c75399b55add5c1de7ce3563
pierre <pierre@freepascal.org>
2009-11-28 07:08:05 +0000

    * Add solaris to the -Xr option list
    git-svn-id: trunk@14284 -

--- compiler/options.pas
+++ compiler/options.pas
@@ -121,7 +121,7 @@ const
                         + [system_i386_netbsd]
                         + [system_i386_wdosx];
                         
-  suppported_targets_x_smallr = system_linux
+  suppported_targets_x_smallr = system_linux + systems_solaris
                              + [system_i386_haiku]
                              + [system_i386_beos];
 
