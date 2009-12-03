5939a0301785327adef6fa69b5776e75f3d07183
Jonas Maebe <jonas@freepascal.org>
2009-12-03 22:25:38 +0000

    + added some extra checks
    git-svn-id: trunk@14316 -

--- tests/webtbs/tw13281.pp
+++ tests/webtbs/tw13281.pp
@@ -25,12 +25,20 @@ begin
   c:=1;
   if interlockeddecrement(c)<>0 then
     halt(1);
+  if (c<>0) then
+    halt(11);
   if interlockedincrement(c)<>1 then
     halt(2);
+  if (c<>1) then
+    halt(12);
   if interlockedexchange(c,1234)<>1 then
     halt(3);
+  if (c<>1234) then
+    halt(13);
   if InterLockedExchangeAdd(c,5)<>1234 then
     halt(4);
+  if (c<>1239) then
+    halt(14);
   if InterlockedCompareExchange(c,$80000000,1239)<>1239 then
     halt(5);
   if (c<>$80000000) then
