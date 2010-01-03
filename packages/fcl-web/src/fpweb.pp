a8a332e029711e5e69cc82fa73f5e0debfb89185
joost <joost@cnoc.nl>
2010-01-03 11:39:26 +0000

    * Cleanup session for the case that a webmodule instance is used for more then one request (wkPooled)
    git-svn-id: trunk@14528 -

--- packages/fcl-web/src/fpweb.pp
+++ packages/fcl-web/src/fpweb.pp
@@ -465,6 +465,12 @@ begin
   UpdateSession(AResponse);
   FRequest := Nil;
   FResponse := Nil;
+  // Clean up session for the case the webmodule is used again
+  if assigned(Session) then
+    begin
+    Session.Free;
+    Session := nil;
+    end;
 {$ifdef cgidebug}
   SendMethodExit('WebModule('+Name+').handlerequest');
 {$endif cgidebug}
