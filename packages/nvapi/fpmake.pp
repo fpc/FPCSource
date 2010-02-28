e9c95ea46f246eebeecd1fda48940a1a9f30b32f
florian <florian@freepascal.org>
2010-02-28 09:56:36 +0000

    + NvApi package for win32 and win64 by Dmitry Boyarintsev, resolves #15771
    git-svn-id: trunk@14951 -

---
+++ packages/nvapi/fpmake.pp
@@ -0,0 +1,36 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('nvapi');
+{$ifdef ALLPACKAGES}
+    P.Directory:='nvapi';
+{$endif ALLPACKAGES}
+    P.Version:='2.4.0';
+    P.Author := 'NVidia, Andreas Hausladen, Dmitry "skalogryz" Boyarintsev';
+    P.License := 'NVidia license';
+    P.HomepageURL := 'nvidia.com';
+    P.Email := '';
+    P.Description := 'NvAPI header';
+    P.NeedLibC:= true;
+
+    P.SourcePath.Add('src');
+    P.IncludePath.Add('src');
+
+    T:=P.Targets.AddUnit('nvapi.pas');
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
