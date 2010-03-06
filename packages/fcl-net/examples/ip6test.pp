d960a13c10cdda0a66e51ece6636ef54148696be
marco <marco@freepascal.org>
2010-03-06 12:28:57 +0000

    --- Merging r14956 into '.': U    packages/fcl-net/examples/ip6test.pp --- Merging r14957 into '.': G    packages/fcl-net/examples/ip6test.pp
    # revisions: 14956,14957
    ------------------------------------------------------------------------
    r14956 | marco | 2010-02-28 17:26:42 +0100 (Sun, 28 Feb 2010) | 2 lines
    Changed paths:
       M /trunk/packages/fcl-net/examples/ip6test.pp
    
     * fixes for demise of 6bone and sockets unit changes.
    
    ------------------------------------------------------------------------
    ------------------------------------------------------------------------
    r14957 | marco | 2010-02-28 17:38:45 +0100 (Sun, 28 Feb 2010) | 2 lines
    Changed paths:
       M /trunk/packages/fcl-net/examples/ip6test.pp
    
     * ip6test errorhandling fix + parameters. (trying to reproduce mantis 4867)
    
    ------------------------------------------------------------------------
    
    git-svn-id: branches/fixes_2_4@14979 -

--- packages/fcl-net/examples/ip6test.pp
+++ packages/fcl-net/examples/ip6test.pp
@@ -14,19 +14,32 @@ var
   i: integer;
   t1,t2:text;
   x: array of thostaddr6;
+  hname : string;
+
+Const
+  ip6hosttest = 'whatismyv6.com';
 
 begin
+  hname:=ip6hosttest;
+  if paramcount>0 then
+    hname:=paramstr(1);
   setlength(x, 100);
-  setlength(x,resolvename6('www.6bone.net', x));
+  i:=resolvename6(hname, x);
+  if i=-1 then
+    begin
+      writeln('Domain not found, ',hname);
+      halt;
+    end;
+  setlength(x,i);
   if length(x) = 0 then halt(2);
   with dest do begin
     sin6_family := PF_INET6;
     sin6_port   := shorthosttonet(80);
-    sin6_addr.u6_addr16 := x[0];
+    sin6_addr.u6_addr16 := x[0].u6_addr16;
   end;
-  sock := socket(PF_INET6, SOCK_STREAM, 6 {TCP});
+  sock := fpsocket(PF_INET6, SOCK_STREAM, 6 {TCP});
 
-  if Connect(sock, dest, sizeof(dest)) then begin
+  if fpConnect(sock, @dest, sizeof(dest))=0 then begin
     sock2text(sock,t1,t2);
     writeln(t2, 'GET / HTTP/1.0');
     writeln(t2);
@@ -35,7 +48,7 @@ begin
       writeln(s);
     end;
   end else begin
-    writeln('not connected: ',getlasterror, ': ', StrError(getlasterror));
+    writeln('not connected: ',socketerror, ': ', StrError(socketerror));
   end;
   closesocket(sock);
 end.
