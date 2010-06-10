af15fa4ec447c74179a5f1ce0dd2e147d67c5405
Jonas Maebe <jonas@freepascal.org>
2010-06-10 12:00:03 +0000

    * fixed test for 64 bit platforms
    git-svn-id: trunk@15402 -

--- tests/webtbs/tw16668.pp
+++ tests/webtbs/tw16668.pp
@@ -8,8 +8,13 @@ procedure Foo;
 var
   a: TObject;
 begin
+{$ifdef cpu32}
   if ptruint(a)<>$55555555 then
     halt(1);
+{$else}
+  if ptruint(a)<>$5555555555555555 then
+    halt(1);
+{$endif}
 end;
 
 begin
