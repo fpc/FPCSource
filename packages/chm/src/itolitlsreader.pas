532fb57eee7e4e836e9c52154a9a112b7741a199
andrew <andrew@idefix.freepascal.org>
2010-07-11 12:46:36 +0000

    * updated copyrights
    git-svn-id: trunk@15549 -

--- packages/chm/src/itolitlsreader.pas
+++ packages/chm/src/itolitlsreader.pas
@@ -1,4 +1,4 @@
-{ Copyright (C) <2009> <Andrew Haines> itloitlsreader.pas
+{ Copyright (C) <2010> <Andrew Haines> itloitlsreader.pas
 
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
--- packages/chm/src/itsftransform.pas
+++ packages/chm/src/itsftransform.pas
@@ -1,4 +1,4 @@
-{ Copyright (C) <2009> <Andrew Haines> itfstransform.pas
+{ Copyright (C) <2010> <Andrew Haines> itfstransform.pas
 
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Library General Public License as published by
