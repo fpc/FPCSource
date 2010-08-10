38da67e08b7f4a6e2a3cac39d476a02ea2fdabb1
paul <paul@idefix.freepascal.org>
2010-08-10 04:30:59 +0000

    tests: add hint directives test
    git-svn-id: trunk@15760 -

---
+++ tests/test/thintdir1.pp
@@ -0,0 +1,16 @@
+program thintdir1;
+
+// test the possibility to use the hint modifiers as regular identifiers
+
+{$mode delphi}
+
+type
+  deprecated = integer;
+const
+  unimplemented = 1;
+  platform = 2;
+var
+  experimental: deprecated = unimplemented;
+
+begin
+end.                      
