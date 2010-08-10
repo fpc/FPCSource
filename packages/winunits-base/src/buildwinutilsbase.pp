9509437a96abd6cb36e3a501f18f4c600a9fc9dd
florian <florian@freepascal.org>
2010-08-10 20:17:20 +0000

    * converted winspool.h from MingW and added it as winspool.pp, resolves #17174
    git-svn-id: trunk@15766 -

--- packages/winunits-base/src/buildwinutilsbase.pp
+++ packages/winunits-base/src/buildwinutilsbase.pp
@@ -23,7 +23,8 @@ interface
 uses
     flatsb, winver, mmsystem, comconst, commctrl, comobj, commdlg,
     ole2, activex, shellapi, shlobj, oleserver,  shfolder, richedit,
-    imagehlp, wininet, uxtheme, dwmapi, multimon, htmlhelp, winutils,comserv;
+    imagehlp, wininet, uxtheme, dwmapi, multimon, htmlhelp, winutils,
+    comserv, winspool;
 
 implementation
 
