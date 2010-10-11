30297619c1d03828a856ee420bf3d23c9e281e23
pierre <pierre@freepascal.org>
2010-10-11 15:42:30 +0000

    --- Merging r15939 into '.': A    tests/webtbs/tw17337.pp U    compiler/x86/agx86att.pas
    # revisions: 15939
    ------------------------------------------------------------------------
    r15939 | pierre | 2010-09-03 01:41:02 +0200 (Fri, 03 Sep 2010) | 1 line
    Changed paths:
       M /trunk/compiler/x86/agx86att.pas
       A /trunk/tests/webtbs/tw17337.pp
    
     * Fix for win64 GNU AS bug
    ------------------------------------------------------------------------
    
    git-svn-id: branches/fixes_2_4@16129 -

---
+++ tests/webtbs/tw17337.pp
@@ -0,0 +1,32 @@
+{ %target=win64, linux, freebsd }
+{ %cpu=x86_64 }
+{ %opt=-Aas }
+
+{$asmmode att}
+
+procedure test_gas;   
+var
+  test : qword;
+begin
+  test:=$5ffffffff;
+  if (test < qword($2ffffffff)) then
+    runerror(1);
+  if (test < qword($ffffffff)) then
+    runerror(2);
+asm
+  movq $0xffffffff,test
+end;
+  if test <> $ffffffff then
+    runerror(5);
+end ;
+  
+var
+  test : qword;
+begin
+  test:=$5ffffffff;
+  if (test < qword($2ffffffff)) then
+    runerror(3);
+  if (test < qword($ffffffff)) then
+    runerror(4);
+  test_gas;
+end.
