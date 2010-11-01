d8eccf008d1066c7ad0f90aad10f038317ac2205
sergei <gorelkin@nanoreflex.ru>
2010-11-01 22:38:15 +0000

    + Test for Mantis #17560
    git-svn-id: trunk@16291 -

---
+++ tests/webtbs/tw17560.pp
@@ -0,0 +1,28 @@
+{ %OPT=-gh }
+// Creating a suspended TThread and then destroying it should not cause memory leaks.
+// Also, Execute() method should not be called.
+
+{$ifdef fpc}{$mode objfpc}{$endif}
+
+uses SysUtils, Classes;
+
+type
+  TMyThread = class(TThread)
+    procedure Execute; override;
+  end;
+
+var
+  t: TThread;
+  Flag: Boolean;
+  
+procedure TMyThread.Execute;
+begin
+  flag := True;
+end;
+
+begin
+  Flag := False;
+  t := TMyThread.Create(True);
+  t.Free;
+  Halt(ord(Flag));
+end.
