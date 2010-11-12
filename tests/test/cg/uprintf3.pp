dbfcb2d5b82a5f7b50aeabfd84d3df3f9028a99b
Jonas Maebe <jonas@freepascal.org>
2010-11-12 22:08:15 +0000

    - removed darwin-specific external name for printf, no longer required after     r16330
    git-svn-id: trunk@16331 -

--- tests/test/cg/uprintf3.pp
+++ tests/test/cg/uprintf3.pp
@@ -20,13 +20,8 @@ const
   int64prefix='I64';
 {$else}
 {$linklib c}
-{$ifndef darwin}
 procedure printf(const formatstr : pchar; ...); external;
 procedure sprintf(p : pchar;const formatstr : pchar; ...); external;
-{$else darwin}
-procedure printf(const formatstr : pchar; ...); external name '_printf';
-procedure sprintf(p : pchar;const formatstr : pchar; ...); external name '_sprintf';
-{$endif darwin}
 const
   int64prefix='ll';
 {$endif}
