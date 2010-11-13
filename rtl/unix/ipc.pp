0cb2fa1baf31d9318cebc88d8de9658f17a73931
Jonas Maebe <jonas@freepascal.org>
2010-11-13 18:37:43 +0000

    + semtimedop() for Linux, marked as "platform" (last part of patch from     mantis #14075)
    git-svn-id: trunk@16339 -

--- rtl/unix/ipc.pp
+++ rtl/unix/ipc.pp
@@ -550,6 +550,9 @@ Type
 Function semget(key:Tkey; nsems:cint; semflg:cint): cint; {$ifdef FPC_USE_LIBC} cdecl; external clib name 'semget'; {$endif}
 Function semop(semid:cint; sops: psembuf; nsops: cuint): cint; {$ifdef FPC_USE_LIBC} cdecl; external clib name 'semop'; {$endif}
 Function semctl(semid:cint; semnum:cint; cmd:cint; var arg: tsemun): cint;
+{$ifdef linux}
+Function semtimedop(semid:cint; sops: psembuf; nsops: cuint; timeOut: ptimespec): cint; platform; {$ifdef FPC_USE_LIBC} cdecl; external name 'semtimedop'; {$endif}
+{$endif}
 
 implementation
 
