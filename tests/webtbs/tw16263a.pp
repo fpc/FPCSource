c14574bb56020c91441fc4a44e143e3f53588ec3
Jonas Maebe <jonas@freepascal.org>
2010-11-14 16:00:25 +0000

    * don't change the fpu control word in the initialisation code of dynamic     libraries (mantis #16263, #16801)
    git-svn-id: trunk@16347 -

---
+++ tests/webtbs/tw16263a.pp
@@ -0,0 +1,26 @@
+{ %norun }
+{ %target=darwin,linux,freebsd,solaris,beos,haiku }
+
+{$mode delphi}
+
+{$ifdef darwin}
+{$PIC+}
+{$endif darwin}
+
+{$ifdef CPUX86_64}
+{$ifndef WINDOWS}
+{$PIC+}
+{$endif WINDOWS}
+{$endif CPUX86_64}
+
+library tw16263a;
+
+function divide(d1,d2: double): double; cdecl;
+begin
+  divide:=d1/d2;
+end;
+
+begin
+  // check that the library does not re-enable fpu exceptions
+  divide(1.0,0.0);
+end.
