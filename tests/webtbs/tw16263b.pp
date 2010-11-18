a5473c33cc178737c0dea854725b61f82275c01f
Jonas Maebe <jonas@freepascal.org>
2010-11-18 22:41:42 +0000

    * part of r16347, forgot to commit
    git-svn-id: trunk@16367 -

---
+++ tests/webtbs/tw16263b.pp
@@ -0,0 +1,31 @@
+{ %target=darwin,linux,freebsd,solaris,beos,haiku }
+{ %NEEDLIBRARY }
+
+{$mode delphi}
+program MainApp;
+
+uses
+  dynlibs,
+  Math;
+
+const
+{$ifdef windows}
+  libname='tw16263a.dll';
+{$else}
+  {$ifdef darwin}
+  libname = './libtw16263a.dylib';
+  {$else darwin}
+  libname = './libtw16263a.so';
+  {$endif darwin}
+{$endif}
+
+var
+  hdl: TLibHandle; 
+begin
+  // the library will perform a div-by-zero in its init code
+  setexceptionmask([exZeroDivide]);
+  hdl := loadlibrary(libname);
+  if (hdl=nilhandle) then
+    halt(1);
+  unloadlibrary(hdl);
+end.
