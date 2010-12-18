49e3c30bd1a4a8c230f46acca9a405b72433a6ef
sergei <gorelkin@nanoreflex.ru>
2010-12-18 18:43:40 +0000

    * Changed the test so it uses typecasting instead of calling the conversion helpers from VarUtils.
    git-svn-id: trunk@16589 -

--- tests/webtbs/tw14536.pp
+++ tests/webtbs/tw14536.pp
@@ -4,7 +4,7 @@ program test_varbyref;
 {$apptype console}
 
 uses
-  Variants, VarUtils;
+  Variants;
 
 var
   V: Variant;
@@ -13,22 +13,21 @@ begin
   P := 1;
   TVarData(V).vtype := varbyref or varinteger;
   TVarData(V).vpointer := @P;
-  WriteLn(VariantToAnsiString(TVarData(V)));
-  WriteLn(VariantToSmallInt(TVarData(V)));
-  WriteLn(VariantToLongint(TVarData(V)));
-  WriteLn(VariantToShortint(TVarData(V)));
-  WriteLn(VariantToCardinal(TVarData(V)));
-  WriteLn(VariantToSingle(TVarData(V)));
-  WriteLn(VariantToDouble(TVarData(V)));
-  WriteLn(VariantToDate(TVarData(V)));
-  WriteLn(VariantToCurrency(TVarData(V)));
-  WriteLn(VariantToBoolean(TVarData(V)));
-  WriteLn(VariantToByte(TVarData(V)));
-  WriteLn(VariantToInt64(TVarData(V)));
-  WriteLn(VariantToQWord(TVarData(V)));
-  WriteLn(VariantToWideString(TVarData(V)));
-  WriteLn(VariantToAnsiString(TVarData(V)));
-  WriteLn(VariantToShortString(TVarData(V)));
+  WriteLn(string(V));
+  WriteLn(SmallInt(V));
+  WriteLn(Longint(V));
+  WriteLn(ShortInt(V));
+  WriteLn(Cardinal(V));
+  WriteLn(Single(V));
+  WriteLn(Double(V));
+  WriteLn(TDateTime(V));
+  WriteLn(Currency(V));
+  WriteLn(Boolean(V));
+  WriteLn(Byte(V));
+  WriteLn(Int64(V));
+  WriteLn(QWord(V));
+  WriteLn(WideString(V));
+  WriteLn(ShortString(V));
   WriteLn(V);
   TVarData(V).vtype := varEmpty;
   TVarData(V).vpointer := nil;
