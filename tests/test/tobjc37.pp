a85b058faaa8dc68ba08a1476bc37adedae077e0
Jonas Maebe <jonas@freepascal.org>
2011-02-10 11:42:51 +0000

    * only for darwin
    git-svn-id: trunk@16904 -

--- tests/test/tobjc37.pp
+++ tests/test/tobjc37.pp
@@ -1,4 +1,8 @@
+{ %target=darwin }
+{ %cpu=powerpc,powerpc64,i386,x86_64,arm }
 { %norun }
+
+{ Written by Jonas Maebe in 2010, released into the public domain }
 
 {$mode objfpc}
 {$modeswitch objectivec1}
