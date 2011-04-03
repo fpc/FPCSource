6a0bd01953ab9b48d6bc15b98b5157bfb24011d4
michael <michael@freepascal.org>
2011-04-03 09:22:27 +0000

    * Added license
    git-svn-id: trunk@17234 -

--- packages/fcl-stl/src/gqueue.pp
+++ packages/fcl-stl/src/gqueue.pp
@@ -1,3 +1,15 @@
+{
+   This file is part of the Free Pascal FCL library.
+   BSD parts (c) 2011 Vlado Boza
+
+   See the file COPYING.FPC, included in this distribution,
+   for details about the copyright.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY;without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+**********************************************************************}
 {$mode objfpc}
 
 unit gqueue;
