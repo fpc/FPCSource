a31c879e7b113f0e8e09f48565ef4121412025b0
florian <florian@freepascal.org>
2011-04-15 20:53:11 +0000

    * run test only if the platform supports the extended type
    git-svn-id: trunk@17325 -

--- tests/webtbs/tw19077.pp
+++ tests/webtbs/tw19077.pp
@@ -3,11 +3,13 @@ uses
 var
   c : currency;
 begin
+{$ifdef FPC_HAS_TYPE_EXTENDED}
   c:=1.5625;
   c:=RoundTo(c,-1);
   if c<>1.6 then
     halt(1);
   writeln('ok');
+{$endif FPC_HAS_TYPE_EXTENDED}
 end.
 
 
