c61d37e616530bffccb4d541c2327853b8676af6
marco <marco@freepascal.org>
2011-05-27 19:55:05 +0000

    --- Merging r16482 into '.': U    rtl/objpas/sysutils/sysinth.inc --- Merging r16744 into '.': U    rtl/objpas/dateutil.inc U    rtl/objpas/sysutils/sysstr.inc U    rtl/objpas/sysutils/dati.inc U    rtl/objpas/cvarutil.inc U    rtl/objpas/fmtbcd.pp --- Merging r16951 into '.': G    rtl/objpas/fmtbcd.pp --- Merging r16952 into '.': G    rtl/objpas/fmtbcd.pp
    # revisions: 16482,16744,16951,16952
    ------------------------------------------------------------------------
    r16482 | marco | 2010-11-30 10:49:19 +0100 (Tue, 30 Nov 2010) | 4 lines
    Changed paths:
       M /trunk/rtl/objpas/sysutils/sysinth.inc
    
     * Delphi XE (2010?) compatible alias for defaultformatsettings.
     * deprecated separate globals (Delphi XE). Causes some deprecated
          warnings in dati.inc that still have to be fixed.
    
    ------------------------------------------------------------------------
    ------------------------------------------------------------------------
    r16744 | sergei | 2011-01-09 16:27:05 +0100 (Sun, 09 Jan 2011) | 2 lines
    Changed paths:
       M /trunk/rtl/objpas/cvarutil.inc
       M /trunk/rtl/objpas/dateutil.inc
       M /trunk/rtl/objpas/fmtbcd.pp
       M /trunk/rtl/objpas/sysutils/dati.inc
       M /trunk/rtl/objpas/sysutils/sysstr.inc
    
    * RTL: Replaced deprecated formatting symbols with their up-to-date counterparts, reduces compiler noise.
    * Also removed some unused variables.
    ------------------------------------------------------------------------
    ------------------------------------------------------------------------
    r16951 | joost | 2011-02-20 18:53:18 +0100 (Sun, 20 Feb 2011) | 1 line
    Changed paths:
       M /trunk/rtl/objpas/fmtbcd.pp
    
     * TryStrToBcd now returns true when it succeeds, and false when it fails, not the other way around
    ------------------------------------------------------------------------
    ------------------------------------------------------------------------
    r16952 | joost | 2011-02-20 18:59:12 +0100 (Sun, 20 Feb 2011) | 1 line
    Changed paths:
       M /trunk/rtl/objpas/fmtbcd.pp
    
     * Fixed BCDToStr for values between -1 and 0
    ------------------------------------------------------------------------
    
    git-svn-id: branches/fixes_2_4@17586 -

--- rtl/objpas/fmtbcd.pp
+++ rtl/objpas/fmtbcd.pp
@@ -1216,8 +1216,8 @@ IMPLEMENTATION
                                 end;
 { find out language-specific ? }
         DecimalPoint_is_System: begin
-                                 dp := DecimalSeparator;
-                                 dc := ThousandSeparator;
+                                 dp := DefaultFormatSettings.DecimalSeparator;
+                                 dc := DefaultFormatSettings.ThousandSeparator;
                                 end;
        end;
      end;
@@ -1417,7 +1417,7 @@ IMPLEMENTATION
                end;
 
     begin
-      result := False;
+      result := True;
       FillChar ( lvars, SizeOf ( lvars ), #0 );
       BCD := NullBCD;
       lav := Length ( aValue );
@@ -1468,31 +1468,31 @@ IMPLEMENTATION
                         '.': if ch = dp
                                then begin
                                  if inife <> inint
-                                   then result := True
+                                   then result := False
                                    else inife := infrac;
                                 end;
                         'e',
                         'E': if inife = inexp
-                               then result := True
+                               then result := False
                                else inife := inexp;
                         '+',
                         '-': if ( inife = inexp ) AND ( fp[inexp] = 0 )
                                then pse := i
-                               else result := True;
+                               else result := False;
                         else begin
-                          result := True;
+                          result := False;
                           errp := i;
                          end;
                        end;
                      end;
-                  if result
+                  if not result
                     then begin
-                      result := False;
+                      result := True;
                       for i := errp TO lav do
                         if aValue[i] <> ' '
-                          then result := True;
+                          then result := False;
                      end;
-                  if result
+                  if not result
                     then EXIT;
 
                   if ps <> 0
@@ -1503,15 +1503,15 @@ IMPLEMENTATION
                     then begin
                       exp := 0;
                       for i := fp[inexp] TO lp[inexp] do
-                        if NOT result
+                        if result
                           then
                             if aValue[i] <> dc
                               then begin
                                 exp := exp * 10 + ( Ord ( aValue[i] ) - Ord ( '0' ) );
                                 if exp > 999
-                                  then result := True;
+                                  then result := False;
                                end;
-                      if result
+                      if not result
                         then EXIT;
 
                       if pse <> 0
@@ -1545,16 +1545,16 @@ IMPLEMENTATION
                                 Dec ( p );
                                 Singles[p] := Ord ( aValue[i] ) - Ord ( '0' );
                                end
-                              else result := True;
+                              else result := False;
                            end;
-                  if result
+                  if not result
                     then EXIT;
 
                   FDig := p;
                   if LDig < 0
                     then LDig := 0;
                   Plac := LDig;
-                  result := NOT pack_BCD ( bh, BCD );
+                  result := pack_BCD ( bh, BCD );
                  end;
              end;
      end;
@@ -1565,7 +1565,7 @@ IMPLEMENTATION
       BCD : tBCD;
 
     begin
-      if TryStrToBCD ( aValue, BCD )
+      if not TryStrToBCD ( aValue, BCD )
         then begin
           RAISE eBCDOverflowException.create ( 'in StrToBCD' );
          end
@@ -1727,7 +1727,10 @@ IMPLEMENTATION
             then begin
 {$ifndef use_ansistring}
               Inc ( l );
-              result[1] := '0';
+              if Neg then
+                result[2] := '0'
+              else
+                result[1] := '0'
 {$else}
               result := result + '0';
 {$endif}
