9608c4eabe329844d2eb4a84a341bf7430decb5e
sergei <gorelkin@nanoreflex.ru>
2011-06-02 10:49:28 +0000

    * Interface wrappers must be global when linking smart, Mantis #19462. Other targets already have this condition in place.
    git-svn-id: trunk@17631 -

--- compiler/sparc/cgcpu.pas
+++ compiler/sparc/cgcpu.pas
@@ -1363,7 +1363,7 @@ implementation
           Internalerror(200109191);
 
         make_global:=false;
-        if (not current_module.is_unit) or
+        if (not current_module.is_unit) or create_smartlink or
            (procdef.owner.defowner.owner.symtabletype=globalsymtable) then
           make_global:=true;
 
