ede0a5a92969b4ca85bcfe90e94dabddff8025f7
pierre <pierre@freepascal.org>
2011-06-20 21:07:23 +0000

    * Restrict test to Windows OS
    git-svn-id: trunk@17778 -

--- tests/test/library/tlib1b.pp
+++ tests/test/library/tlib1b.pp
@@ -1,18 +1,8 @@
-{ %skiptarget=go32v2 }
+{ %target=win32,win64,wince }
 { %needlibrary }
-{$goto on}
-
 
 { Checks that the two functions with the same exported name 'p'
   are each loaded correctly. }
-uses
-  {$ifdef unix}dl,{$endif unix}sysutils;
-
-{$ifdef darwin}
-{$linklib tlib1a}
-{$linklib tlib1a2}
-{$endif darwin}
-
 procedure p(var a : dword);external 'tlib1a' name 'p';
 procedure p2(var a : dword);external 'tlib1a2' name 'p';
 
