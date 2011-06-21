3874bc7d7449057d80796888ebe3bf7c0fc1bef2
pierre <pierre@freepascal.org>
2011-06-21 19:56:55 +0000

    + Check that dll imports  in units also works
    git-svn-id: trunk@17785 -

---
+++ tests/test/library/ulib2a.pp
@@ -0,0 +1,16 @@
+{ %target=win32,win64,wince }
+{ %needlibrary }
+
+unit
+  ulib2a;
+
+interface
+{ Checks that the two functions with the same exported name 'p'
+  are each loaded correctly. }
+procedure p(var a : dword);external 'tlib1a' name 'p';
+procedure p2(var a : dword);external 'tlib1a2' name 'p';
+procedure p3(var a : dword);external 'tlib1a' name 'p';
+
+implementation
+
+end.
