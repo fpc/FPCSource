7d99f95c45e07d471f101c79cdbd87c07c142482
sergei <gorelkin@nanoreflex.ru>
2011-06-24 02:05:56 +0000

    * Always create a section before emitting data to current_asmdata.asmlists[al_typedconsts]. Without it, such data ends up in sections created elsewhere, creating very non-obvious dependencies on other parts of compiler.
    git-svn-id: trunk@17816 -

--- compiler/arm/narmcnv.pas
+++ compiler/arm/narmcnv.pas
@@ -171,7 +171,7 @@ implementation
                         cg.a_jmp_flags(current_asmdata.CurrAsmList,F_GE,l2);
 
                         hregister:=cg.getfpuregister(current_asmdata.CurrAsmList,OS_F64);
-                        current_asmdata.asmlists[al_typedconsts].concat(tai_align.create(const_align(8)));
+                        new_section(current_asmdata.asmlists[al_typedconsts],sec_rodata_norel,l1.name,const_align(8));
                         current_asmdata.asmlists[al_typedconsts].concat(Tai_label.Create(l1));
                         { I got this constant from a test program (FK) }
                         current_asmdata.asmlists[al_typedconsts].concat(Tai_const.Create_32bit($41f00000));
