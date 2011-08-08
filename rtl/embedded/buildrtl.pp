abd5ca46c824ed9d60d2bc62219cfe7dd5e2f611
florian <florian@freepascal.org>
2011-08-08 20:10:19 +0000

    + build unit and lazarus project for the embedded target
    git-svn-id: trunk@18151 -

---
+++ rtl/embedded/buildrtl.pp
@@ -0,0 +1,27 @@
+{ This unit is only used to edit the rtl with lazarus }
+unit buildrtl;
+
+  interface
+
+    uses
+      system, unixtype, ctypes, baseunix, strings, objpas, macpas, syscall, unixutil,
+      fpintres, heaptrc, lineinfo, lnfodwrf,
+      termio, unix, linux, initc, cmem, mmx,
+      crt, printer, linuxvcs,
+      sysutils, typinfo, math, matrix, varutils,
+      charset, ucomplex, getopts,
+      errors, sockets, gpm, ipc, serial, terminfo, dl, dynlibs,
+      video, mouse, keyboard, variants, types, dateutils, sysconst, fmtbcd,
+      cthreads, classes, fgl, convutils, stdconvs, strutils, rtlconsts, dos, objects, cwstring, fpcylix, clocale,
+      exeinfo,
+{$ifdef CPUARM}
+      stellaris
+{$endif CPUARM}
+{$ifdef CPUAVR}
+      atmega128
+{$endif CPUAVR}
+      ;
+
+  implementation
+
+end.
