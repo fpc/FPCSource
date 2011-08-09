bafbbe9f0ff37ba7326a709f86d063a3b3476317
pierre <pierre@freepascal.org>
2011-08-09 15:52:03 +0000

    * Fix bug report 7210
    git-svn-id: trunk@18154 -

--- compiler/x86/agx86nsm.pas
+++ compiler/x86/agx86nsm.pas
@@ -481,7 +481,14 @@ interface
       begin
         AsmLn;
         AsmWrite('SECTION ');
-        AsmWrite(secnames[atype]);
+        { go32v2 stub only loads .text and .data sections, and allocates space for .bss.
+          Thus, data which normally goes into .rodata and .rodata_norel sections must
+          end up in .data section }
+        if (atype in [sec_rodata,sec_rodata_norel]) and
+          (target_info.system=system_i386_go32v2) then
+          AsmWrite('.data')
+        else
+          AsmWrite(secnames[atype]);
         if create_smartlink_sections and
            (atype<>sec_bss) and
            (aname<>'') then
