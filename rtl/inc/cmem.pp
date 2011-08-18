41f25c2b210324536e235b7b47aeb61eba8bddd3
florian <florian@freepascal.org>
2011-08-18 08:47:44 +0000

    * fixes cmem.malloc et al. for windows as proposed by Ludo Brands, resolves #15571
    git-svn-id: trunk@18256 -

--- rtl/inc/cmem.pp
+++ rtl/inc/cmem.pp
@@ -48,10 +48,10 @@ Procedure free (P : pointer); cdecl; external;
 function realloc (P : Pointer; Size : ptruint) : pointer;cdecl; external;
 Function calloc (unitSize,UnitCount : ptruint) : pointer;cdecl; external;
 {$else not USE_STATIC_LIBC}
-Function Malloc (Size : ptruint) : Pointer; {$ifdef win32}stdcall{$else}cdecl{$endif}; external LibName name 'malloc';
-Procedure Free (P : pointer); {$ifdef win32}stdcall{$else}cdecl{$endif}; external LibName name 'free';
-function ReAlloc (P : Pointer; Size : ptruint) : pointer; {$ifdef win32}stdcall{$else}cdecl{$endif}; external LibName name 'realloc';
-Function CAlloc (unitSize,UnitCount : ptruint) : pointer; {$ifdef win32}stdcall{$else}cdecl{$endif}; external LibName name 'calloc';
+Function Malloc (Size : ptruint) : Pointer; cdecl; external LibName name 'malloc';
+Procedure Free (P : pointer); cdecl; external LibName name 'free';
+function ReAlloc (P : Pointer; Size : ptruint) : pointer; cdecl; external LibName name 'realloc';
+Function CAlloc (unitSize,UnitCount : ptruint) : pointer; cdecl; external LibName name 'calloc';
 {$endif not USE_STATIC_LIBC}
 
 implementation
