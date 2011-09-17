a87bb4283db0812a96ebcdbb9dd17713131b4296
paul <paul@idefix.freepascal.org>
2011-09-17 14:13:49 +0000

    merge r17604 from cpstrnew branch by inoussa: tests
    git-svn-id: trunk@19123 -

---
+++ tests/test/tcpstransistr2shortstring.pp
@@ -0,0 +1,44 @@
+﻿{$apptype console}
+uses
+{$ifdef unix}
+  cwstring,
+{$endif unix}
+  sysutils;
+  
+type  
+  ts866 = type string<866>;
+
+  procedure doerror(ANumber : Integer);
+  begin
+    WriteLn('error ',ANumber);
+    Halt(ANumber);
+  end;
+
+var
+  s : ts866;
+  i : Integer;
+  sa : ansistring;
+  ss : ShortString;
+begin
+  sa := '123'#196#200#250;
+  ss := sa;
+  if (Length(sa) <> Length(ss)) then
+    doerror(1);
+  for i := 1 to Length(sa) do
+    begin
+      if (Byte(ss[i]) <> Byte(sa[i])) then
+        doerror(2)
+    end;
+
+  s := '123'#196#200#250;
+  ss := s;
+  if (Length(s) <> Length(ss)) then
+    doerror(3);
+  for i := 1 to Length(s) do
+    begin
+      if (Byte(ss[i]) <> Byte(s[i])) then
+        doerror(4)
+    end;
+
+  WriteLn('Ok');
+end.
