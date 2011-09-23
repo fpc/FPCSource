f0c8563f2d12cd4e5e249e0b1411f16cfb8052f8
Jonas Maebe <jonas@freepascal.org>
2011-09-23 16:43:16 +0000

    + cwstring for unix platforms
    git-svn-id: trunk@19192 -

--- tests/test/tcpstr1.pp
+++ tests/test/tcpstr1.pp
@@ -1,4 +1,10 @@
 {$CODEPAGE cp437}
+
+{$ifdef unix}
+uses
+  cwstring;
+{$endif}
+
 type
   tcpstr437 = type AnsiString(437);
   tcpstr850 = type AnsiString(850);
@@ -13,19 +19,19 @@ begin
   if ord(a2[1])<>195 then
     halt(1);
   if ord(a2[2])<>164 then
-    halt(1);
+    halt(2);
 
   writeln('---');
 
   a3:=a1;
   if ord(a3[1])<>132 then
-    halt(1);
+    halt(3);
 
   writeln('---');
 
   u1:=a1;
   if ord(u1[1])<>228 then
-    halt(1);
+    halt(4);
 
   writeln('ok');
 end.
