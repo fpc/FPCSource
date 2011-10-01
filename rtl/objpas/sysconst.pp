c0995c2ffda96f2521439c61813d27bbd59b025e
michael <michael@freepascal.org>
2011-10-01 14:42:54 +0000

    * Moved string constant to sysconst
    git-svn-id: trunk@19315 -

--- rtl/objpas/sysconst.pp
+++ rtl/objpas/sysconst.pp
@@ -36,6 +36,7 @@ resourcestring
   SDivByZero             = 'Division by zero';
   SEndOfFile             = 'Read past end of file';
   SErrInvalidDateMonthWeek = 'Year %d, month %d, Week %d and day %d is not a valid date.';
+  SerrInvalidHourMinuteSecMsec = '%d:%d:%d.%d is not a valid time specification';
   SErrInvalidDateWeek    = '%d %d %d is not a valid dateweek';
   SErrInvalidDayOfWeek   = '%d is not a valid day of the week';
   SErrInvalidDayOfWeekInMonth = 'Year %d Month %d NDow %d DOW %d is not a valid date';
