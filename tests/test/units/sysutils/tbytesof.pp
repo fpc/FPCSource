14c2f248dfc8afa82de4f02dbaa41c1a49487702
paul <paul@idefix.freepascal.org>
2011-10-06 06:58:07 +0000

    rtl: add WideBytesOf(UnicodeString): TBytes for delphi compatibility
    git-svn-id: trunk@19391 -

--- tests/test/units/sysutils/tbytesof.pp
+++ tests/test/units/sysutils/tbytesof.pp
@@ -17,6 +17,18 @@ begin
       Result := Result and (B[I] = Etalon[I]);
 end;
 
+function CheckWideBytes(const B: TBytes): Boolean;
+const
+  Etalon: array[0..7] of Byte = (84, 00, 101, 00, 115, 00, 116, 00);
+var
+  I: Integer;
+begin
+  Result := Length(B) <= Length(Etalon);
+  if Result then
+    for I := Low(B) to High(B) do
+      Result := Result and (B[I] = Etalon[I]);
+end;
+
 var
   S: AnsiString;
   U: UnicodeString;
@@ -36,4 +48,7 @@ begin
   B := BytesOf(U[1]);
   if not CheckBytes(B) then
     halt(4);
+  B := WideBytesOf(U);
+  if not CheckWideBytes(B) then
+    halt(5);
 end.
