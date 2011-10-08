5955d477e92e6f86b6f8b6d254b6e29a8e57ac09
marco <marco@freepascal.org>
2011-10-08 19:56:20 +0000

    * add openbsd set own target
    git-svn-id: trunk@19422 -

--- compiler/systems.pas
+++ compiler/systems.pas
@@ -717,6 +717,10 @@ begin
     default_target(system_i386_freebsd);
     {$define default_target_set}
    {$endif}
+   {$ifdef openbsd}
+    default_target(system_i386_openbsd);
+    {$define default_target_set}
+   {$endif}
    {$ifdef darwin}
     default_target(system_i386_darwin);
     {$define default_target_set}
