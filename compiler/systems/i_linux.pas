8d2a7f3b8871a0cae76c2c05946a076a146147a3
Jonas Maebe <jonas@freepascal.org>
2011-11-24 23:46:05 +0000

    * switched linux/ppc64 to tf_smartlink_sections (works fine even with     binutils 2.17, which is kind of ancient by today's standards),     since it's much faster than tf_smartlink_library without an internal     assembler
    git-svn-id: trunk@19680 -

--- compiler/systems/i_linux.pas
+++ compiler/systems/i_linux.pas
@@ -289,7 +289,7 @@ unit i_linux;
             name         : 'Linux for PowerPC64';
             shortname    : 'Linux';
             flags        : [tf_needs_symbol_size,tf_needs_symbol_type,tf_files_case_sensitive,
-                            tf_requires_proper_alignment,tf_smartlink_library,tf_has_winlike_resources];
+                            tf_requires_proper_alignment,tf_smartlink_sections,tf_has_winlike_resources];
             cpu          : cpu_powerpc64;
             unit_env     : '';
             extradefines : 'UNIX;HASUNIX';
