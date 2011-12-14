724f41a9af50414df6075105aacfcd5e76b7316e
sergei <gorelkin@nanoreflex.ru>
2011-12-14 22:40:08 +0000

    - Cleaned out FPC_STRTOSHORTSTRPROC and FPC_STRTOCHARARRAYPROC defines. Somebody had to do it one day.
    git-svn-id: trunk@19851 -

--- rtl/inc/strings.pp
+++ rtl/inc/strings.pp
@@ -120,25 +120,13 @@ implementation
 
 { Functions, different from the one in sysutils }
 
-{$ifndef FPC_STRTOSHORTSTRINGPROC}
 
-    { also define alias which can be used inside the system unit }
-    function fpc_pchar_to_shortstr(p:pchar):shortstring;[external name 'FPC_PCHAR_TO_SHORTSTR'];
-
-{$else FPC_STRTOSHORTSTRINGPROC}
-
-    { also define alias which can be used inside the system unit }
     procedure fpc_pchar_to_shortstr(var res : openstring;p:pchar);[external name 'FPC_PCHAR_TO_SHORTSTR'];
 
-{$endif FPC_STRTOSHORTSTRINGPROC}
 
     function strpas(p:pchar):shortstring;{$ifdef SYSTEMINLINE}inline;{$endif}
       begin
-    {$ifndef FPC_STRTOSHORTSTRINGPROC}
-        strpas:=fpc_pchar_to_shortstr(p);
-    {$else FPC_STRTOSHORTSTRINGPROC}
         fpc_pchar_to_shortstr(strpas,p);
-    {$endif FPC_STRTOSHORTSTRINGPROC}
       end;
 
     function stralloc(L : SizeInt) : pchar;
