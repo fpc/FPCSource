898fbdcd264490213618c5c906ea61d3aa7875ad
sergei <gorelkin@nanoreflex.ru>
2012-01-14 04:34:39 +0000

    * Disallow placing hidden 'high' parameter in register if it will be accessed from implicit finally block (happens for <value open array of managed type> parameters).
    git-svn-id: trunk@20072 -

--- compiler/pdecsub.pas
+++ compiler/pdecsub.pas
@@ -409,6 +409,11 @@ implementation
                hvs:=tparavarsym.create('$high'+name,paranr+1,vs_const,sinttype,[vo_is_high_para,vo_is_hidden_para]);
                hvs.symoptions:=[];
                owner.insert(hvs);
+               { don't place to register if it will be accessed from implicit finally block }
+               if (varspez=vs_value) and
+                 is_open_array(vardef) and
+                 is_managed_type(vardef) then
+                 hvs.varregable:=vr_none;
              end
            else
             begin
