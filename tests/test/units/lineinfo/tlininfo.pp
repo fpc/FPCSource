ee07ef72be668ea8b19f3355cc101c21c5b554f1
Tomas Hajny <hajny@freepascal.org>
2012-01-30 23:18:31 +0000

    * corrected file extension and output in case of failure enhanced
    git-svn-id: trunk@20200 -

--- tests/test/units/lineinfo/tlininfo.pp
+++ tests/test/units/lineinfo/tlininfo.pp
@@ -19,12 +19,25 @@ begin
  Success := GetLineInfo (PtrUInt (@TestProc) + 4, Func, Src, Line);
 {$IFDEF DEBUG}
  WriteLn ('Source address = ', HexStr (@TestProc));
- WriteLn ('Success = ', Success);
- WriteLn ('Func = ', Func);
- WriteLn ('Src = ', Src);
- WriteLn ('Line = ', Line);
+ WriteLn ('Success (GetLineInfo call) = ', Success);
+ WriteLn ('Func = ', Func, ' (expected ''TESTPROC'')');
+ WriteLn ('Src = ', Src, ' (expected ''tlininfo.pp'')');
+ WriteLn ('Line = ', Line, ' (expected 8)');
 {$ENDIF DEBUG}
- if not (Success and (Copy (Src, Length (Src) - 11, 12) = 'tlininfo.pas') and
+ if not (Success and (Copy (Src, Length (Src) - 11, 12) = 'tlininfo.pp') and
                                        (Func = 'TESTPROC') and (Line = 8)) then
-  Halt (1)
+  begin
+   WriteLn ('Failed!');
+{$IFNDEF DEBUG}
+   WriteLn ('Success of GetLineInfo call = ', Success);
+   WriteLn ('Func = ', Func, ' (expected ''TESTPROC'')');
+   WriteLn ('Src = ', Src, ' (expected ''tlininfo.pp'')');
+   WriteLn ('Line = ', Line, ' (expected 8)');
+{$ENDIF DEBUG}
+   Halt (1);
+  end
+{$IFDEF DEBUG}
+ else
+  WriteLn ('Test successful.')
+{$ENDIF DEBUG}
 end.
