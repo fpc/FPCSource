2ea60c9daf3c52a38cd76c6f9608b734cec93bbc
Tomas Hajny <hajny@freepascal.org>
2012-01-31 18:57:02 +0000

    * hopefully final correction (changed extension resulted in different length)
    git-svn-id: trunk@20206 -

--- tests/test/units/lineinfo/tlininfo.pp
+++ tests/test/units/lineinfo/tlininfo.pp
@@ -4,34 +4,40 @@ program tlininfo;
 
 { $DEFINE DEBUG}
 
-procedure TESTPROC;
+procedure TestProc;
 begin
  WriteLn ('TestProc running...');
 end;
 
 var
  Success: boolean;
- Func, Src: string;
+ Func, Src, SrcFName: string;
  Line: longint;
 
 begin
  TestProc;
- Success := GetLineInfo (PtrUInt (@TestProc) + 4, Func, Src, Line);
+ Success := GetLineInfo (PtrUInt (@TestProc) + 1, Func, Src, Line);
+ SrcFName := Copy (Src, Length (Src) - (Length ('tlininfo.pp') - 1),
+                                                       Length ('tlininfo.pp'));
 {$IFDEF DEBUG}
  WriteLn ('Source address = ', HexStr (@TestProc));
  WriteLn ('Success (GetLineInfo call) = ', Success);
- WriteLn ('Func = ', Func, ' (expected ''TESTPROC'')');
- WriteLn ('Src = ', Src, ' (expected ''tlininfo.pp'')');
+ WriteLn ('Func = ''', Func, ''' (expected ''TESTPROC'')');
+ WriteLn ('Src = ', Src);
+ WriteLn ('Filename part of source path = ''',
+                                    SrcFName, ''' (expected ''tlininfo.pp'')');
  WriteLn ('Line = ', Line, ' (expected 8)');
 {$ENDIF DEBUG}
- if not (Success and (Copy (Src, Length (Src) - 11, 12) = 'tlininfo.pp') and
+ if not (Success and (SrcFName = 'tlininfo.pp') and
                                        (Func = 'TESTPROC') and (Line = 8)) then
   begin
    WriteLn ('Failed!');
 {$IFNDEF DEBUG}
    WriteLn ('Success of GetLineInfo call = ', Success);
-   WriteLn ('Func = ', Func, ' (expected ''TESTPROC'')');
-   WriteLn ('Src = ', Src, ' (expected ''tlininfo.pp'')');
+   WriteLn ('Func = ''', Func, ''' (expected ''TESTPROC'')');
+   WriteLn ('Src = ', Src);
+   WriteLn ('Filename part of source path = ''',
+                                    SrcFName, ''' (expected ''tlininfo.pp'')');
    WriteLn ('Line = ', Line, ' (expected 8)');
 {$ENDIF DEBUG}
    Halt (1);
