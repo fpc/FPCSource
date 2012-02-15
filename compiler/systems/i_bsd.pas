5b71a7597a884bae0f62143153ebce1cebcb4098
pierre <pierre@freepascal.org>
2012-02-15 13:08:49 +0000

    i386 NetBSD uses GOT for position independent code
    git-svn-id: trunk@20353 -

--- compiler/systems/i_bsd.pas
+++ compiler/systems/i_bsd.pas
@@ -191,7 +191,7 @@ unit i_bsd;
             system       : system_i386_NetBSD;
             name         : 'NetBSD for i386';
             shortname    : 'NetBSD';
-            flags        : [tf_under_development,tf_files_case_sensitive,tf_smartlink_library,tf_has_winlike_resources];
+            flags        : [tf_pic_uses_got,tf_under_development,tf_files_case_sensitive,tf_smartlink_library,tf_has_winlike_resources];
             cpu          : cpu_i386;
             unit_env     : 'BSDUNITS';
             extradefines : 'UNIX;BSD;HASUNIX';
