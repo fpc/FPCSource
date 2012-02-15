f597d18f24ebfa3042d7d25e22c5e12d58fe116f
Tomas Hajny <hajny@freepascal.org>
2012-02-15 21:14:07 +0000

    * include imlib for OS/2 and EMX too
    git-svn-id: trunk@20357 -

--- packages/imlib/fpmake.pp
+++ packages/imlib/fpmake.pp
@@ -25,7 +25,7 @@ begin
     P.Description := 'Headers to imlib, an efficient bitmap manipulation program';
     P.NeedLibC:= true;  // true for headers that indirectly link to libc?
 
-    P.OSes:=AllUnixOSes-[darwin,iphonesim];
+    P.OSes:=AllUnixOSes+[OS2,EMX]-[darwin,iphonesim];
     if Defaults.CPU<>arm then
       P.OSes := P.OSes + [darwin];
 
