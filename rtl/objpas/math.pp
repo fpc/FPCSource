ae1ece93e53cd1552e943ee646d982136e49a90d
marco <marco@freepascal.org>
2012-03-01 22:31:20 +0000

    * fix for #21199, changing epsilon to 0.0 instead of 0 helps overload selection of the     right precision. Leave mantis item over for compiler testing.
    git-svn-id: trunk@20461 -

--- rtl/objpas/math.pp
+++ rtl/objpas/math.pp
@@ -2222,7 +2222,7 @@ end;
 function SameValue(const A, B: Extended): Boolean;inline;
 
 begin
-  Result:=SameValue(A,B,0);
+  Result:=SameValue(A,B,0.0);
 end;
 {$endif FPC_HAS_TYPE_EXTENDED}
 
@@ -2231,7 +2231,7 @@ end;
 function SameValue(const A, B: Double): Boolean;inline;
 
 begin
-  Result:=SameValue(A,B,0);
+  Result:=SameValue(A,B,0.0);
 end;
 
 function SameValue(const A, B: Double; Epsilon: Double): Boolean;
