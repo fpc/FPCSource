e9c5458dd214994db9e99c3baa6ee723350c7e19
florian <florian@freepascal.org>
2012-03-11 21:08:57 +0000

    o patch by Nico Erfurth:
    * Fix for InterLockedCompareExchange on ARMEL
    
    InterLockedCompareExchange would not return the current data on failure.
    Getting this to work correctly is a bit tricky. As kuser_cmpxchg does
    not return the set value, we have to load it.
    There is a tiny chance that we get rescheduled between calling
    kuser_cmpxchg and loading the value. If the value changed in between
    there is the possibility that we would return the Comperand without
    having done an actual swap. Which might cause havoc and destruction.
    
    So, if the exchange failed, compare the value and loop again in case
    of CurrentValue == Comperand.
    
    * Improve testing of InterLockedCompareExchange
    
    Added a test to check for the case when Comperand is different from the
    current value.
    
    git-svn-id: trunk@20514 -

--- tests/test/units/system/interlocked1.pp
+++ tests/test/units/system/interlocked1.pp
@@ -7,9 +7,18 @@ begin
   InterLockedCompareExchange(target,4321,1234);
   if target<>4321 then
     halt(1);
+
   ctarget:=1234;
   InterLockedCompareExchange(ctarget,4321,1234);
   if ctarget<>4321 then
-    halt(1);
+    halt(2);
+
+  // Test what happens if we use a comparend which is NOT currently set
+  target := 12345;
+  if(InterLockedCompareExchange(target, 54321, 123) <> 12345) then
+    halt(3);
+  if target<>12345 then
+    halt(4);
+
   writeln('ok');
 end.
