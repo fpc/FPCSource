5331e66a8d8e8487f180107eeb2ee8649ffbe61b
michael <michael@freepascal.org>
2012-03-16 08:36:12 +0000

    * Added ms-sql server connector by LaCak2
    git-svn-id: trunk@20522 -

---
+++ packages/dblib/fpmake.pp
@@ -0,0 +1,39 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Const
+  DBLibOSes         = [linux,freebsd,netbsd,openbsd,win32];
+  
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('dblib');
+{$ifdef ALLPACKAGES}
+    P.Directory:='dblib';
+{$endif ALLPACKAGES}
+    P.Version:='1.0';
+    P.Author := 'Library: (FreeTDS/Microsoft), header: Lacack2';
+    P.License := 'Library: FreeTDS License, header: LGPL with modification, ';
+    P.HomepageURL := 'www.freepascal.org';
+    P.Email := '';
+    P.Description := 'Headers for the MS SQL Server RDBMS';
+    P.NeedLibC:= true;  // true for headers that indirectly link to libc?
+
+    P.SourcePath.Add('src');
+    P.IncludePath.Add('src');
+
+    T:=P.Targets.AddUnit('dblib.pp',DBLibOSes);
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
