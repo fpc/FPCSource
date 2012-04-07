06c362c02f1f94289b54ae4a14d8443bad3963af
pierre <pierre@freepascal.org>
2012-04-07 15:53:36 +0000

    Accept x86_64_netbsd system for elf64 object writer
    git-svn-id: trunk@20744 -

--- compiler/ogelf.pas
+++ compiler/ogelf.pas
@@ -1286,7 +1286,7 @@ implementation
             asmbin : '';
             asmcmd : '';
             supported_targets : [system_x86_64_linux,system_x86_64_freebsd,
-                                 system_x86_64_openbsd];
+                                 system_x86_64_openbsd,system_x86_64_netbsd];
             flags : [af_outputbinary,af_smartlink_sections,af_supports_dwarf];
             labelprefix : '.L';
             comment : '';
