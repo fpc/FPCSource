347afa80fe9225f62f64ce5e44d09ad38d3df7a1
Jonas Maebe <jonas@freepascal.org>
2012-04-11 18:06:48 +0000

    * adapted so it also succeeds with the lnfogdb unit (gdb reports slightly     different line numbers: the line following the label, instead of the one     preceding the label)
    git-svn-id: trunk@20844 -

--- tests/test/tlib1b.pp
+++ tests/test/tlib1b.pp
@@ -27,17 +27,20 @@ w:
   p(a);
   GetLineInfo(PtrUInt(a),s1,s2,l);
   s2:=ExtractFilename(s2);
-  writeln({ 'Func: ',s1,'} 'Source: ',s2,' Line: ',l);
+  writeln( 'Func: ',s1,' Source: ',s2,' Line: ',l);
   { GetLineInfo of dwarf doesn't return the function name }
-  if { (s1<>'P') or } (s2<>'tlib1a.pp') or (l<>10) then
+  { gdb reports line number 12 instead of 10 for the label
+    (the real one is 11; lineinfo matches it with the previous
+     line, gdb with the next one) }
+  if { (s1<>'P') or } (s2<>'tlib1a.pp') or ((l<>10) and (l<>12)) then
     halt(1);
 
   { main program }
   GetLineInfo(PtrUInt(@w),s1,s2,l);
   s2:=ExtractFilename(s2);
-  writeln({ 'Func: ',s1,'} 'Source: ',s2,' Line: ',l);
+  writeln( 'Func: ',s1,' Source: ',s2,' Line: ',l);
   { GetLineInfo of dwarf doesn't return the function name }
-  if { (s1<>'P') or } (s2<>'tlib1b.pp') or (l<>24) then
+  if { (s1<>'P') or } (s2<>'tlib1b.pp') or ((l<>24) and (l<>27)) then
     halt(1);
 
   writeln('ok');
