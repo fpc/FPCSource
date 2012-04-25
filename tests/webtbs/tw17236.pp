9d1063f94c9562b7c97a1f29e89295b324f1f39b
Jonas Maebe <jonas@freepascal.org>
2012-04-25 20:28:32 +0000

    * adjustments/fixes for AIX/ppc64
    git-svn-id: trunk@21053 -

--- tests/webtbs/tw17236.pp
+++ tests/webtbs/tw17236.pp
@@ -1,4 +1,4 @@
-{ %target=linux,darwin,freebsd,netbsd,openbsd,sunos,beos,haiku,aix }
+{ %target=linux,darwin,freebsd,netbsd,openbsd,sunos,beos,haiku }
 { %cpu=x86_64,powerpc64,mips64,sparc64,ia64,alpha }
 
 { windows does not support statics > 2GB }
