9568693664c431699812ff1c63b77f695506a569
Jonas Maebe <jonas@freepascal.org>
2012-05-06 14:50:38 +0000

    * removed superfluous sign extension from a_load_subsetreg_reg() (the value     is already sign extended at that point if required)
    git-svn-id: trunk@21246 -

--- compiler/cgobj.pas
+++ compiler/cgobj.pas
@@ -1300,9 +1300,7 @@ implementation
              bitmask := high(aword);
            a_op_const_reg(list,OP_AND,sreg.subsetregsize,tcgint(bitmask),tmpreg);
          end;
-       tmpreg := makeregsize(list,tmpreg,subsetsize);
-       a_load_reg_reg(list,tcgsize2unsigned[subsetsize],subsetsize,tmpreg,tmpreg);
-       a_load_reg_reg(list,subsetsize,tosize,tmpreg,destreg);
+       a_load_reg_reg(list,sreg.subsetregsize,tosize,tmpreg,destreg);
      end;
 
 
