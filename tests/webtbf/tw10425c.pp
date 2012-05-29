f8cb3fbfef32b834cafa7a946a8f9c56503cf56f
Jonas Maebe <jonas@freepascal.org>
2012-05-29 09:01:55 +0000

    + old tests I forgot to commit
    git-svn-id: trunk@21429 -

---
+++ tests/webtbf/tw10425c.pp
@@ -0,0 +1,20 @@
+{ %fail }
+
+{$ifdef fpc}
+{$mode delphi}
+{$endif}
+
+unit tw10425c;
+
+interface
+
+function test(p: string[4]): longint;
+
+implementation
+
+
+function test(p: string[4]): longint;
+begin
+end;
+
+end.
