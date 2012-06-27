335d9346dd3431e064c9f8fe47e28765ebaec56d
sergei <gorelkin@nanoreflex.ru>
2012-06-27 18:20:52 +0000

    * Fixed unclosed comment causing the test to fail for a completely unrelated reason.
    git-svn-id: trunk@21725 -

--- tests/tbs/tb0528.pp
+++ tests/tbs/tb0528.pp
@@ -2,7 +2,7 @@
 {%skiptarget=darwin,aix}
 
 { darwin limits statically declared data structures to 32 bit for efficiency reasons }
-{ the aix assembler cannot deal with the way we declare these arrays in assembler code )
+{ the aix assembler cannot deal with the way we declare these arrays in assembler code }
 
 program tb0528;
 
