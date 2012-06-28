ea516b59b0ee64b86b5407e17fabd734a89aa1fe
Jonas Maebe <jonas@freepascal.org>
2012-06-28 14:39:16 +0000

    * extended test
    git-svn-id: trunk@21728 -

--- tests/webtbs/tw20998.pp
+++ tests/webtbs/tw20998.pp
@@ -1,18 +1,23 @@
 var
   i : int64;
-
+  j, k: longint;
 begin
-  i:=6400;
-  i:=i div 64;
-  if i<>100 then
-    halt(1);
+  k:=64;
+  for j:=6400 to 6464 do
+    begin
+      i:=j;
+      if (i div 64) <> (i div k) then
+        halt(1);
+    end;
   i:=6500;
   i:=i div 65;
   if i<>100 then
     halt(1);
-  i:=-6400;
-  i:=i div 64;
-  if i<>-100 then
-    halt(1);
+  for j:=-6400 downto -6464 do
+    begin
+      i:=j;
+      if (i div 64) <> (i div k) then
+        halt(2);
+    end;
   writeln('ok');
 end.
