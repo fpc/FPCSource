32fc7118d472480b2bd82cf6697ef159e97d1b60
paul <paul@idefix.freepascal.org>
2012-09-18 01:32:24 +0000

    compiler: add US-ASCII codepage
    git-svn-id: trunk@22419 -

--- compiler/widestr.pas
+++ compiler/widestr.pas
@@ -67,7 +67,7 @@ unit widestr;
   implementation
 
     uses
-      cp8859_1,cp850,cp437,cp1252,
+      cp8859_1,cp850,cp437,cp1252,cp646,
       { cyrillic code pages }
       cp1251,cp866,cp8859_5,
       globals,cutils;
