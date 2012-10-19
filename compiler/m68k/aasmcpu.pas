b104d9c9e6f3fd35fb6891a9bca2fa67d9e477f8
pierre <pierre@freepascal.org>
2012-10-19 10:18:16 +0000

    Add some missing instructions to spilling_get_operation_type method
    git-svn-id: trunk@22760 -

--- compiler/m68k/aasmcpu.pas
+++ compiler/m68k/aasmcpu.pas
@@ -514,9 +514,15 @@ type
         case opcode of
           A_MOVE, A_MOVEQ, A_ADD, A_ADDQ, A_ADDX, A_SUB, A_SUBQ, A_SUBX,
           A_AND, A_LSR, A_LSL, A_ASR, A_ASL, A_EOR, A_EORI, A_OR,
-          A_MULS, A_MULU:
+          A_MULS, A_MULU, A_DIVS, A_DIVU, A_DIVSL, A_DIVUL:
             if opnr=1 then begin
               result:=operand_write;
+            end else begin
+              result:=operand_read;
+            end;
+          A_DBRA:
+            if opnr=1 then begin
+              result:=operand_readwrite;
             end else begin
               result:=operand_read;
             end;
