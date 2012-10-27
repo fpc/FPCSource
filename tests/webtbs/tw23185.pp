1ecc2672bb66928391d5a16b3627161328a971e9
florian <florian@freepascal.org>
2012-10-27 21:05:56 +0000

    * don't assembler or link test to avoid linker or assembler errors due to wrong architecture
    git-svn-id: trunk@22861 -

--- tests/webtbs/tw23185.pp
+++ tests/webtbs/tw23185.pp
@@ -1,7 +1,9 @@
 { %target=linux }
 { %cpu=arm }
 { %norun }
-{ %opt=-Cparmv7m }
+{ %opt=-Cparmv7m -s }
+
+{ we don't assemble, link and run because we cannot check if the different arm architectures really match }
 begin
 asm
  mrs r0, psp
