43375ef468db271639cf53d932ed7e3dbd179b1f
lacak <lacak@idefix.freepascal.org>
2012-11-06 09:29:22 +0000

    fcl-db: formatting
    git-svn-id: trunk@22939 -

--- packages/fcl-db/src/base/db.pas
+++ packages/fcl-db/src/base/db.pas
@@ -1760,6 +1760,22 @@ type
     property OnMasterDisable: TNotifyEvent read FOnMasterDisable write FOnMasterDisable;
   end;
 
+{ TMasterParamsDataLink }
+
+  TMasterParamsDataLink = Class(TMasterDataLink)
+  Private
+    FParams : TParams;
+    Procedure SetParams(AVAlue : TParams);
+  Protected
+    Procedure DoMasterDisable; override;
+    Procedure DoMasterChange; override;
+  Public
+    constructor Create(ADataSet: TDataSet); override;
+    Procedure RefreshParamNames; virtual;
+    Procedure CopyParamsFromMaster(CopyBound : Boolean); virtual;
+    Property Params : TParams Read FParams Write SetParams;
+  end;
+
 { TDataSource }
 
   TDataChangeEvent = procedure(Sender: TObject; Field: TField) of object;
@@ -1800,7 +1816,7 @@ type
     property OnUpdateData: TNotifyEvent read FOnUpdateData write FOnUpdateData;
   end;
 
- { TDBDataset }
+  { TDBDataset }
 
   TDBDatasetClass = Class of TDBDataset;
   TDBDataset = Class(TDataset)
@@ -1817,7 +1833,7 @@ type
       Property Transaction : TDBTransaction Read FTransaction Write SetTransaction;
     end;
 
- { TDBTransaction }
+  { TDBTransaction }
 
   TDBTransactionClass = Class of TDBTransaction;
   TDBTransaction = Class(TComponent)
@@ -1852,7 +1868,7 @@ type
     property Active : boolean read FActive write setactive;
   end;
 
-    { TCustomConnection }
+  { TCustomConnection }
 
   TLoginEvent = procedure(Sender: TObject; Username, Password: string) of object;
 
@@ -1950,20 +1966,6 @@ type
     property Params : TStrings read FParams Write SetParams;
   end;
 
-
-  TMasterParamsDataLink = Class(TMasterDataLink)
-  Private
-    FParams : TParams;
-    Procedure SetParams(AVAlue : TParams);  
-  Protected  
-    Procedure DoMasterDisable; override;
-    Procedure DoMasterChange; override;
-  Public
-    constructor Create(ADataSet: TDataSet); override;
-    Procedure RefreshParamNames; virtual;
-    Procedure CopyParamsFromMaster(CopyBound : Boolean); virtual;
-    Property Params : TParams Read FParams Write SetParams;  
-  end;
 
 const
   FieldTypetoVariantMap : array[TFieldType] of Integer = (varError, varOleStr, varSmallint,
