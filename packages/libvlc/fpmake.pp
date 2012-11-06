3a5b5f03d5fbddc849cd533891ad22c36c861955
Jonas Maebe <jonas@freepascal.org>
2012-11-06 23:14:16 +0000

    * only enable for Linux and Win32/Win64, since those are presumably the     platforms on which this package has been tested (it definitely won't work     on darwin as is)
    git-svn-id: trunk@22944 -

--- packages/libvlc/fpmake.pp
+++ packages/libvlc/fpmake.pp
@@ -20,8 +20,8 @@ begin
     P.HomepageURL := 'www.freepascal.org';
     P.Email := 'michael@freepascal.org';
     P.Description := 'VLC library (version 2 or higher) interface and component.';
-    T:=P.Targets.AddUnit('src/libvlc.pp');
-    T:=P.Targets.AddUnit('src/vlc.pp');
+    T:=P.Targets.AddUnit('src/libvlc.pp',[linux,win32,win64]);
+    T:=P.Targets.AddUnit('src/vlc.pp',[linux,win32,win64]);
     with T.Dependencies do
       begin
       AddUnit('libvlc');
