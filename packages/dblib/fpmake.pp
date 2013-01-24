8ef46d22d8f701e0a51eb4e9801fee2f27900539
yury <jura@cp-lab.com>
2013-01-24 22:14:31 +0000

    * Build databases support for Android.
    git-svn-id: branches/targetandroid@23518 -

--- packages/dblib/fpmake.pp
+++ packages/dblib/fpmake.pp
@@ -9,7 +9,7 @@ uses fpmkunit;
 procedure add_dblib;
 
 Const
-  DBLibOSes         = [linux,freebsd,netbsd,openbsd,solaris,win32,win64,haiku];
+  DBLibOSes         = [linux,freebsd,netbsd,openbsd,solaris,win32,win64,haiku,android];
 
 Var
   P : TPackage;
