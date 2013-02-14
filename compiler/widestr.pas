849095f5e52261ae81211ed4a7a09f362670a999
yury <jura@cp-lab.com>
2013-02-14 11:29:47 +0000

    + Added Greek code page 1253 to the compiler to have support for the third modern code page (in addition to cp1251 and cp1252). It is needed to be able to define "type AnsiString(1253)" in tests. Previously I used cp1252 for tests with Euro symbol conversions. But for unknown reason cp1252 does not have the Euro symbol with code $80 on Unix systems (libiconv bug?). In other hand cp1253 has the Euro symbol with code $80 as expected.
    git-svn-id: trunk@23615 -

--- compiler/widestr.pas
+++ compiler/widestr.pas
@@ -70,6 +70,8 @@ unit widestr;
       cp8859_1,cp850,cp437,cp1252,cp646,
       { cyrillic code pages }
       cp1251,cp866,cp8859_5,
+      { greek code page }
+      cp1253,
       globals,cutils;
 
 
