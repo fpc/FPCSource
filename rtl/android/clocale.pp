65d610902bddb9ab44290d437b77bcf89eaed799
yury <jura@cp-lab.com>
2013-02-24 13:27:50 +0000

    + Forgot to commit dummy clocale. It has caused Issue #23931.
    git-svn-id: trunk@23648 -

---
+++ rtl/android/clocale.pp
@@ -0,0 +1,23 @@
+{
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 2013 by the Free Pascal development team.
+
+    Android locale support. !!! NOT IMPLEMENTED !!!
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ **********************************************************************}
+
+unit clocale;
+
+{$mode objfpc}
+
+interface
+
+implementation
+
+end.
