98e942397815c4ef57fd558b3b998b6f83d84f6e
pierre <pierre@freepascal.org>
2013-03-22 14:54:04 +0000

    * Generate revision.inc include file depending on last modified comitted file
    git-svn-id: trunk@23954 -

---
+++ utils/fpcm/get_revision.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# List of files which should be considered imprtant
+# for the behavior of fpcmake binary
+# fpcmake.inc and Makefile should be excluded from that list
+important_sources=`ls -1 fpcm*.pp fpcmake.ini Makefile.fpc`
+
+LANG=C
+export LANG
+
+tmpfiles=
+
+for f in $important_sources ; do
+  tmpfile=.tmp.$f
+  tmpfiles="$tmpfiles $tmpfile"
+  svn info $f > $tmpfile
+done
+
+# echo "svn_info is $svn_info"
+svn_date=`gawk '/Last Changed Date: / {print $4 }' $tmpfiles | sort -n | tail -1`
+svn_rev=`gawk '/Last Changed Rev: / {print $4 }' $tmpfiles | sort -n | tail -1`
+echo "for files $important_sources, date is $svn_date, rev is $svn_rev"
+echo "'$svn_date rev $svn_rev'" > revision.inc
+rm -Rf $tmpfiles
+
+
+
