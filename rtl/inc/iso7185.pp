1d6cec856c8bf677ddc8a9e37e3603100196554c
florian <florian@freepascal.org>
2013-04-14 20:13:53 +0000

    + a few helpers for iso pascal compatibility added
    git-svn-id: trunk@24246 -

--- rtl/inc/iso7185.pp
+++ rtl/inc/iso7185.pp
@@ -34,6 +34,14 @@ unit iso7185;
     Function Eoln(Var t: Text): Boolean;
     Function Eoln:Boolean;
 
+    Procedure Page;
+    Procedure Page(Var t: Text);
+
+    Procedure Get(Var t: Text);
+    Procedure Put(Var t: Text);
+
+    Function Eof(var f:TypedFile): Boolean;
+
   implementation
 
   {$i textrec.inc}
@@ -100,6 +108,40 @@ unit iso7185;
     Function Eoln:Boolean;
       Begin
         Eoln:=Eoln(Input);
+      End;
+
+
+    Procedure Page;
+      begin
+        Page(Output);
+      end;
+
+
+    Procedure Page(var t : Text);
+      Begin
+        write(#12);
+      End;
+
+
+    procedure Get(var t : Text);
+      var
+        c : char;
+      Begin
+        Read(t,c);
+      End;
+
+
+    Procedure Put(var t : Text);
+      begin
+        inc(TextRec(t).BufPos);
+      end;
+
+
+    Function Eof(var f:TypedFile): Boolean;
+      Type
+        UnTypedFile = File;
+      Begin
+        Eof:=System.Eof(UnTypedFile(f));
       End;
 
 begin
