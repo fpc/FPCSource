2555885541bf6addabae89baeb794f3054dd9667
joost <joost@cnoc.nl>
2013-05-13 19:56:36 +0000

    * Added fpmake.pp files for pas2fpm, pas2jni and pas2ut
    git-svn-id: trunk@24492 -

---
+++ utils/pas2fpm/fpmake.pp
@@ -0,0 +1,46 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+{$endif ALLPACKAGES}
+
+procedure add_pas2fpm;
+
+Var
+  P : TPackage;
+  T : TTarget;
+
+begin
+  With Installer do
+    begin
+    P:=AddPackage('pas2fpm');
+
+    P.Author := 'Free Pascal Team';
+    P.License := 'LGPL with modification';
+    P.HomepageURL := 'www.freepascal.org';
+    P.Description := 'Generate fpmake.pp for Pascal source.';
+    P.Email := '';
+    P.NeedLibC:= false;
+
+{$ifdef ALLPACKAGES}
+    P.Directory:='pas2fpm';
+{$endif ALLPACKAGES}
+    P.Version:='2.7.1';
+    P.Dependencies.Add('fcl-base');
+    P.Dependencies.Add('fcl-passrc');
+
+    T:=P.Targets.AddProgram('pas2fpm.pp');
+    end;
+end;
+
+{$ifndef ALLPACKAGES}
+begin
+  add_pas2fpm;
+  Installer.Run;
+end.
+{$endif ALLPACKAGES}
+
+
+
+
---
+++ utils/pas2ut/fpmake.pp
@@ -0,0 +1,45 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+{$endif ALLPACKAGES}
+
+procedure add_pas2ut;
+
+Var
+  P : TPackage;
+  T : TTarget;
+
+begin
+  With Installer do
+    begin
+    P:=AddPackage('pas2ut');
+
+    P.Author := 'Free Pascal Team';
+    P.License := 'LGPL with modification';
+    P.HomepageURL := 'www.freepascal.org';
+    P.Description := 'Pascal source to FPC Unit test generator program';
+    P.Email := '';
+    P.NeedLibC:= false;
+
+{$ifdef ALLPACKAGES}
+    P.Directory:='pas2ut';
+{$endif ALLPACKAGES}
+    P.Version:='2.7.1';
+    P.Dependencies.Add('fcl-passrc');
+
+    T:=P.Targets.AddProgram('pas2ut.pp');
+    end;
+end;
+
+{$ifndef ALLPACKAGES}
+begin
+  add_pas2ut;
+  Installer.Run;
+end.
+{$endif ALLPACKAGES}
+
+
+
+
