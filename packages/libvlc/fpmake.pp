dd70204cf4ed74d8e8019a03f608a69141d86b13
marco <marco@freepascal.org>
2013-06-23 10:55:38 +0000

    # revisions: 22944,22946,22948
    - recorded merge only, Fixes already only enabled these target + freebsd
    - merged removing debug comments.
    
    git-svn-id: branches/fixes_2_6@24946 -

--- packages/libvlc/fpmake.pp
+++ packages/libvlc/fpmake.pp
@@ -15,13 +15,15 @@ begin
 {$ifdef ALLPACKAGES}
     P.Directory:='libvlc';
 {$endif ALLPACKAGES}
-    P.Version:='1.0';
+    P.OSes := [win32, win64, linux, freebsd];
+ 
+     P.Version:='1.0';
     P.License := 'LGPL with modification';
     P.HomepageURL := 'www.freepascal.org';
     P.Email := 'michael@freepascal.org';
     P.Description := 'VLC library (version 2 or higher) interface and component.';
-    T:=P.Targets.AddUnit('src/libvlc.pp');
-    T:=P.Targets.AddUnit('src/vlc.pp');
+    T:=P.Targets.AddUnit('src/libvlc.pp',[linux,win32,win64]);
+    T:=P.Targets.AddUnit('src/vlc.pp',[linux,win32,win64]);
     with T.Dependencies do
       begin
       AddUnit('libvlc');
