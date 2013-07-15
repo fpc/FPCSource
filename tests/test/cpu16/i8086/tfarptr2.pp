0d565aeba91f61f128368be45eac3c7ef2e9b538
nickysn <nickysn@gmail.com>
2013-07-15 22:36:12 +0000

    + extended test tfarptr2.pp
    git-svn-id: trunk@25109 -

--- tests/test/cpu16/i8086/tfarptr2.pp
+++ tests/test/cpu16/i8086/tfarptr2.pp
@@ -1,6 +1,9 @@
 { %cpu=i8086 }
 
-{ far pointer equality comparison tests }
+{ far pointer equality (=, <>) comparison tests }
+
+{ = and <> should compare *both* the segment and the offset }
+{ >, <, >= and <= should compare only the offset }
 
 var
   ErrorCode: Integer;
@@ -25,6 +28,7 @@ var
 begin
   ErrorCode := 0;
 
+  Writeln('var, var');
   FarPtr := Ptr($1234, $5678);
   FarPtr2 := Ptr($1234, $5678);
   eq := FarPtr = FarPtr2;
@@ -50,6 +54,52 @@ begin
   FarPtr2 := Ptr($4321, $8765);
   eq := FarPtr = FarPtr2;
   neq := FarPtr <> FarPtr2;
+  if eq or not neq then
+    Error(4);
+
+  Writeln('var, ptr(const)');
+  FarPtr := Ptr($1234, $5678);
+  eq := FarPtr = Ptr($1234, $5678);
+  neq := FarPtr <> Ptr($1234, $5678);
+  if not eq or neq then
+    Error(1);
+
+  FarPtr := Ptr($1234, $5678);
+  eq := FarPtr = Ptr($4321, $5678);
+  neq := FarPtr <> Ptr($4321, $5678);
+  if eq or not neq then
+    Error(2);
+
+  FarPtr := Ptr($1234, $5678);
+  eq := FarPtr = Ptr($1234, $8765);
+  neq := FarPtr <> Ptr($1234, $8765);
+  if eq or not neq then
+    Error(3);
+
+  FarPtr := Ptr($1234, $5678);
+  eq := FarPtr = Ptr($4321, $8765);
+  neq := FarPtr <> Ptr($4321, $8765);
+  if eq or not neq then
+    Error(4);
+
+  Writeln('ptr(const), ptr(const)');
+  eq := Ptr($1234, $5678) = Ptr($1234, $5678);
+  neq := Ptr($1234, $5678) <> Ptr($1234, $5678);
+  if not eq or neq then
+    Error(1);
+
+  eq := Ptr($1234, $5678) = Ptr($4321, $5678);
+  neq := Ptr($1234, $5678) <> Ptr($4321, $5678);
+  if eq or not neq then
+    Error(2);
+
+  eq := Ptr($1234, $5678) = Ptr($1234, $8765);
+  neq := Ptr($1234, $5678) <> Ptr($1234, $8765);
+  if eq or not neq then
+    Error(3);
+
+  eq := Ptr($1234, $5678) = Ptr($4321, $8765);
+  neq := Ptr($1234, $5678) <> Ptr($4321, $8765);
   if eq or not neq then
     Error(4);
 
