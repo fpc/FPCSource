1af312c1296dbf14214522ba52ac0517f9b38d97
Jonas Maebe <jonas@freepascal.org>
2013-08-22 21:37:06 +0000

    * don't overwrite result of librarysearchpath.FindFile() in     GetDarwinPrtobjName() if it found something
    git-svn-id: trunk@25330 -

--- compiler/systems/t_bsd.pas
+++ compiler/systems/t_bsd.pas
@@ -295,11 +295,13 @@ var
   startupfile: TCmdStr;
 begin
   startupfile:=GetDarwinCrt1ObjName(isdll);
-  if (startupfile<>'') and
-     not librarysearchpath.FindFile(startupfile,false,result) then
-    result:='/usr/lib/'+startupfile
+  if startupfile<>'' then
+    begin
+     if not librarysearchpath.FindFile(startupfile,false,result) then
+       result:='/usr/lib/'+startupfile
+    end
   else
-    result:=startupfile;
+    result:='';
   result:=maybequoted(result);
 end;
 
