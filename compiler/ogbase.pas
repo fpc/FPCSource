ddb179ff6184a41e29d9d1175e4a3df38589583a
joost <joost@cnoc.nl>
2013-08-24 10:44:59 +0000

    * Name the sec_rtti section rtti and not extrtti
    git-svn-id: branches/joost/classattributes@25361 -

--- compiler/ogbase.pas
+++ compiler/ogbase.pas
@@ -846,7 +846,7 @@ implementation
           'eh_frame',
           'debug_frame','debug_info','debug_line','debug_abbrev',
           'fpc',
-          'extrtti',
+          'rtti',
           'toc',
           'init',
           'fini',
