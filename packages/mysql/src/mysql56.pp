1ce1c60582aa3abfe1a2b5c8bf9503cd7f3ff332
marco <marco@freepascal.org>
2013-12-25 16:39:30 +0000

    r26190 | michael | 2013-12-06 16:55:26 +0100 (Fri, 06 Dec 2013) | 1 line Changed paths:    M /trunk/packages/mysql/fpmake.pp    M /trunk/packages/mysql/src/mysql.inc    A /trunk/packages/mysql/src/mysql56dyn.pp
    * Added to build, extra fix, added mysql56dyn
    ---
    r26202 | marco | 2013-12-09 19:19:31 +0100 (Mon, 09 Dec 2013) | 1 line
    Changed paths:
       M /trunk/rtl/win/wininc/unifun.inc
    
     * fix for #25402  One argument of GetICMProfileW missed an indirection
    ---
    r26188 | michael | 2013-12-06 14:25:09 +0100 (Fri, 06 Dec 2013) | 1 line
    Changed paths:
       M /trunk/packages/mysql/src/mysql.inc
    
    * MySQL 5.6 support from Laco
    
    git-svn-id: branches/fixes_2_6@26283 -

---
+++ packages/mysql/src/mysql56.pp
@@ -0,0 +1,12 @@
+{
+  Contains the mysql calls for MySQL 5.5
+}
+
+unit mysql56;
+
+{$UNDEF LinkDynamically}
+{$DEFINE MYSQL56}
+
+{$i mysql.inc}
+
+end.
---
+++ packages/mysql/src/mysql56dyn.pp
@@ -0,0 +1,12 @@
+{
+  Contains the MySQL calls for MySQL 5.6
+}
+
+unit mysql56dyn;
+
+{$DEFINE LinkDynamically}
+{$DEFINE MYSQL56}
+
+{$i mysql.inc}
+
+end.
