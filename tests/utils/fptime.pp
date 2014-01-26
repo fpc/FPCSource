0ea0b5c34d530edb0e70e143b040def22d9f704b
florian <florian@freepascal.org>
2014-01-26 11:40:46 +0000

    * quote parameters
    git-svn-id: trunk@26584 -

--- tests/utils/fptime.pp
+++ tests/utils/fptime.pp
@@ -9,7 +9,7 @@ begin
   if paramcount>0 then
     begin
       for i:=2 to paramcount do
-        ps:=ps+' '+paramstr(i);
+        ps:=ps+' "'+paramstr(i)+'"';
       sticks:=GetMicroSTicks;
       ExecuteProcess(paramstr(1),ps);
       writeln(stderr,(GetMicroSTicks-sticks)/1000:0:3,' ms');
