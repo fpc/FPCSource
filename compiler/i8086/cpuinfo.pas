e83ef05b7434fc19624939155c37afcef5dc2468
nickysn <nickysn@gmail.com>
2014-01-26 02:08:08 +0000

    - disable cs_opt_nodecse from -O2 on i8086, because it breaks building packages   since r26579
    git-svn-id: trunk@26583 -

--- compiler/i8086/cpuinfo.pas
+++ compiler/i8086/cpuinfo.pas
@@ -125,7 +125,7 @@ Const
 
    level1optimizerswitches = genericlevel1optimizerswitches + [cs_opt_peephole];
    level2optimizerswitches = genericlevel2optimizerswitches + level1optimizerswitches +
-     [{cs_opt_regvar,}cs_opt_stackframe,cs_opt_tailrecursion,cs_opt_nodecse];
+     [{cs_opt_regvar,}cs_opt_stackframe,cs_opt_tailrecursion{,cs_opt_nodecse}];
    level3optimizerswitches = genericlevel3optimizerswitches + level2optimizerswitches + [{,cs_opt_loopunroll}];
    level4optimizerswitches = genericlevel4optimizerswitches + level3optimizerswitches + [cs_useebp];
 
