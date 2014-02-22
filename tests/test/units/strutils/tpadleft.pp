ff2b6a90d9c040c9e207372f187bb5c55f48fa94
michael <michael@freepascal.org>
2014-02-22 12:40:31 +0000

    * Added tests for various strutils functions by Karl-Michael Schindler (bug ID 25756)
    git-svn-id: trunk@26833 -

---
+++ tests/test/units/strutils/tpadleft.pp
@@ -0,0 +1,40 @@
+{$mode objfpc}
+{$h+}
+{$hints on}
+{$warnings on}
+
+uses
+  StrUtils;
+
+var
+  exitCode: integer = 0;
+
+procedure padLeftTest(const s: ansistring;
+                      n: integer;
+                      const expectation: ansistring;
+                      const testnr: integer);
+
+  begin
+    if padLeft(s, n) <> expectation then
+    begin
+      writeln('Testing strUtils/PadLeft: Test ', testnr, 
+              ' with N = ', n, ' failed.');
+      exitCode := 1;
+    end;
+  end; 
+
+var
+  i, j: integer;
+  testString: ansistring;
+
+begin
+  for i := 1 to 1024 do
+  begin
+    testString := 'abcd';
+    for j := 1 to i - 4 do
+      testString := ' ' + testString;
+    padLeftTest('abcd', i, testString, i);
+  end;
+  
+  halt(exitCode);
+end.
