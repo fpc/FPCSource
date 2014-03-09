2c7a83cd78ab30f92fb5ec93ee2c5b054600721d
nickysn <nickysn@gmail.com>
2014-03-09 21:26:39 +0000

    * use integer for the property indexes, instead of longint. Fixes compilation   of package on i8086 after r27062.
    git-svn-id: trunk@27063 -

--- packages/regexpr/src/regexpr.pas
+++ packages/regexpr/src/regexpr.pas
@@ -307,8 +307,8 @@ type
     // if it's in format 'ismxrg-ismxrg'.
     procedure SetModifierStr (const AModifiers : RegExprString);
 
-    function GetModifier (AIndex : longint) : boolean;
-    procedure SetModifier (AIndex : longint; ASet : boolean);
+    function GetModifier (AIndex : integer) : boolean;
+    procedure SetModifier (AIndex : integer; ASet : boolean);
 
     procedure Error (AErrorID : integer); virtual; // error handler.
     // Default handler raise exception ERegExpr with
@@ -1386,7 +1386,7 @@ procedure TRegExpr.SetModifierStr (const AModifiers : RegExprString);
  end; { of procedure TRegExpr.SetModifierStr
 --------------------------------------------------------------}
 
-function TRegExpr.GetModifier (AIndex : longint) : boolean;
+function TRegExpr.GetModifier (AIndex : integer) : boolean;
  var
   Mask : integer;
  begin
@@ -1407,7 +1407,7 @@ function TRegExpr.GetModifier (AIndex : longint) : boolean;
  end; { of function TRegExpr.GetModifier
 --------------------------------------------------------------}
 
-procedure TRegExpr.SetModifier (AIndex : longint; ASet : boolean);
+procedure TRegExpr.SetModifier (AIndex : integer; ASet : boolean);
  var
   Mask : integer;
  begin
