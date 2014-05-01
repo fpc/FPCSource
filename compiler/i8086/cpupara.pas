3dfc6ac7e1e45b21eee97ab927e9238ca22b21a3
nickysn <nickysn@gmail.com>
2014-05-01 16:45:49 +0000

    * fixed the passing of extended floating type parameters on i8086 for   pocall_register after r24761 (it is the same change as in r24766, but for   pocall_register)
    git-svn-id: trunk@27709 -

--- compiler/i8086/cpupara.pas
+++ compiler/i8086/cpupara.pas
@@ -695,8 +695,8 @@ unit cpupara;
                                     end
                                   else
                                     begin
-                                      { We can allocate at maximum 32 bits per location }
-                                      if paralen>sizeof(aint) then
+                                      { We can allocate at maximum 16 bits per location }
+                                      if paralen>=sizeof(aint) then
                                         begin
                                           l:=sizeof(aint);
                                           paraloc^.def:=uinttype;
