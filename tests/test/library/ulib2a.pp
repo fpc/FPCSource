bdb2993277725cdb033f6c937cc564ac72c6f51d
sergei <gorelkin@nanoreflex.ru>
2014-06-04 11:53:07 +0000

    * Renamed tlib* tests so all file names of dependent tests differ only in last letter. This fixes failures caused by inability to locate dlls at runtime.
    git-svn-id: trunk@27848 -

--- tests/test/library/ulib2a.pp
+++ tests/test/library/ulib2a.pp
@@ -8,7 +8,7 @@ interface
 { Checks that the two functions with the same exported name 'p'
   are each loaded correctly. }
 procedure p(var a : dword);external 'tlib1a' name 'p';
-procedure p2(var a : dword);external 'tlib1a2' name 'p';
+procedure p2(var a : dword);external 'tlib1b' name 'p';
 procedure p3(var a : dword);external 'tlib1a' name 'p';
 
 implementation
