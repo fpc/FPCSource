dc17b5550545056dbbb67671ae08148a0a6f26c9
nickysn <nickysn@gmail.com>
2014-06-25 22:02:03 +0000

    * switch to asmmode_i8086_intel instead of asmmode_i386_intel on i8086 when tp   or delphi mode is selected. This fixes the error "Selected assembler reader   not supported" in programs that specify $mode tp/delphi, but do not explicitly   set $asmmode intel.
    git-svn-id: trunk@28068 -

--- compiler/scanner.pas
+++ compiler/scanner.pas
@@ -515,7 +515,11 @@ implementation
            { Default to intel assembler for delphi/tp7 on i386/i8086 }
            if (m_delphi in current_settings.modeswitches) or
               (m_tp7 in current_settings.modeswitches) then
+{$ifdef i8086}
+             current_settings.asmmode:=asmmode_i8086_intel;
+{$else i8086}
              current_settings.asmmode:=asmmode_i386_intel;
+{$endif i8086}
            if changeinit then
              init_settings.asmmode:=current_settings.asmmode;
 {$endif i386 or i8086}
