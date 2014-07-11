6013e4d8c732d4733302c2ef2ec2202bc99f3b27
michael <michael@freepascal.org>
2014-07-11 07:34:11 +0000

    * Added copyright notice
    git-svn-id: trunk@28195 -

--- packages/fcl-web/src/jsonrpc/fpextdirect.pp
+++ packages/fcl-web/src/jsonrpc/fpextdirect.pp
@@ -1,3 +1,17 @@
+{
+    This file is part of the Free Component Library
+
+    Ext.Direct support - http part
+    Copyright (c) 2007 by Michael Van Canneyt michael@freepascal.org
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
 unit fpextdirect;
 
 {$mode objfpc}{$H+}
