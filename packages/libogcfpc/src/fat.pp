c09d2e20965eaadda394d4d204b7edfb094f2649
Legolas <legolas@idefix.freepascal.org>
2014-08-02 15:34:58 +0000

    * Fixed filesystem handling in libogc
    git-svn-id: trunk@28300 -

--- packages/libogcfpc/src/fat.pp
+++ packages/libogcfpc/src/fat.pp
@@ -19,6 +19,23 @@ function fatMount(name_: pcchar; interface_: PDISC_INTERFACE; startSector: sec_t
 procedure fatUnmount(name_: pcchar); cdecl; external;
 procedure fatGetVolumeLabel(name_, label_: pcchar); cdecl; external;
 
+// File attributes
+const
+  ATTR_ARCHIVE = $20;         // Archive
+  ATTR_DIRECTORY = $10;       // Directory
+  ATTR_VOLUME = $08;          // Volume
+  ATTR_SYSTEM = $04;          // System
+  ATTR_HIDDEN = $02;          // Hidden
+  ATTR_READONLY = $01;        // Read only
+
+(*
+Methods to modify DOS File Attributes
+*)
+function FAT_getAttr(const _file: pcchar): cint; cdecl; external;
+function FAT_setAttr(const _file: pcchar; attr: cint): cint; cdecl; external;
+
+
+
 implementation
 
 initialization
