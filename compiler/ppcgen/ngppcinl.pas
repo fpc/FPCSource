979a3d7e1d45ff7b094e61c0309605e2151c9d5d
Jonas Maebe <jonas@freepascal.org>
2014-08-19 20:22:29 +0000

    * channged gettemptyped() into gethltemp() call, as gettemptyped() is only     intended/required for managed types
    git-svn-id: branches/hlcgllvm@28483 -

--- compiler/ppcgen/ngppcinl.pas
+++ compiler/ppcgen/ngppcinl.pas
@@ -183,8 +183,8 @@ implementation
          current_asmdata.CurrAsmList.concat(taicpu.op_reg_reg(op,tmpreg,
            left.location.register));
          location_reset_ref(location,LOC_REFERENCE,def_cgsize(resultdef),0);
-         tg.gettemptyped(current_asmdata.CurrAsmList,resultdef,tt_normal,
-           location.reference);
+         tg.gethltemp(current_asmdata.CurrAsmList,resultdef,resultdef.size,
+           tt_normal,location.reference);
          cg.a_loadfpu_reg_ref(current_asmdata.CurrAsmList,OS_F64,OS_F64,tmpreg,
            location.reference);
        end;
