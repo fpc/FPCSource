54a3e7c4a1e02090b33bda47556b12f5e2ea3b66
marco <marco@freepascal.org>
2015-01-13 09:07:10 +0000

    --- Merging r29447 into '.': U    compiler/systems/t_linux.pas
    # revisions: 29447
    
    git-svn-id: branches/fixes_3_0@29460 -

--- compiler/systems/t_linux.pas
+++ compiler/systems/t_linux.pas
@@ -146,6 +146,7 @@ begin
 {$endif}
 {$endif arm}
 {$ifdef x86_64}
+  if not Dontlinkstdlibpath Then
     LibrarySearchPath.AddPath(sysrootpath,'/usr/lib/x86_64-linux-gnu',true);
 {$endif x86_64}
 end;
