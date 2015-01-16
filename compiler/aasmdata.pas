697552b9f19c25fc2aeab37c6bc9e32a6b4a784a
Jonas Maebe <jonas@freepascal.org>
2015-01-16 20:08:48 +0000

    * disabled hints/warnings about changed symbol visibility by default, use     -dextdebug to see them. This fixes 4 tests.
    git-svn-id: trunk@29487 -

--- compiler/aasmdata.pas
+++ compiler/aasmdata.pas
@@ -411,10 +411,15 @@ implementation
              GOT might have been used, so change might be harmful. }
            if (_bind<>hp.bind) and (hp.getrefs>0) then
              begin
+{$ifdef extdebug}
+               { the changes that matter must become internalerrors, the rest
+                 should be ignored; a used cannot change anything about this,
+                 so printing a warning/hint is not useful }
                if (_bind=AB_LOCAL) then
                  Message3(asmw_w_changing_bind_type,s,asmsymbindname[hp.bind],asmsymbindname[_bind])
                else
                  Message3(asmw_h_changing_bind_type,s,asmsymbindname[hp.bind],asmsymbindname[_bind]);
+{$endif extdebug}
              end;
            hp.bind:=_bind;
          end
