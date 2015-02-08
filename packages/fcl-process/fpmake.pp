2e9abd5d92281fe9d09996a62a584cc4fcaa2ac0
marco <marco@freepascal.org>
2015-02-08 16:37:55 +0000

    # revisions: 29637
    git-svn-id: branches/fixes_3_0@29655 -

--- packages/fcl-process/fpmake.pp
+++ packages/fcl-process/fpmake.pp
@@ -30,8 +30,8 @@ begin
     P.SourcePath.Add('src');
     P.IncludePath.Add('src/unix',AllUnixOSes);
     P.IncludePath.Add('src/win',[win32,win64]);
-    P.IncludePath.Add('src/dummy',AllOSes-[win32,win64]-AllUnixOSes);
     P.IncludePath.Add('src/$(OS)',AllOSes-[win32,win64]-AllUnixOSes);
+    P.IncludePath.Add('src/dummy',AllOSes-[win32,win64]-AllUnixOSes);
 
     T:=P.Targets.AddUnit('pipes.pp');
       T.Dependencies.AddInclude('pipes.inc');
