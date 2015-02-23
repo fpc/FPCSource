cfd7bfc8a5b68e75dab6de18a722660ca40bba70
Jonas Maebe <jonas@freepascal.org>
2015-02-23 22:52:11 +0000

    + dummy AArch64 cpupi unit
    git-svn-id: trunk@29905 -

---
+++ compiler/aarch64/cpupi.pas
@@ -0,0 +1,44 @@
+{
+    Copyright (c) 2002 by Florian Klaempfl
+
+    This unit contains the CPU specific part of tprocinfo
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+ ****************************************************************************
+}
+unit cpupi;
+
+{$i fpcdefs.inc}
+
+interface
+
+  uses
+    psub;
+
+  type
+    taarch64procinfo=class(tcgprocinfo)
+      { no need to override anything, as the ABI requires us to use a frame
+        pointer at all times }
+    end;
+
+implementation
+
+  uses
+    procinfo;
+
+begin
+  cprocinfo:=taarch64procinfo;
+end.
