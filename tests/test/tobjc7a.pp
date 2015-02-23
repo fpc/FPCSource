c6daaa28d0605517811a4cb87fba302826457585
Jonas Maebe <jonas@freepascal.org>
2015-02-23 22:54:24 +0000

    * enable all Objective-C tests for Darwin/AArch64
    git-svn-id: trunk@29945 -

--- tests/test/tobjc7a.pp
+++ tests/test/tobjc7a.pp
@@ -1,5 +1,5 @@
 { %target=darwin }
-{ %cpu=powerpc,powerpc64,i386,x86_64,arm }
+{ %cpu=powerpc,powerpc64,i386,x86_64,arm,aarch64 }
 { %fail }
 
 { Written by Jonas Maebe in 2009, released into the public domain }
