52d13e6ddb069e9259911e93b1eff85f1822c9f7
nickysn <nickysn@gmail.com>
2015-03-06 21:10:57 +0000

    + added 16-bit int overloads for TGDBController.GetIntRegister. Useful for   e.g. getting the segment registers on x86.
    git-svn-id: trunk@30113 -

--- ide/gdbmicon.pas
+++ ide/gdbmicon.pas
@@ -66,6 +66,8 @@ type
     function GetIntRegister(const RegName: string; var Value: Int64): Boolean;
     function GetIntRegister(const RegName: string; var Value: UInt32): Boolean;
     function GetIntRegister(const RegName: string; var Value: Int32): Boolean;
+    function GetIntRegister(const RegName: string; var Value: UInt16): Boolean;
+    function GetIntRegister(const RegName: string; var Value: Int16): Boolean;
     { set command }
     function SetCommand(Const SetExpr : string) : boolean;
     { print }
@@ -292,6 +294,24 @@ var
 begin
   GetIntRegister := GetIntRegister(RegName, U32Value);
   Value := Int32(U32Value);
+end;
+
+function TGDBController.GetIntRegister(const RegName: string; var Value: UInt16): Boolean;
+var
+  U64Value: UInt64;
+begin
+  GetIntRegister := GetIntRegister(RegName, U64Value);
+  Value := UInt16(U64Value);
+  if (U64Value shr 16) <> 0 then
+    GetIntRegister := False;
+end;
+
+function TGDBController.GetIntRegister(const RegName: string; var Value: Int16): Boolean;
+var
+  U16Value: UInt16;
+begin
+  GetIntRegister := GetIntRegister(RegName, U16Value);
+  Value := Int16(U16Value);
 end;
 
 
