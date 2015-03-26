b9b253dd02846465dab2f77c6507307160b09950
lacak <lacak@idefix.freepascal.org>
2015-03-26 06:50:11 +0000

    fcl-db: tests: apply test for RecordCount for closed DataSet introduced in rev.30295 (only for TBufDataSet in TestSpecificTBufDataset) also to other datasets (in TestDBBasics, which covers all TDataSet descendants)
    git-svn-id: trunk@30325 -

--- packages/fcl-db/src/base/bufdataset.pas
+++ packages/fcl-db/src/base/bufdataset.pas
@@ -1252,7 +1252,7 @@ begin
   InitDefaultIndexes;
   CalcRecordSize;
 
-  FBRecordcount := 0;
+  FBRecordCount := 0;
 
   for IndexNr:=0 to FIndexesCount-1 do with FIndexes[IndexNr] do
     InitialiseSpareRecord(IntAllocRecordBuffer);
@@ -1283,6 +1283,7 @@ var r  : integer;
 begin
   FOpen:=False;
   FReadFromFile:=False;
+  FBRecordCount:=0;
 
   if FIndexesCount>0 then with FIndexes[0] do if IsInitialized then
     begin
