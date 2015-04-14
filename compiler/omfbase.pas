10722abfac8ba4c709c607fb5a1f37e31729bd83
nickysn <nickysn@gmail.com>
2015-04-14 13:32:12 +0000

    + implemented TOmfRecord_LNAMES.DecodeFrom
    git-svn-id: trunk@30583 -

--- compiler/omfbase.pas
+++ compiler/omfbase.pas
@@ -689,9 +689,18 @@ implementation
     end;
 
   procedure TOmfRecord_LNAMES.DecodeFrom(RawRecord: TOmfRawRecord);
+    var
+      NextOfs: Integer;
+      Name: string;
     begin
-      {TODO: implement}
-      internalerror(2015040101);
+      if RawRecord.RecordType<>RT_LNAMES then
+        internalerror(2015040301);
+      NextOfs:=0;
+      while NextOfs<(RawRecord.RecordLength-1) do
+        begin
+          NextOfs:=RawRecord.ReadStringAt(NextOfs,Name);
+          Names.Add(Name);
+        end;
     end;
 
   procedure TOmfRecord_LNAMES.EncodeTo(RawRecord: TOmfRawRecord);
