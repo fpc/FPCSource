58c66b4759897514a34ecf2f46d63e0184d492f3
florian <florian@freepascal.org>
2015-04-17 20:15:10 +0000

    * the mul instructions do not modify the operands
    git-svn-id: trunk@30633 -

--- compiler/avr/aasmcpu.pas
+++ compiler/avr/aasmcpu.pas
@@ -250,7 +250,8 @@ implementation
           A_MOV,A_MOVW,A_POP:
             if opnr=0 then
               result:=operand_write;
-          A_CP,A_CPC,A_CPI,A_PUSH,A_SBRC,A_SBRS,A_ST,A_STD,A_STS:
+          A_CP,A_CPC,A_CPI,A_PUSH,A_SBRC,A_SBRS,A_ST,A_STD,A_STS,
+          A_MUL, A_MULS,A_FMUL,A_FMULS,A_FMULSU:
             ;
           else
             begin
