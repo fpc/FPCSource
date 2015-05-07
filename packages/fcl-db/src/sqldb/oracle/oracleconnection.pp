31c1581b9fb430b7ce099b0eeac071b163fea964
lacak <lacak@idefix.freepascal.org>
2015-05-07 05:38:12 +0000

    fcl-db: oracle: fix transaction parameters handling (only one of  OCI_TRANS_READONLY, OCI_TRANS_READWRITE, OCI_TRANS_SERIALIZABLE should be used). Bug #28043
    git-svn-id: trunk@30814 -

--- packages/fcl-db/src/sqldb/oracle/oracleconnection.pp
+++ packages/fcl-db/src/sqldb/oracle/oracleconnection.pp
@@ -767,30 +767,27 @@ end;
 
 function TOracleConnection.StartDBTransaction(trans: TSQLHandle; AParams: string): boolean;
 var
-  x_flags : ub4;
+  flags : ub4;
   i : Integer;
   s : string;
   locTrans : TOracleTrans;
 begin
-  locTrans := TOracleTrans(trans);
-  if ( Length(AParams) = 0 ) then begin
-    x_flags := OCI_TRANS_NEW or OCI_TRANS_READWRITE;
-  end else begin
-    x_flags := OCI_DEFAULT;
+  flags := OCI_TRANS_NEW or OCI_TRANS_READWRITE;
+  if AParams <> '' then begin
     i := 1;
     s := ExtractSubStr(AParams,i,StdWordDelims);
     while ( s <> '' ) do begin
       if ( s = 'readonly' ) then
-        x_flags := x_flags and OCI_TRANS_READONLY
+        flags := flags and OCI_TRANS_READONLY
       else if ( s = 'serializable' ) then
-        x_flags := x_flags and OCI_TRANS_SERIALIZABLE
+        flags := flags and OCI_TRANS_SERIALIZABLE
       else if ( s = 'readwrite' ) then
-        x_flags := x_flags and OCI_TRANS_READWRITE;
+        flags := flags and OCI_TRANS_READWRITE;
       s := ExtractSubStr(AParams,i,StdWordDelims);
     end;
-    x_flags := x_flags and OCI_TRANS_NEW;
   end;
-  locTrans.FOciFlags := x_flags;
+  locTrans := TOracleTrans(trans);
+  locTrans.FOciFlags := flags;
   InternalStartDBTransaction(locTrans);
   Result := True;
 end;
