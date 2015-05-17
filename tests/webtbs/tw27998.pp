be6dc49aa54674bdab437c46febcda4026d1588b
Jonas Maebe <jonas@freepascal.org>
2015-05-17 11:08:05 +0000

    * fixed test for Windows (the fact that it worked for Unix was due to a bug     in the Unix file locking implementation)
    git-svn-id: trunk@30876 -

--- tests/webtbs/tw27998.pp
+++ tests/webtbs/tw27998.pp
@@ -10,16 +10,13 @@ begin
   try
     H := TFileStream.Create(lockFile, fmCreate);
     h.Write(H, 4);
-    { flush }
-    H.free;
-    { reopen in exclusive mode }
-    H := TFileStream.Create(lockfile, fmOpenWrite);
     { should fail with an exception due to exclusion }
     H := TFileStream.Create(lockFile, fmCreate);
     Halt(1);
   except
     { check the size of the file, to ensure that the second
       create didn't overwrite the file }
+    H.free;
     H := TFileStream.create(LockFile, fmOpenRead or fmShareDenyNone);
     if H.Size<>4 then
       halt(2);
