3605c0655aa922cfcd12906f20fb8b714b2dd71f
marco <marco@freepascal.org>
2015-06-17 11:23:29 +0000

    --- Merging r30653 into '.': U    rtl/inc/fexpand.inc --- Recording mergeinfo for merge of r30653 into '.':  U   . --- Merging r30654 into '.': U    tests/test/units/dos/tfexpand.pp --- Recording mergeinfo for merge of r30654 into '.':  G   . --- Merging r30774 into '.': G    tests/test/units/dos/tfexpand.pp --- Recording mergeinfo for merge of r30774 into '.':  G   . --- Merging r30788 into '.': U    rtl/amiga/system.pp --- Recording mergeinfo for merge of r30788 into '.':  G   . --- Merging r30789 into '.': U    rtl/morphos/system.pp --- Recording mergeinfo for merge of r30789 into '.':  G   . --- Merging r30790 into '.': U    packages/fcl-process/fpmake.pp A    packages/fcl-process/src/amicommon A    packages/fcl-process/src/amicommon/process.inc A    packages/fcl-process/src/amicommon/pipes.inc A    packages/fcl-process/src/amicommon/simpleipc.inc --- Recording mergeinfo for merge of r30790 into '.':  G   . --- Merging r30803 into '.': U    packages/fcl-process/src/amicommon/simpleipc.inc --- Recording mergeinfo for merge of r30803 into '.':  G   . --- Merging r30805 into '.': U    packages/fcl-process/src/amicommon/process.inc U    packages/fcl-process/src/amicommon/pipes.inc --- Recording mergeinfo for merge of r30805 into '.':  G   . --- Merging r30806 into '.': G    packages/fcl-process/src/amicommon/process.inc G    packages/fcl-process/src/amicommon/pipes.inc --- Recording mergeinfo for merge of r30806 into '.':  G   . --- Merging r30812 into '.': U    packages/morphunits/src/amigados.pas U    packages/amunits/src/coreunits/amigados.pas G    packages/fcl-process/src/amicommon/process.inc G    packages/fcl-process/src/amicommon/pipes.inc U    packages/arosunits/src/amigados.pas --- Recording mergeinfo for merge of r30812 into '.':  G   . --- Merging r30813 into '.': U    rtl/aros/system.pp --- Recording mergeinfo for merge of r30813 into '.':  G   .
    # revisions: 30653,30654,30774,30788,30789,30790,30803,30805,30806,30812,30813
    
    git-svn-id: branches/fixes_3_0@31082 -

--- tests/test/units/dos/tfexpand.pp
+++ tests/test/units/dos/tfexpand.pp
@@ -59,9 +59,16 @@ uses
 {$ENDIF LINUX}
 
 {$IFDEF AMIGA}
+ {$IFNDEF HASAMIGA}
+  {$DEFINE HASAMIGA}
+ {$ENDIF HASAMIGA}
+{$ENDIF AMIGA}
+
+{$IFDEF HASAMIGA}
  {$DEFINE VOLUMES}
  {$DEFINE NODRIVEC}
-{$ENDIF AMIGA}
+ {$DEFINE NODOTS}
+{$ENDIF HASAMIGA}
 
 {$IFDEF NETWARE}
  {$DEFINE VOLUMES}
@@ -103,13 +110,13 @@ const
  DriveSeparator = '/';
  FileNameCasePreserving = true;
    {$ELSE UNIX}
-    {$IFDEF AMIGA}
- DirectorySeparator = ':';
+    {$IFDEF HASAMIGA}
+ DirectorySeparator = '/';
  FileNameCasePreserving = true;
-    {$ELSE AMIGA}
+    {$ELSE HASAMIGA}
  DirectorySeparator = '\';
  FileNameCasePreserving = false;
-    {$ENDIF AMIGA}
+    {$ENDIF HASAMIGA}
    {$ENDIF UNIX}
   {$ENDIF MACOS}
  {$ENDIF DIRECT}
@@ -117,11 +124,7 @@ const
  {$IFDEF MACOS}
  DriveSep = '';
  {$ELSE MACOS}
-  {$IFDEF AMIGA}
- DriveSep = '';
-  {$ELSE AMIGA}
  DriveSep = DriveSeparator;
-  {$ENDIF AMIGA}
  {$ENDIF MACOS}
  {$IFDEF UNIX}
  CDrive = '';
@@ -129,11 +132,11 @@ const
   {$IFDEF MACOS}
  CDrive = 'C';
   {$ELSE MACOS}
-   {$IFDEF AMIGA}
+   {$IFDEF HASAMIGA}
  CDrive = 'C';
-   {$ELSE AMIGA}
+   {$ELSE HASAMIGA}
  CDrive = 'C:';
-   {$ENDIF AMIGA}
+   {$ENDIF HASAMIGA}
   {$ENDIF MACOS}
  {$ENDIF UNIX}
 {$ENDIF FPC}
@@ -289,11 +292,11 @@ begin
  GetDir (3, CDir);
 {$ENDIF NODRIVEC}
  Check (' ', CurDir + DirSep + ' ');
-{$IFDEF AMIGA}
+{$IFDEF HASAMIGA}
  Check ('', CurDir);
-{$ELSE AMIGA}
+{$ELSE HASAMIGA}
  Check ('', CurDir + DirSep);
-{$ENDIF AMIGA}
+{$ENDIF HASAMIGA}
 {$IFDEF MACOS}
  Check (':', CurDir + DirSep);
 {$ELSE MACOS}
@@ -304,26 +307,26 @@ begin
 if CDir [Length (CDir)] = DirSep then Check ('c:anything', CDir + 'anything')
                          else Check ('c:anything', CDir + DirSep + 'anything');
  Check (CC + DirSep, CDrive + DirSep);
-{$IFDEF NODOTS}
+ {$IFDEF NODOTS}
  Check ('C:.', 'C:.');
  Check (CC + DirSep + '.', CDrive + DirSep + '.');
  Check (CC + DirSep + '..', CDrive + DirSep + '..');
-{$ELSE NODOTS}
+ {$ELSE NODOTS}
  Check ('C:.', CDir);
  Check (CC + DirSep + '.', CDrive + DirSep);
  Check (CC + DirSep + '..', CDrive + DirSep);
-{$ENDIF NODOTS}
+ {$ENDIF NODOTS}
  Check (CC + DirSep + 'DOS', CDrive + DirSep + 'DOS');
-{$IFNDEF NODOTS}
+ {$IFNDEF NODOTS}
  Check (CC + DirSep + '..' + DirSep + 'DOS', CDrive + DirSep + 'DOS');
-{$ENDIF NODOTS}
+ {$ENDIF NODOTS}
  Check (CC + DirSep + 'DOS.', CDrive + DirSep + 'DOS.');
-{$IFDEF AMIGA}
+ {$IFDEF HASAMIGA} (* This has no effect - AMIGA has NODRIVEC defined... *)
  Check (CC + DirSep + 'DOS' + DirSep, CDrive + DirSep);
-{$ELSE AMIGA}
+ {$ELSE HASAMIGA}
  Check (CC + DirSep + 'DOS' + DirSep, CDrive + DirSep + 'DOS' + DirSep);
-{$ENDIF AMIGA}
-{$IFNDEF NODOTS}
+ {$ENDIF HASAMIGA}
+ {$IFNDEF NODOTS}
  Check (CC + DirSep + 'DOS' + DirSep + '.', CDrive + DirSep + 'DOS');
  Check (CC + DirSep + 'DOS' + DirSep + '..', CDrive + DirSep);
  Check (CC + DirSep + 'DOS' + DirSep + '..' + DirSep, CDrive + DirSep);
@@ -331,14 +334,20 @@ if CDir [Length (CDir)] = DirSep then Check ('c:anything', CDir + 'anything')
                                                                DirSep + 'DOS');
  Check ('C:' + DirSep + 'DOS' + DirSep + 'TEST' + DirSep + '..' + DirSep,
                                              CDrive + DirSep + 'DOS' + DirSep);
-{$ENDIF NODOTS}
+ {$ENDIF NODOTS}
 {$ENDIF NODRIVEC}
 
 {$IFNDEF MACOS}
+ {$IFDEF HASAMIGA}
+ Check (DirSep, TestDir + TestDir1Name);
+ Check (DirSep + DirSep + TestFileName, TestDir + TestFileName);
+ Check (DirSep + 'DOS', TestDir + TestDir1Name + DirSep + 'DOS');
+ {$ELSE HASAMIGA}
  Check (DirSep, TestDrive + DirSep);
  Check (DirSep + '.', TestDrive + DirSep);
  Check (DirSep + '..', TestDrive + DirSep);
  Check (DirSep + 'DOS', TestDrive + DirSep + 'DOS');
+ {$ENDIF HASAMIGA}
 {$ENDIF MACOS}
  Check ('d', CurDir + DirSep + 'd');
 {$IFDEF MACOS}
@@ -367,15 +376,15 @@ if CDir [Length (CDir)] = DirSep then Check ('c:anything', CDir + 'anything')
  Check ('.special', CurDir + DirSep + '.special');
  Check ('..special', CurDir + DirSep + '..special');
  Check ('special..', CurDir + DirSep + 'special..');
-{$IFDEF AMIGA}
- Check ('special.' + DirSep, CurDir);
-{$ELSE AMIGA}
+{$IFDEF HASAMIGA}
+ Check ('special.' + DirSep, CurDir + DirSep + 'special.' + DirSep);
+{$ELSE HASAMIGA}
  {$IFDEF MACOS}
  Check ('special.' + DirSep, 'special.' + DirSep);
  {$ELSE MACOS}
  Check ('special.' + DirSep, CurDir + DirSep + 'special.' + DirSep);
  {$ENDIF MACOS}
-{$ENDIF AMIGA}
+{$ENDIF HASAMIGA}
 {$IFDEF MACOS}
  Check (DirSep + DirSep, TestDir + TestDir1Name + DirSep);
  Check (DirSep + DirSep + TestFileName, TestDir + TestDir1Name + DirSep
@@ -481,7 +490,11 @@ if CDir [Length (CDir)] = DirSep then Check ('c:anything', CDir + 'anything')
  {$ENDIF NODRIVEC}
 {$ENDIF UNIX}
 {$IFDEF VOLUMES}
+ {$IFDEF HASAMIGA}
+ Check ('VolName' + DriveSep + 'DIR1', 'VolName' + DriveSep + 'DIR1');
+ {$ELSE HASAMIGA}
  Check ('VolName' + DriveSep + DirSep + 'DIR1', 'VolName' + DriveSep + DirSep + 'DIR1');
+ {$ENDIF HASAMIGA}
  {$IFNDEF NODOTS}
  Check ('VolName' + DriveSep + DirSep + 'DIR1' + DirSep + '..', 'VolName' + DriveSep + DirSep);
  Check ('VolName' + DriveSep + DirSep + 'DIR1' + DirSep + '..' + DirSep + '..',
@@ -496,13 +509,13 @@ if CDir [Length (CDir)] = DirSep then Check ('c:anything', CDir + 'anything')
  Check ('SrvName/VolName' + DriveSep + DirSep + 'TEST', 'SrvName' + DirSep + 'VolName' +
                                                          DriveSep + DirSep + 'TEST');
  {$ENDIF NETWARE}
- {$IFDEF AMIGA}
+ {$IFDEF HASAMIGA}
   {$IFDEF NODOTS}
  Check ('.', CurDir + DirSep + '.');
   {$ELSE NODOTS}
  Check ('.', CurDir);
   {$ENDIF NODOTS}
- {$ENDIF AMIGA}
+ {$ENDIF HASAMIGA}
 {$ENDIF VOLUMES}
  Erase (F);
 {$IFNDEF NODRIVEC}
