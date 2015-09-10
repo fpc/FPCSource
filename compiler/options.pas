8fcac217b53e6f6d62d60d44babd1ef76b6a3be2
steve <steve@idefix.freepascal.org>
2015-09-10 15:16:51 +0000

    0007 Added FPC_HAS_EXTENDEDINTERFACERTTI define
    git-svn-id: branches/interfacertti@31583 -

--- compiler/options.pas
+++ compiler/options.pas
@@ -3258,6 +3258,7 @@ begin
   def_system_macro('FPC_HAS_INTERNAL_ABS_INT64');
 {$endif x86_64 or powerpc64 or aarch64}
 
+  def_system_macro('FPC_HAS_EXTENDEDINTERFACERTTI');
   def_system_macro('FPC_HAS_UNICODESTRING');
   def_system_macro('FPC_RTTI_PACKSET1');
   def_system_macro('FPC_HAS_CPSTRING');
