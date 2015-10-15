457107d16545f3f1622812ce2cc98f144f04112f
nickysn <nickysn@gmail.com>
2015-10-15 13:11:47 +0000

    * check UseIncDec before generating an INC instruction in tx86addnode.emit_generic_code
    git-svn-id: trunk@32057 -

--- compiler/x86/nx86add.pas
+++ compiler/x86/nx86add.pas
@@ -146,7 +146,8 @@ unit nx86add;
                  if (op=A_ADD) and
                     (right.location.loc=LOC_CONSTANT) and
                     (right.location.value=1) and
-                    not(cs_check_overflow in current_settings.localswitches) then
+                    not(cs_check_overflow in current_settings.localswitches) and
+                    UseIncDec then
                   begin
                     emit_reg(A_INC,TCGSize2Opsize[opsize],left.location.register);
                   end
