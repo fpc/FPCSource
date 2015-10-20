9437d0e4d810b3f8a27199f760c2044616b9358b
Jonas Maebe <jonas@freepascal.org>
2015-10-20 12:58:53 +0000

    * Linux/AArch64 must also copy value parameters passed by reference on the     caller side, as this is an AArch64 ABI requirement (unrelated to Darwin).     Fixes webtbs/tw3523 compiled with optimisations on Linux/AArch64
    git-svn-id: trunk@32102 -

--- compiler/systems.pas
+++ compiler/systems.pas
@@ -357,7 +357,7 @@ interface
 
        { all systems where a value parameter passed by reference must be copied
          on the caller side rather than on the callee side }
-       systems_caller_copy_addr_value_para = [system_aarch64_darwin];
+       systems_caller_copy_addr_value_para = [system_aarch64_darwin,system_aarch64_linux];
 
        { pointer checking (requires special code in FPC_CHECKPOINTER,
          and can never work for libc-based targets or any other program
