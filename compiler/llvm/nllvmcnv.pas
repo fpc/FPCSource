bd4787c716df18502b5772d7e37cd5d8345195fe
Jonas Maebe <jonas@freepascal.org>
2015-11-21 12:36:48 +0000

    * force complex procvars immediately to memory for llvm, so we don't have to     deal with the hacky register/registerhi complex locations
    git-svn-id: trunk@32407 -

--- compiler/llvm/nllvmcnv.pas
+++ compiler/llvm/nllvmcnv.pas
@@ -33,6 +33,7 @@ interface
          protected
           function first_int_to_real: tnode; override;
           function first_int_to_bool: tnode; override;
+          function first_nil_to_methodprocvar: tnode; override;
           procedure second_int_to_int;override;
          { procedure second_string_to_string;override; }
          { procedure second_cstring_to_pchar;override; }
@@ -45,6 +46,7 @@ interface
          { procedure second_real_to_real;override; }
          { procedure second_cord_to_pointer;override; }
          { procedure second_proc_to_procvar;override; }
+         procedure second_nil_to_methodprocvar; override;
          procedure second_bool_to_int;override;
          procedure second_int_to_bool;override;
          { procedure second_load_smallset;override;  }
@@ -63,7 +65,7 @@ uses
   llvmbase,aasmllvm,
   procinfo,
   symconst,symtype,symdef,defutil,
-  cgbase,cgutils,hlcgobj,pass_2;
+  cgbase,cgutils,tgobj,hlcgobj,pass_2;
 
 { tllvmtypeconvnode }
 
@@ -83,6 +85,15 @@ function tllvmtypeconvnode.first_int_to_bool: tnode;
                not(left.location.loc in [LOC_FLAGS,LOC_JUMP])) then
           expectloc:=LOC_JUMP;
       end;
+  end;
+
+
+function tllvmtypeconvnode.first_nil_to_methodprocvar: tnode;
+  begin
+    result:=inherited;
+    if assigned(result) then
+      exit;
+    expectloc:=LOC_REFERENCE;
   end;
 
 
@@ -135,6 +146,19 @@ procedure tllvmtypeconvnode.second_int_to_real;
     location.register:=hlcg.getfpuregister(current_asmdata.CurrAsmList,resultdef);
     hlcg.location_force_reg(current_asmdata.CurrAsmList,left.location,left.resultdef,left.resultdef,true);
     current_asmdata.CurrAsmList.concat(taillvm.op_reg_size_reg_size(op,location.register,left.resultdef,left.location.register,resultdef));
+  end;
+
+
+procedure tllvmtypeconvnode.second_nil_to_methodprocvar;
+  var
+    href: treference;
+  begin
+    tg.gethltemp(current_asmdata.CurrAsmList,resultdef,resultdef.size,tt_normal,href);
+    location_reset_ref(location,LOC_REFERENCE,def_cgsize(resultdef),href.alignment);
+    location.reference:=href;
+    hlcg.g_ptrtypecast_ref(current_asmdata.CurrAsmList,cpointerdef.getreusable(resultdef),cpointerdef.getreusable(methodpointertype),href);
+    hlcg.g_load_const_field_by_name(current_asmdata.CurrAsmList,trecorddef(methodpointertype),'proc',0,href);
+    hlcg.g_load_const_field_by_name(current_asmdata.CurrAsmList,trecorddef(methodpointertype),'self',0,href);
   end;
 
 
