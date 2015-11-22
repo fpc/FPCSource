c4b347c271aee877cbb8bfc8bb132d5b99e3f08d
Jonas Maebe <jonas@freepascal.org>
2015-11-22 10:52:20 +0000

    * support anonymous procvar definitions for jvm outside parameter lists (in a     parameter list they represent nested procvars, which aren't supported yet)
    git-svn-id: trunk@32454 -

--- tests/test/jvm/testall.sh
+++ tests/test/jvm/testall.sh
@@ -187,3 +187,5 @@ java -Dfile.encoding=UTF-8 -cp ../../../rtl/units/$RTLDIR:. tsetstring
 $PPC -O2 -g -B -Sa tnestcallpass1
 $PPC -O2 -g -B -Sa tformalclass
 java -Dfile.encoding=UTF-8 -cp ../../../rtl/units/$RTLDIR:. tformalclass
+$PPC -O2 -g -B -Sa tprocvaranon
+java -Dfile.encoding=UTF-8 -cp ../../../rtl/units/$RTLDIR:. tprocvaranon
