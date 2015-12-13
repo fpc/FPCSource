a3964d9ee06769cd424f7a5b75ec82ca141ce230
florian <florian@freepascal.org>
2015-12-13 13:28:51 +0000

    + support for RDTSCP, resolves issue #28916
    git-svn-id: trunk@32652 -

---
+++ tests/webtbs/tw28916.pp
@@ -0,0 +1,16 @@
+{ %cpu=i386,x86_64 }
+program Project1;
+
+{$mode delphi}
+{$asmmode intel}
+
+function RDTSCP: Int64; assembler;
+asm
+  RDTSCP
+end;
+
+begin
+  if (PByte(@RDTSCP)[0]<>$0f) or (PByte(@RDTSCP)[1]<>$01) or (PByte(@RDTSCP)[2]<>$f9) then
+    halt(1);
+  writeln('ok');
+end.
