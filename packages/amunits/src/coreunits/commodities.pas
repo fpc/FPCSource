5d7b08d28f01c428582643926bfb40171f05bded
Károly Balogh <karoly@freepascal.org>
2015-12-24 05:00:08 +0000

    amunits: fixed CreateCxObj argument locations in commodities
    git-svn-id: trunk@32702 -

--- packages/amunits/src/coreunits/commodities.pas
+++ packages/amunits/src/coreunits/commodities.pas
@@ -234,7 +234,7 @@ FUNCTION ActivateCxObj(co : pCxObj location 'a0'; tru : LONGINT location 'd0') :
 PROCEDURE AddIEvents(events : pInputEvent location 'a0'); syscall CxBase 180;
 PROCEDURE AttachCxObj(headObj : pCxObj location 'a0'; co : pCxObj location 'a1'); syscall CxBase 084;
 PROCEDURE ClearCxObjError(co : pCxObj location 'a0'); syscall CxBase 072;
-FUNCTION CreateCxObj(typ : ULONG location 'd0'; arg1 : LONGINT location 'a1'; arg2 : LONGINT location 'a2') : pCxObj; syscall CxBase 030;
+FUNCTION CreateCxObj(typ : ULONG location 'd0'; arg1 : LONGINT location 'a0'; arg2 : LONGINT location 'a1') : pCxObj; syscall CxBase 030;
 FUNCTION CxBroker(nb : pNewBroker location 'a0'; error : pCxObj location 'd0') : pCxObj; syscall CxBase 036;
 FUNCTION CxMsgData(cxm : pCxMsg location 'a0') : POINTER; syscall CxBase 144;
 FUNCTION CxMsgID(cxm : pCxMsg location 'a0') : LONGINT; syscall CxBase 150;
