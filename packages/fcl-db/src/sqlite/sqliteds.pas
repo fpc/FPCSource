adcebe7a8c3fdef9a3c5401299833f12c2592c40
blikblum <blikblum@idefix.freepascal.org>
2015-12-27 10:53:33 +0000

    sqliteds: set memo conversion procedure in TSqliteDataset
    git-svn-id: trunk@32755 -

--- packages/fcl-db/src/sqlite/sqliteds.pas
+++ packages/fcl-db/src/sqlite/sqliteds.pas
@@ -186,10 +186,14 @@ begin
     end;
     FieldDefs.Add(FieldDefs.MakeNameUnique(String(ColumnNames[i])), AType, DataSize);
     //Set the pchar2sql function
-    if AType in [ftString, ftMemo] then
-      FGetSqlStr[i] := @Char2SQLStr
+    case AType of
+      ftString:
+        FGetSqlStr[i] := @Char2SQLStr;
+      ftMemo:
+        FGetSqlStr[i] := @Memo2SQLStr;
     else
       FGetSqlStr[i] := @Num2SQLStr;
+    end;
   end;
   sqlite_finalize(vm, nil);
   {
