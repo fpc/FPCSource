d93babffde20df024d7965e10b57ca00d8b60ab2
michael <michael@freepascal.org>
2015-12-30 10:04:41 +0000

    * Prevent timing attack, patch by Thaddy De Koning
    git-svn-id: trunk@32794 -

--- packages/hash/src/sha1.pp
+++ packages/hash/src/sha1.pp
@@ -313,7 +313,10 @@ var
   A: array[0..4] of Cardinal absolute Digest1;
   B: array[0..4] of Cardinal absolute Digest2;
 begin
+{$push}
+{$B+}
   Result := (A[0] = B[0]) and (A[1] = B[1]) and (A[2] = B[2]) and (A[3] = B[3]) and (A[4] = B[4]);
+{$pop}
 end;
 
 end.
