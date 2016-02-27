7d38224c65ef2af30e26147c7584a9b81fa1c9a3
michael <michael@freepascal.org>
2016-02-27 15:16:36 +0000

    * Do not activate client/server in design mode
    git-svn-id: trunk@33129 -

--- packages/fcl-process/src/simpleipc.pp
+++ packages/fcl-process/src/simpleipc.pp
@@ -269,7 +269,7 @@ procedure TSimpleIPC.SetActive(const AValue: Boolean);
 begin
   if (FActive<>AValue) then
     begin
-    if (csLoading in ComponentState) then
+    if ([]<>([csLoading,csDesigning]*ComponentState)) then
       FActive:=AValue
     else  
       If AValue then
