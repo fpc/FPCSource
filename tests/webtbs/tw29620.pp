315b32918e21bc0233f23f4011a7ef97c4c035e6
Jonas Maebe <jonas@freepascal.org>
2016-03-06 14:16:47 +0000

    + Tconstexprint-version of ispowerof2() to correctly deal with constants     > high(int64) (mantis #29620)
    git-svn-id: trunk@33191 -

---
+++ tests/webtbs/tw29620.pp
@@ -0,0 +1,12 @@
+var x:uint64;
+    y:longword;
+begin
+ y:=123;
+ x:=y*uint64(10116239910488455739);
+ if x<>8365656051540097625 then
+   halt(1);
+ x:=y*uint64($8000000000000000);
+ if x<>(uint64(123) shl 63) then
+   halt(2);
+end.
+
