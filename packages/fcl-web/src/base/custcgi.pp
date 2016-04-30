5c2f941c32c457fc8a0b4502a4c0cea50a70ae2f
michael <michael@freepascal.org>
2016-04-30 06:29:05 +0000

    * Correct PATH_INFO for IIS, patch by Marcos Douglas (bug ID 30073)
    git-svn-id: trunk@33576 -

--- packages/fcl-web/src/base/custcgi.pp
+++ packages/fcl-web/src/base/custcgi.pp
@@ -373,6 +373,9 @@ begin
         end;
       end;
     end;
+  // Microsoft-IIS hack. IIS includes the script name in the PATH_INFO
+  if Pos('IIS', ServerSoftware) > 0 then
+    SetHTTPVariable(hvPathInfo,StringReplace(PathInfo, ScriptName, '', [rfReplaceAll, rfIgnoreCase]));
   R:=UpCase(Method);
   if (R='POST') or (R='PUT') or (ContentLength>0) then
     ReadContent;
