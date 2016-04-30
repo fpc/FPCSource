87be61c80788fc10bc6dbcea7f37cddc1ce30dfd
michael <michael@freepascal.org>
2016-04-30 08:29:26 +0000

    * Added HPACK implementation by José Mejuto (bug ID 30058)
    git-svn-id: trunk@33579 -

--- packages/hash/fpmake.pp
+++ packages/hash/fpmake.pp
@@ -36,7 +36,11 @@ begin
     T:=P.Targets.AddUnit('src/uuid.pas');
     T:=P.Targets.AddUnit('src/hmac.pp');
     T:=P.Targets.AddUnit('src/unixcrypt.pas');
-      T.OSes:=[Linux];
+    T:=P.Targets.AddUnit('src/uhpacktables.pp');
+    T:=P.Targets.AddUnit('src/uhpackimp.pp');
+    T:=P.Targets.AddUnit('src/uhpack.pp');
+    
+    T.OSes:=[Linux];
     T:=P.Targets.AddExampleunit('examples/mdtest.pas');
     T:=P.Targets.AddExampleunit('examples/crctest.pas');
     T:=P.Targets.AddExampleunit('examples/sha1test.pp');
