0a2e5dd24bc9cb0644ff2cf5151050c17360c285
nickysn <nickysn@gmail.com>
2016-05-02 15:11:15 +0000

    + added type PXEventClass (pointer to TXEventClass)
    git-svn-id: trunk@33618 -

--- packages/x11/src/xi.pp
+++ packages/x11/src/xi.pp
@@ -348,6 +348,7 @@ type
 {$ELSE}
         XEventClass = culong;
 {$ENDIF}
+        PXEventClass = ^TXEventClass;
         TXEventClass = XEventClass;
 
 (*******************************************************************
