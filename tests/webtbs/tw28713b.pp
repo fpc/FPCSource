25695174d0004a83cc9a8bbbbd264a977bdaab13
Jonas Maebe <jonas@freepascal.org>
2016-07-03 15:31:07 +0000

    --- Merging r32602 into '.': A    tests/webtbs/tw28713b.pp A    tests/webtbs/tw28713.pp U    compiler/nmem.pas --- Recording mergeinfo for merge of r32602 into '.':  U   . --- Merging r33195 into '.': G    compiler/nmem.pas --- Recording mergeinfo for merge of r33195 into '.':
    git-svn-id: branches/fixes_3_0@34053 -

---
+++ tests/webtbs/tw28713b.pp
@@ -0,0 +1,31 @@
+{ %OPT=-O3 }
+{$mode objfpc}
+// Compiled with option -O3 for Win32-I386
+
+type
+  PWordArray = ^TWordArray;
+  TWordArray = array [0..1023]of LongWord;
+
+  TMyclass = class
+    LoByte,HiByte:Byte
+  end;
+
+var
+  Buffer:TWordArray;
+  OldMousePos:LongInt = 0;
+  ScreenBuffer:Pointer = @Buffer;
+
+procedure Show(ScreenBuffer:Pointer);
+begin
+  TMyClass(PWordArray(ScreenBuffer)^[OldMousePos]).HiByte:=(not
+  TMyClass(PWordArray(ScreenBuffer)^[OldMousePos]).HiByte)and $7F
+  // he forgets to write the result into the array
+end;
+
+begin
+  TMyClass(Buffer[0]):=TMyClass.Create;
+  Show(ScreenBuffer);
+  if TMyClass(Buffer[0]).HiByte<>$7F then
+    halt(1);
+  writeln('ok');
+end.
