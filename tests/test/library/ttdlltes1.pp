d4945f4fd057dd306386832cae20ac77005cafcb
pierre <pierre@freepascal.org>
2016-07-19 11:35:21 +0000

    Avoid range check error for Win64 by correcting type of parameters for GetModuleFileNameA WinAPI function
    git-svn-id: trunk@34148 -

--- tests/test/library/ttdlltes1.pp
+++ tests/test/library/ttdlltes1.pp
@@ -9,7 +9,7 @@
 }
 library testdll;
 
-function GetModuleFileName(hModule:longint;lpszPath:pchar;cchPath:longint):longint;
+function GetModuleFileName(hModule:HModule;lpszPath:pchar;cchPath:dword):dword;
   stdcall; external 'kernel32' name 'GetModuleFileNameA';
 procedure beep(ID:longint);
   stdcall; external 'user32' name 'MessageBeep';
