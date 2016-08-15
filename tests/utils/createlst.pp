cdda4da2d3be75abbb0f6894d8ed28501ffddede
svenbarth <pascaldragon@googlemail.com>
2016-08-15 15:58:16 +0000

    * sort the paths alphabetically
    git-svn-id: trunk@34319 -

--- tests/utils/createlst.pp
+++ tests/utils/createlst.pp
@@ -1,26 +1,35 @@
 program createlst;
 
 uses
-  SysUtils;
+  SysUtils, Classes;
 
 var
   i: LongInt;
   sr: TSearchRec;
   path: String;
+  sl: TStringList;
 begin
   if ParamCount = 0 then begin
     Writeln('createlst PATH [PATH [...]]');
     Exit;
   end;
 
+  sl := TStringList.Create;
+
   for i := 1 to ParamCount do begin
     path := IncludeTrailingPathDelimiter(ParamStr(i));
     if FindFirst(path + 't*.pp', 0, sr) = 0 then begin
       repeat
-        Writeln(path + sr.Name);
+        sl.Add(path + sr.Name);
       until FindNext(sr) <> 0;
 
       FindClose(sr);
     end;
   end;
+
+  sl.Sort;
+  for i := 0 to sl.Count - 1 do
+    Writeln(sl[i]);
+
+  sl.Free;
 end.
