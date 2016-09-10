7d4b3dc7aa6e560785b9eeced7abf6d227eeb04f
michael <michael@freepascal.org>
2016-09-10 19:34:00 +0000

    * Tests for bug ID #28201
    git-svn-id: trunk@34499 -

--- packages/fcl-base/tests/tcmaskutils.pp
+++ packages/fcl-base/tests/tcmaskutils.pp
@@ -5,7 +5,7 @@ unit tcmaskutils;
 interface
 
 uses
-  Classes, SysUtils, fpcunit;
+  Classes, SysUtils, fpcunit, testregistry, maskutils;
 
 Type
 
@@ -14,6 +14,8 @@ Type
   TTestMaskUtils = Class(TTestCase)
   Published
     Procedure Test1;
+    Procedure Test2;
+    Procedure Test3;
   end;
 
 implementation
@@ -23,6 +25,16 @@ implementation
 procedure TTestMaskUtils.Test1;
 begin
   AssertEquals('H1H357-K808K-44616-YK8720',FormatMaskText('!>cccccc\-ccccc\-ccccc\-cccccc;0;*', 'H1H357K808K44616YK8720'))
+end;
+
+procedure TTestMaskUtils.Test2;
+begin
+  AssertEquals('555.   .   .   ',FormatMaskText('999.999.999.999','555555'));
+end;
+
+procedure TTestMaskUtils.Test3;
+begin
+  AssertEquals('555.   .   .   ',FormatMaskText('999.999.999.999;1;_','555555'));
 end;
 
 initialization
