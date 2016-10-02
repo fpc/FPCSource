e5a12319311d364e4ca0e252e15d5e87f1237f0b
Jonas Maebe <jonas@freepascal.org>
2016-10-02 12:56:59 +0000

    * updated FSF address (mantis #30198, patch by Paul Gevers)
    git-svn-id: trunk@34598 -

--- utils/fppkg/lnet/lmimestreams.pp
+++ utils/fppkg/lnet/lmimestreams.pp
@@ -14,7 +14,7 @@
 
   You should have received a Copy of the GNU Library General Public License
   along with This library; if not, Write to the Free Software Foundation,
-  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
   This license has been modified. See File LICENSE.ADDON for more inFormation.
   Should you find these sources without a LICENSE File, please contact
