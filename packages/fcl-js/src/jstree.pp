3a3f49023444c10da955e67cbe7c987b126f279f
michael <michael@freepascal.org>
2016-10-08 08:11:41 +0000

    * Added copyright header
    git-svn-id: trunk@34651 -

--- packages/fcl-js/src/jstree.pp
+++ packages/fcl-js/src/jstree.pp
@@ -1,3 +1,17 @@
+{ ********************************************************************* 
+    This file is part of the Free Component Library (FCL)
+    Copyright (c) 2016 Michael Van Canneyt.
+       
+    Javascript syntax tree definitions
+            
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+                   
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+                                
+  **********************************************************************}
 unit jstree;
 
 {$mode objfpc}{$H+}
