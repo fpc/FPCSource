265923a2ee3e508a5ccacb1393c278ee3e0f6e9b
Jonas Maebe <jonas@freepascal.org>
2016-10-08 20:45:45 +0000

    * several fixes for TMultiReadExclusiveWriteSynchronizer, based on patch by     Derek (mantis #28830)    o ability for a thread to acquire a readlock when it already holds a      write lock, or vice versa    o detect when thread1 releases a lock while it hadn't acquired one    o correct result of beginwrite (return true only when another thread      held/acquired a write lock before the current thread got it)    o extra compared to Derek's patch: replaced some additional      (mostly pre-existing) interlocked*() hacks with proper memory      barriers, made formatting consistent, fixed compilation on platforms      where tthreadid is not an integer type, improved tthreadid hashing      function for 64 bit platforms, fixed some comments
    git-svn-id: trunk@34678 -

--- tests/test/units/sysutils/trwsync.pp
+++ tests/test/units/sysutils/trwsync.pp
@@ -11,7 +11,9 @@ uses
 
 var
   lock: TMultiReadExclusiveWriteSynchronizer;
+  event1, event2: prtlevent;
   gcount: longint;
+  gotdeadlockexception,
   waiting: boolean;
 
 type
@@ -36,6 +38,49 @@ type
     procedure execute; override;
   end;
   
+  treadwritecounter = class(tcounter)
+   private
+    ftrywriteupgrade: boolean;
+   public
+    constructor create(trywriteupgrade: boolean);
+    procedure execute; override;
+  end;
+
+  tdeadlock1 = class(tthread)
+    procedure execute; override;
+  end;
+
+  tdeadlock2 = class(tthread)
+    procedure execute; override;
+  end;
+
+  tdoublereadonewrite1 = class(tthread)
+    procedure execute; override;
+  end;
+
+  tdoublereadonewrite2 = class(tthread)
+    procedure execute; override;
+  end;
+
+  twrongthreadendacquire = class(tthread)
+    ftestwrongreadrelease: boolean;
+    constructor create(testwrongreadrelease: boolean);
+    procedure execute; override;
+  end;
+
+  twrongthreadendrelease = class(tthread)
+    ftestwrongreadrelease: boolean;
+    constructor create(testwrongreadrelease: boolean);
+    procedure execute; override;
+  end;
+
+  tdoublewrite = class(tthread)
+    fsecondwritethread: boolean;
+    constructor create(secondwritethread: boolean);
+    procedure execute; override;
+  end;
+
+
 constructor tcounter.create;
   begin
     { create suspended }
@@ -100,7 +145,199 @@ procedure twritecounter.execute;
           sleep(r);
       end;
   end;
-  
+
+
+constructor treadwritecounter.create(trywriteupgrade: boolean);
+  begin
+    ftrywriteupgrade:=trywriteupgrade;
+    inherited create;
+  end;
+
+
+procedure treadwritecounter.execute;
+  var
+    i: longint;
+    l: longint;
+    r: longint;
+  begin
+    for i:=1 to 100000 do
+      begin
+        lock.beginread;
+        if ftrywriteupgrade and
+           ((i=50000) or
+            (random(10000)=0)) then
+          begin
+            inc(flocalcount);
+            lock.beginwrite;
+            l:=gcount;
+            { guarantee at least one sleep }
+            if i=50000 then
+              sleep(20+random(30))
+            else if (random(5)=0) then
+              sleep(20);
+            lock.beginwrite;
+            gcount:=l+1;
+            lock.endwrite;
+            lock.endwrite;
+          end;
+        lock.endread;
+        r:=random(30000);
+        if (r=0) then
+          sleep(30);
+      end;
+  end;
+
+
+procedure tdeadlock1.execute;
+  var
+    localgotdeadlockexception: boolean;
+  begin
+    localgotdeadlockexception:=false;
+    lock.beginread;
+    RTLEventSetEvent(event2);
+    RTLEventWaitFor(event1);
+    try
+      lock.beginwrite;
+    except
+      localgotdeadlockexception:=true;
+      gotdeadlockexception:=true;
+    end;
+    if not localgotdeadlockexception then
+      lock.endwrite;
+    lock.endread;
+  end;
+
+
+procedure tdeadlock2.execute;
+  var
+    localgotdeadlockexception: boolean;
+  begin
+    localgotdeadlockexception:=false;
+    lock.beginread;
+    RTLEventSetEvent(event1);
+    RTLEventWaitFor(event2);
+    try
+      lock.beginwrite;
+    except
+      localgotdeadlockexception:=true;
+      gotdeadlockexception:=true;
+    end;
+    if not localgotdeadlockexception then
+      lock.endwrite;
+    lock.endread;
+  end;
+
+
+procedure tdoublereadonewrite1.execute;
+  begin
+    // 1)
+    lock.beginread;
+    // 2)
+    RTLEventSetEvent(event2);
+    // 5)
+    RTLEventWaitFor(event1);
+    { ensure tdoublereadonewrite2 has time to get stuck in beginwrite }
+    sleep(500);
+    // 6)
+    lock.beginread;
+    // 7)
+    lock.endread;
+    // 8)
+    lock.endread;
+  end;
+
+
+procedure tdoublereadonewrite2.execute;
+  begin
+    // 3)
+    RTLEventWaitFor(event2);
+    // 4)
+    RTLEventSetEvent(event1);
+    // 4a -- block until after 8)
+    lock.beginwrite;
+    // 9)
+    lock.endwrite;
+  end;
+
+
+constructor twrongthreadendacquire.create(testwrongreadrelease: boolean);
+  begin
+    ftestwrongreadrelease:=testwrongreadrelease;
+    inherited create(false);
+  end;
+
+
+procedure twrongthreadendacquire.execute;
+  begin
+    if ftestwrongreadrelease then
+      lock.beginread
+    else
+      lock.beginwrite;
+    RTLEventSetEvent(event1);
+    RTLEventWaitFor(event2);
+    try
+      if ftestwrongreadrelease then
+        lock.endread
+      else
+        lock.endwrite;
+    except
+      halt(30);
+    end;
+  end;
+
+
+constructor twrongthreadendrelease.create(testwrongreadrelease: boolean);
+  begin
+    ftestwrongreadrelease:=testwrongreadrelease;
+    inherited create(false);
+  end;
+
+
+procedure twrongthreadendrelease.execute;
+  var
+    caught: boolean;
+  begin
+    RTLEventWaitFor(event1);
+    caught:=false;
+    try
+      if ftestwrongreadrelease then
+        lock.endread
+      else
+        lock.endwrite;
+    except
+      caught:=true;
+    end;
+    RTLEventSetEvent(event2);
+    if not caught then
+      halt(40);
+  end;
+
+
+constructor tdoublewrite.create(secondwritethread: boolean);
+  begin
+    fsecondwritethread:=secondwritethread;
+    inherited create(false);
+  end;
+
+
+procedure tdoublewrite.execute;
+  begin
+    if fsecondwritethread then
+      begin
+        RTLEventWaitFor(event1);
+        if lock.beginwrite then
+          halt(50);
+      end
+    else
+      begin
+        if not lock.beginwrite then
+          halt(51);
+        RTLEventSetEvent(event1);
+        // give the other thread the time to get to its beginwrite call
+        Sleep(500);
+      end;
+    lock.endwrite;
+  end;
 
 
 procedure terrorcheck.execute;
@@ -119,16 +356,65 @@ end;
 var
   r1,r2,r3,r4,r5,r6: treadcounter;
   w1,w2,w3,w4: twritecounter;
+  rw1,rw2,rw3: treadwritecounter;
+  d1: tdeadlock1;
+  d2: tdeadlock2;
+  dr1: tdoublereadonewrite1;
+  dr2: tdoublereadonewrite2;
+  wr1: twrongthreadendacquire;
+  wr2: twrongthreadendrelease;
+  dw1, dw2: tdoublewrite;
+  caught: boolean;
 begin
   waiting:=false;
   terrorcheck.create(false);
   randomize;
   lock:=TMultiReadExclusiveWriteSynchronizer.create;
+  event1:=RTLEventCreate;
+  event2:=RTLEventCreate;
+
   { verify that the lock is recursive }
-  lock.beginwrite;
-  lock.beginwrite;
+  if not lock.beginwrite then
+    halt(10);
+  if not lock.beginwrite then
+    halt(11);
   lock.endwrite;
   lock.endwrite;
+
+  { verify that we can upgrade a read lock to a write lock }
+  lock.beginread;
+  if not lock.beginwrite then
+    halt(12);
+  lock.endwrite;
+  lock.endread;
+
+  { verify that owning a write lock does not prevent getting a read lock }
+  if not lock.beginwrite then
+    halt(13);
+  lock.beginread;
+  lock.endread;
+  lock.endwrite;
+
+  { verify that calling endread without beginread throws an exception }
+  caught:=false;
+  try
+    lock.endread;
+  except
+    caught:=true;
+  end;
+  if not caught then
+    halt(14);
+
+  { verify that calling endwrite without beginwrite throws an exception }
+  caught:=false;
+  try
+    lock.endwrite;
+  except
+    caught:=true;
+  end;
+  if not caught then
+    halt(15);
+
 
   { first try some writers }
   w1:=twritecounter.create;
@@ -200,6 +486,80 @@ begin
   r6.free;
   w1.free;
   w2.free;
+
+  { mixed readers and writers without proper synchronisation }
+  gcount:=0;
+  rw1:=treadwritecounter.create(true);
+  rw2:=treadwritecounter.create(false);
+  rw3:=treadwritecounter.create(false);
+
+  rw1.resume;
+  rw2.resume;
+  rw3.resume;
+
+  rw1.waitfor;
+  rw2.waitfor;
+  rw3.waitfor;
+
+  { must not have caused any data races }
+  if (gcount<>rw1.localcount+rw2.localcount+rw3.localcount) then
+    begin
+      writeln('error 5');
+      halt(5);
+    end;
+
+  RTLEventResetEvent(event1);
+  RTLEventResetEvent(event2);
+
+  { check deadlock detection }
+  d1:=tdeadlock1.create(false);
+  d2:=tdeadlock2.create(false);
+
+  d1.waitfor;
+  d2.waitfor;
+  if not gotdeadlockexception then
+    halt(6);
+
+  d1.free;
+  d2.free;
+
+
+  { check that a waiting writer does not block a reader trying to get
+    a recursive read lock it already holds }
+  dr1:=tdoublereadonewrite1.create(false);
+  dr2:=tdoublereadonewrite2.create(false);
+
+  dr1.waitfor;
+  dr2.waitfor;
+
+  dr1.free;
+  dr2.free;
+
+  { check that releasing a lock in another thread compared to where it
+    was acquired causes an exception }
+  wr1:=twrongthreadendacquire.create(true);
+  wr2:=twrongthreadendrelease.create(true);
+  wr1.waitfor;
+  wr2.waitfor;
+  wr1.free;
+  wr2.free;
+
+  wr1:=twrongthreadendacquire.create(false);
+  wr2:=twrongthreadendrelease.create(false);
+  wr1.waitfor;
+  wr2.waitfor;
+  wr1.free;
+  wr2.free;
+
+  dw1:=tdoublewrite.create(false);
+  dw2:=tdoublewrite.create(true);
+  dw1.waitfor;
+  dw2.waitfor;
+  dw1.free;
+  dw2.free;
+
+  RTLEventDestroy(event1);
+  RTLEventDestroy(event2);
 
   lock.free;
 
