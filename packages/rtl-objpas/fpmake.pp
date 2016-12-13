43212c45072638901f99eb7f62825abeed947ce2
svenbarth <pascaldragon@googlemail.com>
2016-12-13 22:06:28 +0000

    * use a separate OS set for the Rtti unit than CommonSrcOSes
    git-svn-id: trunk@35124 -

--- packages/rtl-objpas/fpmake.pp
+++ packages/rtl-objpas/fpmake.pp
@@ -24,6 +24,7 @@ Const
   StdConvsOSes  = [NativeNT,Win32,win64,os2,msdos,go32v2]+UnixLikes-[BeOS];
   FmtBCDOSes    = [atari,emx,gba,go32v2,msdos,nativent,nds,netware,netwlibc,os2,win32,win64,wince]+UnixLikes+AllAmigaLikeOSes;
   VariantsOSes  = [atari,emx,gba,go32v2,msdos,nativent,nds,netware,netwlibc,os2,watcom,wii,win32,win64,wince]+UnixLikes+AllAmigaLikeOSes;
+  RttiOSes      = [atari,emx,gba,go32v2,msdos,nativent,nds,netware,netwlibc,os2,watcom,wii,win32,win64,wince]+UnixLikes+AllAmigaLikeOSes;
   AllTargetsObjPas = DateUtilsOses +DateUtilOSes+
                   VarutilsOses + ConvutilsOSes + ConvutilOSes + StdConvsOSes+
                   FmtBCDOSes + StrUtilsOSes;
@@ -117,7 +118,7 @@ begin
        // AddUnit('Math');
      end;
 
-    T:=P.Targets.AddUnit('rtti.pp',CommonSrcOSes);
+    T:=P.Targets.AddUnit('rtti.pp',RttiOSes);
     T.ResourceStrings:=true;
   end
 end;
