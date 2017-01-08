25842d70c841ddeb657222805123aa34dc27c5c0
svenbarth <pascaldragon@googlemail.com>
2017-01-08 12:42:17 +0000

    * adjust test so it also checks the full ParamFlags instead of only the reduced one
    git-svn-id: trunk@35263 -

--- tests/test/trtti9.pp
+++ tests/test/trtti9.pp
@@ -11,7 +11,10 @@ type
 
 function TestParam(Param: PProcedureParam; Flags: TParamFlags; ParamType: Pointer; Name: ShortString): Boolean;
 begin
-  Result := (Param^.Flags = PByte(@Flags)^) and (Param^.ParamType = ParamType) and (Param^.Name = Name);
+  Result := (Param^.Flags = PByte(@Flags)^) and 
+            (Param^.ParamFlags = Flags) and
+            (Param^.ParamType = ParamType) and 
+            (Param^.Name = Name);
 end;
 
 var
