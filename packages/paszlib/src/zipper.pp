ef26fbd83d69360169d9de95141af39fbee53804
marco <marco@freepascal.org>
2017-02-22 16:52:46 +0000

    * attributes longword, mantis #30800
    git-svn-id: trunk@35466 -

--- packages/paszlib/src/zipper.pp
+++ packages/paszlib/src/zipper.pp
@@ -343,7 +343,7 @@ Type
     FArchiveFileName: String; //Name of the file as it appears in the zip file list
     FUTF8FileName : UTF8String;
     FUTF8DiskFileName : UTF8String;
-    FAttributes: LongInt;
+    FAttributes: LongWord;
     FDateTime: TDateTime;
     FDiskFileName: String; {Name of the file on disk (i.e. uncompressed. Can be empty if based on a stream.);
     uses local OS/filesystem directory separators}
@@ -378,7 +378,7 @@ Type
     Property Size : Int64 Read FSize Write FSize;
     Property DateTime : TDateTime Read FDateTime Write FDateTime;
     property OS: Byte read FOS write FOS;
-    property Attributes: LongInt read FAttributes write FAttributes;
+    property Attributes: LongWord read FAttributes write FAttributes;
     Property CompressionLevel: TCompressionlevel read FCompressionLevel write FCompressionLevel;
   end;
 
