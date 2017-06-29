a12111e1bdca000df147f25c645260a0190a59a4
Károly Balogh <karoly@freepascal.org>
2017-06-29 00:33:55 +0000

    m68k: define safecall_is_cdecl on m68k too in tcalvar6 and tcalst6
    git-svn-id: trunk@36613 -

--- tests/test/cg/tcalcst6.pp
+++ tests/test/cg/tcalcst6.pp
@@ -28,9 +28,9 @@ program tcalcst6;
   {$define tp}
 {$endif}
 
-{ On linux/i386 and linux/x86-64safecall is the same as cdecl, so it does not       }
-{ support all parameter types.                                      }
-{$if ((defined(linux) or defined(android)) and (defined(cpui386) or defined(cpux86_64)))}
+{ On linux/i386 and linux/x86-64 safecall is the same as cdecl, so it does not }
+{ support all parameter types.                                                 }
+{$if ((defined(linux) or defined(android)) and (defined(cpui386) or defined(cpux86_64) or defined(cpum68k))}
   {$define safecall_is_cdecl}
 {$endif}
 
--- tests/test/cg/tcalvar6.pp
+++ tests/test/cg/tcalvar6.pp
@@ -30,9 +30,9 @@ program tcalvar6;
   {$define tp}
 {$endif}
 
-{ On linux/i386 and linux/x86-64 afecall is the same as cdecl, so it does not       }
-{ support all parameter types.                                      }
-{$if ((defined(linux) or defined(android)) and (defined(cpui386) or defined(cpux86_64)))}
+{ On linux/i386 and linux/x86-64 safecall is the same as cdecl, so it does not }
+{ support all parameter types.                                                 }
+{$if ((defined(linux) or defined(android)) and (defined(cpui386) or defined(cpux86_64) or defined(cpum68k)))}
   {$define safecall_is_cdecl}
 {$endif}
 
