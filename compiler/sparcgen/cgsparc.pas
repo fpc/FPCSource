f30bf547e216c716c056fd551a044a3e834f7789
florian <florian@freepascal.org>
2017-07-09 18:14:14 +0000

    * TCGSparcGen.maybeadjustresult adapted for sparc64
    git-svn-id: trunk@36711 -

--- compiler/sparcgen/cgsparc.pas
+++ compiler/sparcgen/cgsparc.pas
@@ -637,8 +637,8 @@ implementation
         overflowops = [OP_MUL,OP_SHL,OP_ADD,OP_SUB,OP_NOT,OP_NEG];
       begin
         if (op in overflowops) and
-           (size in [OS_8,OS_S8,OS_16,OS_S16]) then
-          a_load_reg_reg(list,OS_32,size,dst,dst);
+           (size in [OS_8,OS_S8,OS_16,OS_S16{$ifdef SPARC64},OS_32,OS_S32{$endif SPARC64}]) then
+          a_load_reg_reg(list,OS_INT,size,dst,dst);
       end;
 
 
