f281ee339e29406f240cf86c94d86b3459068b57
pierre <pierre@freepascal.org>
2017-08-04 08:54:34 +0000

    Add obsolete prefix to cpu_ia64, system_ia64_win64 and system_ia64_embedded, ia64 specfic code has been removed  in rev 30836 dated 2015-05-09
    git-svn-id: trunk@36822 -

--- compiler/systems.pas
+++ compiler/systems.pas
@@ -268,7 +268,7 @@ interface
        systems_embedded = [system_i386_embedded,system_m68k_embedded,
                            system_powerpc_embedded,
                            system_sparc_embedded,system_vm_embedded,
-                           system_iA64_embedded,system_x86_64_embedded,
+                           obsolete_system_ia64_embedded,system_x86_64_embedded,
                            system_mips_embedded,system_arm_embedded,
                            system_powerpc64_embedded,system_avr_embedded,
                            system_jvm_java32,system_mipseb_embedded,system_mipsel_embedded,
