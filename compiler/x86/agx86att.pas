6a416a6aa1e015c97decb855dc4c7c1c08b9f5fb
pierre <pierre@freepascal.org>
2017-08-11 16:04:40 +0000

    use --32/--64 for Darwin GNU assembler calls
    git-svn-id: trunk@36870 -

--- compiler/x86/agx86att.pas
+++ compiler/x86/agx86att.pas
@@ -446,7 +446,7 @@ interface
             id     : as_darwin;
             idtxt  : 'AS-DARWIN';
             asmbin : 'as';
-            asmcmd : '-o $OBJ $EXTRAOPT $ASM -arch x86_64';
+            asmcmd : '--64 -o $OBJ $EXTRAOPT $ASM';
             supported_targets : [system_x86_64_darwin,system_x86_64_iphonesim];
             flags : [af_needar,af_smartlink_sections,af_supports_dwarf];
             labelprefix : 'L';
@@ -520,7 +520,7 @@ interface
             id     : as_darwin;
             idtxt  : 'AS-DARWIN';
             asmbin : 'as';
-            asmcmd : '-o $OBJ $EXTRAOPT $ASM -arch i386';
+            asmcmd : '--32 -o $OBJ $EXTRAOPT $ASM';
             supported_targets : [system_i386_darwin,system_i386_iphonesim];
             flags : [af_needar,af_smartlink_sections,af_supports_dwarf,af_stabs_use_function_absolute_addresses];
             labelprefix : 'L';
