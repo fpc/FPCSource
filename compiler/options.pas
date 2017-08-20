7f72c780d814850fdcd4e6866d6aae4c9807c84e
florian <florian@freepascal.org>
2017-08-20 17:20:42 +0000

    * cleanup
    git-svn-id: trunk@36960 -

--- compiler/options.pas
+++ compiler/options.pas
@@ -3423,11 +3423,8 @@ begin
 {$endif}
 {$endif}
 
-{ these cpus have an inline sar implementaion }
 { currently, all supported CPUs have an internal sar implementation }
-{ $if defined(x86) or defined(arm) or defined(powerpc) or defined(powerpc64) or defined(sparc) or defined(mips)}
   def_system_macro('FPC_HAS_INTERNAL_SAR');
-{ $endif}
 
 {$ifdef powerpc64}
   def_system_macro('FPC_HAS_LWSYNC');
