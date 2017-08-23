4f6fffa331bb1761fff75b56ee66aa9184e80b58
michael <michael@freepascal.org>
2017-08-23 07:18:29 +0000

    * Add libfontconfig
    git-svn-id: trunk@37040 -

---
+++ packages/libfontconfig/fpmake.pp
@@ -0,0 +1,31 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('libfontconfig');
+    P.ShortName:='libfc';
+{$ifdef ALLPACKAGES}
+    P.Directory:=ADirectory;
+{$endif ALLPACKAGES}
+    P.Version:='3.1.1';
+    P.SourcePath.Add('src');
+    P.IncludePath.Add('src');
+    P.OSes := [linux,freebsd, darwin]; // Darwin was tested!
+    T:=P.Targets.AddUnit('libfontconfig.pp');
+    P.ExamplePath.Add('examples');
+    P.Targets.AddExampleProgram('testfc.pp');
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
