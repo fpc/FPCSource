3d17111e2f0c496b2d1768a57d4132682e4d3a30
michael <michael@freepascal.org>
2017-09-28 12:32:17 +0000

    * Remove double inline; chokes fpdoc (2nd occurrence)
    git-svn-id: trunk@37349 -

--- rtl/objpas/character.pas
+++ rtl/objpas/character.pas
@@ -114,7 +114,7 @@ type
     class function IsLetterOrDigit(AChar : UnicodeChar) : Boolean; overload; static; inline;
     class function IsLetterOrDigit(const AString : UnicodeString; AIndex : Integer) : Boolean; overload; static; inline;
     
-    class function IsLower(AChar : UnicodeChar) : Boolean; overload; static; inline; inline;
+    class function IsLower(AChar : UnicodeChar) : Boolean; overload; static; inline;
     class function IsLower(const AString : UnicodeString; AIndex : Integer) : Boolean; overload; static; inline;
 
     class function IsNumber(AChar : UnicodeChar) : Boolean; overload; static; inline;
