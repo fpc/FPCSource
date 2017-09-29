cb142cdf7b4dc45d672b88b9a84a5987407f245d
svenbarth <pascaldragon@googlemail.com>
2017-09-29 17:07:33 +0000

    * use our license header instead of the original MIT one
    git-svn-id: trunk@37357 -

--- packages/libffi/src/ffi.pp
+++ packages/libffi/src/ffi.pp
@@ -1,28 +1,17 @@
-(* -----------------------------------------------------------------*-C-*-
-   libffi 3.2.1 - Copyright (c) 2011, 2014 Anthony Green
-                    - Copyright (c) 1996-2003, 2007, 2008 Red Hat, Inc.
+{
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 2017 by Sven Barth
+    member of the Free Pascal development team.
 
-   Permission is hereby granted, free of charge, to any person
-   obtaining a copy of this software and associated documentation
-   files (the ``Software''), to deal in the Software without
-   restriction, including without limitation the rights to use, copy,
-   modify, merge, publish, distribute, sublicense, and/or sell copies
-   of the Software, and to permit persons to whom the Software is
-   furnished to do so, subject to the following conditions:
+    Interface unit for the Foreign Function Interface library.
 
-   The above copyright notice and this permission notice shall be
-   included in all copies or substantial portions of the Software.
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
 
-   THE SOFTWARE IS PROVIDED ``AS IS'', WITHOUT WARRANTY OF ANY KIND,
-   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-   NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-   DEALINGS IN THE SOFTWARE.
-
-   ----------------------------------------------------------------------- *)
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+}
 
 (* -------------------------------------------------------------------
    The basic API is described in the README file.
