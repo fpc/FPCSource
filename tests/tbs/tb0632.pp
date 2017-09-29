81a0f88d8cfe3caba3b4ccbf415d7be85b6ab727
svenbarth <pascaldragon@googlemail.com>
2017-09-29 21:19:45 +0000

    + add Delphi compatible TThread.ForceQueue() which enqueues the method also for the main thread (at least if the RTL is in multi threading mode, otherwise it's still executed right away - whether this is Delphi compatible needs to be tested as I only have a 10.1 currently which does not yet support TThread.ForceQueue()) + added test
    git-svn-id: trunk@37359 -

---
+++ tests/tbs/tb0632.pp
@@ -0,0 +1,77 @@
+{ Note: needs multi threading }
+program tb0632;
+
+{$mode objfpc}
+
+uses
+{$ifdef unix}
+  cthreads,
+{$endif}
+  classes;
+
+type
+  TTest = class
+  private
+    fValue: LongInt;
+    procedure DoTest;
+  public
+    procedure Test;
+  end;
+
+  TDummyThread = class(TThread)
+  public
+    constructor Create;
+  protected
+    procedure Execute; override;
+  end;
+
+{ TDummyThread }
+
+constructor TDummyThread.Create;
+begin
+  inherited Create(True);
+  FreeOnTerminate := True;
+end;
+
+procedure TDummyThread.Execute;
+begin
+  { empty }
+end;
+
+{ TTest }
+
+procedure TTest.DoTest;
+begin
+  Inc(fValue);
+end;
+
+procedure TTest.Test;
+begin
+  TThread.Queue(Nil, @DoTest);
+  if fValue <> 1 then
+    Halt(1);
+  TThread.ForceQueue(Nil, @DoTest);
+  if fValue <> 1 then
+    Halt(2);
+  CheckSynchronize;
+  if fValue <> 2 then
+    Halt(3);
+  Writeln('Ok');
+end;
+
+var
+  t: TTest;
+begin
+{$ifdef FPC_HAS_FEATURE_THREADING}
+  { ensure that the RTL is in multi threading mode, otherwise CheckSynchronize
+    ignores the queue }
+  TDummyThread.Create.Start;
+
+  t := TTest.Create;
+  try
+    t.Test;
+  finally
+    t.Free;
+  end;
+{$endif}
+end.
