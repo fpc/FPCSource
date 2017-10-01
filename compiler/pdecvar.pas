c0feaf1f1e427897bcfde102c9031a49f5330fb0
florian <florian@freepascal.org>
2017-10-01 19:54:44 +0000

    + allow absolute to absolute symbols, resolves issue #32474
    git-svn-id: trunk@37379 -

--- compiler/pdecvar.pas
+++ compiler/pdecvar.pas
@@ -1240,7 +1240,7 @@ implementation
               if (hp.nodetype=loadn) then
                 begin
                   { we should check the result type of loadn }
-                  if not (tloadnode(hp).symtableentry.typ in [fieldvarsym,staticvarsym,localvarsym,paravarsym]) then
+                  if not (tloadnode(hp).symtableentry.typ in [fieldvarsym,staticvarsym,localvarsym,paravarsym,absolutevarsym]) then
                     Message(parser_e_absolute_only_to_var_or_const);
                   abssym:=cabsolutevarsym.create(vs.realname,vs.vardef);
                   abssym.fileinfo:=vs.fileinfo;
---
+++ tests/webtbs/tw32474.pp
@@ -0,0 +1,7 @@
+{ %norun }
+var
+  b1: byte absolute $50;
+  b2: byte absolute b1;
+begin
+  b1 := 3;
+end.
