4b113579a59ec3cfff076357fa1378c86b0771c6
Károly Balogh <karoly@freepascal.org>
2017-11-17 11:31:49 +0000

    some tabs to spaces indentation cleanup. no functional change
    git-svn-id: trunk@37599 -

--- compiler/link.pas
+++ compiler/link.pas
@@ -180,13 +180,13 @@ Implementation
       begin
         result:=0;
         bufsize:=64*1024;
-	      fs:=CFileStreamClass.Create(fn,fmOpenRead or fmShareDenyNone);
-	      if CStreamError<>0 then
-	        begin
-	          fs.Free;
-	          Comment(V_Error,'Can''t open file: '+fn);
-	          exit;
-	        end;
+        fs:=CFileStreamClass.Create(fn,fmOpenRead or fmShareDenyNone);
+        if CStreamError<>0 then
+          begin
+            fs.Free;
+            Comment(V_Error,'Can''t open file: '+fn);
+            exit;
+          end;
         getmem(buf,bufsize);
         repeat
           bufcount:=fs.Read(buf^,bufsize);
