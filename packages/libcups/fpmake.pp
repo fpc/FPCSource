4b8dcd676c92830d7495eb580ab900bfca74d87b
florian <florian@freepascal.org>
2017-11-26 09:33:34 +0000

    * libcups interface unit included in FPC, resolves #32729
    git-svn-id: trunk@37619 -

---
+++ packages/libcups/fpmake.pp
@@ -0,0 +1,44 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('libcups');
+{$ifdef ALLPACKAGES}
+    P.Directory:=ADirectory;
+{$endif ALLPACKAGES}
+    P.Version:='3.1.1';
+
+    P.Author := 'Header: Olivier GUILBAUD';
+    P.License := 'Header: LGPL with modi ';
+    P.HomepageURL := 'www.freepascal.org';
+    P.OSes := [beos,haiku,freebsd,darwin,iphonesim,solaris,netbsd,openbsd,linux,aix,dragonfly];
+    P.Email := '';
+    P.Description := 'This package provides an interface with Common UNIX Printing System.';
+    P.NeedLibC:= false;  // true for headers that indirectly link to libc?
+    
+    P.Dependencies.Add('rtl-extra');
+    
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('cupsdyn.pp');
+
+    P.ExamplePath.Add('examples');
+//    P.Targets.AddExampleProgram('testcups.pp');
+//    P.Sources.AddExampleFiles('examples/*',P.Directory,false,'.');
+
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
