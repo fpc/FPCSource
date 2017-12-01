2a852a490a3d7ab2ff0f7a7cb108243ca1b4b4a5
nickysn <nickysn@gmail.com>
2017-12-01 19:46:46 +0000

    + enabled compilation of unit 'objects' on win16
    git-svn-id: trunk@37646 -

--- packages/rtl-extra/fpmake.pp
+++ packages/rtl-extra/fpmake.pp
@@ -23,7 +23,7 @@ Const
   SerialOSes    = [android,linux,netbsd,openbsd,win32,win64];
   UComplexOSes  = [atari,emx,gba,go32v2,msdos,nativent,nds,netware,netwlibc,os2,symbian,watcom,wii,wince,win32,win64]+UnixLikes+AllAmigaLikeOSes;
   MatrixOSes    = [atari,emx,gba,go32v2,msdos,nativent,nds,netware,netwlibc,os2,symbian,watcom,wii,win32,win64,wince]+UnixLikes+AllAmigaLikeOSes;
-  ObjectsOSes   = [atari,emx,gba,go32v2,macos,msdos,nds,netware,netwlibc,os2,symbian,watcom,wii,win32,win64,wince]+UnixLikes+AllAmigaLikeOSes;
+  ObjectsOSes   = [atari,emx,gba,go32v2,macos,msdos,nds,netware,netwlibc,os2,symbian,watcom,wii,win16,win32,win64,wince]+UnixLikes+AllAmigaLikeOSes;
   WinsockOSes   = [win32,win64,wince,os2,emx,netware,netwlibc];
   WinSock2OSes  = [win32,win64,wince];
   SocketsOSes   = UnixLikes+AllAmigaLikeOSes+[netware,netwlibc,os2,emx,wince,win32,win64];
@@ -83,7 +83,7 @@ begin
 
     // Add clocale for Android first in order to compile the source file
     // from the 'android' dir, not the 'unix' dir.
-    T:=P.Targets.AddUnit('real48utils.pp',AllTargetsextra-[msdos]);  { msdos excluded temporarily, until bitpacked records containing longints on 16-bit targets are fixed }
+    T:=P.Targets.AddUnit('real48utils.pp',AllTargetsextra-[msdos,win16]);  { msdos,win16 excluded temporarily, until bitpacked records containing longints on 16-bit targets are fixed }
     T:=P.Targets.AddUnit('clocale.pp',[android]);
 
     T:=P.Targets.AddUnit('ucomplex.pp',UComplexOSes);
