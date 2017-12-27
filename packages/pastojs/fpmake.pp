4a566ceec1440eb7ee6f4be3d540da350001430a
michael <michael@freepascal.org>
2017-12-27 17:12:19 +0000

    * Move compiler files to package. * Separate lib compiler from library file.
    git-svn-id: trunk@37835 -

--- packages/pastojs/fpmake.pp
+++ packages/pastojs/fpmake.pp
@@ -21,19 +21,30 @@ begin
     P.Version:='3.1.1';
     P.OSes := AllOses-[embedded,msdos,win16,macos,palmos];
     P.Dependencies.Add('fcl-js');
+    P.Dependencies.Add('fcl-json');
     P.Dependencies.Add('fcl-passrc');
+    Defaults.Options.Add('-Sc');
 
     P.Author := 'Free Pascal development team';
     P.License := 'LGPL with modification, ';
     P.HomepageURL := 'www.freepascal.org';
 
     P.SourcePath.Add('src');
+    P.IncludePath.Add('src');
     P.SupportBuildModes := [bmOneByOne];
 
     P.Options.Add('-S2h');
 
     T:=P.Targets.AddUnit('fppas2js.pp');
     T:=P.Targets.AddUnit('fppjssrcmap.pp');
+    T:=P.Targets.AddUnit('pas2jsfilecache.pp');
+    T:=P.Targets.AddUnit('pas2jsfileutils.pp');
+    T.Dependencies.AddInclude('pas2jsfileutilsunix.inc',AllUnixOSes);
+    T.Dependencies.AddInclude('pas2jsfileutilswin.inc',AllWindowsOSes);
+    T:=P.Targets.AddUnit('pas2jslogger.pp');
+    T:=P.Targets.AddUnit('pas2jspparser.pp');
+    T:=P.Targets.AddUnit('pas2jscompiler.pp');
+    T:=P.Targets.AddUnit('pas2jslibcompiler.pp');
 {$ifndef ALLPACKAGES}
     Run;
     end;
--- utils/pas2js/fpmake.pp
+++ utils/pas2js/fpmake.pp
@@ -21,33 +21,16 @@ begin
     P.HomepageURL := 'www.freepascal.org';
     P.Description := 'Convert pascal sources to javascript.';
     P.Email := 'michael@freepascal.org';
-    Defaults.Options.Add('-Sc');
     P.NeedLibC:= false;
 
     P.Directory:=ADirectory;
     P.Version:='3.1.1';
+    P.Dependencies.Add('fcl-json');
     P.Dependencies.Add('fcl-js');
     P.Dependencies.Add('fcl-passrc');
     P.Dependencies.Add('pastojs');
-    T:=P.Targets.AddUnit('pas2jscompiler.pp');
-    T:=P.Targets.AddUnit('pas2jsfilecache.pp');
-    T:=P.Targets.AddUnit('pas2jsfileutils.pp');
-    T.Dependencies.AddInclude('pas2jsfileutilsunix.inc',AllUnixOSes);
-    T.Dependencies.AddInclude('pas2jsfileutilswin.inc',AllWindowsOSes);
-    T:=P.Targets.AddUnit('pas2jslogger.pp');
-    T:=P.Targets.AddUnit('pas2jspparser.pp');
     PT:=P.Targets.AddProgram('pas2js.pp');
-    PT.Dependencies.AddUnit('pas2jscompiler');
-    PT.Dependencies.AddUnit('pas2jsfileutils');
-    PT.Dependencies.AddUnit('pas2jsfilecache');
-    PT.Dependencies.AddUnit('pas2jslogger');
-    PT.Dependencies.AddUnit('pas2jspparser');
     PT:=P.Targets.AddLibrary('pas2jslib.pp');
-    PT.Dependencies.AddUnit('pas2jscompiler');
-    PT.Dependencies.AddUnit('pas2jsfileutils');
-    PT.Dependencies.AddUnit('pas2jsfilecache');
-    PT.Dependencies.AddUnit('pas2jslogger');
-    PT.Dependencies.AddUnit('pas2jspparser');
     end;
 end;
 
