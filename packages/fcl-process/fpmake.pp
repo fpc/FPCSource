bde64031d505f89080c727d033a3c7828f453a7a
Károly Balogh <karoly@freepascal.org>
2017-12-30 06:47:18 +0000

    packages: disable some packages and files, in preparation of turning off threading feature for atari tos
    git-svn-id: trunk@37876 -

--- packages/fcl-process/fpmake.pp
+++ packages/fcl-process/fpmake.pp
@@ -25,7 +25,7 @@ begin
     P.Description := 'Process (execution) related parts of Free Component Libraries (FCL), FPC''s OOP library.';
     P.Options.Add('-S2h');
     P.NeedLibC:= false;
-    P.OSes:=AllOSes-[embedded,msdos,win16,go32v2,nativent,macos,palmos];
+    P.OSes:=AllOSes-[embedded,msdos,win16,go32v2,nativent,macos,palmos,atari];
 
     P.SourcePath.Add('src');
     P.IncludePath.Add('src/unix',AllUnixOSes);
