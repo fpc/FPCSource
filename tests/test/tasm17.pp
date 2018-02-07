0c1062bb453bb1d84564235b78af43ccde026645
nickysn <nickysn@gmail.com>
2018-02-07 16:33:03 +0000

    + extended test tasm17 with 'DW/DD xx+constant' and 'DW/DD OFFSET xx+constant'
    git-svn-id: trunk@38155 -

--- tests/test/tasm17.pp
+++ tests/test/tasm17.pp
@@ -15,12 +15,24 @@ const
   expect1: array [0..6] of word =
     (Ofs(xx),Ofs(xx),Ofs(xx),Seg(xx),Ofs(xx),0,$aa55);
 
+var
+  expect2: array [0..6] of word;
+
 procedure test1; assembler;
 asm
   dw xx
   dw offset xx
   dd xx
   dd offset xx
+  db 55h, 0aah
+end;
+
+procedure test2; assembler;
+asm
+  dw xx+5
+  dw offset xx+6
+  dd xx+7
+  dd offset xx+8
   db 55h, 0aah
 end;
 
@@ -46,6 +58,16 @@ end;
 
 begin
   if not CompareCode(CodePointer(@test1), @expect1, SizeOf(expect1)) then
+    Error;
+
+  expect2[0] := Ofs(xx)+5;
+  expect2[1] := Ofs(xx)+6;
+  expect2[2] := Ofs(xx)+7;
+  expect2[3] := Seg(xx);
+  expect2[4] := Ofs(xx)+8;
+  expect2[5] := 0;
+  expect2[6] := $aa55;
+  if not CompareCode(CodePointer(@test2), @expect2, SizeOf(expect2)) then
     Error;
   Writeln('Ok!');
 end.
