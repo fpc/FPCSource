8c99f3f70d90ec298a0e2a6667670fab756ce04a
nickysn <nickysn@gmail.com>
2018-02-20 16:06:29 +0000

    + added tests tasm21a.pp and tasm21b.pp - same as tasm21.pp, but for i386 and x86_64
    git-svn-id: trunk@38294 -

---
+++ tests/test/tasm21b.pp
@@ -0,0 +1,57 @@
+{ %CPU=x86_64 }
+
+{$MODE TP}
+{$ASMMODE INTEL}
+
+program tasm21b;
+
+var
+  test2a_ofs, test2b_ofs: qword;
+
+procedure Error;
+begin
+  Writeln('Error!');
+  Halt(1);
+end;
+
+function test1a: qword; assembler;
+asm
+  mov rax, offset test1a;
+end;
+
+function test1b: qword;
+begin
+  asm
+    mov rax, offset test1b;
+    mov @Result, rax
+  end;
+end;
+
+procedure test2a; assembler;
+asm
+  mov rax, offset test2a;
+  mov test2a_ofs, rax
+end;
+
+procedure test2b;
+begin
+  asm
+    mov rax, offset test2b;
+    mov test2b_ofs, rax
+  end;
+end;
+
+begin
+  if test1a <> Ofs(test1a) then
+    Error;
+  if test1b <> Ofs(test1b) then
+    Error;
+  test2a;
+  if test2a_ofs <> Ofs(test2a) then
+    Error;
+  test2b;
+  if test2b_ofs <> Ofs(test2b) then
+    Error;
+
+  Writeln('Ok!');
+end.
