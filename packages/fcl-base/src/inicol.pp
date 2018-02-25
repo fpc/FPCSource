451203512be1c256da283a1fcd2632fb9d7e93b7
marco <marco@freepascal.org>
2018-02-25 15:53:37 +0000

    * add copyright headers, mantis #22912
    git-svn-id: trunk@38347 -

--- packages/fcl-base/src/inicol.pp
+++ packages/fcl-base/src/inicol.pp
@@ -1,3 +1,16 @@
+{
+    This file is part of the Free Component Library (FCL)
+    Copyright (c) 1999-2000 by Michael Van Canneyt and Florian Klaempfl
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
 {$mode objfpc}
 {$h+}
 unit inicol;
