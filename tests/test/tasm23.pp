c4f66a2492e26b6a66ce0b6dba09def271402271
nickysn <nickysn@gmail.com>
2018-02-26 17:38:42 +0000

    * fixed comment in test
    git-svn-id: trunk@38356 -

--- tests/test/tasm23.pp
+++ tests/test/tasm23.pp
@@ -4,7 +4,7 @@ program tasm23;
 
 procedure t; assembler;
 asm
-  mov ax, [bx[5]][di][54][-17][45][4] { mov ax, ss:[bx+di+5Bh] }
+  mov ax, [bx[5]][di][54][-17][45][4] { mov ax, [bx+di+5Bh] }
 end;
 
 begin
