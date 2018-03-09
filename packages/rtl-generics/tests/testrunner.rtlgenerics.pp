b0b119995b8f1caa25e72f80a4acd3d86048992a
maciej-izak <mizak.public@gmail.com>
2018-03-09 00:01:18 +0000

    Big update for rtl-generics (aka Generics.Collections 2.0 - sync with https://github.com/maciej-izak/generics.collections). What is new: * New collections:     - TSortedList<T>     - THashSet<T>     - TAVLTreeMap<TKey, TValue>     - TIndexedAVLTreeMap<TKey, TValue>     - TAVLTree<T>     - TIndexedAVLTree<T>     - TSortedSet<T>     - TSortedHashSet<T> (this one collection is especially interesting - optimized mix of dictionary and AVL tree) * Ptr property for all collections * New hash functions (the optimal hash function for collections is selected depending on environment) * Bug fixes * Tests
    git-svn-id: trunk@38462 -

---
+++ packages/rtl-generics/tests/testrunner.rtlgenerics.pp
@@ -0,0 +1,30 @@
+{ %CONFIGFILE=fpcunit-console-defaults.ini testdefaults.ini }
+
+program testrunner.rtlgenerics;
+
+{$mode objfpc}{$H+}
+
+uses
+  consoletestrunner,
+  tests.generics.bugs,
+  tests.generics.hashmaps,
+  tests.generics.arrayhelper,
+  tests.generics.trees,
+  tests.generics.stdcollections,
+  tests.generics.sets
+  ;
+
+var
+  Application: TTestRunner;
+
+begin
+  Application := TTestRunner.Create(nil);
+  Application.Initialize;
+  Application.Title := 'RTL-Generics unit tests';
+  Application.Run;
+  Application.Free;
+{$IFDEF WAIT_FOR_ENTER}
+  WriteLn('Press enter...');
+  ReadLn;
+{$ENDIF}
+end.
