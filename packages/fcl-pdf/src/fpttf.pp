b1626d7c0bffef87421e4bb59b246812ccd16ade
michael <michael@freepascal.org>
2018-03-28 07:37:03 +0000

    * Ensure compilation with older FPC versions (Work by Christian Ulrich)
    git-svn-id: trunk@38640 -

--- packages/fcl-pdf/src/fpttf.pp
+++ packages/fcl-pdf/src/fpttf.pp
@@ -525,13 +525,23 @@ procedure TFPFontCacheList.ReadStandardFonts;
   {$ifdef mswindows}
   function GetWinFontsDir: string;
   var
+    {$if FPC_FULLVERSION < 30400}
+    w :  Array[0..MaxPathLen] of Char;
+    {$ELSE}
     w : pwidechar;
+    {$ENDIF}
   begin
+    {$if FPC_FULLVERSION < 30400}
+    SHGetSpecialFolderPath(0,w,CSIDL_FONTS,false);
+    {$else}
     SHGetKnownFolderPath(FOLDERID_Fonts,0,0,w);
+    {$endif}
     Result := w;
+    {$if FPC_FULLVERSION > 30400}
     CoTaskMemFree(w);
+    {$endif}
   end;
-  {$endif}
+{$endif}
 
 {$ifdef HasFontsConf}
 var
@@ -693,4 +703,5 @@ finalization
   uFontCacheList.Free;
 
 end.
+
 
