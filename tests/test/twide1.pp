5ae4724a5b4c52d934c4e4dbe0aa9036df578508
pierre <pierre@freepascal.org>
2018-04-12 20:51:13 +0000

    Use USE_INTERNAL_UNICODE to be able to test fpwidestrings on different targets
    git-svn-id: trunk@38758 -

--- tests/test/twide1.pp
+++ tests/test/twide1.pp
@@ -1,18 +1,26 @@
 {$ifdef go32v2}
+  {$define USE_INTERNAL_UNICODE}
+{$endif}
+
+{$ifdef USE_INTERNAL_UNICODE}
   {$define USE_FPWIDESTRING_UNIT}
   {$define USE_UNICODEDUCET_UNIT}
+  {$define USE_CPALL_UNIT}
 {$endif}
 uses
- {$ifdef unix}
- {$ifdef darwin}iosxwstr{$else}cwstring{$endif};
- {$endif}
+ {$ifndef USE_INTERNAL_UNICODE}
+  {$ifdef darwin}iosxwstr{$else}cwstring{$endif},
+ {$endif ndef USE_INTERNAL_UNICODE}
  {$ifdef USE_FPWIDESTRING_UNIT}
   fpwidestring,
  {$endif}
  {$ifdef USE_UNICODEDUCET_UNIT}
-  unicodeducet;
+  unicodeducet,
  {$endif}
-
+ {$ifdef USE_CPALL_UNIT}
+  cpall,
+ {$endif}
+  strings;
   
 var
   w : widestring;
--- tests/test/twrstr9.pp
+++ tests/test/twrstr9.pp
@@ -1,20 +1,26 @@
 {$codepage utf8}
 
 {$ifdef go32v2}
-  {$define USE_FPWIDESTRING_UNIT}
-  {$define USE_UNICODEDUCET_UNIT}
+  {$define USE_INTERNAL_UNICODE}
 {$endif}
 
-{$ifdef unix}
+{$ifdef USE_INTERNAL_UNICODE}
+  {$define USE_FPWIDESTRING_UNIT}
+  {$define USE_UNICODEDUCET_UNIT}
+  {$define USE_CPALL_UNIT}
+{$endif}
 uses
-  {$ifdef darwin}iosxwstr{$else}cwstring{$endif};
-{$else}
-uses
+ {$ifndef USE_INTERNAL_UNICODE}
+  {$ifdef darwin}iosxwstr{$else}cwstring{$endif},
+ {$endif ndef USE_INTERNAL_UNICODE}
  {$ifdef USE_FPWIDESTRING_UNIT}
   fpwidestring,
  {$endif}
  {$ifdef USE_UNICODEDUCET_UNIT}
   unicodeducet,
+ {$endif}
+ {$ifdef USE_CPALL_UNIT}
+  cpall,
  {$endif}
  { The unit strings is not really used here,
    but simpifies the conditional construction
