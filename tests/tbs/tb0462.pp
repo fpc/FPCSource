52b830a434176b1e14aaeb0107904640ab874d30
pierre <pierre@freepascal.org>
2018-04-12 20:39:34 +0000

    Use USE_INTERNAL_UNICODE to be able to test fpwidestrings on different targets
    git-svn-id: trunk@38757 -

--- tests/tbs/tb0462.pp
+++ tests/tbs/tb0462.pp
@@ -1,26 +1,28 @@
 { %version=1.1 }
 
 {$ifdef go32v2}
-  {$define USE_FPWIDESTRING_UNIT}
-  {$define USE_UNICODEDUCET_UNIT}
+  {$define USE_INTERNAL_UNICODE}
 {$endif}
 
-{$ifdef fpc}
-{$ifdef unix}
-  {$ifdef darwin}
-uses
-  iosxwstr;
-  {$else}
-uses
-  cwstring;
-  {$endif}
+{$ifdef USE_INTERNAL_UNICODE}
+  {$define USE_FPWIDESTRING_UNIT}
+  {$define USE_UNICODEDUCET_UNIT}
+  {$define USE_CPALL_UNIT}
 {$endif}
-{$ifdef go32v2}
 uses
-    fpwidestring,
-    unicodeducet;
-{$endif}
-{$endif}
+ {$ifndef USE_INTERNAL_UNICODE}
+  {$ifdef darwin}iosxwstr{$else}cwstring{$endif},
+ {$endif ndef USE_INTERNAL_UNICODE}
+ {$ifdef USE_FPWIDESTRING_UNIT}
+  fpwidestring,
+ {$endif}
+ {$ifdef USE_UNICODEDUCET_UNIT}
+  unicodeducet,
+ {$endif}
+ {$ifdef USE_CPALL_UNIT}
+  cpall,
+ {$endif}
+  strings;
 
 type
   RR = record
--- tests/tbs/tb0488b.pp
+++ tests/tbs/tb0488b.pp
@@ -3,20 +3,28 @@
 }
 
 {$ifdef go32v2}
+  {$define USE_INTERNAL_UNICODE}
+{$endif}
+
+{$ifdef USE_INTERNAL_UNICODE}
   {$define USE_FPWIDESTRING_UNIT}
   {$define USE_UNICODEDUCET_UNIT}
+  {$define USE_CPALL_UNIT}
 {$endif}
 uses
-{$ifdef unix}
+ {$ifndef USE_INTERNAL_UNICODE}
   {$ifdef darwin}iosxwstr{$else}cwstring{$endif},
-{$endif}
-{$ifdef USE_FPWIDESTRING_UNIT}
+ {$endif ndef USE_INTERNAL_UNICODE}
+ {$ifdef USE_FPWIDESTRING_UNIT}
   fpwidestring,
-{$endif}
-{$ifdef USE_UNICODEDUCET_UNIT}
+ {$endif}
+ {$ifdef USE_UNICODEDUCET_UNIT}
   unicodeducet,
-{$endif}
-SysUtils,erroru;
+ {$endif}
+ {$ifdef USE_CPALL_UNIT}
+  cpall,
+ {$endif}
+  SysUtils,erroru;
 
 procedure Check(a,b:ansistring);
 begin
