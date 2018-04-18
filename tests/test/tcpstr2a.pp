dbc0dd81fd7182e26f4a0a459df85b4b4975a134
pierre <pierre@freepascal.org>
2018-04-18 10:21:26 +0000

    Modify the test to reflect the fact the compiler prefers conversion of ansiString to RawByteString over to UTF8String
    git-svn-id: trunk@38781 -

--- tests/test/tcpstr2a.pp
+++ tests/test/tcpstr2a.pp
@@ -1,13 +1,18 @@
-{ %fail }
 procedure p(const S : RawByteString); overload;
   begin
+    writeln(s,'AnsiString prefers RawByteString');
   end;
 
 procedure p(const S : UTF8String); overload;
   begin
+    writeln(s,'AnsiString prefers UTF8String');
+    { This is not expected, so generate error }
+    writeln('Unexpected, changed behavior!');
+    halt(1);
   end;
 var
   s1 : Ansistring;
 begin
+  s1:='Test: ';
   p(s1);
 end.
