65c88ce4e260351c2f10ab9ce1d7360f5f32c737
pierre <pierre@freepascal.org>
2018-08-09 11:43:16 +0000

    fcl-pdf does not need winunits-base package for wince OS target
    git-svn-id: trunk@39594 -

--- packages/fcl-pdf/fpmake.pp
+++ packages/fcl-pdf/fpmake.pp
@@ -30,7 +30,7 @@ begin
     P.Dependencies.Add('fcl-image');
     P.Dependencies.Add('fcl-xml');
     P.Dependencies.Add('paszlib');
-    P.Dependencies.add('winunits-base',AllWindowsOSes);
+    P.Dependencies.add('winunits-base',AllWindowsOSes-[wince]);
     P.Version:='3.1.1';
     T:=P.Targets.AddUnit('src/fpttfencodings.pp');
     T:=P.Targets.AddUnit('src/fpparsettf.pp');
