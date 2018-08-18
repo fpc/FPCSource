9b969c6ccaad0749f86e760b832fed542ace9e4b
marco <marco@freepascal.org>
2018-08-18 13:56:21 +0000

    * second part processunicode commit.
    git-svn-id: trunk@39627 -

---
+++ packages/fcl-process/src/process.pp
@@ -0,0 +1,49 @@
+{
+    This file is part of the Free Component Library (FCL)
+    Copyright (c) 1999-2000 by the Free Pascal development team
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+{$mode objfpc}
+{$h+}
+unit process;
+
+interface
+
+Uses Classes,
+     pipes,
+     SysUtils,
+     Math;
+
+Type
+  TProcessOption = (poRunSuspended,poWaitOnExit,
+                    poUsePipes,poStderrToOutPut,
+                    poNoConsole,poNewConsole,
+                    poDefaultErrorMode,poNewProcessGroup,
+                    poDebugProcess,poDebugOnlyThisProcess,
+                    poPassInput);
+
+  TShowWindowOptions = (swoNone,swoHIDE,swoMaximize,swoMinimize,swoRestore,swoShow,
+                        swoShowDefault,swoShowMaximized,swoShowMinimized,
+                        swoshowMinNOActive,swoShowNA,swoShowNoActivate,swoShowNormal);
+
+  TStartupOption = (suoUseShowWindow,suoUseSize,suoUsePosition,
+                    suoUseCountChars,suoUseFillAttribute);
+
+  TProcessPriority = (ppHigh,ppIdle,ppNormal,ppRealTime);
+
+  TProcessOptions = set of TProcessOption;
+  TStartupOptions = set of TStartupOption;
+
+{$macro on}
+{define processunicodestring}
+{$define TProcessnamemacro:=TProcess}
+
+{$i processbody.inc}
+end.
