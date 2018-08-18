4dbc2ae873e36a7adab5b0c776249234c588b3f8
marco <marco@freepascal.org>
2018-08-18 14:09:56 +0000

    * enable processunicode in fpmake and .fpcmake for win32 and win64.
    git-svn-id: trunk@39628 -

--- packages/fcl-process/fpmake.pp
+++ packages/fcl-process/fpmake.pp
@@ -46,8 +46,13 @@ begin
     T:=P.Targets.AddUnit('pipes.pp');
       T.Dependencies.AddInclude('pipes.inc');
     T:=P.Targets.AddUnit('process.pp');
+      T.Dependencies.AddInclude('processbody.inc');
       T.Dependencies.AddInclude('process.inc');
-    T.ResourceStrings:=True;
+      T.ResourceStrings:=True;
+    T:=P.Targets.AddUnit('processunicode.pp',[win32,win64]);
+      T.Dependencies.AddInclude('processbody.inc');
+      T.Dependencies.AddInclude('process.inc');
+      T.ResourceStrings:=True;
     T:=P.Targets.AddUnit('simpleipc.pp');
       T.Dependencies.AddInclude('simpleipc.inc');
       T.ResourceStrings:=True;
