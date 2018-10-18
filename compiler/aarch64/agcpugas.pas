4643a0b1ff63db2d460d9a7a17d0950653a205e6
yury <jura@cp-lab.com>
2018-10-18 11:48:27 +0000

    * Register external gas assembler for aarch64-android and x86_64-android.
    git-svn-id: trunk@39969 -

--- compiler/aarch64/agcpugas.pas
+++ compiler/aarch64/agcpugas.pas
@@ -274,7 +274,7 @@ unit agcpugas;
             idtxt  : 'AS';
             asmbin : 'as';
             asmcmd : '-o $OBJ $EXTRAOPT $ASM';
-            supported_targets : [system_aarch64_linux];
+            supported_targets : [system_aarch64_linux,system_aarch64_android];
             flags : [af_needar,af_smartlink_sections];
             labelprefix : '.L';
             comment : '// ';
