703f5ea97e91654cd1386cf90244fb92a0cf13ba
Mattias Gaertner <nc-gaertnma@netcologne.de>
2018-10-25 16:09:20 +0000

    pas2js: started nodepas2js
    git-svn-id: trunk@40037 -

---
+++ utils/pas2js/nodepas2js.pp
@@ -0,0 +1,15 @@
+program nodepas2js;
+
+{$mode objfpc}
+
+uses
+  JS, NodeJS, NodeJSFS, Math,
+  Classes, SysUtils,
+  jstree, jswriter,
+  fpjson,
+  PasTree, PScanner, PParser, PasResolveEval, PasResolver, PasUseAnalyzer,
+  FPPas2Js;
+
+begin
+  // Your code here
+end.
