fa58ed1df75915721facdafdc11c368704af5eb5
marco <marco@freepascal.org>
2018-12-11 20:17:15 +0000

    --- Merging r39691 into '.': U    packages/chm/src/chmcmd.lpi --- Recording mergeinfo for merge of r39691 into '.':  U   . --- Merging r39758 into '.': U    packages/fcl-base/src/fpexprpars.pp --- Recording mergeinfo for merge of r39758 into '.':  G   . --- Merging r39831 into '.': U    packages/fcl-net/src/ssockets.pp --- Recording mergeinfo for merge of r39831 into '.':  G   . --- Merging r39840 into '.': U    rtl/inc/fexpand.inc U    tests/test/units/dos/tfexpand.pp --- Recording mergeinfo for merge of r39840 into '.':  G   . --- Merging r39843 into '.': U    rtl/objpas/sysutils/syshelp.inc --- Recording mergeinfo for merge of r39843 into '.':  G   .
    # revisions: 39691,39758,39831,39840,39843
    
    git-svn-id: branches/fixes_3_2@40527 -

--- packages/fcl-net/src/ssockets.pp
+++ packages/fcl-net/src/ssockets.pp
@@ -1026,7 +1026,7 @@ begin
     result := FpFcntl(ASocket, F_SetFl, flags and (not O_NONBLOCK)) = 0;
 {$endif}
 {$ifdef windows}
-  result := ioctlsocket(ASocket,FIONBIO,@ABlockMode) = 0;
+  result := ioctlsocket(ASocket,longint(FIONBIO),@ABlockMode) = 0;
 {$endif}
 end;
 
