aca3933f6df82d9393ad67c37ab8879feab8c61d
nickysn <nickysn@gmail.com>
2019-01-24 16:14:01 +0000

    * fixed register in comment
    git-svn-id: trunk@41054 -

--- packages/graph/src/msdos/graph.pp
+++ packages/graph/src/msdos/graph.pp
@@ -2642,7 +2642,7 @@ const CrtAddress: word = 0;
     (* End multiply by 80  *)
     add ax, [StartXViewPort]
     mov cx, ax
-    {DI = Y * LINESIZE, BX = X, coordinates admissible}
+    {DI = Y * LINESIZE, CX = X, coordinates admissible}
     shr ax, 1
     shr ax, 1
     add di, ax                ; {DI = Y * LINESIZE + (X SHR 2) }
