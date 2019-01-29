6c888949992e530b72e641603d5200b2d3f57b9b
florian <florian@freepascal.org>
2019-01-29 20:16:12 +0000

    * do not call the internal assembler if -s is passed
    git-svn-id: trunk@41126 -

--- compiler/options.pas
+++ compiler/options.pas
@@ -4004,8 +4004,10 @@ begin
       Message(option_w_unsupported_debug_format);
 
   { switch assembler if it's binary and we got -a on the cmdline }
-  if (cs_asm_leave in init_settings.globalswitches) and
-     (af_outputbinary in target_asm.flags) then
+  if ((cs_asm_leave in init_settings.globalswitches) and
+     (af_outputbinary in target_asm.flags)) or
+     { if -s is passed, we shouldn't call the internal assembler }
+     (cs_asm_extern in init_settings.globalswitches) then
    begin
      Message(option_switch_bin_to_src_assembler);
      set_target_asm(target_info.assemextern);
