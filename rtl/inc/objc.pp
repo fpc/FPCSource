3b87b10904a84da0c8bd786401e17e7fb43bbdfd
Jonas Maebe <jonas@freepascal.org>
2019-02-06 19:48:47 +0000

    * explicitly link libobjc to prevent the symbol __objc_empty_cache from     resolving to CoreFoundation on macOS 10.14 (which will break running     such programs on older macOS/(Mac) OS X versions)
    git-svn-id: trunk@41243 -

--- rtl/inc/objc.pp
+++ rtl/inc/objc.pp
@@ -3,6 +3,9 @@ unit objc;
 
 {$ifdef darwin}
 {$define targethandled}
+
+{$linklib objc}
+
 {$if defined(iphonesim) or defined(cpuarm) or defined(cpux86_64) or defined(cpupowerpc64) or defined(cpuaarch64)}
 {$i objcnf.inc}
 {$endif}
