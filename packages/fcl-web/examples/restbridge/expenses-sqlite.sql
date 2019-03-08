a5f257ad2013a49031a78e3023f8966c8f0d9057
michael <michael@freepascal.org>
2019-03-08 10:38:24 +0000

    * Firebird sample DB script, same for sqlite
    git-svn-id: trunk@41634 -

--- packages/fcl-web/examples/restbridge/expenses-sqlite.sql
+++ packages/fcl-web/examples/restbridge/expenses-sqlite.sql
@@ -1,6 +1,12 @@
-create sequence seqExpenseTypesID;
+create table t2(id integer primary key autoincrement);
+insert into  sqlite_sequence (name,seq) values ('seqExpenseTypesID',1);
+insert into  sqlite_sequence (name,seq) values ('seqExpenseID',1);
+insert into  sqlite_sequence (name,seq) values ('seqUsersID',1);
+insert into  sqlite_sequence (name,seq) values ('seqProjectsID',1);
+drop table t2;
+ 
 create table ExpenseTypes (
-  etID bigint not null default nextval('seqExpenseTypesID'),
+  etID bigint primary key,
   etName varchar(50) not null,
   etDescription varchar(100) not null,
   etMaxAmount decimal(10,2),
@@ -8,36 +14,28 @@ create table ExpenseTypes (
   etActive boolean not null default true
 );
 
-create sequence seqUsersID;
 create table Users (
-  uID bigint not null default nextval('seqUsersID'),
+  uID bigint primary key,
   uLogin varchar(50) not null,
   uFullName varchar(100) not null,
   uPassword varchar(100) not null,
   uActive boolean not null default true 
 );
 
-create sequence seqProjectsID;
 create table Projects (
-  pID bigint not null default nextval('seqProjectsID'),
+  pID bigint primary key,
   pName varchar(50) not null,
   pDescription varchar(100) not null,
   pActive boolean not null default true
 );
 
-create sequence seqExpenseID;
-drop table Expenses;
 create table Expenses (
-  eID bigint not null default nextval('seqExpenseID'),
+  eID bigint primary key,
   eUserFK bigint not null,
   eProjectFK bigint not null,
   eTypeFK bigint not null,
   eAmount decimal(10,2) not null,
-  eDate date not null default 'today',
+  eDate date not null default CURRENT_DATE,
   eComment varchar(1024)
 );
 
-alter table ExpenseTypes add constraint pkExpenseTypes primary key (etID);
-alter table Users add constraint pkUsers primary key (uID);
-alter table Projects add constraint pkProjects primary key (pID);
-alter table Expenses add  constraint pkExpenses primary key (eID);
