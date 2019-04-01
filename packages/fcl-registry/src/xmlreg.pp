b521f6a17774d5906c8ed5076d286806f5b7f8ab
michael <michael@freepascal.org>
2019-04-01 16:46:48 +0000

    * Patch from Bart Broersma to implement Int64 support in xmlreg (bug ID 35227)
    git-svn-id: trunk@41810 -
    (cherry picked from commit 17aa8da99f5fe4049cb9050522830c9798630f36)

--- packages/fcl-registry/src/xmlreg.pp
+++ packages/fcl-registry/src/xmlreg.pp
@@ -27,7 +27,7 @@ uses
 
 Type
 
-  TDataType = (dtUnknown,dtDWORD,dtString,dtBinary,dtStrings);
+  TDataType = (dtUnknown,dtDWORD,dtString,dtBinary,dtStrings,dtQWord);
   TDataInfo = record
     DataType : TDataType;
     DataSize : Integer;
@@ -324,6 +324,7 @@ Var
   U : UnicodeString;
   HasData: Boolean;
   D : DWord;
+  Q : QWord;
   
 begin
   //writeln('TXmlRegistry.DoGetValueData: Name=',Name,' IsUnicode=',IsUnicode);
@@ -349,6 +350,12 @@ begin
                   Result:=HasData and TryStrToDWord(String(DataNode.NodeValue),D) and (DataSize>=NS);
                   if Result then
                     PCardinal(@Data)^:=D;
+                  end;
+        dtQWORD : begin   // DataNode is required
+                  NS:=SizeOf(QWORD);
+                  Result:=HasData and TryStrToQWord(String(DataNode.NodeValue),Q) and (DataSize>=NS);
+                  if Result then
+                    PUInt64(@Data)^:=Q;
                   end;
         dtString : // DataNode is optional
                    if HasData then
@@ -414,6 +421,7 @@ begin
 
     Case DataType of
       dtDWORD : SW:=UnicodeString(IntToStr(PCardinal(@Data)^));
+      dtQWORD : SW:=UnicodeString(IntToStr(PUInt64(@Data)^));
       dtString : begin
                  if IsUnicode then
                    SW:=UnicodeString(PUnicodeChar(@Data))
