0225a3a39ada0e83847ece4f3c8c6a772d6f0d3d
Jonas Maebe <jonas@freepascal.org>
2019-04-20 18:18:03 +0000

    * ppudump: fixed reading of ibresource section
    git-svn-id: trunk@41905 -

--- compiler/utils/ppuutils/ppudump.pp
+++ compiler/utils/ppuutils/ppudump.pp
@@ -775,7 +775,7 @@ end;
 
 Procedure ReadContainer(const prefix:string);
 {
-  Read a serie of strings and write to the screen starting every line
+  Read a series of strings and write to the screen starting every line
   with prefix
 }
 begin
@@ -3838,7 +3838,7 @@ begin
 
          ibresources :
            if not silent then
-             ReadLinkContainer('Resource file: ');
+             ReadContainer('Resource file: ');
 
          iberror :
            begin
