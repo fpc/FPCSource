ea28cd5268583ccfdcb18d8bba65236214502269
Mattias Gaertner <nc-gaertnma@netcologne.de>
2019-06-26 19:34:59 +0000

    pastojs: clean up
    git-svn-id: trunk@42293 -

--- packages/pastojs/src/fppas2js.pp
+++ packages/pastojs/src/fppas2js.pp
@@ -11344,7 +11344,6 @@ begin
     RaiseInconsistency(20170210120659,El);
   Param:=El.Params[0];
   AContext.Resolver.ComputeElement(Param,ResolvedEl,[]);
-  writeln('AAA1 TPasToJSConverter.ConvertBuiltIn_LowHigh ',GetResolverResultDbg(ResolvedEl));
   case ResolvedEl.BaseType of
     btContext:
       begin
