900fd35f4abd574488c8c85d8a62874814ea2a3b
svenbarth <pascaldragon@googlemail.com>
2019-07-19 15:58:42 +0000

    * increase PPU version now that all generic procdefs store their local symtables
    git-svn-id: trunk@42460 -

--- compiler/ppu.pas
+++ compiler/ppu.pas
@@ -50,7 +50,7 @@ const
   CurrentPPUVersion = 207;
   { for any other changes to the ppu format, increase this version number
     (it's a cardinal) }
-  CurrentPPULongVersion = 3;
+  CurrentPPULongVersion = 4;
 
 { unit flags }
   uf_big_endian          = $000004;
