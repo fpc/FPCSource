789f28877190ef79df431a76d5ec95fffc0a9e43
Jonas Maebe <jonas@freepascal.org>
2019-07-28 11:14:58 +0000

    * fixed distribution of random(int64) based on patch by Pangea     (mantis #35878)
    git-svn-id: trunk@42508 -

---
+++ tests/webtbs/tw35878.pp
@@ -0,0 +1,30 @@
+program random_test;
+{$mode objfpc}{$H+}
+{$APPTYPE CONSOLE}
+const
+   l: UInt64 = 6148914691236517205;
+var
+   s,n: UInt64;
+   i,j: UInt64;
+begin
+  WriteLn('Experiment:', LineEnding);
+  WriteLn(' Draw a natural number r from the intervall [0,l-1] and');
+  WriteLn(' increment a counter s when r < l div 2 is satisfied.');
+  WriteLn(' Repeat this step n times and calculate the ratio s/n.', LineEnding);
+
+  WriteLn(' Expected ratio: ', (l div 2)/l:30, LineEnding);
+
+  WriteLn('Input size n':16, 'Observed ratio s/n':30);
+  l := 6148914691236517205;
+  for j := 4 to 18 do
+  begin
+    n := (Int64(1) shl j);
+    s := 0;
+    for i := 0 to n-1 do
+      if Random(Int64(l)) < l div 2 then
+        Inc(s);
+    WriteLn( (UInt64(1) shl j):16, s/n:30);
+  end;
+  if abs(0.5-(s/n))>0.1 then
+    halt(1);
+end.
---
+++ tests/webtbs/tw35878a.pp
@@ -0,0 +1,14 @@
+var
+  i, rand, val: int64;
+begin
+  rand:=-5;
+  for i:=1 to 10000 do
+    begin
+      val:=random(rand);
+      if (val<-5) or (val>0) then
+        begin
+          writeln('error: ', val);
+          halt(1);
+        end;
+    end;
+end.
