65bbb09f4b341bd065f800916df82dc12d68b843
Károly Balogh <karoly@freepascal.org>
2019-08-07 09:04:57 +0000

    m68k/vasm: remove bits of Linux support. this combination has never really been tested or supported, and got broken recently. m68k-elf-linux has good enough GNU binutils anyway
    git-svn-id: trunk@42597 -

--- compiler/m68k/ag68kvasm.pas
+++ compiler/m68k/ag68kvasm.pas
@@ -99,7 +99,6 @@ unit ag68kvasm;
           system_m68k_amiga: objtype:='-Felf';
           { atari never had a standard object format, a.out is limited, vasm/vlink author recommends vobj }
           system_m68k_atari: objtype:='-Fvobj';
-          system_m68k_linux: objtype:='-Felf';
         else
           internalerror(2016052601);
         end;
@@ -133,7 +132,7 @@ unit ag68kvasm;
          idtxt  : 'VASM';
          asmbin : 'vasmm68k_std';
          asmcmd:  '-quiet -elfregs -gas $OTYPE $ARCH -o $OBJ $EXTRAOPT $ASM';
-         supported_targets : [system_m68k_amiga,system_m68k_atari,system_m68k_linux];
+         supported_targets : [system_m68k_amiga,system_m68k_atari];
          flags : [af_needar,af_smartlink_sections];
          labelprefix : '.L';
          comment : '# ';
