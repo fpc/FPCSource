bf938bd538528a295b24c0be1aaabce217854ac3
michael <michael@freepascal.org>
2019-09-02 07:21:20 +0000

    * Add TTestBufferedFileStream
    git-svn-id: trunk@42897 -

--- packages/fcl-base/tests/fclbase-unittests.pp
+++ packages/fcl-base/tests/fclbase-unittests.pp
@@ -4,7 +4,7 @@ program fclbase_unittests;
 
 uses
   Classes, consoletestrunner, tests_fptemplate, tchashlist,
-  testexprpars, tcmaskutils, tcinifile, tccsvreadwrite;
+  testexprpars, tcmaskutils, tcinifile, tccsvreadwrite,tcbufferedfilestream;
 
 var
   Application: TTestRunner;
