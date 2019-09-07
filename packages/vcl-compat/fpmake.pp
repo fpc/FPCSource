8cfee113450e351c4ec2784644314f9a42d48a26
michael <michael@freepascal.org>
2019-09-07 19:20:04 +0000

    * Initial version of System.NetEncoding.pp
    git-svn-id: trunk@42937 -

---
+++ packages/vcl-compat/fpmake.pp
@@ -0,0 +1,43 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('vcl-compat');
+    P.ShortName:='vclcomp';
+    P.Author := 'Michael Van Canneyt';
+    P.License := 'LGPL with modification';
+    P.HomepageURL := 'www.freepascal.org';
+    P.Email := '';
+    P.Description := 'Various non-visual VCL compatibility units.';
+      
+{$ifdef ALLPACKAGES}
+    P.Directory:=ADirectory;
+{$endif ALLPACKAGES}
+    P.Version:='3.3.1';
+    P.Dependencies.Add('fcl-base');
+    P.Dependencies.Add('fcl-xml');
+    P.Dependencies.Add('fcl-web');
+    P.Dependencies.Add('rtl-extra'); 
+  
+    P.SourcePath.Add('src');
+    P.IncludePath.Add('src');
+
+
+    T:=P.Targets.AddUnit('System.NetEncoding.pp');
+    T.ResourceStrings := True;
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
---
+++ packages/vcl-compat/tests/testcompat.lpr
@@ -0,0 +1,30 @@
+program testcompat;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}cwstring,{$ENDIF}
+  Classes, consoletestrunner, tcnetencoding;
+
+type
+
+  { TMyTestRunner }
+
+  TMyTestRunner = class(TTestRunner)
+  protected
+  // override the protected methods of TTestRunner to customize its behavior
+  end;
+
+var
+  Application: TMyTestRunner;
+
+begin
+  DefaultRunAllTests:=true;
+  DefaultFormat:=fPlain;
+  Application := TMyTestRunner.Create(nil);
+
+  Application.Initialize;
+  Application.Title:='testcompat';
+  Application.Run;
+  Application.Free;
+end.
