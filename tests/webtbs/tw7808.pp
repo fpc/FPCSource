72391dc4b882907e20a4275eeaf20e9f9a5942d6
florian <florian@freepascal.org>
2019-09-15 17:17:26 +0000

    * fix tests for targets with fixed stack which must be 16 byte aligned
    git-svn-id: trunk@43005 -

--- tests/webtbs/tw7808.pp
+++ tests/webtbs/tw7808.pp
@@ -2,6 +2,9 @@
 { %target=go32v2,linux,freebsd,win32,haiku}
 { %opt=-Sew -vw }
 
+{ do not warn about the lea esp,[esp+16] }
+{$WARN 7105 OFF}
+
 {$mode delphi}
 
 procedure test(l: longint); stdcall;
@@ -12,8 +15,14 @@ end;
 
 begin
   asm
+{$if FPC_STACKALIGNMENT=16}
+    lea esp,[esp-12]
+{$endif FPC_STACKALIGNMENT=16}  
     push word $dead
     push word $beef
-    call test
+    call test    
+{$if FPC_STACKALIGNMENT=16}
+    lea esp,[esp+16]
+{$endif FPC_STACKALIGNMENT=16}  
   end;
 end.
