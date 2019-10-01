396160931f4c9892821393fafd8db65ec0d0fd42
pierre <pierre@freepascal.org>
2019-10-01 12:50:26 +0000

    Remove symbian from fppkg OS list
    git-svn-id: trunk@43103 -

--- utils/fppkg/fpmake.pp
+++ utils/fppkg/fpmake.pp
@@ -31,7 +31,7 @@ begin
     P.Directory:=ADirectory;
     P.Version:='3.3.1';
 
-    P.OSes:=AllOSes-[embedded,msdos,win16,go32v2,nativent,macos,palmos,atari];
+    P.OSes := P.OSes - [embedded,nativent,msdos,go32v2,win16,atari,macos,palmos,symbian];
     if Defaults.CPU=jvm then
       P.OSes := P.OSes - [java,android];
 
