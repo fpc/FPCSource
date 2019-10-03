b4e998b3f83a3d815276371b93cbc20e3a19d809
svenbarth <pascaldragon@googlemail.com>
2019-10-03 12:34:42 +0000

    + add tests to check for correct definition of the FPC_* mode defines
    git-svn-id: trunk@43115 -

---
+++ tests/tbs/tb0659f.pp
@@ -0,0 +1,25 @@
+program tb0659f;
+
+{$mode iso}
+
+{$ifdef FPC_TP}
+{$fatal FPC_TP defined}
+{$endif}
+{$ifdef FPC_OBJFPC}
+{$fatal FPC_OBJFPC defined}
+{$endif}
+{$ifdef FPC_DELPHI}
+{$fatal FPC_DELPHI defined}
+{$endif}
+{$ifdef FPC_MACPAS}
+{$fatal FPC_MACPAS defined}
+{$endif}
+{$ifndef FPC_ISO}
+{$fatal FPC_ISO not defined}
+{$endif}
+{$ifdef FPC_EXTENDEDPASCAL}
+{$fatal FPC_EXTENDEDPASCAL defined}
+{$endif}
+
+begin
+end.
