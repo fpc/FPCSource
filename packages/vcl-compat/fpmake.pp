e05774c6c8837c6f1fdfc44080cd028aa1e35366
pierre <pierre@freepascal.org>
2019-10-13 07:06:54 +0000

    Fix list of targets for vcl-compat package   by also merging commits  #4294 and 42967 ------------------------------------------------------------------------ r42941 | karoly | 2019-09-07 21:01:14 +0000 (Sat, 07 Sep 2019) | 1 line
    vcl-compat: disabled it for palmos, atari, msdos and go32v2. fcl-web is not compiled for these, and that is a dependency.
    ------------------------------------------------------------------------
    --- Merging r42941 into '.':
    U    packages/vcl-compat/fpmake.pp
    --- Recording mergeinfo for merge of r42941 into '.':
     U   .
    ------------------------------------------------------------------------
    r42967 | pierre | 2019-09-09 08:27:53 +0000 (Mon, 09 Sep 2019) | 1 line
    
     Copy supported systems from fcl-web/fpmake.pp to avoid several build failures
    ------------------------------------------------------------------------
    --- Merging r42967 into '.':
    G    packages/vcl-compat/fpmake.pp
    --- Recording mergeinfo for merge of r42967 into '.':
     G   .
    
    git-svn-id: branches/fixes_3_2@43180 -

--- packages/vcl-compat/fpmake.pp
+++ packages/vcl-compat/fpmake.pp
@@ -19,8 +19,10 @@ begin
     P.HomepageURL := 'www.freepascal.org';
     P.Email := '';
     P.Description := 'Various non-visual VCL compatibility units.';
-    P.OSes := P.OSes - [embedded];
-      
+    P.OSes := [beos,haiku,freebsd,darwin,iphonesim,solaris,netbsd,openbsd,linux,win32,win64,wince,aix,amiga,aros,morphos,dragonfly,android];
+    if Defaults.CPU=jvm then
+      P.OSes := P.OSes - [java,android];
+
 {$ifdef ALLPACKAGES}
     P.Directory:=ADirectory;
 {$endif ALLPACKAGES}
@@ -29,7 +31,7 @@ begin
     P.Dependencies.Add('fcl-xml');
     P.Dependencies.Add('fcl-web');
     P.Dependencies.Add('rtl-extra'); 
-  
+
     P.SourcePath.Add('src');
     P.IncludePath.Add('src');
 
