5bfebeea4e3e4f41ca19efef058cc9f09e14d874
pierre <pierre@freepascal.org>
2019-10-23 10:39:49 +0000

    Avoid GPF if currPath is empty in AddPath method, as this is accepted and handled later
    git-svn-id: trunk@43306 -

--- compiler/cfileutl.pas
+++ compiler/cfileutl.pas
@@ -1076,7 +1076,7 @@ end;
          DePascalQuote(currPath);
          { GNU LD convention: if library search path starts with '=', it's relative to the
            sysroot; otherwise, interpret it as a regular path }
-         if currPath[1]='=' then
+         if (length(currPath) >0) and (currPath[1]='=') then
            currPath:=sysroot+FixPath(copy(currPath,2,length(currPath)-1),false)
          else
            currPath:=sysroot+FixPath(currPath,false);
