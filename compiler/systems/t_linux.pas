266daa62f1b5a9bcd770c88479e67d6b607b6ca4
florian <florian@freepascal.org>
2019-12-05 20:07:16 +0000

    * updated arm linker script as proposed by Alfred, resolves #36395
    git-svn-id: trunk@43651 -

--- compiler/systems/t_linux.pas
+++ compiler/systems/t_linux.pas
@@ -1048,7 +1048,7 @@ begin
               add('SECTIONS');
               add('{');
               add('  /* Read-only sections, merged into text segment: */');
-              add('  PROVIDE (__executable_start = 0x8000); . = 0x8000 + SIZEOF_HEADERS;');
+              add('  PROVIDE (__executable_start = SEGMENT_START("text-segment", 0x10000)); . = SEGMENT_START("text-segment", 0x10000) + SIZEOF_HEADERS;');
               add('  .interp         : { *(.interp) }');
               add('  .note.gnu.build-id : { *(.note.gnu.build-id) }');
               add('  .hash           : { *(.hash) }');
