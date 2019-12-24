b89487e7217f9e9c534ae10e24e80d86705f0a77
Jonas Maebe <jonas@freepascal.org>
2019-12-24 22:12:37 +0000

    * fix paraloc def for vectors with vectorcall calling convention (was a single     element instead of a vector)
    git-svn-id: trunk@43784 -

--- compiler/x86_64/cpupara.pas
+++ compiler/x86_64/cpupara.pas
@@ -1553,6 +1553,7 @@ unit cpupara;
                               else
                                 InternalError(2018012901);
                             end;
+                            paraloc^.def:=carraydef.getreusable_no_free_vector(paraloc^.def,j);
                           end;
                         else
                           if (x86_64_use_ms_abi(p.proccalloption) and (p.proccalloption <> pocall_vectorcall)) then
@@ -1858,6 +1859,7 @@ unit cpupara;
                                   else
                                     InternalError(2018012903);
                                 end;
+                                paraloc^.def:=carraydef.getreusable_no_free_vector(paraloc^.def,j);
                               end;
                             else
                               if (use_ms_abi and (p.proccalloption <> pocall_vectorcall)) then
