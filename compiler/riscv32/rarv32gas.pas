f59eae4f81228d9a7f06952dd15349c8f96e5162
svenbarth <pascaldragon@googlemail.com>
2019-12-25 15:23:28 +0000

    * correctly handle local reference in the RISC V assembler readers (both 32 and 64 bit)
    git-svn-id: trunk@43790 -

--- compiler/riscv32/rarv32gas.pas
+++ compiler/riscv32/rarv32gas.pas
@@ -568,7 +568,11 @@ Unit rarv32gas;
 
             oper.opr.ref.refaddr:=refaddr;
             Consume(AS_RPAREN);
-          end;
+          end
+        else if (oper.opr.typ=OPR_REFERENCE) and
+           (oper.opr.ref.refaddr=addr_no) and
+           assigned(oper.opr.ref.symbol) then
+          oper.opr.ref.refaddr:=addr_full;
       end;
 
 
