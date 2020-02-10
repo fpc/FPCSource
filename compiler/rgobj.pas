40a60591430e4639b7786a1f0a3c34451bf73bbb
pierre <pierre@freepascal.org>
2020-02-10 09:37:53 +0000

    Modify code to avoid error when compiling with -CriotR option
    git-svn-id: trunk@44145 -

--- compiler/rgobj.pas
+++ compiler/rgobj.pas
@@ -1598,11 +1598,12 @@ unit rgobj;
           {        while compiling the compiler. }
           tmpr:=NR_STACK_POINTER_REG;
           { e.g. AVR does not have a stack pointer register }
-{$push}{$warnings off}
-          if (RS_STACK_POINTER_REG<>RS_INVALID) and
-{$pop}
-            (regtype=getregtype(tmpr)) then
+{$if defined(RS_STACK_POINTER_REG)}
+  {$if (RS_STACK_POINTER_REG<>RS_INVALID)}
+          if (regtype=getregtype(tmpr)) then
             include(adj_colours,RS_STACK_POINTER_REG);
+  {$ifend}
+{$ifend}
           {Assume a spill by default...}
           found:=false;
           {Search for a colour not in this list.}
