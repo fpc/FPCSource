3060799961dd157f6bec0f43498872f84df7e6e5
florian <florian@freepascal.org>
2020-02-20 20:51:46 +0000

    * fixed tests for architectures with FPC_COMP_IS_INT64 defined
    git-svn-id: trunk@44222 -
    (cherry picked from commit 66aa2e1416e53e42ce5593ea5c71e244fd3dbb4e)

--- tests/tbs/tb0621.pp
+++ tests/tbs/tb0621.pp
@@ -7,7 +7,22 @@ VAR x1, x2 : comp;
    Markus Greim / 29.jun.2016 *)
 
 BEGIN
-
+{$ifdef FPC_COMP_IS_INT64}
+x1 := 8;
+writeln('x1 : ',x1);
+x2 := round(x1 / 2);
+writeln('x2 = x1/2 should be 4 but is : ', x2);
+if x2<>4 then
+  halt(1);
+x2 := round(x1 / 4);
+writeln('x2 = x1/4 should be 2 but is : ', x2);
+if x2<>2 then
+  halt(2);
+x2 := round(x1 / 8);
+writeln('x2 = x1/8 should be 1 and is : ', x2);
+if x2<>1 then
+  halt(3);
+{$else FPC_COMP_IS_INT64}
 x1 := 8;
 writeln('x1 : ',x1);
 x2 := x1 / 2;
@@ -22,6 +37,7 @@ x2 := x1 / 8;
 writeln('x2 = x1/8 should be 1 and is : ', x2);
 if x2<>1 then
   halt(3);
+{$endif FPC_COMP_IS_INT64}
 
 
 END. 
