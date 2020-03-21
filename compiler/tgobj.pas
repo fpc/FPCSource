30a4f667c171a8a352952bb008f7f7fc7e91284c
Jeppe Johansen <jeppe@j-software.dk>
2020-03-21 20:59:32 +0000

    tgobj: Change direction of stack.
    git-svn-id: trunk@44331 -

--- compiler/tgobj.pas
+++ compiler/tgobj.pas
@@ -184,7 +184,7 @@ implementation
        tempfreelist:=nil;
        templist:=nil;
        { we could create a new child class for this but I don't if it is worth the effort (FK) }
-{$if defined(powerpc) or defined(powerpc64) or defined(avr) or defined(jvm) or defined(aarch64)}
+{$if defined(powerpc) or defined(powerpc64) or defined(avr) or defined(jvm) or defined(aarch64) or defined(xtensa)}
        direction:=1;
 {$else}
        direction:=-1;
