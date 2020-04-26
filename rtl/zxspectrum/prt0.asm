78c40a6d70927055415acaf072b1bd8f46e14d35
nickysn <nickysn@gmail.com>
2020-04-26 16:04:29 +0000

    * allow setting the stack size from FPC
    git-svn-id: branches/z80@45104 -

--- rtl/zxspectrum/prt0.asm
+++ rtl/zxspectrum/prt0.asm
@@ -1,12 +1,10 @@
                         .area _CODE
                         .globl PASCALMAIN
                         .globl FPC_SAVE_IY
+                        .globl __fpc_stackarea_start
+                        .globl __fpc_stackarea_end
+
 start::
-                        ld sp, #stack_area_end
+                        ld sp, #__fpc_stackarea_end
                         ld (FPC_SAVE_IY), iy
                         jp PASCALMAIN
-
-                        .area _DATA
-stack_area_start:       ;.rs 1022
-                        .rs 254
-stack_area_end:         .rs 2
