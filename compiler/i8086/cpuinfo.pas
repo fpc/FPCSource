28395337766e49f89b58e690ec80b3a140201bdd
yury <jura@cp-lab.com>
2020-07-24 16:45:44 +0000

    * i8086: Enabled the cs_opt_regvar optimization. It works fine now.
    git-svn-id: trunk@45847 -

--- compiler/i8086/cpuinfo.pas
+++ compiler/i8086/cpuinfo.pas
@@ -151,7 +151,7 @@ Const
 
    level1optimizerswitches = genericlevel1optimizerswitches;
    level2optimizerswitches = genericlevel2optimizerswitches + level1optimizerswitches +
-     [{$ifndef llvm}{cs_opt_regvar,}{$endif}cs_opt_stackframe,cs_opt_tailrecursion{,cs_opt_nodecse}];
+     [cs_opt_regvar,cs_opt_stackframe,cs_opt_tailrecursion{,cs_opt_nodecse}];
    level3optimizerswitches = genericlevel3optimizerswitches + level2optimizerswitches + [{,cs_opt_loopunroll}];
    level4optimizerswitches = genericlevel4optimizerswitches + level3optimizerswitches + [cs_useebp];
 
