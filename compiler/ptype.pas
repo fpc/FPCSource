52be8c663187cb2ce061c7973638a729f2ab5fff
svenbarth <pascaldragon@googlemail.com>
2020-08-02 17:51:34 +0000

    Merged revision(s) 42776 from trunk: * fix for Mantis #35981: ensure that the "specialize" token is only really used in non-Delphi modes ........
    git-svn-id: branches/fixes_3_2@45994 -

--- compiler/ptype.pas
+++ compiler/ptype.pas
@@ -359,7 +359,7 @@ implementation
          if checkcurrentrecdef and
             try_parse_structdef_nested_type(def,current_structdef,isforwarddef) then
            exit;
-         if not allowunitsym and (idtoken=_SPECIALIZE) then
+         if not allowunitsym and not (m_delphi in current_settings.modeswitches) and (idtoken=_SPECIALIZE) then
            begin
              consume(_ID);
              is_specialize:=true;
@@ -490,7 +490,7 @@ implementation
 
                _ID:
                  begin
-                   if try_to_consume(_SPECIALIZE) then
+                   if not (m_delphi in current_settings.modeswitches) and try_to_consume(_SPECIALIZE) then
                      begin
                        if ([stoAllowSpecialization,stoAllowTypeDef] * options = []) then
                          begin
