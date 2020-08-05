16aa4f0dfd2739042eaabba35dad9ee9f3b923a6
nickysn <nickysn@gmail.com>
2020-08-05 16:02:51 +0000

    + added the minimal system unit from the wasm demo
    git-svn-id: branches/wasm@46260 -

---
+++ rtl/wasi/system.pp
@@ -0,0 +1,21 @@
+unit system;
+
+interface
+
+type
+  integer = longint;
+  hresult = integer; 
+  ttypekind = integer;
+  filerec = integer;
+  textrec = integer;
+  pbyte = ^byte;
+
+procedure fpc_lib_exit; compilerproc;
+
+implementation
+
+procedure fpc_lib_exit; compilerproc;
+begin
+end;
+
+end.
