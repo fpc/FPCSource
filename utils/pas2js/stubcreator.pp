a78d5ba112f6345c0fc0a360830aabf0cc26320e
michael <michael@freepascal.org>
2020-08-08 09:09:18 +0000

    * Fix bug id #37514 : type helper not recognized
    git-svn-id: trunk@46319 -

--- utils/pas2js/stubcreator.pp
+++ utils/pas2js/stubcreator.pp
@@ -340,7 +340,7 @@ begin
   FLineNumberWidth:=4;
   FIndentSize:=2;
   FExtraUnits:='';
-  FOptions:=[woNoImplementation,woNoExternalClass,woNoExternalVar,woNoExternalFunc,woNoAsm,woSkipPrivateExternals];
+  FOptions:=[woNoImplementation,woNoExternalClass,woNoExternalVar,woNoExternalFunc,woNoAsm,woSkipPrivateExternals,woAlwaysRecordHelper];
 end;
 
 destructor TStubCreator.Destroy;
