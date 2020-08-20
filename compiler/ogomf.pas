1002a7c5901cdb185711fc197b4b0c6cee1de91d
yury <jura@cp-lab.com>
2020-08-20 16:35:27 +0000

    * 8086: Fixed very long section names when $HUGECODE is ON and section based smartlinking is used. * Added a utility function TrimStrCRC32().
    git-svn-id: trunk@46511 -

--- compiler/ogomf.pas
+++ compiler/ogomf.pas
@@ -803,7 +803,7 @@ implementation
 
     uses
        SysUtils,
-       cutils,verbose,globals,
+       cutils,verbose,globals,fpccrc,
        fmodule,aasmtai,aasmdata,
        ogmap,owomflib,elfbase,
        version
@@ -1053,7 +1053,7 @@ implementation
         if current_settings.x86memorymodel in x86_far_code_models then
           begin
             if cs_huge_code in current_settings.moduleswitches then
-              result:=aname + '_TEXT'
+              result:=TrimStrCRC32(aname,30) + '_TEXT'
             else
               result:=current_module.modulename^ + '_TEXT';
           end
