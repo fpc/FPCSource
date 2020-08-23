02393dbf786ada4cbfc97e8dee3d32d3307d351a
michael <michael@freepascal.org>
2020-08-23 10:34:08 +0000

    * Merging revisions r45395 from trunk:     ------------------------------------------------------------------------     r45395 | michael | 2020-05-17 09:54:41 +0200 (Sun, 17 May 2020) | 1 line
        * Add switch to ignore unknown resources
        ------------------------------------------------------------------------
    
    git-svn-id: branches/fixes_3_2@46644 -

--- packages/pastojs/src/pas2jsfiler.pp
+++ packages/pastojs/src/pas2jsfiler.pp
@@ -106,7 +106,8 @@ const
     'CheckCondFunction',
     'StopOnErrorDirective',
     'ExtClassConstWithoutExpr',
-    'StopOnUnitInterface');
+    'StopOnUnitInterface',
+    'IgnoreUnknownResource');
 
   PCUDefaultModeSwitches: TModeSwitches = [
     msObjfpc,
