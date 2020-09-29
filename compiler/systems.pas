b1249dcc32f83506745c5ca6edaaaf87251df759
pierre <pierre@freepascal.org>
2020-09-29 14:53:00 +0000

    * Merge commit #45720 ------------------------------------------------------------------------ r45720 | pierre | 2020-07-02 16:26:13 +0000 (Thu, 02 Jul 2020) | 1 line
     Obsolete system_mips_embedded, as it is a duplicate of system_mipseb_embedded
    ------------------------------------------------------------------------
    --- Merging r45720 into '.':
    U    compiler/systems.inc
    U    compiler/systems.pas
    --- Recording mergeinfo for merge of r45720 into '.':
     U   .
    
    git-svn-id: branches/fixes_3_2@47004 -

--- compiler/systems.pas
+++ compiler/systems.pas
@@ -279,7 +279,7 @@ interface
                            system_powerpc_embedded,
                            system_sparc_embedded,obsolete_system_vm_embedded,
                            obsolete_system_ia64_embedded,system_x86_64_embedded,
-                           system_mips_embedded,system_arm_embedded,
+                           obsolete_system_mips_embedded,system_arm_embedded,
                            system_powerpc64_embedded,system_avr_embedded,
                            system_jvm_java32,system_mipseb_embedded,system_mipsel_embedded,
                            system_i8086_embedded];
