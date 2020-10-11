15182843488b82712f27d9916e7cc25d641dd6a2
marco <marco@freepascal.org>
2020-10-11 14:03:03 +0000

    # revisions: 43975,43998
    git-svn-id: branches/fixes_3_2@47089 -

--- packages/libffi/src/ffi.pp
+++ packages/libffi/src/ffi.pp
@@ -184,7 +184,11 @@ const
   FFI_DEFAULT_ABI = ffi_abi(Ord(FFI_SYSV) {$ifdef FREEBSD}or FFI_SYSV_STRUCT_RET{$endif});
   {$endif}
 {$else}
+  {$ifdef WIN32}
+    FFI_DEFAULT_ABI = FFI_STDCALL;
+  {$else}
     FFI_DEFAULT_ABI = FFI_SYSV;
+  {$endif}
 {$endif}
 
 const
@@ -216,7 +220,7 @@ const
 {$endif}
 
 {$if defined(CPUARM)}
-  {$if definde(DARWIN)}
+  {$if defined(DARWIN)}
   FFI_TRAMPOLINE_SIZE = 12;
   FFI_TRAMPOLINE_CLOSURE_OFFSET = 8;
   {$elseif FFI_EXEC_TRAMPOLINE_TABLE}
@@ -260,6 +264,16 @@ const
   FFI_TRAMPOLINE_SIZE = 12;
 {$elseif defined(CPUM68K)}
   FFI_TRAMPOLINE_SIZE = 16;
+{$elseif defined(CPURISCV32)}
+  FFI_TRAMPOLINE_SIZE = 24;
+{$elseif defined(CPURISCV64)}
+  FFI_TRAMPOLINE_SIZE = 24;
+{$elseif defined(CPUMIPS32)}
+  FFI_TRAMPOLINE_SIZE = 20;
+{$elseif defined(CPUMIPS64)}
+  FFI_TRAMPOLINE_SIZE = 56;
+#endif
+
 {$endif}
 
 {
