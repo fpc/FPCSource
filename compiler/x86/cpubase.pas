47066f0ce7e8dc21393d07516de90e094610e52b
florian <florian@freepascal.org>
2020-11-08 19:39:18 +0000

    * moved UseAVX from cgx86 to cpubase + UseAVX512
    git-svn-id: trunk@47346 -

--- compiler/x86/cpubase.pas
+++ compiler/x86/cpubase.pas
@@ -385,11 +385,15 @@ topsize2memsize: array[topsize] of integer =
     function requires_fwait_on_8087(op: TAsmOp): boolean;
 {$endif i8086}
 
+   function UseAVX: boolean;
+   function UseAVX512: boolean;
+
 implementation
 
     uses
       globtype,
-      rgbase,verbose;
+      rgbase,verbose,
+      cpuinfo;
 
     const
     {$if defined(x86_64)}
@@ -946,6 +950,19 @@ implementation
         end;
       end;
 {$endif i8086}
+
+
+  function UseAVX: boolean;
+    begin
+      Result:={$ifdef i8086}false{$else i8086}(FPUX86_HAS_AVXUNIT in fpu_capabilities[current_settings.fputype]){$endif i8086};
+    end;
+
+
+  function UseAVX512: boolean;
+    begin
+      // Result:=(current_settings.fputype in fpu_avx_instructionsets) {$ifndef i8086}or (CPUX86_HAS_AVXUNIT in cpu_capabilities[current_settings.cputype]){$endif i8086};
+      Result:=false;
+    end;
 
 
 end.
