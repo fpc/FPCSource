a477c98e3284376ac09d7152c897eec988312b1e
michael <michael@freepascal.org>
2020-11-20 11:35:51 +0000

    * More base address fixes
    git-svn-id: trunk@47477 -
    (cherry picked from commit be4d0be10cd79935869ee57b2a2bb1c95d633335)

--- packages/fcl-passrc/src/pparser.pp
+++ packages/fcl-passrc/src/pparser.pp
@@ -5373,7 +5373,8 @@ begin
         else
           // remove legacy or basesysv on MorphOS syscalls
           begin
-          if CurTokenIsIdentifier('legacy') or CurTokenIsIdentifier('BaseSysV') or CurTokenIsIdentifier('_ExecBase') then
+          if CurTokenIsIdentifier('legacy')
+             or (Curtoken=tkIdentifier) and (Pos('base',LowerCase(CurtokenText))>0) then
             NextToken;
           end;
       end;
