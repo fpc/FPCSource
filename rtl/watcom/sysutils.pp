91da9eae170879a977272dc1df88c958aa263079
Tomas Hajny <hajny@freepascal.org>
2020-11-24 00:35:52 +0000

    + TZ variable based offset calculation included for DOS targets
    git-svn-id: trunk@47546 -

--- rtl/watcom/sysutils.pp
+++ rtl/watcom/sysutils.pp
@@ -49,6 +49,7 @@ implementation
 
 {$DEFINE FPC_FEXPAND_UNC} (* UNC paths are supported *)
 {$DEFINE FPC_FEXPAND_DRIVES} (* Full paths begin with drive specification *)
+{$DEFINE HAS_LOCALTIMEZONEOFFSET}
 
 { Include platform independent implementation part }
 {$i sysutils.inc}
@@ -635,6 +636,8 @@ end;
                               Time Functions
 ****************************************************************************}
 
+{$I tzenv.inc}
+
 Procedure GetLocalTime(var SystemTime: TSystemTime);
 var
   Regs: Registers;
@@ -914,6 +917,7 @@ Initialization
   InitInternational;    { Initialize internationalization settings }
   InitDelay;
   OnBeep:=@SysBeep;
+  InitTZ;
 Finalization
   FreeTerminateProcs;
   DoneExceptions;
