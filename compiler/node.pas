348be7657df8af9c28ad0230bd81e85ebb42dc2c
pierre <pierre@freepascal.org>
2020-12-08 11:28:50 +0000

    Fix bug introduced in commit #47709, by only calling node_complexity if first pass was already done (bug report 38183)
    git-svn-id: trunk@47720 -

--- compiler/node.pas
+++ compiler/node.pas
@@ -896,7 +896,9 @@ implementation
                 first:=false;
               write(t, i);
             end;
-        write(t,'], cmplx = ',node_complexity(self));
+        write(t,']');
+        if (nf_pass1_done in flags) then
+          write(t,', cmplx = ',node_complexity(self));
       end;
 
 
@@ -942,7 +944,8 @@ implementation
                 Write(T, ',', i)
             end;
 
-        write(t,'" complexity="',node_complexity(self),'"');
+        if (nf_pass1_done in flags) then
+          write(t,'" complexity="',node_complexity(self),'"');
       end;
 
     procedure tnode.XMLPrintNodeData(var T: Text);
