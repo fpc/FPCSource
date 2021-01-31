afccc19a9e9bd47fed97c21b6b66a4f5749aa4ea
florian <florian@freepascal.org>
2021-01-31 16:23:23 +0000

    * properly check for linux version supporting statx
    git-svn-id: trunk@48469 -
    (cherry picked from commit 33cd5778cd1c2f1543029f9f30e4497fd6ef4c75)

--- tests/test/units/linux/tstatx.pp
+++ tests/test/units/linux/tstatx.pp
@@ -1,7 +1,7 @@
 { %target=linux }
 uses
   ctypes,baseunix,linux;
-  
+
 var
   un : utsname;
   mystatx : statx;
@@ -21,13 +21,13 @@ begin
         major:=system.copy(st,1,p-1);
         system.val(major,major_release,err);
         if err<>0 then
-          begin 
+          begin
             writeln('Unable to parse first part of linux version ',st,'(',major,') correctly');
             halt(2);
           end;
         break;
       end;
-  
+
   for i:=p+1 to UTSNAME_LENGTH do
     if st[i]='.' then
       begin
@@ -35,20 +35,20 @@ begin
         minor:=system.copy(st,p+1,e-p-1);
         system.val(minor,minor_release,err);
         if err<>0 then
-          begin 
+          begin
             writeln('Unable to second part of parse linux version ',st,'i(',minor,') correctly');
             halt(2);
           end;
         break;
       end;
-  if (major_release<4) or (minor_release<11) then
+  if (major_release<4) or ((major_release=4) and (minor_release<11)) then
     begin
       writeln('This version of Linux: ',st,' does not have fstatx syscall');
       halt(0);
     end
   else
     writeln('This linux version ',st,' should support statx syscall');
-     
+
   assign(f,'test.txt');
   rewrite(f);
   write(f,'ccccc');
