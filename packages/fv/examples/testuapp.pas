646ddb82173c7a55d7b4fa34c7b2cc75e9fa7c87
nickysn <nickysn@gmail.com>
2021-02-22 02:08:08 +0000

    + Unicode aware implementation of TStaticText.Draw
    git-svn-id: branches/unicodekvm@48776 -

--- packages/fv/examples/testuapp.pas
+++ packages/fv/examples/testuapp.pas
@@ -3,7 +3,7 @@ program testuapp;
 {$codepage UTF8}
 
 uses
-  Objects, UDrivers, UViews, UMenus, UApp, SysUtils;
+  Objects, UDrivers, UViews, UMenus, UApp, UMsgBox, SysUtils;
 
 const
   cmOrderNew    = 200;
@@ -24,13 +24,50 @@ type
   { TMyUnicodeApp }
 
   TMyUnicodeApp = object(TApplication)
+    procedure HandleEvent(var Event : TEvent);virtual;
+    procedure InitMenuBar; virtual;
     procedure InitStatusLine; virtual;
+    procedure ShowAboutBox;
   end;
 
 var
   MyUnicodeApp: TMyUnicodeApp;
 
 { TMyUnicodeApp }
+
+procedure TMyUnicodeApp.HandleEvent(var Event: TEvent);
+begin
+  inherited HandleEvent(Event);
+  if Event.What = evCommand then
+  begin
+    case Event.Command of
+      cmAbout:
+        ShowAboutBox;
+      else
+        Exit;
+    end;
+  end;
+  ClearEvent(Event);
+end;
+
+procedure TMyUnicodeApp.InitMenuBar;
+var
+  R: TRect;
+begin
+  GetExtent(R);
+  R.B.Y := R.A.Y + 1;
+  MenuBar := new (PMenuBar, Init(R, NewMenu(
+                 NewSubMenu('打开', hcNoContext, NewMenu(NewItem('~Н~ов打тест по пъ́тя',  'Еф2', kbF2, cmNew, hcNew,
+                      NewItem('~O~pen', '💩', kbF3, cmOpen, hcOpen,
+                      NewLine(
+                      NewItem('E~x~it', 'ъ́ъ́ъ́打', kbAltX, cmQuit, hcNoContext, nil))))),
+                 NewSubMenu('~E~dit', hcNoContext, NewMenu({GetEditMenuItems(nil)}nil),
+                 NewSubMenu('~O~rders', hcNoContext, {NewMenu(GetOrdersMenuItems(nil))}nil,
+                 NewSubMenu('O~p~tions', hcNoContext, {NewMenu(GetOptionsMenuItems(nil))}nil,
+                 NewSubMenu('~W~indow', hcNoContext, {NewMenu(GetWindowMenuItems(nil))}nil,
+                 NewSubMenu('~H~elp', hcNoContext, NewMenu(NewItem('~A~bout...','',kbNoKey,cmAbout,hcNoContext,
+                       nil)), nil)))))))));
+end;
 
 procedure TMyUnicodeApp.InitStatusLine;
 var
@@ -51,6 +88,15 @@ begin
           NewStatusKey('~F6~ Next', kbF6, cmOrderNext,
           NewStatusKey('~Shift+F6~ Pref', kbShiftF6, cmOrderPrev,
           nil)),nil))));
+end;
+
+procedure TMyUnicodeApp.ShowAboutBox;
+begin
+  MessageBox(#3'Free Vision TUI Framework'#13 +
+    #3'Test/Demo Application'#13+
+    #3'Мога да ям стъкло, то не ми вреди.'#13+
+    #3'我能吞下玻璃而不伤身体。',
+    nil, mfInformation or mfOKButton);
 end;
 
 begin
