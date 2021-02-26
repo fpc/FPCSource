8d0ef71e37e6a59066547ebb898b17ca21e3b601
Károly Balogh <karoly@freepascal.org>
2021-02-26 10:57:05 +0000

    m68k-linux: revert maxCrecordalign to 2, as an experiment to fix some linking-against-C regressions since the alignment changes
    git-svn-id: trunk@48809 -

--- compiler/systems/i_linux.pas
+++ compiler/systems/i_linux.pas
@@ -234,7 +234,7 @@ unit i_linux;
                 localalignmax   : 8;
                 recordalignmin  : 0;
                 recordalignmax  : 16;
-                maxCrecordalign : 4;
+                maxCrecordalign : 2;
               );
             first_parm_offset : 8;
             stacksize    : 8*1024*1024;
