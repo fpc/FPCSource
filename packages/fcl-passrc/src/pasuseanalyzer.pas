638efba36e3c6d3589e4d9da5fcfd8fb5d9861fb
Mattias Gaertner <nc-gaertnma@netcologne.de>
2021-03-01 22:01:31 +0000

    fcl-passrc: useanalyzer: fixed marking nested type
    git-svn-id: trunk@48861 -
    (cherry picked from commit 8c38953440b52e427e92b51c796a3a8d08f770b9)

--- packages/fcl-passrc/src/pasuseanalyzer.pas
+++ packages/fcl-passrc/src/pasuseanalyzer.pas
@@ -2357,11 +2357,18 @@ begin
     else if IsModuleInternal(Member) then
       // private or strict private
       continue
-    else if (Mode=paumAllPasUsable) and FirstTime
-        and ((Member.ClassType=TPasProperty) or (Member is TPasType)) then
+    else if (Mode=paumAllPasUsable) and FirstTime then
       begin
-      // non private property can be used by typeinfo by descendants in other units
-      UseTypeInfo(Member);
+      if Member.ClassType=TPasProperty then
+        begin
+        // non private property can be used by typeinfo by descendants in other units
+        UseTypeInfo(Member);
+        end
+      else if Member is TPasType then
+        begin
+        // non private type can be used by descendants in other units
+        UseType(TPasType(Member),Mode);
+        end
       end
     else
       ; // else: class/record is in unit interface, mark all non private members
