83ce289ea4f738bc0599fedb7fe153e804d1ea42
florian <florian@freepascal.org>
2021-03-06 15:03:33 +0000

    * last commit fixed, hopefully final fix
    git-svn-id: trunk@48886 -

--- compiler/nadd.pas
+++ compiler/nadd.pas
@@ -1715,7 +1715,7 @@ implementation
              (left.resultdef.typ=orddef) and
              (left.nodetype=andn) and
              (right.nodetype=andn) and
-             not(is_boolean(resultdef) or not(might_have_sideeffects(self,[mhs_exceptions])) or not(doshortbooleval(self))) and
+             (not(is_boolean(resultdef)) or not(might_have_sideeffects(self,[mhs_exceptions])) or not(doshortbooleval(self))) and
              { this test is not needed but it speeds up the test and allows to bail out early }
              ((taddnode(left).left.nodetype=notn) or (taddnode(left).right.nodetype=notn) or
               (taddnode(right).left.nodetype=notn) or (taddnode(right).right.nodetype=notn)
