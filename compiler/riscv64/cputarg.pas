d1fb44044f4711db2430d267ab8c884d242cb13e
florian <florian@freepascal.org>
2021-03-07 08:53:03 +0000

    * unified RiscV32 and RiscV64 GAS readers
    git-svn-id: trunk@48894 -

--- compiler/riscv64/cputarg.pas
+++ compiler/riscv64/cputarg.pas
@@ -55,7 +55,7 @@ implementation
 **************************************}
 
   {$ifndef NoRaRVGas}
-       ,rarv64gas
+       ,rarvgas
   {$endif NoRaRVGas}
 
 {**************************************
