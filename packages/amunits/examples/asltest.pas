6a9279860356ec61d0f133b00345da12744fef1f
marcus <marcus@idefix.freepascal.org>
2021-03-15 20:46:29 +0000

    amiga: examples fixed and updated
    git-svn-id: trunk@48988 -

--- packages/amunits/examples/asltest.pas
+++ packages/amunits/examples/asltest.pas
@@ -28,18 +28,18 @@ BEGIN
     fr := AllocAslRequestTags(ASL_FileRequest,[
                           ASLFR_InitialPattern, AsTag('#?'),
                           ASLFR_TitleText, AsTag('Test av ASL-Requester by NS'),
-                          ASLFR_DoPatterns, LTrue,
+                          ASLFR_DoPatterns, AsTag(True),
                           TAG_DONE]);
 
     IF fr <> nil THEN BEGIN
         dummy := AslRequest(fr,NIL);
         if dummy then begin
            MessageBox('Test of Asl',
-                      ' The path is :' +
-                      strpas(fr^.rf_Dir) +
+                      ' The path is:" ' +
+                      string(fr^.rf_Dir) + '"' +
                       chr(10) +
-                      'And the file is :' +
-                      strpas(fr^.rf_File),
+                      'And the file is: "' +
+                      string(fr^.rf_File) + '"', 
                       'OK');
         end else MessageBox('Test of Asl','You canceled','OK');
         FreeAslRequest(fr);
