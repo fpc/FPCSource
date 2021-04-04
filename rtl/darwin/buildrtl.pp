755c574a87c2505c793dc7fa51fdcb2c572ace70
florian <florian@freepascal.org>
2021-04-04 14:54:56 +0000

    + lazarus project
    git-svn-id: trunk@49119 -

---
+++ rtl/darwin/buildrtl.pp
@@ -0,0 +1,29 @@
+{ This unit is only used to edit the rtl with lazarus }
+unit buildrtl;
+
+  interface
+
+    uses
+      { those units are directly build using make:
+        system uuchar objpas macpas iso7185 cpall lineinfo lnfodwrf
+      }
+      macpas, iso7185,
+      fpintres, // $(SYSINIT_UNITS) \
+      si_prc, si_c, si_g, si_dll,
+      unixtype, ctypes, baseunix, strings, extpas, syscall, unixutil,
+      heaptrc,
+      termio, unix, initc, cmem,
+{$ifdef CPUI386}
+      mmx,
+{$endif CPUI386}
+      linuxvcs,
+      sysutils, typinfo, math,
+      charset, cpall, character, unixcp, getopts,
+      errors, dl, dynlibs,
+      types, sysconst, fpwidestring,
+      cthreads, sortbase, classes, fgl, rtlconsts, dos, cwstring, bsd, fpcylix,
+      softfpu, sfpux80, ufloatx80, sfpu128, ufloat128;
+
+  implementation
+
+end.
