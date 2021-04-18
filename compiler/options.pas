70760208bbc0574bd170e3acd7e8baad3cd90cf2
pierre <pierre@freepascal.org>
2021-04-18 15:41:11 +0000

    Add systems_openbsd to suppported_targets_x_smallr and modify GenerateExecutable in t_bsd unit accordingly
    git-svn-id: trunk@49229 -

--- compiler/options.pas
+++ compiler/options.pas
@@ -140,6 +140,7 @@ const
 
 
   suppported_targets_x_smallr = systems_linux + systems_solaris + systems_android
+                             + systems_openbsd
                              + [system_i386_haiku,system_x86_64_haiku]
                              + [system_i386_beos]
                              + [system_m68k_amiga];
