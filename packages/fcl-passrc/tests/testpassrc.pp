246bff92d2c595840ff748a2996a25ce8aa46fb7
florian <florian@freepascal.org>
2021-08-15 11:51:42 +0200

    + include fcl-passrc in regression tests

---
+++ packages/fcl-passrc/tests/testpassrc.pp
@@ -0,0 +1,32 @@
+{ %OPT=-Sc }
+program testpassrc;
+
+{$mode objfpc}{$H+}
+
+uses
+  //MemCheck,
+  Classes, consoletestrunner, tcscanner,  tctypeparser, tcstatements,
+  tcbaseparser, tcmoduleparser, tconstparser, tcvarparser, tcclasstype,
+  tcexprparser, tcprocfunc, tcpassrcutil, tcresolver, tcgenerics,
+  tcuseanalyzer, pasresolveeval, tcresolvegenerics;
+
+type
+
+  { TLazTestRunner }
+
+  TMyTestRunner = class(TTestRunner)
+  protected
+  // override the protected methods of TTestRunner to customize its behavior
+  end;
+
+var
+  Application: TMyTestRunner;
+
+begin
+  Application := TMyTestRunner.Create(nil);
+  DefaultFormat:=fplain;
+  DefaultRunAllTests:=True;
+  Application.Initialize;
+  Application.Run;
+  Application.Free;
+end.
