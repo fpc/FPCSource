d36e239f45011d9dea8d15bab4ee3f46f29ff85b
Nikolay Nikolov <nickysn@gmail.com>
2021-09-17 21:20:14 +0300

    + enabled the exception handling instructions in the llvm-mc command line

--- compiler/wasm32/agllvmmc.pas
+++ compiler/wasm32/agllvmmc.pas
@@ -328,7 +328,7 @@ implementation
          id     : as_wasm32_llvm_mc;
          idtxt  : 'LLVM-MC';
          asmbin : 'llvm-mc';
-         asmcmd : '--assemble --arch=wasm32 -mattr=+sign-ext --filetype=obj -o $OBJ $EXTRAOPT $ASM';
+         asmcmd : '--assemble --arch=wasm32 -mattr=+sign-ext,+exception-handling --filetype=obj -o $OBJ $EXTRAOPT $ASM';
          supported_targets : [system_wasm32_embedded,system_wasm32_wasi];
          flags : [af_smartlink_sections];
          labelprefix : '.L';
