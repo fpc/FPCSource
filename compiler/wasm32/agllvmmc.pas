d8665f8a517e90116976cc529a71615fbbdd1bd4
Nikolay Nikolov <nickysn@gmail.com>
2021-09-17 23:40:55 +0300

    * write the .globaltype directive and the imports in the beginning of the   assembly file, because that appears to be required by llvm-mc from git

--- compiler/wasm32/agllvmmc.pas
+++ compiler/wasm32/agllvmmc.pas
@@ -153,10 +153,10 @@ implementation
 
   procedure TLLVMMachineCodePlaygroundAssembler.WriteAsmList;
     begin
-      inherited;
-      { print all global procedures/functions }
       writer.AsmWriteLn(#9'.globaltype'#9+STACK_POINTER_SYM+', i32');
+      { print all global procedures/functions }
       WriteImports;
+      inherited;
     end;
 
 
