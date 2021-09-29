8822be8a1f1c1b6affca58be1bf337be4c345bb1
Nikolay Nikolov <nickysn@gmail.com>
2021-09-29 04:04:03 +0300

    + implemented abs(real) for WebAssembly via the f32.abs and f64.abs instructions

--- compiler/wasm32/nwasminl.pas
+++ compiler/wasm32/nwasminl.pas
@@ -34,7 +34,9 @@ interface
 
       twasminlinenode = class(tcginlinenode)
       private
+        function first_abs_real:tnode;override;
         function first_sqrt_real:tnode;override;
+        procedure second_abs_real;override;
         procedure second_sqrt_real;override;
         procedure second_high; override;
         procedure second_memory_size;
@@ -66,10 +68,39 @@ implementation
                                twasminlinenode
 *****************************************************************************}
 
+    function twasminlinenode.first_abs_real: tnode;
+      begin
+        expectloc:=LOC_FPUREGISTER;
+        result:=nil;
+      end;
+
+
     function twasminlinenode.first_sqrt_real: tnode;
       begin
         expectloc:=LOC_FPUREGISTER;
         result:=nil;
+      end;
+
+
+    procedure twasminlinenode.second_abs_real;
+      begin
+        secondpass(left);
+        hlcg.location_force_fpureg(current_asmdata.CurrAsmList,left.location,left.resultdef,true);
+
+        thlcgwasm(hlcg).a_load_loc_stack(current_asmdata.CurrAsmList,left.resultdef,left.location);
+
+        case left.location.size of
+          OS_F32:
+            current_asmdata.CurrAsmList.Concat(taicpu.op_none(a_f32_abs));
+          OS_F64:
+            current_asmdata.CurrAsmList.Concat(taicpu.op_none(a_f64_abs));
+          else
+            internalerror(2021092902);
+        end;
+
+        location_reset(location,LOC_FPUREGISTER,def_cgsize(resultdef));
+        location.register:=hlcg.getregisterfordef(current_asmdata.CurrAsmList,resultdef);
+        thlcgwasm(hlcg).a_load_stack_loc(current_asmdata.CurrAsmList,resultdef,location);
       end;
 
 
