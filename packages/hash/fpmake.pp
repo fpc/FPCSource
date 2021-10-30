a354527c5dd07778da3662b834280b0316a0ea20
Michaël Van Canneyt <michael@freepascal.org>
2021-10-30 20:15:44 +0200

    * fcl-base dependency
    (cherry picked from commit 1bc8aea2b3baa25b03528ed83e38a75fa8f9d909)

--- packages/hash/fpmake.pp
+++ packages/hash/fpmake.pp
@@ -28,6 +28,7 @@ begin
       P.OSes := P.OSes - [java,android];
 
     P.Dependencies.Add('rtl-objpas');
+    P.Dependencies.Add('fcl-base');
 
     P.Version:='3.2.3';
     T:=P.Targets.AddUnit('src/md5.pp');
