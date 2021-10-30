64fbc1847ff03034bedec336a106ea2fba349742
Michaël Van Canneyt <michael@freepascal.org>
2021-10-30 20:09:29 +0200

    * Fix license

--- packages/hash/src/ecc.pp
+++ packages/hash/src/ecc.pp
@@ -1,3 +1,14 @@
+{ This file is part of the Free Component Library.
+
+  Elliptic curve crypto hashing
+  
+  See the file COPYING.FPC, included in this distribution,
+  for details about the copyright.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+}
 {$mode delphi}
 unit ECC;
 
