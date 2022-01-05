fd54c958c945827151fcc66cf98f4ecf3cd70888
J. Gareth "Curious Kit" Moreton <gareth@moreton-family.com>
2022-01-05 20:01:05 +0000

    New POPCNT tests to evaluate new optimisations

---
+++ tests/test/opt/tpopcnt2.pp
@@ -0,0 +1,20 @@
+{ %CPU=i386,x86_64 }
+{ %OPT=-O2 -CpCOREAVX }
+
+var
+  X, Y: LongWord;
+begin
+  for X := 0 to 9 do
+    begin
+      Y := PopCnt(X);
+      { Condition arranged this way so the input of PopCnt is checked first.
+        If both X and Y are zero or both X and Y are not zero, then all is well. }
+      if (X = 0) xor (Y = 0) then
+        begin
+          WriteLn('FAIL: Condition implies PopCnt(', X, ') = ', Y, ' and not ', PopCnt(X));
+          Halt(1);
+        end;
+    end;
+
+  WriteLn('ok');
+end.
