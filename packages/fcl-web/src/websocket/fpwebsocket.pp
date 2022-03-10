9e631db928b5bf3bd379830b9376b31fddc343f0
Michaël Van Canneyt <michael@freepascal.org>
2022-03-10 17:23:03 +0100

    * Rephrase more clear

--- packages/fcl-web/src/websocket/fpwebsocket.pp
+++ packages/fcl-web/src/websocket/fpwebsocket.pp
@@ -267,7 +267,7 @@ type
     Sequences : TFrameSequences;
     IsText : Boolean;
     // Use these only when IsText is true (PayLoad contains valid UTF-8).
-    // You may use try to use them when IsText is false, but there is no guarantee they will give correct results.
+    // You may use them when IsText is false, but only if you know there is valid UTF-8 in payload.
     // Return Payload as a UTF8 string
     Property AsString : UTF8String Read GetAsString;
     // Return Payload as a UTF8 string
