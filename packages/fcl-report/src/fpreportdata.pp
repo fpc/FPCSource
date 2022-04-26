476a4f0f91f1b98692124417815f4f2852d48335
marcoonthegit <marco@freepascal.org>
2022-04-26 11:17:33 +0200

    * fix headers other packages up to (but not including) fcl-res (which are generated files)   bug #39481

--- packages/fcl-report/src/fpreportdata.pp
+++ packages/fcl-report/src/fpreportdata.pp
@@ -1,5 +1,20 @@
-unit fpreportdata;
+{
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 1999-2022 by Michael van Canneyt and other members of the
+    Free Pascal development team
 
+    report data 
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
+unit fpreportdata;
 {$mode objfpc}{$H+}
 
 interface
