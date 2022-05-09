8bef28ccbeaa95f39020c4c954ba9bee9ee5c0a9
Jonas Maebe <jonas@freepascal.org>
2022-05-09 21:19:06 +0200

    PPU version: bump
    fcaea210758 changed name mangling

--- compiler/ppu.pas
+++ compiler/ppu.pas
@@ -48,7 +48,7 @@ const
   CurrentPPUVersion = 208;
   { for any other changes to the ppu format, increase this version number
     (it's a cardinal) }
-  CurrentPPULongVersion = 14;
+  CurrentPPULongVersion = 15;
 
 { unit flags }
   uf_big_endian          = $000004;
