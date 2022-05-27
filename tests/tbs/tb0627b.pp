c9ed14f87bf4bfac5231cb2a65ca3cae06cf958c
Nikolay Nikolov <nickysn@gmail.com>
2022-05-27 22:56:05 +0300

    + enable the 64-bit SAR instruction on the WebAssembly target

--- tests/tbs/tb0627b.pp
+++ tests/tbs/tb0627b.pp
@@ -47,10 +47,10 @@ program tb0627b;
 {$define FPC_HAS_INTERNAL_SAR_DWORD}
 { $endif defined(cpux86_64) or defined(cpui386) or defined(cpuarm) or defined(cpupowerpc) or defined(cpupowerpc64) or defined(cpumips) or defined(cpumipsel)}
 
-{$if defined(cpux86_64) or defined(cpupowerpc64) or defined(cpuaarch64) or defined(cpuriscv64)}
+{$if defined(cpux86_64) or defined(cpupowerpc64) or defined(cpuaarch64) or defined(cpuriscv64) or defined(cpuwasm32)}
 {$define FPC_HAS_INTERNAL_SAR_QWORD}
 {$define FPC_HAS_INTERNAL_SAR_ASSIGN_QWORD}
-{$endif defined(cpux86_64) or defined(cpupowerpc64) or defined(cpuaarch64) or defined(cpuriscv64)}
+{$endif defined(cpux86_64) or defined(cpupowerpc64) or defined(cpuaarch64) or defined(cpuriscv64) or defined(cpuwasm32)}
 
 {$endif FPC_HAS_INTERNAL_SAR}
 
