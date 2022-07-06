b2ea782eb4cfd6fed9a85619390793815c204dda
Jonas Maebe <jonas@freepascal.org>
2022-07-06 21:12:57 +0200

    agllvm: fix writing strings in metadata arrays
    They were interpreted as part of larger strings

--- compiler/llvm/agllvm.pas
+++ compiler/llvm/agllvm.pas
@@ -1124,6 +1124,7 @@ implementation
                         writer.AsmWrite(' ');
                         if (tai_abstracttypedconst(p).adetyp=tck_simple) and
                            assigned(tai_simpletypedconst(p).val) and
+                           (tai_simpletypedconst(p).def<>charpointertype) and
                            (tai_simpletypedconst(p).val.typ=ait_string) then
                           begin
                             gotstring:=true;
