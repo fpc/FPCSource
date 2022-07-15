078aa5d04677fbbfb0818bcd0b004719c742c5e4
Nikolay Nikolov <nickysn@gmail.com>
2022-07-15 15:53:55 +0300

    + pass '-m wasm32' to the linker, when compiling for the wasm32-embedded target

--- compiler/systems/t_embed.pas
+++ compiler/systems/t_embed.pas
@@ -2155,7 +2155,7 @@ constructor TLinkerEmbedded_Wasm.Create;
 
 procedure TLinkerEmbedded_Wasm.SetDefaultInfo;
   begin
-    Info.DllCmd[1] := 'wasm-ld $SONAME $GCSECTIONS -z stack-size=$STACKSIZE -o $EXE';
+    Info.DllCmd[1] := 'wasm-ld -m wasm32 $SONAME $GCSECTIONS -z stack-size=$STACKSIZE -o $EXE';
     //Info.DllCmd[2] := 'wasmtool --exportrename $INPUT $EXE';
   end;
 
