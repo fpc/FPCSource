92fc7edd7e76448bf92bd166d4aa2736b0d74ffe
florian <florian@freepascal.org>
2022-08-12 22:11:39 +0200

    * ensure that the executable path used in the test is absolute
    (cherry picked from commit 8028a769f408d54a475bab50ec467531e547c13f)

--- tests/test/units/sysutils/tfileage.pp
+++ tests/test/units/sysutils/tfileage.pp
@@ -1,18 +1,21 @@
 uses
   sysutils;
+var
+  ExecutableName: String;
 begin
-  if 3600*24*(now()-FileDateToDateTime(FileAge(paramstr(0))))>7200 then
+  ExecutableName:=ExpandFileName(paramstr(0));
+  if 3600*24*(now()-FileDateToDateTime(FileAge(ExecutableName)))>7200 then
     begin
-      writeln('FileAge returns: ',FileDateToDateTime(FileAge(paramstr(0))));
-      writeln('Compilation time and run time differ too much, SysUtils.FileAge buggy?');
+      writeln('FileAge returns: ',FileDateToDateTime(FileAge(ExecutableName)));
+      writeln('Executable file time and run time differ too much, SysUtils.FileAge buggy?');
       halt(1);
     end;
 
   { test with relative path }
-  if 3600*24*(now()-FileDateToDateTime(FileAge(ExtractRelativePath(GetCurrentDir+DirectorySeparator,paramstr(0)))))>7200 then
+  if 3600*24*(now()-FileDateToDateTime(FileAge(ExtractRelativePath(GetCurrentDir+DirectorySeparator,ExecutableName))))>7200 then
     begin
-      writeln('FileAge returns: ',FileDateToDateTime(FileAge(paramstr(0))));
-      writeln('Compilation time and run time differ too much, SysUtils.FileAge buggy?');
+      writeln('FileAge returns: ',FileDateToDateTime(FileAge(ExecutableName)));
+      writeln('Executable file time (relative path) and run time differ too much, SysUtils.FileAge buggy?');
       halt(1);
     end;
 end.
