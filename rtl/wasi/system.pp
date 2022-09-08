8c8f8088ebaf68c83500668e97b9c16c41f7d92e
Michaël Van Canneyt <michael@freepascal.org>
2022-09-08 13:09:50 +0200

    * Seems not all was committed

--- rtl/wasi/system.pp
+++ rtl/wasi/system.pp
@@ -66,10 +66,43 @@ procedure __fpc_set_wasm_suspender(v: WasmExternRef);
 
 property __fpc_wasm_suspender: WasmExternRef read __fpc_get_wasm_suspender write __fpc_set_wasm_suspender;
 
+Procedure DebugWriteln(aString : ShortString);
+
 implementation
 
 {$I wasitypes.inc}
 {$I wasiprocs.inc}
+
+function IntToStr(I : Longint) : ShortString;
+
+Var
+  S : ShortString;
+
+begin
+  Str(I,S);
+  IntToStr:=S;
+end;
+
+Procedure DebugWriteln(aString : ShortString);
+
+var
+  our_iov: __wasi_ciovec_t;
+  our_nwritten: longint;
+  res: __wasi_errno_t;
+
+begin
+  our_iov.buf := @aString[1];
+  our_iov.buf_len := Length(aString);
+  repeat
+    res:=__wasi_fd_write(1, @our_iov, 1, @our_nwritten);
+    if res=__WASI_ERRNO_SUCCESS then
+      begin
+      our_iov.buf_len:=our_iov.buf_len-our_nwritten;
+      our_iov.buf:=our_iov.buf+our_nwritten;
+      end;
+  Until (our_iov.buf_len=0) or (res=__WASI_ERRNO_SUCCESS) or ((res<>__WASI_ERRNO_INTR) and (res<>__WASI_ERRNO_AGAIN));
+end;
+
 
 function WasiAlloc (aLength : Longint) : Pointer; [public, alias: 'wasiAlloc'];
 
@@ -83,7 +116,7 @@ begin
   FreeMem(aMem);
 end;
 
-exports 
+exports
   WasiAlloc,WasiFree;
 
 function __fpc_get_wasm_suspender: WasmExternRef;
@@ -172,7 +205,7 @@ procedure System_exit;
 begin
   if ExitCode>=126 then
     begin
-      writeln(stderr,'##WASI-EXITCODE: ',ExitCode,' -> 125##');
+      Debugwriteln('##WASI-EXITCODE: '+IntToStr(ExitCode)+' -> 125##');
       ExitCode:=125;
     end;
   __wasi_proc_exit(ExitCode);
@@ -402,13 +435,15 @@ begin
   InitHeap;
   SysInitExceptions;
   initunicodestringmanager;
-  { Setup stdin, stdout and stderr }
-  SysInitStdIO;
   { Reset IO Error }
   InOutRes:=0;
-{$ifdef FPC_HAS_FEATURE_THREADING}
+{$ifdef FPC_WASM_THREADS}
   InitSystemThreads;
+  InitThreadVars(@WasiRelocateThreadVar);
 {$endif}
+  { Setup stdin, stdout and stderr }
+  SysInitStdIO;
   Setup_Environment;
   Setup_PreopenedDirs;
+  TLSInfoBlock:=Nil;
 end.
