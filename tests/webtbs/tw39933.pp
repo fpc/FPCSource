2396b36c21c58a00b1a2e49b288393470de5cf88
florian <florian@freepascal.org>
2022-10-03 10:26:09 +0200

    * nil =/<> dyn. array is a valid expression, so isbinaryoperatoroverloadable needs to take care of it, resolves #39933

---
+++ tests/webtbs/tw39933.pp
@@ -0,0 +1,13 @@
+{ %norun }
+program Project1;
+
+{$mode delphi}
+
+var a: TArray<integer>;
+
+begin
+  writeln ( a = nil );
+  writeln ( a <> nil );
+  writeln ( nil = a );   // project1.lpr(10,17) Error: Operator is not overloaded: "Pointer" = "TArray$1$crc9F312717"
+  writeln ( nil <> a );  // project1.lpr(11,17) Error: Operator is not overloaded: "Pointer" = "TArray$1$crc9F312717"
+end.
