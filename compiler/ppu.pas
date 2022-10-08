920e1c8d1916b723d2aa60268f1c6a8b5527712c
Sven/Sarah Barth <pascaldragon@gmail.com>
2022-10-08 13:46:28 +0200

    * change PPU version as the change regarding $P/$OpenStrings affects the token buffers of generics (which contains the current settings)

--- compiler/ppu.pas
+++ compiler/ppu.pas
@@ -48,7 +48,7 @@ const
   CurrentPPUVersion = 208;
   { for any other changes to the ppu format, increase this version number
     (it's a cardinal) }
-  CurrentPPULongVersion = 16;
+  CurrentPPULongVersion = 17;
 
 { unit flags }
   uf_big_endian          = $000004;
