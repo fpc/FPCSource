53d7a0a603e5e8a21792c5080e3893a14a47cf7e
Pierre Muller <pierre@freepascal.org>
2022-10-22 15:34:38 -0500

    Add system_powerpc_wii to supported_targets of powerpc generic GNU assembler
    (cherry picked from commit f3179cb718ed68d61805a758793dd8487d085a45)

--- compiler/ppcgen/agppcgas.pas
+++ compiler/ppcgen/agppcgas.pas
@@ -640,7 +640,9 @@ unit agppcgas;
 {$else cpu64bitaddr}
          asmcmd: '$ENDIAN -o $OBJ $EXTRAOPT $ARCH $ASM';
 {$endif cpu64bitaddr}
-         supported_targets : [system_powerpc_linux,system_powerpc_netbsd,system_powerpc_openbsd,system_powerpc_MorphOS,system_powerpc_Amiga,system_powerpc64_linux,system_powerpc_embedded,system_powerpc64_embedded];
+         supported_targets : [system_powerpc_linux,system_powerpc_netbsd,system_powerpc_openbsd,
+                              system_powerpc_MorphOS,system_powerpc_Amiga,system_powerpc_wii,
+                              system_powerpc64_linux,system_powerpc_embedded,system_powerpc64_embedded];
          flags : [af_needar,af_smartlink_sections];
          labelprefix : '.L';
          comment : '# ';
