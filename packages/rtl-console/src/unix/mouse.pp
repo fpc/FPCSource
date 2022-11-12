1853e94b98100336410c9093ce5f6ff2c63a3bf4
florian <florian@freepascal.org>
2022-11-12 22:07:58 +0100

    * attempt to fix #39900

--- packages/rtl-console/src/unix/mouse.pp
+++ packages/rtl-console/src/unix/mouse.pp
@@ -16,11 +16,11 @@
 unit Mouse;
 interface
 
-{$if defined(aix) or defined(solaris)}
+{$if defined(aix) or defined(solaris) or (defined(bsd) and not(defined(darwin)))}
 {$define NOMOUSE}
 {$endif}
 
-{$if defined(bsd) or defined(haiku) or defined(beos)}
+{$if defined(darwin) or defined(haiku) or defined(beos)}
 {$define NOGPM}
 {$endif}
 
