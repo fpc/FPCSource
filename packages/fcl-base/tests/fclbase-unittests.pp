88597da1c4d85e4e2cb70cf1483b371ffd7fb6ee
Michaël Van Canneyt <michael@freepascal.org>
2022-11-23 09:01:27 +0100

    * Chained stream implementation

--- packages/fcl-base/tests/fclbase-unittests.pp
+++ packages/fcl-base/tests/fclbase-unittests.pp
@@ -4,7 +4,7 @@ program fclbase_unittests;
 
 uses
   Classes, consoletestrunner, tests_fptemplate, tchashlist,
-  testexprpars, tcmaskutils, tcinifile, tccsvreadwrite,tcbufferedfilestream, tccsvdocument;
+  testexprpars, tcmaskutils, tcinifile, tccsvreadwrite,tcbufferedfilestream, tccsvdocument, utcchainstream;
 
 var
   Application: TTestRunner;
