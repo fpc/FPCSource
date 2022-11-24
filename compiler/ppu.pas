bf463e53328c92abc6c912e46f186aad97afaa4b
J. Gareth "Curious Kit" Moreton <gareth@moreton-family.com>
2022-11-24 21:16:47 +0000

    * New 'do not execute' flag for notes and respective internal error triggering

--- compiler/ppu.pas
+++ compiler/ppu.pas
@@ -48,7 +48,7 @@ const
   CurrentPPUVersion = 208;
   { for any other changes to the ppu format, increase this version number
     (it's a cardinal) }
-  CurrentPPULongVersion = 17;
+  CurrentPPULongVersion = 18;
 
 { unit flags }
   uf_big_endian          = $000004;
