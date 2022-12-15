ac4575291a466c1775e12a078531b01600cee0f2
Karoly Balogh <karoly@freepascal.org>
2022-12-15 10:47:26 +0100

    aros: build the CPU unit for i386 and x86_64 aros
    (cherry picked from commit 5b0669d036cfef022030c9f36bf455161c0fd9a7)

--- rtl/aros/buildrtl.pp
+++ rtl/aros/buildrtl.pp
@@ -8,7 +8,13 @@ unit buildrtl;
       softfpu, sfpux80, ufloatx80, sfpu128, ufloat128,
       ctypes, strings,
       rtlconsts, sysconst, math, types,
-      lineinfo,
+      exeinfo,
+{$ifdef cpui386}
+      cpu, mmx, lineinfo,
+{$endif}
+{$ifdef cpux86_64}
+      cpu,
+{$endif}
       typinfo, fgl, classes,
       charset, character, getopts,
       fpintres;
