04f88eb3b650da36310776924d06ce8eee5ec4d6
Tomas Hajny <XHajT03@hajny.biz>
2022-12-31 15:10:44 +0100

    * fixed test program - bug #40072

---
+++ rtl/emx/tests/helloos2.pas
@@ -0,0 +1,29 @@
+program HelloOS2;
+
+var A,B: ^word;
+
+begin
+    WriteLn ('Hello World.');
+    case os_mode of
+     osDOS: WriteLn ('Running under DOS.');
+     osDPMI: WriteLn ('Running under DPMI (RSX extender).');
+     else WriteLn ('Running under OS/2.');
+    end;
+    {
+    WriteLn ('Free memory: ', MemAvail);
+    WriteLn ('Largest block: ', MaxAvail);
+    WriteLn ('Heap start: ',longint(heaporg));
+    WriteLn ('Heap end: ',longint(heapend));
+    }
+    WriteLn ('Memory allocation.');
+    GetMem (A, 1000);
+    GetMem (B, 2000);
+    A^ := 2;
+    B^ := 10;
+    {
+    WriteLn ('Free memory: ', MemAvail);
+    WriteLn ('Largest block: ', MaxAvail);
+    }
+    FreeMem (A, 1000);
+    FreeMem (B, 2000);
+end.
