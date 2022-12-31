8b19b2896570a180fee3e87e0db12da4805c0eb5
Tomas Hajny <XHajT03@hajny.biz>
2022-12-31 15:26:00 +0100

    * fixed test program - bug #40072

--- rtl/os2/tests/calc_e.pas
+++ rtl/os2/tests/calc_e.pas
@@ -4,12 +4,12 @@ program calc_e;
 
 const   fac:array[0..7] of word=(1,1,2,6,24,120,720,5040);
 
-var e:fixed;
+var e:real;
     i:byte;
 
 begin
     e:=0;
     for i:=0 to 7 do
-        e:=e+fixed(1)/fac[i];
+        e:=e+1/fac[i];
     writeln(e);
 end.
