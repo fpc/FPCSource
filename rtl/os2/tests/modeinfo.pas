abbec0eb9b78621b03948bf3f72381f1aaa87a0f
Tomas Hajny <XHajT03@hajny.biz>
2022-12-31 15:26:00 +0100

    * fixed test program - bug #40072

--- rtl/os2/tests/modeinfo.pas
+++ rtl/os2/tests/modeinfo.pas
@@ -1,31 +1,12 @@
 program modeinfo;
 
-type    viomodeinfo=record
-            cb:word;                         { length of the entire data structure }
-            fbType,                          { bit mask of mode being set }
-            color: byte;                     { number of colors (power of 2) }
-            col,                             { number of text columns }
-            row,                             { number of text rows }
-            hres,                            { horizontal resolution }
-            vres: word;                      { vertical resolution }
-            fmt_ID,                          { attribute format }
-            attrib: byte;                    { number of attributes }
-            buf_addr,
-            buf_length,
-            full_length,
-            partial_length:longint;
-            ext_data_addr:pointer;
-        end;
-        Pviomodeinfo=^viomodeinfo;
+uses viocalls;
 
-function _VioGetMode (var Amodeinfo:viomodeinfo;viohandle:word):word;[C];
-function _VioSetMode (var Amodeinfo:viomodeinfo;viohandle:word):word;[C];
-
-var mode:viomodeinfo;
+var mode:Tviomodeinfo;
 
 begin
     mode.cb:=sizeof(mode);
-    writeln('getmode= ',_viogetmode(mode,0));
+    writeln('getmode= ',viogetmode(mode,0));
     writeln('cb= ',mode.cb);
     writeln('fbtype= ',mode.fbtype);
     writeln('color= ',mode.color);
