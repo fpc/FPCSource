aaa6f0d9c5ea729efccfb200f4ee0125cfa6c847
Pierre Muller <pierre@freepascal.org>
2023-01-12 22:48:43 +0100

    Only signed extension is needed

--- compiler/aarch64/cgcpu.pas
+++ compiler/aarch64/cgcpu.pas
@@ -1004,9 +1004,7 @@ implementation
                       list.concat(taicpu.op_reg_reg_const_const(A_BFI,tmpreg,tmpreg2,i*8,8));
                     end;
                   if (tosize in [OS_S8,OS_S16]) then
-                    list.concat(taicpu.op_reg_reg(A_SXTH,tmpreg,tmpreg))
-                  else if (tosize in [OS_8,OS_16]) then
-                    list.concat(taicpu.op_reg_reg(A_UXTH,tmpreg,tmpreg));
+                    list.concat(taicpu.op_reg_reg(A_SXTH,tmpreg,tmpreg);
                   a_load_reg_reg(list,fromsize,tosize,tmpreg,register);
                 end;
             end;
