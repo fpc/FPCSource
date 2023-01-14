e55862828f6464963edc517d654aa58064d51185
Michael VAN CANNEYT <mvancanneyt@tixeo.com>
2023-01-14 18:03:09 +0100

    * PChar -> PAnsiChar

--- packages/fcl-extra/src/fileinfo.pp
+++ packages/fcl-extra/src/fileinfo.pp
@@ -206,7 +206,7 @@ var
   Stream: TResourceStream;
 begin
   FreeResources;
-  Stream := TResourceStream.CreateFromID(Instance, 1, {$ifdef FPC_OS_UNICODE}PWideChar{$else}PChar{$endif}(RT_VERSION));
+  Stream := TResourceStream.CreateFromID(Instance, 1, {$ifdef FPC_OS_UNICODE}PWideChar{$else}PAnsiChar{$endif}(RT_VERSION));
   try
     FVersionInfo:=TVersionResource.Create;
     FVersionInfo.SetCustomRawDataStream(Stream);
