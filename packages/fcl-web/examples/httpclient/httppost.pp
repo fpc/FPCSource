ddb25669c735002875dadf1790ab2c47a96716de
Michael VAN CANNEYT <mvancanneyt@tixeo.com>
2023-01-25 18:16:42 +0100

    * WideStringManager needed

--- packages/fcl-web/examples/httpclient/httppost.pp
+++ packages/fcl-web/examples/httpclient/httppost.pp
@@ -9,6 +9,8 @@ Var
   F : TFileStream;
   Vars : TStrings;
   i : integer;
+  Fmt : UNicodeString;
+
 begin
   With TFPHTTPClient.Create(Nil) do
     begin
@@ -17,7 +19,10 @@ begin
       Vars:=TstringList.Create;
       try
         For i:=1 to 10 do
-          Vars.Add(Format('Var%d=Value %d',[i,i]));
+          begin
+          Fmt:='Var%d=Value %d';
+          Vars.Add(Format(Fmt,[i,i]));
+          end;
         FormPost(ParamStr(1),vars,f);
       finally
         Vars.Free;
