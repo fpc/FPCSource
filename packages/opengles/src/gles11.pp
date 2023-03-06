c357977a1ee3df9cf918c2bf1ba0370dd7b063fd
Michaël Van Canneyt <michael@freepascal.org>
2023-03-06 14:57:55 +0100

    * Dotted filenames for package opengles

--- packages/opengles/src/gles11.pp
+++ packages/opengles/src/gles11.pp
@@ -1,10 +1,17 @@
 
 {$mode objfpc}
+{$IFNDEF FPC_DOTTEDUNITS}
 unit gles11;
+{$ENDIF FPC_DOTTEDUNITS}
 interface
 
+{$IFDEF FPC_DOTTEDUNITS}
+uses
+  System.CTypes;
+{$ELSE FPC_DOTTEDUNITS}
 uses
   ctypes;
+{$ENDIF FPC_DOTTEDUNITS}
 
 {
   Automatically converted by H2Pas 1.0.0 from gles.h
@@ -834,8 +841,13 @@ uses
 
 implementation
 
+{$IFDEF FPC_DOTTEDUNITS}
+  uses
+    System.SysUtils, System.DynLibs, System.Math;
+{$ELSE FPC_DOTTEDUNITS}
   uses
     sysutils, dynlibs, math;
+{$ENDIF FPC_DOTTEDUNITS}
 
   var
     hlib : tlibhandle;
