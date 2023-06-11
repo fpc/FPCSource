d531ec05c95dc7063deca4d1004f210a49000384
Nikolay Nikolov <nickysn@gmail.com>
2023-06-11 04:27:05 +0300

    + some basic tests for WebAssembly funcref types added

---
+++ tests/test/wasm/twasmfuncref1.pp
@@ -0,0 +1,34 @@
+{ %cpu=wasm32 }
+{ %norun }
+
+program twasmfuncref1;
+
+{$MODE fpc}
+
+type
+  TWasmFuncRef = function(a: longint; b: int64): longint; WasmFuncRef;
+
+procedure testproc;
+var
+  p: TWasmFuncRef;
+begin
+end;
+
+procedure testproc2(q: TWasmFuncRef);
+begin
+end;
+
+function testproc3: TWasmFuncRef;
+begin
+end;
+
+function testproc4(a, b, c: longint; d: TWasmFuncRef; e: int64): TWasmFuncRef;
+var
+  q: TWasmFuncRef;
+begin
+  q := d;
+  testproc4 := q;
+end;
+
+begin
+end.
