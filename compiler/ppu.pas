bbedb958f61b03c8af63ca52af882115d051ec71
Nikolay Nikolov <nickysn@gmail.com>
2023-06-19 15:54:33 +0300

    * bump CurrentPPULongVersion

--- compiler/ppu.pas
+++ compiler/ppu.pas
@@ -48,7 +48,7 @@ const
   CurrentPPUVersion = 208;
   { for any other changes to the ppu format, increase this version number
     (it's a cardinal) }
-  CurrentPPULongVersion = 18;
+  CurrentPPULongVersion = 19;
 
 { unit flags }
   uf_big_endian          = $000004;
