1425f3eb3c255425018e0055bece801c6b6c1bdb
marcoonthegit <marco@freepascal.org>
2023-07-31 10:14:58 +0200

    * dotted variant

--- rtl/win32/buildrtl.pp
+++ rtl/win32/buildrtl.pp
@@ -2,7 +2,21 @@ unit buildrtl;
 
   interface
 
+{$IFDEF FPC_DOTTEDUNITS}
     uses
+      sysinitpas, sysinitcyg, sysinitgprof,
+      extpas,
+      System.CTypes, System.Strings,
+      WinApi.Windows,  initc,  System.CMem, System.DynLibs, signals,
+      TP.DOS, WinApi.Messages,
+      System.RtlConsts, System.SysConst, System.SysUtils, System.Math, System.Types,
+      System.TypInfo, System.FGL, System.Classes,    
+      System.Cpu, mmx, System.CharSet, System.Character, System.GetOpts,
+      System.fpwidestring,
+      System.Winsysut,  WinApi.ShareMem, fpintres, WinApi.WinDirs,
+      System.SoftFPU, System.SoftFpuX80, System.SoftFpu128, System.UFloatX80, System.UFloat128;
+{$ELSE FPC_DOTTEDUNITS}
+  uses
       sysinitpas, sysinitcyg, sysinitgprof,
       extpas,
       ctypes, strings,
@@ -14,7 +28,7 @@ unit buildrtl;
       fpwidestring,
       winsysut, sharemem, fpintres, windirs,
       softfpu, sfpux80, sfpu128, ufloatx80, ufloat128;
-
+{$ENDIF FPC_DOTTEDUNITS}
   implementation
 
 end.
