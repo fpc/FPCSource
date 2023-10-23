6fda6f79d8d31ac5d0014aa281e3f374c4490add
Michaël Van Canneyt <michael@freepascal.org>
2023-10-23 23:33:34 +0200

    * License header

--- packages/fcl-web/src/base/fpdebugcapturesvc.pp
+++ packages/fcl-web/src/base/fpdebugcapturesvc.pp
@@ -1,3 +1,17 @@
+{
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 2023 by the Free Pascal development team
+
+    Class to collect debug output from a pas2js application.
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
 unit fpdebugcapturesvc;
 
 {$mode ObjFPC}{$H+}
