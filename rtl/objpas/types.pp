9904727f75aa86ba7931d04c929750d06bef19ba
Michaël Van Canneyt <michael@freepascal.org>
2023-10-26 23:29:43 +0200

    * add TClassicByteDynArray for delphi compatibility

--- rtl/objpas/types.pp
+++ rtl/objpas/types.pp
@@ -62,6 +62,8 @@ type
 
   TBooleanDynArray = array of Boolean;
   TByteDynArray = array of Byte;
+  TClassicByteDynArray = TByteDynArray;
+  
   TCardinalDynArray = array of Cardinal;
   TInt64DynArray = array of Int64;
   TIntegerDynArray = array of Integer;
