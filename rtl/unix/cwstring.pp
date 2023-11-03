743382ef1e9648e1be98f6f7fd5389bf46d085ce
Michaël Van Canneyt <michael@freepascal.org>
2023-11-03 17:01:03 +0100

    * Ensure null-terminated strings. Patch by modersohn, fixes issue #40499

--- rtl/unix/cwstring.pp
+++ rtl/unix/cwstring.pp
@@ -285,7 +285,8 @@ begin
 {$endif}
   toencoding:=iconvname;
   if not assigned(iconvctl) then
-    toencoding:=toencoding+TransLitName;
+    toencoding:=toencoding+TransLitName+#0;
+  iconvname:=iconvname+#0;
   iconv_wide2ansi:=iconv_open(PAnsiChar(@toencoding[1]),unicode_encoding2);
   iconv_ansi2wide:=iconv_open(unicode_encoding2,PAnsiChar(@iconvname[1]));
   if assigned(iconvctl) and
