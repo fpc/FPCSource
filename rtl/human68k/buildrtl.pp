c4c3c2a7aa39e10488c5683a24f692340a2f90a5
Karoly Balogh <karoly@freepascal.org>
2023-11-21 07:02:25 +0100

    human68k: add an RTL skeleton, almost nothing is implemented yet

---
+++ rtl/human68k/buildrtl.pp
@@ -0,0 +1,20 @@
+unit buildrtl;
+
+  interface
+
+    uses
+{$ifdef cpum68k}
+      si_prc,
+{$endif}
+      sysutils, {dos,}
+
+      ctypes, strings,
+      rtlconsts, sysconst, math, types,
+      typinfo, sortbase, fgl, classes,
+      charset, character, getopts,
+      fpwidestring,
+      softfpu, sfpux80, ufloatx80, sfpu128, ufloat128;
+
+  implementation
+
+end.
---
+++ rtl/human68k/classes.pp
@@ -0,0 +1,70 @@
+{
+    This file is part of the Free Component Library (FCL)
+    Copyright (c) 2016 by the Free Pascal development team
+
+    Classes unit for Human 68k
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
+{$mode objfpc}
+{$H+}
+{$modeswitch advancedrecords}
+{$IF FPC_FULLVERSION>=30301}
+{$modeswitch FUNCTIONREFERENCES}
+{$define FPC_HAS_REFERENCE_PROCEDURE}
+{$endif}
+
+{ determine the type of the resource/form file }
+{$define Win16Res}
+
+{$IFNDEF FPC_DOTTEDUNITS}
+unit Classes;
+{$ENDIF FPC_DOTTEDUNITS}
+
+interface
+
+{$IFDEF FPC_DOTTEDUNITS}
+uses
+  System.SysUtils,
+  System.RtlConsts,
+  System.Types,
+  System.SortBase,
+{$ifdef FPC_TESTGENERICS}
+  System.FGL,
+{$endif}
+  System.TypInfo;
+{$ELSE FPC_DOTTEDUNITS}
+uses
+  sysutils,
+  rtlconsts,
+  types,
+  sortbase,
+{$ifdef FPC_TESTGENERICS}
+  fgl,
+{$endif}
+  typinfo;
+{$ENDIF FPC_DOTTEDUNITS}
+
+{$i classesh.inc}
+
+
+implementation
+
+{ OS - independent class implementations are in /inc directory. }
+{$i classes.inc}
+
+
+initialization
+  CommonInit;
+
+finalization
+  CommonCleanup;
+
+end.
