932849c1bf6043f7c7f4f114bf26542f4f8ba59e
Karoly Balogh <karoly@freepascal.org>
2023-12-03 09:48:24 +0100

    human68k: refactored some type naming used in the startup code

--- rtl/human68k/si_prc.pp
+++ rtl/human68k/si_prc.pp
@@ -25,22 +25,12 @@ var
   stacktop: pointer; public name '__stktop';
   stklen: longint; external name '__stklen';
 
-type
-  Th68k_startup = record
-    mcb: pbyte;
-    bss_end: pbyte;
-    comm: pbyte;
-    environ: pbyte;
-    entry: pbyte;
-    intr: pbyte;
-  end;
-  Ph68k_startup = ^Th68k_startup;
-
 var
-  h68k_startup: Th68k_startup; public name '_h68k_startup';
+  h68k_startup: Th68kdos_startup; public name '_h68k_startup';
+  h68k_psp: Ph68kdos_psp; public name '_h68k_psp';
 
 procedure PascalMain; external name 'PASCALMAIN';
-procedure PascalStart(const startparams: Ph68k_startup); noreturn; forward;
+procedure PascalStart(const startparams: Ph68kdos_startup); noreturn; forward;
 
 { this function must be the first in this unit which contains code }
 procedure _FPC_proc_start; assembler; nostackframe; noreturn; public name '_start';
@@ -50,7 +40,7 @@ asm
   jbsr PascalStart
 end;
 
-procedure PascalStart(const startparams: Ph68k_startup); noreturn;
+procedure PascalStart(const startparams: Ph68kdos_startup); noreturn;
 var
   bss_start: pbyte;
 begin
