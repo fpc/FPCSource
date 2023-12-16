1fdc6ef6a7abf174c93818b8d435018c089c3aaa
Michaël Van Canneyt <michael@freepascal.org>
2023-12-16 22:00:04 +0100

    * Fixes and tests

---
+++ packages/fcl-process/tests/docat.pp
@@ -0,0 +1,29 @@
+program docat;
+
+Procedure ReadAndWrite(var O : Text);
+
+var
+  S : AnsiString;
+
+begin
+  While not EOF(O) do
+    begin
+    Readln(O,S);
+    Writeln(S);
+    end;
+end;
+
+var
+  F : Text;
+
+begin
+  if ParamStr(1)<>'' then
+    begin
+    Assign(F,ParamStr(1));
+    Reset(F);
+    ReadAndWrite(F);
+    end
+  else
+    ReadAndWrite(INput);
+end.
+
---
+++ packages/fcl-process/tests/testprocess.pp
@@ -0,0 +1,28 @@
+program testprocess;
+
+{$mode objfpc}{$H+}
+
+uses
+  Classes, consoletestrunner, utcprocess;
+
+type
+
+  { TMyTestRunner }
+
+  TMyTestRunner = class(TTestRunner)
+  protected
+  // override the protected methods of TTestRunner to customize its behavior
+  end;
+
+var
+  Application: TMyTestRunner;
+
+begin
+  DefaultRunAllTests:=True;
+  DefaultFormat:=fPlain;
+  Application := TMyTestRunner.Create(nil);
+  Application.Initialize;
+  Application.Title := 'FPCUnit Console test runner';
+  Application.Run;
+  Application.Free;
+end.
