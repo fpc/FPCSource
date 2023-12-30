74833bda09fc43a7c1bc00bc4f1f12d36525ab81
Sven/Sarah Barth <pascaldragon@gmail.com>
2023-12-30 13:58:09 +0100

    * split the two error cases of tanonfunc25 into two different tests

--- tests/test/tanonfunc74.pp
+++ tests/test/tanonfunc74.pp
@@ -1,10 +1,11 @@
 { %FAIL }
 
-program tanonfunc25;
+program tanonfunc74;
 
 {$mode objfpc}
 {$modeswitch anonymousfunctions}
 {$modeswitch functionreferences}
+{$modeswitch nestedprocvars}
 
 { verify that nested procedures aren't accessible from anonymous functions
   in the captured procedure }
@@ -23,9 +24,12 @@ procedure foo;
   begin
   end;
 
+type
+  TNested = procedure is nested;
+
 begin
   bar;
-  baz(procedure begin bar end);
+  baz(procedure var n: TNested; begin n := @bar; end);
 end;
 
 begin
