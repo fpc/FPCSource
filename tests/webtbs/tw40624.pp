12031e5faf22e328b02c5970f4d2c44cac491783
Pierre Muller <pierre@freepascal.org>
2024-02-06 13:09:15 +0000

    Add some output to explain failure for test tests/webtbs/tw40624.pp
    (cherry picked from commit 46508f6af16b7f676ca05bc9f84f904d3c2aac23)

--- tests/webtbs/tw40624.pp
+++ tests/webtbs/tw40624.pp
@@ -14,9 +14,15 @@ begin
   DefaultFormatSettings.DecimalSeparator := '.';
 
   if CurrToStr(C) <> '822337203685477.5807' then
-    Halt(1);
+    begin
+      Writeln('Currentcy variable C as string is ',CurrToStr(C),' expecting 822337203685477.5807');
+      Halt(1);
+    end;
   S := V;
   if S <> '822337203685477.5807' then
-    Halt(2);
+    begin
+      Writeln('Currency variable C passed to Variant variable V is ',S,' expecting 822337203685477.5807');
+      Halt(2);
+    end;
   WriteLn('OK');
 end.
