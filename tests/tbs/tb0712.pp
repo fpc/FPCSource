8aca9104355b184c9d296c7a962471a196ef5630
florian <florian@freepascal.org>
2024-03-21 22:52:34 +0100

    * improve test so it exits with the proper error code

--- tests/tbs/tb0712.pp
+++ tests/tbs/tb0712.pp
@@ -5,18 +5,24 @@ begin
   yy:=random(0)+1980;
   If (yy<1980) or (yy>2099) then
     res:=1;
+  if res<>0 then
+    halt(res);
 
   yy:=random(0)+2099;
   If (yy<1980) or (yy>2099) then
     res:=2;
+  if res<>0 then
+    halt(res);
 
   yy:=random(0)+1980;
   If (yy<=1979) or (yy>2099) then
     res:=3;
+  if res<>0 then
+    halt(res);
 
   yy:=random(0)+2099;
   If (yy<1979) or (yy>=2100) then
     res:=4;
-
-  halt(res);
+  if res<>0 then
+    halt(res);
 end.
