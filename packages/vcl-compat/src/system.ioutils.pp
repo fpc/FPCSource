37f145707a63dd51e27022b10599537c3dd7c675
Michaël Van Canneyt <michael@freepascal.org>
2024-03-31 11:21:10 +0200

    * Fix compilation

--- packages/vcl-compat/src/system.ioutils.pp
+++ packages/vcl-compat/src/system.ioutils.pp
@@ -2210,7 +2210,7 @@ var
 begin
   Result:=false;
   CurSrcDir:=ExpandFileName(DirectoryName);
-  CurSrcDir:=IncludTrailingPathDelimiter(CurSrcDir);
+  CurSrcDir:=IncludeTrailingPathDelimiter(CurSrcDir);
   if FindFirst(CurSrcDir+AllFilesMask,DeleteMask,FileInfo)=0 then
     Try
       repeat
