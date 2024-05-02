801e14c33be6e586c49eb926de753a504ae2f512
Rika Ichinose <rrunewalsh@gmail.com>
2024-05-02 18:37:55 +0300

    Faster Insert(ansi/unicodestring).

--- tests/webtbs/tw28850.pp
+++ tests/webtbs/tw28850.pp
@@ -11,10 +11,6 @@ begin
   { if the destination is empty, insert must create a new string
     with the same code page as the source }
   Insert(s1, s2, 1);
-  if StringRefCount(s1)<>1 then
-    halt(1);
-  if StringRefCount(s2)<>1 then
-    halt(2);
   if stringcodepage(s2)<>stringcodepage(s1) then
     halt(3);
 end.
