1fa83a91abb262f6acfcef71e3976ac427dd830c
florian <florian@freepascal.org>
2024-05-11 23:41:33 +0200

    + cs_opt_consts for sparc64

--- compiler/optcse.pas
+++ compiler/optcse.pas
@@ -608,9 +608,9 @@ unit optcse;
         Result:=(n.nodetype=loadn) and (tloadnode(n).symtableentry.typ=staticvarsym)
           and ((vo_is_thread_var in tstaticvarsym(tloadnode(n).symtableentry).varoptions) or
             (cs_create_pic in current_settings.moduleswitches)
-{$if defined(aarch64) or defined(sparc)}
+{$if defined(aarch64) or defined(sparc) or defined(sparc64)}
             or (not(tabstractvarsym(tloadnode(n).symtableentry).is_regvar(false)))
-{$endif defined(aarch64) or defined(sparc)}
+{$endif defined(aarch64) or defined(sparc) or defined(sparc64)}
            );
       end;
 
