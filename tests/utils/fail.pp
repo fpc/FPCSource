9f69dd28b4fe82ac966aac8118dd4173ac93cfa5
Pierre Muller <pierre@freepascal.org>
2024-05-24 14:29:20 +0200

    Allow to pass antoher exitcode to fail utility by command line first parameter

--- tests/utils/fail.pp
+++ tests/utils/fail.pp
@@ -1,5 +1,16 @@
 program fail;
 
+var
+  i : int64;
+  err : word;
 begin
+  if paramstr(1)<>'' then
+    begin
+      val(paramstr(1),i,err);
+      if err<>0 then
+        writeln('Error with ',paramstr(1))
+      else
+        halt(i);
+    end;
   halt(1);
 end.
