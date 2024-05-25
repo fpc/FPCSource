0a886833104abbbb3742c40aa8f965ab14547e12
florian <florian@freepascal.org>
2024-05-25 20:09:02 +0200

    + do do_consttovar on RiscV

--- compiler/optcse.pas
+++ compiler/optcse.pas
@@ -608,9 +608,9 @@ unit optcse;
         Result:=(n.nodetype=loadn) and (tloadnode(n).symtableentry.typ=staticvarsym)
           and ((vo_is_thread_var in tstaticvarsym(tloadnode(n).symtableentry).varoptions) or
             (cs_create_pic in current_settings.moduleswitches)
-{$if defined(aarch64) or defined(sparc) or defined(sparc64)}
+{$if defined(aarch64) or defined(sparc) or defined(sparc64) or defined(riscv)}
             or (not(tabstractvarsym(tloadnode(n).symtableentry).is_regvar(false)))
-{$endif defined(aarch64) or defined(sparc) or defined(sparc64)}
+{$endif defined(aarch64) or defined(sparc) or defined(sparc64) or defined(riscv)}
            );
       end;
 
