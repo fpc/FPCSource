7949e53d8ac045552c53fb29d969246b8ae505b6
Margers <margers.roked@inbox.lv>
2024-09-01 21:29:10 +0000

    revert unwanted change

--- packages/ide/fpide.pas
+++ packages/ide/fpide.pas
@@ -1088,7 +1088,7 @@ begin
       StdStatusKeys(
       NewStatusKey('~Cursor~ Move', kbNoKey, 65535,
       NewStatusKey('~Shift+Cursor~ Size', kbNoKey, 65535,
-      NewStatusKey('~'#17'��~ Done', kbNoKey, 65535, {#17 = left arrow}
+      NewStatusKey('~'#17'~ Done', kbNoKey, 65535, {#17 = left arrow}
       NewStatusKey('~Esc~ Cancel', kbNoKey, 65535,
       nil)))))),
     NewStatusDef(hcStackWindow, hcStackWindow,
