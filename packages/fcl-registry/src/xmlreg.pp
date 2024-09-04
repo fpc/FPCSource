c60217a3803fe13671e698fda8de7d708b653f17
Michaël Van Canneyt <michael@freepascal.org>
2024-09-04 14:34:01 +0200

    * Patch from Iluha Companets to avoid crash when registry XML file is empty. Fixes issue #40912
    (cherry picked from commit cd13d5bb3a6930d0b5d41a6602e4d081e171cbcd)

--- packages/fcl-registry/src/xmlreg.pp
+++ packages/fcl-registry/src/xmlreg.pp
@@ -596,9 +596,13 @@ begin
     FDocument.Free;
     FDocument:=Nil;
     end;
-  ReadXMLFile(FDocument,S);
-  if (FDocument=Nil) then
+  try
+    ReadXMLFile(FDocument,S);
+    if (FDocument=Nil) then
+      CreateEmptyDoc;
+  except
     CreateEmptyDoc;
+  end;
   SetRootKey('HKEY_CURRENT_USER');
   FDirty:=False;
 end;
