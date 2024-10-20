bb684c0bb310f38d34ad4b0b5bc5cc3d60bb32c0
Nikolay Nikolov <nickysn@gmail.com>
2024-10-20 12:40:00 +0300

    * forgot to add fpmake.pp in previous commit

---
+++ packages/fcl-fpterm/fpmake.pp
@@ -0,0 +1,38 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses {$ifdef unix}cthreads,{$endif} fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('fcl-fpterm');
+{$ifdef ALLPACKAGES}
+    P.Directory:=ADirectory;
+{$endif ALLPACKAGES}
+    P.Version:='3.3.1';
+
+    P.Author := 'Nikolay Nikolov';
+    P.License := 'LGPL with modification, ';
+    P.HomepageURL := 'https://sourceforge.net/projects/fpterm/';
+    P.Description := 'Terminal emulator library, written in Free Pascal.';
+    P.OSes := P.OSes - [embedded,nativent,msdos,win16,macosclassic,palmos,zxspectrum,msxdos,amstradcpc,sinclairql,ps1];
+//    p.OSes:=[linux,win32,win64,go32v2,macosx,openbsd,freebsd];
+
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('system.terminal.base.pas');
+
+    //P.NamespaceMap:='namespaces.lst';
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
