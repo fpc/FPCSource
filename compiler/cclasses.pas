partouf/tunnelpascal

Outline freeing TFPList with disposing its items into TFPList.FreeAndNilDisposing.

compiler/cclasses.pas
@@ -114,8 +114,11 @@ type
     { Add to list, creating it if required. }
     class procedure AddOnDemand(var Lst: TFPList; Item: Pointer); static;
 
-    { FreeAndNil the list, and its items as TObjects. }
+    { FreeAndNil the list, and free its items as TObjects. }
     class procedure FreeAndNilAsObjects(var LstStore: TFPList); static;
+
+    { FreeAndNil the list, and dispose() its items. 'ItemType' is TypeInfo() of items. }
+    class procedure FreeAndNilDisposing(var LstStore: TFPList; ItemType: Pointer); static;
   end;
 
 
@@ -1055,6 +1058,26 @@ begin
       LstStore := nil;
       for I := 0 to Lst.Count-1 do
         TObject(Lst.FList^[I]).Free;
+      Lst.Free;
+    end;
+end;
+
+class procedure TFPList.FreeAndNilDisposing(var LstStore: TFPList; ItemType: Pointer);
+var
+  Lst: TFPList;
+  P: Pointer;
+  I: SizeInt;
+begin
+  Lst := LstStore;
+  if Assigned(Lst) then
+    begin
+      LstStore := nil;
+      for I := 0 to Lst.Count-1 do
+        begin
+          P := Lst.FList^[I];
+          FinalizeArray(P,ItemType,1);
+          FreeMem(P);
+        end;
       Lst.Free;
     end;
 end;

fpc/FPCSource

compiler/cclasses.pas
@@ -116,6 +116,9 @@ type
 
     { FreeAndNil the list, and its items as TObjects. }
     class procedure FreeAndNilObjects(var Lst: TFPList); static;
+
+    { FreeAndNil the list, and dispose() its items. 'ItemType' is TypeInfo() of items. }
+    class procedure FreeAndNilDisposing(var Lst: TFPList; ItemType: Pointer); static;
   end;
 
 
@@ -1054,6 +1057,26 @@ begin
   Lp := Lst.FList;
   for I := 0 to Lst.Count-1 do
     TObject(Lp[I]).Free;
+  Lst.Free;
+  Lst := nil;
+end;
+
+procedure fpc_finalize(data, typeinfo: pointer); external;
+
+class procedure TFPList.FreeAndNilDisposing(var Lst: TFPList; ItemType: Pointer);
+var
+  Lp: PPointer;
+  I: SizeInt;
+begin
+  if not Assigned(Lst) then
+    exit;
+  Lp := Lst.FList;
+  for I := 0 to Lst.Count-1 do
+    if Assigned(Lp[I]) then
+      begin
+        fpc_finalize(Lp[I],ItemType);
+        FreeMem(Lp[I]);
+      end;
   Lst.Free;
   Lst := nil;
 end;