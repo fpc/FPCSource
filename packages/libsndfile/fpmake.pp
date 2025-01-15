c229096983d325ab5ca2a077abc6ecdb167076cf
Nikolay Nikolov <nickysn@gmail.com>
2025-01-15 02:05:11 +0200

    + fix fpmake.pp and enable compilation of package libsndfile

---
+++ packages/libsndfile/fpmake.pp
@@ -0,0 +1,43 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses {$ifdef unix}cthreads,{$endif} fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('libsndfile');
+    P.ShortName:='lsndfile';
+{$ifdef ALLPACKAGES}
+    P.Directory:=ADirectory;
+{$endif ALLPACKAGES}
+    P.Version:='3.3.1';
+    P.Author := 'Library: libsndfile';
+    P.License := 'Library: LGPL';
+    P.HomepageURL := 'https://libsndfile.github.io/libsndfile/';
+    P.Email := '';
+    P.Description := 'Headers for the libsndfile library';
+    P.NeedLibC:= true;  // true for headers that indirectly link to libc?
+    P.OSes := AllUnixOSes-[qnx];
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('sndfile.pp');
+
+    P.Sources.AddSrc('README');
+
+    P.ExamplePath.Add('examples');
+    P.Targets.AddExampleProgram('sfplay.pp');
+
+    P.NamespaceMap:='namespaces.lst';
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
