c3a9b671184e86219aff35da1497d49e95bb00aa
florian <florian@freepascal.org>
2025-01-26 10:33:54 +0100

    * couple of wrong test runner directives fixed

--- tests/test/cg/teontest2.pp
+++ tests/test/cg/teontest2.pp
@@ -1,5 +1,5 @@
 { %CPU=AARCH64 }
-{ %OPT--O2 }
+{ %OPT=-O2 }
 program teontest2;
 
 { This test evaluates the AArch64 node parsing code that converts an
@@ -15,10 +15,10 @@ const
   InputsList: array[0..15] of Word     = ($0000, $0000, $0000, $0000, $FFFF, $FFFF, $FFFF, $FFFF, $5A5A, $5A5A, $5A5A, $5A5A, $A5A5, $A5A5, $A5A5, $A5A5);
   InvertList: array[0..15] of Word     = ($0000, $FFFF, $5A5A, $A5A5, $0000, $FFFF, $5A5A, $A5A5, $0000, $FFFF, $5A5A, $A5A5, $0000, $FFFF, $5A5A, $A5A5);
   ExpectList: array[0..15] of LongWord = ($FFFF, $0000, $A5A5, $5A5A, $0000, $FFFF, $5A5A, $A5A5, $A5A5, $5A5A, $FFFF, $0000, $5A5A, $A5A5, $0000, $FFFF);
-  
+
 var
   X: Integer; Output: LongWord;
-  
+
 begin
   for X := Low(InputsList) to High(InputsList) do
     begin
@@ -27,8 +27,8 @@ begin
         begin
           WriteLn('FAIL: Test index ', X, ', EON($', hexstr(InputsList[X], 4), ', $', hexstr(InputsList[X], 4), ')  expected $', hexstr(ExpectList[X], 8), ' but got $', hexstr(Output, 8));
           Halt(1);
-        end; 
+        end;
     end;
-    
+
   WriteLn('ok');
 end.
--- tests/test/cg/teontest4.pp
+++ tests/test/cg/teontest4.pp
@@ -1,5 +1,5 @@
 { %CPU=AARCH64 }
-{ %OPT--O2 }
+{ %OPT=-O2 }
 program teontest4;
 
 { This test evaluates the AArch64 node parsing code that converts an
@@ -15,10 +15,10 @@ const
   InputsList: array[0..15] of SmallInt = (            $0000,          $0000,             $0000,          $0000, SmallInt($FFFF),   SmallInt($FFFF), SmallInt($FFFF),   SmallInt($FFFF),             $5A5A,          $5A5A,             $5A5A,          $5A5A, SmallInt($A5A5),   SmallInt($A5A5), SmallInt($A5A5),   SmallInt($A5A5));
   InvertList: array[0..15] of SmallInt = (            $0000, SmallInt($FFFF),            $5A5A, SmallInt($A5A5),         $0000,    SmallInt($FFFF),          $5A5A,    SmallInt($A5A5),             $0000, SmallInt($FFFF),            $5A5A, SmallInt($A5A5),         $0000,    SmallInt($FFFF),          $5A5A,    SmallInt($A5A5));
   ExpectList: array[0..15] of LongInt  = (LongInt($FFFFFFFF),         $0000, LongInt($FFFFA5A5),         $5A5A,          $0000, LongInt($FFFFFFFF),          $5A5A, LongInt($FFFFA5A5), LongInt($FFFFA5A5),         $5A5A, LongInt($FFFFFFFF),         $0000,          $5A5A, LongInt($FFFFA5A5),          $0000, LongInt($FFFFFFFF));
-  
+
 var
   X: Integer; Output: LongInt;
-  
+
 begin
   for X := Low(InputsList) to High(InputsList) do
     begin
@@ -27,8 +27,8 @@ begin
         begin
           WriteLn('FAIL: Test index ', X, ', EON($', hexstr(InputsList[X], 4), ', $', hexstr(InputsList[X], 4), ')  expected $', hexstr(ExpectList[X], 8), ' but got $', hexstr(Output, 8));
           Halt(1);
-        end; 
+        end;
     end;
-    
+
   WriteLn('ok');
 end.
