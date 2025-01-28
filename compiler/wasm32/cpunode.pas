5f9ea00b38ce9de9c48f7fbd8c1dcea937f75753
Nikolay Nikolov <nickysn@gmail.com>
2025-01-28 10:15:47 +0200

    * fixed WebAssembly code generator internal error when passing real constants as   constref parameters. Also fixes test webtbs/tw41011

--- compiler/wasm32/cpunode.pas
+++ compiler/wasm32/cpunode.pas
@@ -33,7 +33,7 @@ implementation
     ncgbas,ncgflw,ncgcnv,ncgld,ncgmem,ncgcon,ncgset,
     ncgadd, ncgcal,ncgmat,ncginl,
     
-    nwasmbas,nwasmadd,nwasmcal,nwasmmat,nwasmflw,nwasmcon,nwasmcnv,nwasmset,nwasminl,nwasmld,
+    nwasmbas,nwasmadd,nwasmcal,nwasmmat,nwasmflw,nwasmcnv,nwasmset,nwasminl,nwasmld,
     nwasmmem,
     { these are not really nodes }
     nwasmutil,
