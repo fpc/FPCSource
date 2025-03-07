cee132935791e2d54035a51be53873f692fe7c56
Michaël Van Canneyt <michael@freepascal.org>
2025-03-07 09:07:15 +0100

    * Add support for int128 integers. Patch by Lacak, fixes issue #41178
    (cherry picked from commit 6215d7e8d2e2c26ffd8e59cfe00b0e0a6025b8a4)

--- packages/fcl-base/src/syncobjs.pp
+++ packages/fcl-base/src/syncobjs.pp
@@ -75,7 +75,6 @@ type
       property Handle : TEventHandle read FHandle;
       property LastError : Integer read FLastError;
    end;
-   THandleObjectArray = array of THandleObject;
 
    TEventObject = class(THandleObject)
    private
