8819ee38ad58ac615b3d6950c6952c9af6196af9
Michaël Van Canneyt <michael@freepascal.org>
2025-03-16 19:14:55 +0100

    * IN case of IO error, MD5File will return an empty digest. Fixes issue #41194
    (cherry picked from commit 2256593e92fa68236a3e5adafa321787e9ddfd12)

--- packages/hash/src/md5.pp
+++ packages/hash/src/md5.pp
@@ -685,17 +685,20 @@ begin
   Reset(F, 1);
   {$pop}
 
-  if IOResult = 0 then
-  begin
-    GetMem(Buf, BufSize);
-    repeat
-      BlockRead(F, Buf^, Bufsize, Count);
-      if Count > 0 then
-        MDUpdate(Context, Buf^, Count);
-    until Count < BufSize;
-    FreeMem(Buf, BufSize);
-    Close(F);
-  end;
+  if IOResult <> 0 then
+    begin
+    Result:=Default(TMDDigest);
+    Exit;
+    end;
+
+  GetMem(Buf, BufSize);
+  repeat
+    BlockRead(F, Buf^, Bufsize, Count);
+    if Count > 0 then
+      MDUpdate(Context, Buf^, Count);
+  until Count < BufSize;
+  FreeMem(Buf, BufSize);
+  Close(F);
 
   MDFinal(Context, Result);
   FileMode := ofm;
@@ -718,17 +721,19 @@ begin
   Reset(F, 1);
   {$pop}
 
-  if IOResult = 0 then
-  begin
-    GetMem(Buf, BufSize);
-    repeat
-      BlockRead(F, Buf^, Bufsize, Count);
-      if Count > 0 then
-        MDUpdate(Context, Buf^, Count);
-    until Count < BufSize;
-    FreeMem(Buf, BufSize);
-    Close(F);
-  end;
+  if IOResult <> 0 then
+    begin
+    Result:=Default(TMDDigest);
+    Exit;
+    end;
+  GetMem(Buf, BufSize);
+  repeat
+    BlockRead(F, Buf^, Bufsize, Count);
+    if Count > 0 then
+      MDUpdate(Context, Buf^, Count);
+  until Count < BufSize;
+  FreeMem(Buf, BufSize);
+  Close(F);
 
   MDFinal(Context, Result);
   FileMode := ofm;
