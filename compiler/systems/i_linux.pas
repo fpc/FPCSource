bd1da8ffb7240bee2f5ab4055c94dffc48c30290
florian <florian@freepascal.org>
2025-04-04 22:54:06 +0200

    * align stack to 16 bytes on RiscV32 as required by the ABI

--- compiler/systems/i_linux.pas
+++ compiler/systems/i_linux.pas
@@ -1308,7 +1308,7 @@ unit i_linux;
               );
             first_parm_offset : 0;
             stacksize    : 32*1024*1024;
-            stackalign   : 8;
+            stackalign   : 16;
             abi : abi_riscv_ilp32;
             llvmdatalayout : 'e-p:32:32:32-i1:8:8-i8:8:32-i16:16:32-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-n32-S64';
           );
