0863858c2f4406a0a4cf93bfb4473573c18ef566
marcoonthegit <marco@freepascal.org>
2025-06-27 18:13:21 +0200

    * Add CoGetCancelObject, a w2000+ function reported on the forum.
    (cherry picked from commit b1ddc57f5d4aec68ba57a037b300579e1fb8bc51)

--- packages/winunits-base/src/activex.pp
+++ packages/winunits-base/src/activex.pp
@@ -3844,6 +3844,7 @@ type
 
 { redefinitions }
   function CoCreateGuid(out _para1:TGUID):HRESULT;stdcall;external 'ole32.dll' name 'CoCreateGuid';
+  function CoGetCancelObject (InThreadId : DWORD; constref InIid : TIID; out ppunk : IUnknown):HRESULT;stdcall;external 'ole32.dll' name 'CoGetCancelObject';
 
 { additional definitions }
 {$ifndef wince}
