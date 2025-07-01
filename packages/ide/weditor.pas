dfe06b6599b886e523aa782ae6cd562dc5e83d2d
Margers <margers.roked@inbox.lv>
2025-07-01 07:04:43 +0000

    Fix Ctrl-k b mark beginning of select, Ctrl-k k end select.
    (cherry picked from commit 7dfa763e6747e70eb6b0e29bb30b77459c331dc6)

--- packages/ide/weditor.pas
+++ packages/ide/weditor.pas
@@ -3686,7 +3686,10 @@ begin
               PrevP:=P;
               if PointOfs(P)<PointOfs(StartP)
                  then SetSelection(P,StartP)
-                 else SetSelection(StartP,P);
+              else if PointOfs(P)>PointOfs(StartP)
+                 then SetSelection(StartP,P)
+              else if PointOfs(SelStart)<>PointOfs(SelEnd) { if selected only then remove selection }
+                 then SetSelection(StartP,P);
               DrawView;
               UnLock;
             end;
@@ -5537,14 +5540,14 @@ end;
 
 procedure TCustomCodeEditor.EndSelect;
 var P: TPoint;
-    LS: sw_integer;
+   { LS: sw_integer;}
 begin
   P:=CurPos;
-{  P.X:=Min(SelEnd.X,length(GetLineText(SelEnd.Y)));}
-  LS:=length(GetLineText(SelEnd.Y));
-  if LS<P.X then P.X:=LS;
-  CheckSels;
+  { don't try to jump to end of line, not for now
+  LS:=length(GetLineText(P.Y));
+  if LS<P.X then P.X:=LS; }
   SetSelection(SelStart,P);
+  CheckSels;
   DrawView;
 end;
 
