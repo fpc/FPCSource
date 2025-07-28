149b95c2255abfaf4e09d022fb221ca30ba7e4a2
Michaël Van Canneyt <michael@freepascal.org>
2025-07-28 08:55:29 +0200

    * Forgot to commit

---
+++ tests/test/umultilinestring2.pp
@@ -0,0 +1,17 @@
+unit umultilinestring2;
+
+{$modeswitch MultiLineStrings}
+{$MultiLineStringTrimLeft 2}
+
+interface
+
+procedure DoIt;
+
+implementation
+
+procedure DoIt; public name `SingleLineMultiLine`;
+begin
+  Write('Ok!');
+end;
+
+end.
