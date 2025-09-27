d3ac07ad274b6c6b09ff9b08a9d7007aaf780aa6
Michaël Van Canneyt <michael@freepascal.org>
2025-09-27 17:04:37 +0200

    * Add copyright header

--- rtl/wasm32/wasmsem.pas
+++ rtl/wasm32/wasmsem.pas
@@ -1,3 +1,17 @@
+{
+    This file is part of the Free Pascal run time library.
+    Copyright (c) 2025 by Michael Van Canneyt
+
+    This unit contains a webassembly-specific semaphore implementation.
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
 {$IFNDEF FPC_DOTTEDUNITS}
 unit WasmSem;
 {$ENDIF}
