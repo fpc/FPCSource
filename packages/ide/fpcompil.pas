5e97b8ed3fde0ae143e03a93dbefa8f7279adba9
Margers <margers.roked@inbox.lv>
2026-01-26 15:49:19 +0200

    Compiler status dialog information exceed shortstring, split it in two strings.

--- packages/ide/fpcompil.pas
+++ packages/ide/fpcompil.pas
@@ -78,6 +78,7 @@ type
     PCompilerStatusDialog = ^TCompilerStatusDialog;
     TCompilerStatusDialog = object(TCenterDialog)
       ST    : PAdvancedStaticText;
+      ST2   : PAdvancedStaticText;
       KeyST : PColorStaticText;
       starttime : real;
       constructor Init;
@@ -580,8 +581,13 @@ begin
   starttime:=getrealtime;
   GetExtent(R); R.B.Y:=11;
   R.Grow(-3,-2);
+  R.B.Y:=R.B.Y-4;
   New(ST, Init(R, ''));
   Insert(ST);
+  GetExtent(R); R.B.Y:=11;
+  R.Grow(-3,-2); R.A.Y:=R.A.Y+3;
+  New(ST2, Init(R, ''));
+  Insert(ST2);
   GetExtent(R); R.B.Y:=11;
   R.Grow(-1,-1); R.A.Y:=R.B.Y-1;
   New(KeyST, Init(R, '', Blue*16+White+longint($80+Blue*16+White)*256,true));
@@ -662,6 +668,13 @@ begin
   AddFormatParamStr(ShrinkPath(SmartPath(MainFile),
     MaxFileNameSize-Length('Main file: %s')));
   AddFormatParamStr(StatusS);
+  ST^.SetText(
+   FormatStrF(
+    'Main file: %s'#13+
+    '%s'+#13#13,
+   FormatParams)
+  );
+  ClearFormatParams;
   AddFormatParamStr(KillTilde(TargetSwitches^.ItemName(TargetSwitches^.GetCurrSel)));
   AddFormatParamInt(Status.CurrentLine);
   AddFormatParamInt(Status.CompiledLines);
@@ -672,10 +685,8 @@ begin
   r:=getrealtime;
   AddFormatParamInt(trunc(r-starttime));
   AddFormatParamInt(trunc(frac(r-starttime)*10));
-  ST^.SetText(
+  ST2^.SetText(
    FormatStrF(
-    'Main file: %s'#13+
-    '%s'+#13#13+
     'Target: %s'#13+
     'Line number: %6d     '+'Total lines:      %6d'+#13+
     'Used memory: %6dK    '+'Allocated memory: %6dK'#13+
