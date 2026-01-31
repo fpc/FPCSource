d0ca7d6664f2dfa390aa026e183563c151e41828
Michaël Van Canneyt <michael@freepascal.org>
2026-01-31 15:49:14 +0100

    * Copyright header

--- packages/vcl-compat/src/system.json.utils.pp
+++ packages/vcl-compat/src/system.json.utils.pp
@@ -1,3 +1,17 @@
+{
+    This file is part of the Free Component Library
+    Copyright (c) 2026 by Michael Van Canneyt michael@freepascal.org
+
+    Delphi-compatible JSON utilities unit
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
 {$mode objfpc}
 {$h+}
 
