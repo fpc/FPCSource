f38cc17c05fbbac2884b55c66fb04d237dfff3db
Michaël Van Canneyt <michael@freepascal.org>
2026-02-14 18:48:31 +0100

    * Deleted records should not be written to file. Fixes issue #40169
    (cherry picked from commit 5998ae0e58da1a3988f5e335cdf7c88ddda70749)

--- packages/fcl-db/src/base/csvdataset.pp
+++ packages/fcl-db/src/base/csvdataset.pp
@@ -102,6 +102,7 @@ Type
     function GetPacketReader(const Format: TDataPacketFormat; const AStream: TStream): TDataPacketReader; override;
     procedure LoadBlobIntoBuffer(FieldDef: TFieldDef;ABlobBuf: PBufBlobField); override;
     procedure InternalInitFieldDefs; override;
+    procedure DoBeforeClose; override;
   Public
     Constructor Create(AOwner : TComponent); override;
     Destructor Destroy; override;
@@ -274,6 +275,8 @@ Var
   I : integer;
 
 begin
+  if (rsvDeleted in ARowState) or (rsvOriginal in ARowState) then
+    Exit;
   For I:=0 to Dataset.Fields.Count-1 do
     FBuilder.AppendCell(Dataset.Fields[i].AsString);
   FBuilder.AppendRow;
@@ -393,6 +396,12 @@ begin
   finally
     F.Free;
   end;
+end;
+
+procedure TCustomCSVDataset.DoBeforeClose;
+begin
+  MergeChangeLog;
+  inherited DoBeforeClose;
 end;
 
 procedure TCustomCSVDataset.SaveToCSVStream(AStream: TStream);
