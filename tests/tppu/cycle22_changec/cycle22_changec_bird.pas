0ae4f73b0b3b1d00751e4db5d890a8a9b3cdc442
mattias <nc-gaertnma@netcologne.de>
2026-02-27 08:54:09 +0100

    tests: ppus: cycles with crc changes

--- tests/tppu/cycle22_changec/cycle22_changec_bird.pas
+++ tests/tppu/cycle22_changec/cycle22_changec_bird.pas
@@ -1,16 +1,22 @@
-unit touchleaf1_bird;
+unit cycle22_changec_bird;
 
 {$mode objfpc}
 
 interface
 
+uses cycle22_changec_cat;
+
+const Factor = 7;
+
 function Fly(w : word): word;
 
 implementation
 
+uses cycle22_changec_ant;
+
 function Fly(w : word): word;
 begin
-  Result:=w*7;
+  Result:=Jump(w)*5;
 end;
 
 end.
---
+++ tests/tppu/cycle32_changec/src2/cycle32_changec_cat.pas
@@ -0,0 +1,18 @@
+unit cycle32_changec_cat;
+
+{$mode objfpc}
+
+interface
+
+function Jump(w : word): word;
+
+implementation
+
+uses cycle32_changec_bird, cycle32_changec_ant;
+
+function Jump(w : word): word; inline; // inline changes the crc
+begin
+  Result:=w*7*Factor;
+end;
+
+end.
