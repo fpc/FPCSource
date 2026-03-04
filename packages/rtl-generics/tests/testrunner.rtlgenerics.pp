6d02919c08dda689642a68e2a701725a94ae6fe0
Michaël Van Canneyt <michael@freepascal.org>
2026-03-04 09:04:52 +0100

    * Add IndexOf/Min/Max/Contains to array helper. Fixes issue #40665
    (cherry picked from commit 0c19232ff76f2f16afcae5178a0fa6cedc4b5842)

--- packages/rtl-generics/tests/testrunner.rtlgenerics.pp
+++ packages/rtl-generics/tests/testrunner.rtlgenerics.pp
@@ -27,8 +27,10 @@ begin
   Application.Title := 'RTL-Generics unit tests';
   Application.Run;
   Application.Free;
+{$IFDEF WINDOWS}  
 {$IFDEF WAIT_FOR_ENTER}
   WriteLn('Press enter...');
   ReadLn;
+{$ENDIF}
 {$ENDIF}
 end.
