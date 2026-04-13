3371ae157726e728b6e238cc5680dd877f21ece4
Pierre Muller <pierre@freepascal.org>
2026-04-13 23:17:40 +0200

    Remove restriction for supported FreeBSD OS

--- packages/libvlc/fpmake.pp
+++ packages/libvlc/fpmake.pp
@@ -23,8 +23,8 @@ begin
     P.HomepageURL := 'www.freepascal.org';
     P.Email := 'michael@freepascal.org';
     P.Description := 'VLC library (version 2 or higher) interface and component.';
-    T:=P.Targets.AddUnit('src/libvlc.pp',[linux,win32,win64]);
-    T:=P.Targets.AddUnit('src/vlc.pp',[linux,win32,win64]);
+    T:=P.Targets.AddUnit('src/libvlc.pp');
+    T:=P.Targets.AddUnit('src/vlc.pp');
     with T.Dependencies do
       begin
       AddUnit('libvlc');
