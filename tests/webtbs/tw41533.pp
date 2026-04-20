41fa80f5917d8ca6c0b8be02e8425d5473c57944
Pierre Muller <pierre@freepascal.org>
2026-04-20 14:36:51 +0000

    Adapt test to COMP_IS_INT64 macro

--- tests/webtbs/tw41533.pp
+++ tests/webtbs/tw41533.pp
@@ -1,18 +1,25 @@
 program FloatCastTest;
 {$mode Delphi}
 uses Variants, Math;
+
+{$ifndef COMP_IS_INT64}
+  {$define TEST_COMP_REAL_TYPE}
+{$endif}
 var
   C: Currency;
   D, D2: Double;
   S: Single;
-  Cp: Comp;
   V: Variant;
+{$ifdef TEST_COMP_REAL_TYPE}
+  Cp: Comp;
+  I: int64;
+{$endif}
+
 begin
   C := 5.5556;
   D := Double(C);
   S := Single(D);
   D2 := Single(D);
-  Cp := Comp(D);
   V := Double(C);
 
   Writeln('C: ', C);
@@ -28,11 +35,21 @@ begin
   if not SameValue(D2, S) then // D2 and S must be the same
     Halt(3);
 
-  Writeln('Cp:', Cp);
-  if not SameValue(Cp, 6) then // Cp is 6
-    Halt(4);
-
   Writeln('V:  ', V);
   if not SameValue(D, V) then // D and V must be the same
+    Halt(4);
+
+{$ifdef TEST_COMP_REAL_TYPE}
+  Cp := Comp(D);
+  Writeln('Cp:', Cp);
+  if not SameValue(Cp, 6) then // Cp is 6
     Halt(5);
+{$else not TEST_COMP_REAL_TYPE}
+  Cp := Comp(D);
+  I := int64(D);
+  Writeln('Cp:', Cp);
+  Writeln('I:', I);
+  if not SameValue(Cp, I) then // Cp is a binary copy of D bit-pattern, as is I
+    Halt(6);
+{$endif not TEST_COMP_REAL_TYPE}
 end.
