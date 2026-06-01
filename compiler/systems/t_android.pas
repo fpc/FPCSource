7854fafe3a2aebcac313c2250bde1dccd6c543c7
Nikolay Nikolov <nickysn@gmail.com>
2026-06-02 00:34:11 +0300

    * use the AsmData parameter instead of current_asmdata in texportlibandroid.generatelib

--- compiler/systems/t_android.pas
+++ compiler/systems/t_android.pas
@@ -148,8 +148,8 @@ const
         // If JNI_OnLoad is exported, export a system proxy function instead
         create_hlcodegen(compiler);
         hlcg:=compiler.hlcg;
-        new_section(current_asmdata.asmlists[al_procedures],sec_code,'',0);
-        hlcg.g_external_wrapper(current_asmdata.asmlists[al_procedures],nil,SJNI_OnLoad,'FPC_JNI_ON_LOAD_PROXY',true);
+        new_section(AsmData.asmlists[al_procedures],sec_code,'',0);
+        hlcg.g_external_wrapper(AsmData.asmlists[al_procedures],nil,SJNI_OnLoad,'FPC_JNI_ON_LOAD_PROXY',true);
         destroy_hlcodegen(compiler);
         exportedsymnames.insert(SJNI_OnLoad);
       end;
