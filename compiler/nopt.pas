fpc/FPCSource

commit a6a9dcdd726c8fd9fe1d6da31f17867e536afda3

fix dynarray + chain aliasing operands (#41087, #41798)
each operand was passed to fpc_dynarray_concat_multi as a bare
pointer, so function-call and literal temps were freed before
the call and all aliased the last operand.

compiler/nopt.pas
@@ -436,22 +436,37 @@ end;
 
 function genmultidynarrayadd(p: taddnode): tnode;
 var
-  hp,sn : tnode;
+  hp    : tnode;
   arrp  : tarrayconstructornode;
   newstatement : tstatementnode;
   tempnode    : ttempcreatenode;
   para : tcallparanode;
+
+  { store the operand in a reference counted persistent temp so it stays
+    alive until the concat call, then hand the helper a plain pointer to it }
+  function operandref(n: tnode): tnode;
+    var
+      optemp : ttempcreatenode;
+    begin
+      if not is_array_constructor(n.resultdef) then
+        inserttypeconv(n,p.resultdef);
+      optemp:=ctempcreatenode.create(p.resultdef,p.resultdef.size,tt_persistent,true);
+      addstatement(newstatement,optemp);
+      addstatement(newstatement,cassignmentnode.create(ctemprefnode.create(optemp),n));
+      addstatement(newstatement,ctempdeletenode.create_normal_temp(optemp));
+      operandref:=ctypeconvnode.create_internal(ctemprefnode.create(optemp),voidpointertype);
+    end;
+
 begin
+  result:=internalstatements(newstatement);
   arrp:=nil;
   hp:=p;
   while assigned(hp) and (hp.nodetype=addn) do
     begin
-      sn:=ctypeconvnode.create_internal(taddnode(hp).right.getcopy,voidpointertype);
-      arrp:=carrayconstructornode.create(sn,arrp);
+      arrp:=carrayconstructornode.create(operandref(taddnode(hp).right.getcopy),arrp);
       hp:=taddnode(hp).left;
     end;
-  sn:=ctypeconvnode.create_internal(hp.getcopy,voidpointertype);
-  arrp:=carrayconstructornode.create(sn,arrp);
+  arrp:=carrayconstructornode.create(operandref(hp.getcopy),arrp);
   Include(arrp.arrayconstructornodeflags, acnf_allow_array_constructor);
   if assigned(aktassignmentnode) and
      (aktassignmentnode.right=p) and
@@ -465,15 +480,16 @@ begin
             ccallparanode.create(
               ctypeconvnode.create_internal(aktassignmentnode.left.getcopy,voidpointertype),nil)
           ));
-      result:=ccallnode.createintern(
-                'fpc_dynarray_concat_multi',
-                para
-              );
+      addstatement(newstatement,
+        ccallnode.createintern(
+          'fpc_dynarray_concat_multi',
+          para
+        )
+      );
       include(aktassignmentnode.assignmentnodeflags,anf_assign_done_in_right);
     end
   else
     begin
-      result:=internalstatements(newstatement);
       tempnode:=ctempcreatenode.create(p.resultdef,p.resultdef.size,tt_persistent ,true);
       addstatement(newstatement,tempnode);
       { initialize the temp, since it will be passed to a

tests/webtbs/tw41087.pp
@@ -1,0 +1,23 @@
+{ Strange bug while concatenating TBytes arrays }
+program tw41087;
+
+{$mode objfpc}
+{$modeswitch arrayoperators}
+
+type
+  TBytes = array of Byte;
+
+function U8(v: Byte): TBytes;
+begin
+  SetLength(Result, 1);
+  Result[0] := v;
+end;
+
+var
+  r: TBytes;
+begin
+  { three function results in one + chain must not alias the last one }
+  r := U8($11) + U8($22) + U8($33);
+  if (Length(r) <> 3) or (r[0] <> $11) or (r[1] <> $22) or (r[2] <> $33) then
+    halt(1);
+end.

tests/webtbs/tw41798.pp
@@ -1,0 +1,20 @@
+{ Failure in multiple-array concatenation }
+program tw41798;
+
+{$mode objfpc}
+{$modeswitch arrayoperators}
+
+type
+  TIntArr = array of PtrInt;
+
+var
+  x, y: TIntArr;
+begin
+  x := [1, 2];
+  { the two literal temps must not alias the last one }
+  y := x + [3, 4] + [5, 6];
+  if (Length(y) <> 6) or
+     (y[0] <> 1) or (y[1] <> 2) or (y[2] <> 3) or
+     (y[3] <> 4) or (y[4] <> 5) or (y[5] <> 6) then
+    halt(1);
+end.

commit 3b01ac7dd6b6b6c9bc9e21e8ef1dcfcc50a71d5f

patch to fix the tarray23 regression introduced in a6a9dcdd7

compiler/nopt.pas
@@ -442,12 +442,26 @@ var
   tempnode    : ttempcreatenode;
   para : tcallparanode;
 
-  { store the operand in a reference counted persistent temp so it stays
-    alive until the concat call, then hand the helper a plain pointer to it }
+  { operands read from a variable (possibly through fields/elements/derefs)
+    are anchored by that storage: they need no lifetime help and their
+    refcount must stay untouched so fpc_dynarray_concat_multi can still
+    detect refcount=1 and append to the destination in place }
+  function is_stored_operand(n: tnode): boolean;
+    begin
+      while n.nodetype in [subscriptn,vecn,derefn] do
+        n:=tunarynode(n).left;
+      is_stored_operand:=n.nodetype=loadn;
+    end;
+
+  { store any other operand (call result, literal, nested concat) in a
+    reference counted persistent temp so it stays alive until the concat
+    call, then hand the helper a plain pointer to it }
   function operandref(n: tnode): tnode;
     var
       optemp : ttempcreatenode;
     begin
+      if is_stored_operand(n) then
+        exit(ctypeconvnode.create_internal(n,voidpointertype));
       if not is_array_constructor(n.resultdef) then
         inserttypeconv(n,p.resultdef);
       optemp:=ctempcreatenode.create(p.resultdef,p.resultdef.size,tt_persistent,true);