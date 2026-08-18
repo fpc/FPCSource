4a3f2687fb62bed77feefb9ec44414fe1cc0de26
Nikolay Nikolov <nickysn@gmail.com>
2026-08-19 00:05:04 +0300

    * loongarch64 compilation fixed

--- compiler/dbgstabx.pas
+++ compiler/dbgstabx.pas
@@ -368,7 +368,7 @@ implementation
                   if curincludefileinfo.fileindex<>0 then
                     begin
                       infile:=compiler.get_module(curincludefileinfo.moduleindex).sourcefiles.get_file(curincludefileinfo.fileindex);
-                      list.insertbefore(Tai_stab.Create_str(stabx_ei,'"'+FixFileName(infile.name)+'"'),inclinsertpos);
+                      list.insertbefore(Tai_stab.Create_str(stabx_ei,'"'+compiler.CFileUtl.FixFileName(infile.name)+'"'),inclinsertpos);
                       curincludefileinfo.fileindex:=0;
                     end;
                   if currfileinfo.fileindex<>1 then
@@ -376,7 +376,7 @@ implementation
                       infile:=compiler.get_module(currfileinfo.moduleindex).sourcefiles.get_file(currfileinfo.fileindex);
                       if assigned(infile) then
                         begin
-                          list.insertbefore(Tai_stab.Create_str(stabx_bi,'"'+FixFileName(infile.name)+'"'),inclinsertpos);
+                          list.insertbefore(Tai_stab.Create_str(stabx_bi,'"'+compiler.CFileUtl.FixFileName(infile.name)+'"'),inclinsertpos);
                           curincludefileinfo:=currfileinfo;
                           { force new line info }
                           lastfileinfo.line:=-1;
@@ -440,7 +440,7 @@ implementation
       if curincludefileinfo.fileindex<>0 then
         begin
           infile:=compiler.get_module(curincludefileinfo.moduleindex).sourcefiles.get_file(curincludefileinfo.fileindex);
-          list.insertbefore(Tai_stab.Create_str(stabx_ei,'"'+FixFileName(infile.name)+'"'),last);
+          list.insertbefore(Tai_stab.Create_str(stabx_ei,'"'+compiler.CFileUtl.FixFileName(infile.name)+'"'),last);
           curincludefileinfo.fileindex:=0;
         end;
     end;
