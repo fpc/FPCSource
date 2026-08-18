1c3da6cab45ea712f8ed01da5581f3b7933da702
Nikolay Nikolov <nickysn@gmail.com>
2026-08-19 02:30:01 +0300

    * got rid of current_compiler by using the Globals property in TCompilerFileUtils.FileFileInExeLocations

--- compiler/cfileutl.pas
+++ compiler/cfileutl.pas
@@ -1234,12 +1234,10 @@ end;
 
   function  TCompilerFileUtils.FindFileInExeLocations(const bin:TCmdStr;allowcache:boolean;var foundfile:TCmdStr):boolean;
     var
-      compiler: TCompilerBase absolute current_compiler;  { TODO: fix node compiler reference!!! }
-    var
       Path : TCmdStr;
       found : boolean;
     begin
-       found:=FindFile(FixFileName(bin),compiler.globals.exepath,allowcache,foundfile);
+       found:=FindFile(FixFileName(bin),TReadOnlyCompilerGlobals(Globals).exepath,allowcache,foundfile);
       if not found then
        begin
 {$ifdef macos}
