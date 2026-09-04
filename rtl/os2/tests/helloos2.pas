fa6bc3343f0370421689708864f940f291e3cc7c
Tomas Hajny <9055380-xhajt03@users.noreply.gitlab.com>
2026-09-04 23:25:04 +0200

    * needed modifications reflecting current state of RTL - as suggested by ghr in forum

--- rtl/os2/tests/helloos2.pas
+++ rtl/os2/tests/helloos2.pas
@@ -9,17 +9,21 @@ begin
      osDPMI: WriteLn ('Running under DPMI (RSX extender).');
      else WriteLn ('Running under OS/2.');
     end;
+    {
     WriteLn ('Free memory: ', MemAvail);
     WriteLn ('Largest block: ', MaxAvail);
     WriteLn ('Heap start: ',longint(heaporg));
     WriteLn ('Heap end: ',longint(heapend));
+    }
     WriteLn ('Memory allocation.');
     GetMem (A, 1000);
     GetMem (B, 2000);
     A^ := 2;
     B^ := 10;
+    {
     WriteLn ('Free memory: ', MemAvail);
     WriteLn ('Largest block: ', MaxAvail);
+    }
     FreeMem (A, 1000);
     FreeMem (B, 2000);
 end.
