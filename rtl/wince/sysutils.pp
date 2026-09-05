a3bfb805c303a4f62352332bd657f8c5e0aeb948
Michael Van Canneyt <michael.vancanneyt@gmail.com>
2026-09-05 20:11:05 +0000

    * fix #41858: for FileGetAttr() using StringToPWideChar() on a UnicodeString argument leads to an unnecessary conversion to AnsiString, so just directly convert the UnicodeString argument to PWideChar like is done for FileSetAttr()
    (cherry picked from commit a04884fd0ba74d18f1d9a19f6e809f419452d150)
    
    Co-authored-by: Sven/Sarah Barth <pascaldragon@gmail.com>
    
    (cherry picked from commit 23246a54e76769dca58daf2f405e0978c4659912)
    
    Co-authored-by: Michael Van Canneyt <michael.vancanneyt@gmail.com>

--- rtl/wince/sysutils.pp
+++ rtl/wince/sysutils.pp
@@ -405,12 +405,8 @@ end;
 
 
 Function FileGetAttr (Const FileName : UnicodeString) : Longint;
-var
-  fn: PWideChar;
 begin
-  fn:=StringToPWideChar(FileName);
-  Result:=GetFileAttributes(fn);
-  FreeMem(fn);
+  Result:=GetFileAttributes(PWideChar(FileName));
 end;
 
 
