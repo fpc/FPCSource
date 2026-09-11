3a45bc79b77d8f7d31b4a2bb5902147e660957ff
Sven/Sarah Barth <pascaldragon@gmail.com>
2026-09-11 14:52:47 +0200

    * fix #41859: apply patch from Sven Harazim for RELOC_ADR_PREL_PG_HI21 relocations
    ToDo: try to find a test that triggers a RELOC_ADR_PREL_LO21 relocation which was accidentally changed in 325146b5feb8dee66ca7149f761c30d3939a4f28, but which does not appear to trigger any issues

--- compiler/ogcoff.pas
+++ compiler/ogcoff.pas
@@ -1499,8 +1499,10 @@ const pemagic : array[0..3] of byte = (
                     { sign extend the value if necessary }
                     if (addend and (1 shl 20)) <> 0 then
                       addend:=addend or sarint64(1 shl 63, 12);
-                    relocval:=relocval shr 12;
-                    relocval:=int64((relocval-(objsec.mempos+objreloc.dataoffset) shr 12)+addend);
+                    { the addend is a byte offset, so it has to be applied to the
+                      symbol address before taking the page number: ADRP encodes
+                      page(S+A)-page(P), not page(S)-page(P)+A }
+                    relocval:=int64(((relocval+addend) shr 12)-((objsec.mempos+objreloc.dataoffset) shr 12));
                     address:=address and not (($3 shl 29) or ($7ffff shl 5));
                     address:=address or ((relocval and $3) shl 29) or (((relocval shr 2) and $7ffff) shl 5);
                   end;
