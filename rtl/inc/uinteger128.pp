8700bdf98914f3024b8b1becc5d246be36a454a8
Nikolay Nikolov <nickysn@gmail.com>
2026-09-22 18:28:07 +0300

    * uinteger128: use 'out' instead of 'var' parameters in qword_add

--- rtl/inc/uinteger128.pp
+++ rtl/inc/uinteger128.pp
@@ -139,7 +139,7 @@ unit uinteger128;
       end;
 
 {$push} {$q-,r-}
-    procedure qword_add(const i1, i2: QWord; carry_in: Boolean; var o: QWord; var carry_out: Boolean);
+    procedure qword_add(const i1, i2: QWord; carry_in: Boolean; out o: QWord; out carry_out: Boolean);
       begin
         o := i1 + i2;
         carry_out := o < i1;
