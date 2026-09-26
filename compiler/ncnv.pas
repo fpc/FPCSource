3a1566c158c472dc946bb5bdb0b83f7b547f72c7
Nikolay Nikolov <nickysn@gmail.com>
2026-09-26 16:23:58 +0300

    * 128-bit int fix in ncnv.pas:ttypeconvnode.simplify

--- compiler/ncnv.pas
+++ compiler/ncnv.pas
@@ -3806,7 +3806,7 @@ implementation
                    if (target_info.system in systems_managed_vm) and
                       (tordconstnode(left).value<>0) then
                      message(parser_e_feature_unsupported_for_vm);
-                   hp:=cpointerconstnode.create(TConstPtrUInt(tordconstnode(left).value.uvalue),resultdef);
+                   hp:=cpointerconstnode.create(TConstPtrUInt(tordconstnode(left).value.AsQWord),resultdef);
                    if ([nf_explicit,nf_internal] * flags <> []) then
                      include(hp.flags, nf_explicit);
                    result:=hp;
